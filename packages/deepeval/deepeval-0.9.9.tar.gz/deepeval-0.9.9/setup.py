f9326cccc52873e80d3c250af1cf0d5b959dd69b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-25 15:08:33 +0000

    Add package deepeval/0.9.9/deepeval-0.9.9.tar.gz

---
+++ packages/deepeval/deepeval-0.9.9.tar.gz/deepeval-0.9.9/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+from deepeval._version import __version__
+
+with open("requirements.txt", "r") as f:
+    requirements = f.read().splitlines()
+
+
+setup(
+    name="deepeval",
+    version=__version__,
+    url="https://github.com/mr-gpt/deepeval",
+    author="Confident AI",
+    author_email="jacky@twilix.io",
+    description="Deep eval provides evaluation platform to accelerate development of LLMs and Agents",
+    packages=find_packages(),
+    # TODO - make pandas an 'extra' requirement in the future
+    install_requires=[
+        "requests",
+        "tqdm",
+        "transformers",
+        "pytest",
+        "tabulate",
+        "sentence-transformers",  # for similarity
+        "detoxify",  # for toxic classifier
+        "pytest",
+        "pytest-asyncio",  # To be able to run tests
+    ],
+    extras_require={
+        "bias": [
+            "tensorflow",  # for bias
+            "Dbias",  # for bias
+        ]
+    },
+)
