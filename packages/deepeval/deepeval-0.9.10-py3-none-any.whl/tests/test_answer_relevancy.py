b0cca86487cd73d01d447d92e7c56a6da9d1d635
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-25 16:10:06 +0000

    Add package deepeval/0.9.10/deepeval-0.9.10-py3-none-any.whl

---
+++ packages/deepeval/deepeval-0.9.10-py3-none-any.whl/tests/test_answer_relevancy.py
@@ -0,0 +1,21 @@
+"""Tests for answer relevancy
+"""
+import pytest
+
+query = "What is Python?"
+answer = "Python is a programming language?"
+
+
+@pytest.mark.asyncio
+async def test_answer_relevancy():
+    from deepeval.metrics.answer_relevancy import assert_answer_relevancy
+
+    assert_answer_relevancy(query, answer, minimum_score=0.5)
+
+
+@pytest.mark.asyncio
+async def test_query_answer_relevancy():
+    from deepeval.metrics.answer_relevancy import AnswerRelevancy
+
+    scorer = AnswerRelevancy(minimum_score=0.5)
+    result = scorer.measure(query=query, output=answer)
