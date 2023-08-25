cfcc0c1d61156ce3b573799e1408ced4803844c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-25 14:08:53 +0000

    Add package aws-prototyping-sdk.cdk-graph-plugin-diagram/0.19.68/aws_prototyping_sdk.cdk_graph_plugin_diagram-0.19.68-py3-none-any.whl

---
+++ packages/aws-prototyping-sdk.cdk-graph-plugin-diagram/aws_prototyping_sdk.cdk_graph_plugin_diagram-0.19.68-py3-none-any.whl/aws_prototyping_sdk/cdk_graph_plugin_diagram/_jsii/__init__.py
@@ -0,0 +1,30 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+from typeguard import check_type
+
+import aws_cdk._jsii
+import aws_prototyping_sdk.aws_arch._jsii
+import aws_prototyping_sdk.cdk_graph._jsii
+import constructs._jsii
+import projen._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@aws-prototyping-sdk/cdk-graph-plugin-diagram",
+    "0.19.68",
+    __name__[0:-6],
+    "cdk-graph-plugin-diagram@0.19.68.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
