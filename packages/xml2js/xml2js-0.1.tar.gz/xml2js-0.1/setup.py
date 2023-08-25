e18e0277775f0153b102d3eea5e125fc68c50a51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-25 15:07:19 +0000

    Add package xml2js/0.1/xml2js-0.1.tar.gz

---
+++ packages/xml2js/xml2js-0.1.tar.gz/xml2js-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="xml2js",
+    version="0.1",
+    author="Hiren Vikhram",
+    description="xml2js",
+    packages=find_packages(),
+    install_requires=[
+        # List your package dependencies here
+    ],
+)
+
