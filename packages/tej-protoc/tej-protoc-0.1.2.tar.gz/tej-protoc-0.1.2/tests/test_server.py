b6dc9fed4369070a1c5a4fefb90db89db1a536c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-25 15:07:01 +0000

    Add package tej-protoc/0.1.2/tej-protoc-0.1.2.tar.gz

---
+++ packages/tej-protoc/tej-protoc-0.1.2.tar.gz/tej-protoc-0.1.2/tests/test_server.py
@@ -0,0 +1,28 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from src.tej_protoc.serve import Server
+from src.tej_protoc import protocol
+from src.tej_protoc.logger import Log
+
+Log.enabled = True
+
+
+class Callback(protocol.Callback):
+    def start(self):
+        builder = protocol.BytesBuilder()
+        builder.set_message(b'Hello')
+        self.client.send(builder.bytes())
+
+    def receive(self, files, message):
+        print('---- Received in server ----')
+        for file in files:
+            print(file.name)
+        print('Message: ', message.decode())
+        print('---------------------------------')
+
+
+server = Server('localhost', 8000, Callback, log=True)
+server.start()
