23fd1db8010b9d86725e0fb1d7b73ec9d759d1e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-25 14:07:03 +0000

    Add package hatch-cython/0.0.1/hatch_cython-0.0.1.tar.gz

---
+++ packages/hatch-cython/hatch_cython-0.0.1.tar.gz/hatch_cython-0.0.1/src/hatch_cython/plugin.py
@@ -0,0 +1,32 @@
+import sys
+from unittest.mock import patch
+
+from Cython.Build import cythonize
+from hatchling.builders.hooks.plugin.interface import BuildHookInterface
+from setuptools import Extension, setup
+
+COMPILEARGS = ["-O2"]
+DIRECTIVES = {"binding": True, "language_level": 3}
+
+
+def build():
+    exts = [Extension("*", ["src/funcclasses/**/*.pyx"], extra_compile_args=COMPILEARGS)]
+    ext_modules = cythonize(exts, compiler_directives=DIRECTIVES)
+    setup(ext_modules=ext_modules)
+
+
+class CythonBuildHook(BuildHookInterface):
+    PLUGIN_NAME = "Cython"
+
+    def initialize(self, version: str, build_data: dict):  # noqa: ARG002
+        sys.path.append(self.root)
+        with patch.object(
+            sys,
+            "argv",
+            [
+                "hatch_build.py",
+                "build_ext",
+                "--inplace",
+            ],
+        ):
+            build()
