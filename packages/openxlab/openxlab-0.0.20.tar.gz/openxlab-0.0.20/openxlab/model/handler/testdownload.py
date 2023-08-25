023f4ddefba7a70723ab65fc00e7d417f501fe0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-25 15:07:17 +0000

    Add package openxlab/0.0.20/openxlab-0.0.20.tar.gz

---
+++ packages/openxlab/openxlab-0.0.20.tar.gz/openxlab-0.0.20/openxlab/model/handler/testdownload.py
@@ -0,0 +1,16 @@
+from openxlab.model.handler.download_file import download, download_from_url
+from openxlab.model import init
+from openxlab.model import wget
+
+
+# inference("mmocr/SVTR", ['./demo_text_ocr.jpg'])
+# download("dongxiaozhuang/digo_01", "model_1", overwrite=False)
+# download("alvin123/digo_01", "model_1", overwrite=False)
+# download("thomas-yanxin/MindChat-InternLM-7B", "config.json", overwrite=False)
+# download("dongxiaozhuang/digo_01", "mm", overwrite=True)
+# download("dongxiaozhuang/yy_001", "model_1", overwrite=True)
+# wget("https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0/resolve/main/sd_xl_offset_example-lora_1.0.safetensors", overwrite=True)
+# wget("https://static.openxlab.org.cn/puyu/staging/9205894-7602aeab-8264-4b3a-a40c-70b2d0fd5bbe.mp4", overwrite=True)
+# download_from_url("https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0/resolve/main/model_index.json", overwrite=True)
+init(path=r'C:\\Users\\meijiawen\\automationTest\python')
+
