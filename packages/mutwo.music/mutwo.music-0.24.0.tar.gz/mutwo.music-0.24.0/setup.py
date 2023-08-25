3dbcec4c97fc8e54cd0a47b99c7264212c18a147
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-25 14:08:40 +0000

    Add package mutwo.music/0.24.0/mutwo.music-0.24.0.tar.gz

---
+++ packages/mutwo.music/mutwo.music-0.24.0.tar.gz/mutwo.music-0.24.0/setup.py
@@ -0,0 +1,43 @@
+import setuptools  # type: ignore
+
+version = {}
+with open("mutwo/music_version/__init__.py") as fp:
+    exec(fp.read(), version)
+
+VERSION = version["VERSION"]
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+extras_require = {"testing": ["pytest>=7.1.1"]}
+
+setuptools.setup(
+    name="mutwo.music",
+    version=VERSION,
+    license="GPL",
+    description="music extension for event based framework for generative art",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Levin Eric Zimmermann",
+    author_email="levin.eric.zimmermann@posteo.eu",
+    url="https://github.com/mutwo-org/mutwo.music",
+    project_urls={"Documentation": "https://mutwo-org.github.io"},
+    packages=[
+        package
+        for package in setuptools.find_namespace_packages(
+            include=["mutwo.*", "mutwo_third_party.*"]
+        )
+        if package[:5] != "tests"
+    ],
+    setup_requires=[],
+    install_requires=[
+        "mutwo.core>=1.3.0, <2.0.0",
+        "epitran>=1.23, <2.0.0",
+        "sympy>=1.10.1, <2.0.0",
+        "python-ranges>=1.2.0, <2.0.0",
+        "numpy>=1.18, <2.00",
+        "scipy>=1.4.1, <2.0.0",
+    ],
+    extras_require=extras_require,
+    python_requires=">=3.10, <4",
+)
