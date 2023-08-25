7393ba65b6444d395a4f239b22913926a6d2f90f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-25 14:08:06 +0000

    Add package datasette-jellyfish/2.0/datasette-jellyfish-2.0.tar.gz

---
+++ packages/datasette-jellyfish/datasette-jellyfish-2.0.tar.gz/datasette-jellyfish-2.0/tests/test_jellyfish.py
@@ -0,0 +1,28 @@
+from datasette_jellyfish import prepare_connection
+import sqlite3
+import pytest
+
+
+@pytest.mark.parametrize(
+    "sql,expected",
+    (
+        ('soundex("hello")', "H400"),
+        ('metaphone("hello")', "HL"),
+        ('nysiis("hello")', "HAL"),
+        ('match_rating_codex("hello")', "HL"),
+        ('levenshtein_distance("hello", "hello world")', 6),
+        ('damerau_levenshtein_distance("hello", "hello world")', 6),
+        ('hamming_distance("hello", "hello world")', 6),
+        ('jaro_similarity("hello", "hello world")', pytest.approx(0.8181818181818182)),
+        (
+            'jaro_winkler_similarity("hello", "hello world")',
+            pytest.approx(0.890909090909091),
+        ),
+        ('match_rating_comparison("hello", "helloo")', 1),
+    ),
+)
+def test_jellyfish(sql, expected):
+    conn = sqlite3.connect(":memory:")
+    prepare_connection(conn)
+    result = conn.execute("select " + sql).fetchone()[0]
+    assert expected == result
