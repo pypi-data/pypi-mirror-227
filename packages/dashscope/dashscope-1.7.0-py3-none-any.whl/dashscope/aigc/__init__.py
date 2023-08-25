643482d0b7cae2391aa2eba79410f3dae6c5a4b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-25 15:06:53 +0000

    Add package dashscope/1.7.0/dashscope-1.7.0-py3-none-any.whl

---
+++ packages/dashscope/dashscope-1.7.0-py3-none-any.whl/dashscope/aigc/__init__.py
@@ -0,0 +1,11 @@
+from .conversation import Conversation, History, HistoryItem
+from .generation import Generation
+from .image_synthesis import ImageSynthesis
+
+__all__ = [
+    Generation,
+    Conversation,
+    HistoryItem,
+    History,
+    ImageSynthesis,
+]
