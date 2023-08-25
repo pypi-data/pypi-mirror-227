5325fd362fae4d4f92a42d2316fd4392aee66338
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-25 03:09:13 +0000

    Add package dup-fmt/0.0.4/dup_fmt-0.0.4-py3-none-any.whl

---
+++ packages/dup-fmt/dup_fmt-0.0.4-py3-none-any.whl/dup_fmt/__init__.py
@@ -0,0 +1,22 @@
+# -------------------------------------------------------------------------
+# Copyright (c) 2022 Korawich Anuttra. All rights reserved.
+# Licensed under the MIT License. See LICENSE in the project root for
+# license information.
+# --------------------------------------------------------------------------
+
+from .formatter import (
+    FORMATTERS,
+    FORMATTERS_ADJUST,
+    Constant,
+    ConstantType,
+    Datetime,
+    EnvConstant,
+    Formatter,
+    FormatterGroup,
+    Naming,
+    OrderFormatter,
+    ReturnFormattersType,
+    ReturnPrioritiesType,
+    Serial,
+    Version,
+)
