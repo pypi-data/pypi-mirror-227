c46cd263ad901ea51db20124ccb42d36133086c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-25 04:09:24 +0000

    Add package querysource/3.7.2/querysource-3.7.2-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/querysource/querysource-3.7.2-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/querysource/events/__init__.py
@@ -0,0 +1,53 @@
+import asyncio
+from datetime import datetime
+import socket
+from asyncdb import AsyncDB
+from navconfig.logging import logging
+from querysource.conf import (
+    QS_EVENT_BACKEND,
+    QS_EVENT_CREDENTIALS,
+    QS_EVENT_TABLE,
+    ENVIRONMENT,
+    INFLUX_DATABASE
+)
+
+EVENT_HOST = socket.gethostbyname(socket.gethostname())
+
+async def LogEvent(
+    payload: dict,
+    status: str = None,
+    **kwargs
+):
+
+    event_loop = asyncio.get_event_loop()
+    influx = AsyncDB(
+        QS_EVENT_BACKEND,
+        params=QS_EVENT_CREDENTIALS,
+        loop=event_loop
+    )
+    if status is None:
+        status = 'event'
+    async with await influx.connection() as conn:
+        try:
+            # saving the log into metric database:
+            start_time = datetime.utcnow()
+            data = {
+                "measurement": QS_EVENT_TABLE,
+                "location": ENVIRONMENT,
+                "timestamp": start_time,
+                "fields": {
+                    "status": status
+                },
+                "tags": {
+                    "host": EVENT_HOST,
+                    "region": ENVIRONMENT,
+                    "start_time": start_time,
+                    **payload,
+                    **kwargs
+                }
+            }
+            await conn.write(data, bucket=INFLUX_DATABASE)
+        except Exception as e:
+            logging.error(
+                f'DI: Error saving Query Execution: {e}'
+            )
