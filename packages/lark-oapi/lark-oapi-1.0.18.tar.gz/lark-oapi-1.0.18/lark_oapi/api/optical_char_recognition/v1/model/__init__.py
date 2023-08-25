cb2ea8345923059be4c92de4c6c9fc866d300064
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-25 15:07:29 +0000

    Add package lark-oapi/1.0.18/lark-oapi-1.0.18.tar.gz

---
+++ packages/lark-oapi/lark-oapi-1.0.18.tar.gz/lark-oapi-1.0.18/lark_oapi/api/optical_char_recognition/v1/model/__init__.py
@@ -0,0 +1,5 @@
+from .basic_recognize_image_request import *
+from .basic_recognize_image_request_body import *
+from .basic_recognize_image_response import *
+from .basic_recognize_image_response_body import *
+from .image import *
