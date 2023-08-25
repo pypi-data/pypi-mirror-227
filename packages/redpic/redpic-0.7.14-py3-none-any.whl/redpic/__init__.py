5492ec662dda566345f0cfeb65c3a9f5001c73c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-25 03:08:01 +0000

    Add package redpic/0.7.14/redpic-0.7.14-py3-none-any.whl

---
+++ packages/redpic/redpic-0.7.14-py3-none-any.whl/redpic/__init__.py
@@ -0,0 +1,14 @@
+"""
+Relativictic Difference Scheme Particle-in-Cell code (REDPIC) init file.
+
+"""
+
+from .accelerator import *
+from .beam import *
+from .constants import *
+from .solver import *
+
+__version__ = "0.7.14"
+__doc__ = """Relativictic Difference Scheme Particle-in-Cell code (REDPIC)"""
+
+__all__ = constants.__all__ + beam.__all__ + solver.__all__ + accelerator.__all__
