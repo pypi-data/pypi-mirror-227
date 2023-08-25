75ea8d5401fd82be9b9cabfe0f7114bd74ba52d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-25 15:07:28 +0000

    Add package Aicloudlp/0.1/Aicloudlp-0.1.tar.gz

---
+++ packages/Aicloudlp/Aicloudlp-0.1.tar.gz/Aicloudlp-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Aicloudlp',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[],  # List any dependencies here
+)
