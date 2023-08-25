7ff3ee611405262ee9833b27b57ea1b6f8c3d40f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-25 04:09:13 +0000

    Add package clean-text-my/0.1.0/clean_text_my-0.1.0.tar.gz

---
+++ packages/clean-text-my/clean_text_my-0.1.0.tar.gz/clean_text_my-0.1.0/clean_text_my/__init__.py
@@ -0,0 +1,7 @@
+__version__ = "0.1.0"
+
+from . import deduplication
+from . import postprocessing
+from . import download_dataset
+from . import utils
+from . import logging
---
+++ packages/clean-text-my/clean_text_my-0.1.0.tar.gz/clean_text_my-0.1.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+__packagename__ = "clean_text_my"
+
+with open("requirements.txt") as fopen:
+    req = list(filter(None, fopen.read().split("\n")))
+
+
+def readme():
+    # TODO https://github.com/huseinzol05/malaya/blob/master/README-pypi.rst
+    with open("README.md") as f:
+        return f.read()
+
+
+setup(
+    name=__packagename__,
+    description="",
+    version="0.1.0",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    python_requires=">=3.6",
+    install_requires=req,
+    author="Malaysia AI",
+    author_email="malaysia.ai2020@gmail.com",
+    url="https://github.com/malaysia-ai/clean_text_my",
+)
