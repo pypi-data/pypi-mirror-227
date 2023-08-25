c6466666648538fc7e5a15690e715019ced7436b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-25 14:07:57 +0000

    Add package py-telegram-notifier/0.2.4/py_telegram_notifier-0.2.4.tar.gz

---
+++ packages/py-telegram-notifier/py_telegram_notifier-0.2.4.tar.gz/py_telegram_notifier-0.2.4/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="py_telegram_notifier",
+    version="0.2.4",
+    author="Jonathan Townsend",
+    description="A simple notifier for sending a message to a Telegram chat using the Telegram Bot API.",
+    install_requires=[
+        "requests",
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jon-edward/py-telegram-notifier",
+    packages=setuptools.find_packages(
+        include=[
+            "telegram_notifier",
+        ]
+    ),
+    python_requires=">=3.7",
+)
