92122337366f13d131080b363204952906c0e105
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-25 03:09:02 +0000

    Add package blocksworld3d/0.2.3/blocksworld3d-0.2.3-py3-none-any.whl

---
+++ packages/blocksworld3d/blocksworld3d-0.2.3-py3-none-any.whl/blocksworld3d/__init__.py
@@ -0,0 +1,17 @@
+import gymnasium as gym
+from .blocksworld3d import BlocksWorld3D
+from .utils.problems import get_problem_list, get_problem_instance
+
+__all__ = [
+    "BlocksWorld3D",
+    "get_problem_list",
+    "get_problem_instance",
+]
+
+gym.register(
+    id="BlocksWorld3D-v0",
+    entry_point="blocksworld3d.blocksworld3d:BlocksWorld3D",
+)
+
+def env(render_mode, max_cycles=100):
+    return gym.make("BlocksWorld3D-v0", view='agent', render_mode=render_mode, max_episode_steps=max_cycles)
---
+++ packages/blocksworld3d/blocksworld3d-0.2.3-py3-none-any.whl/blocksworld3d/manual_control.py
@@ -0,0 +1,97 @@
+import pyglet
+import numpy as np
+import gymnasium as gym
+
+from pyglet.window import key
+from blocksworld3d.utils.problems import get_num_problems
+
+
+class ManualControl:
+    def __init__(self, env):
+        self.env = env
+        num_probs = get_num_problems()
+        self.problem_id = np.random.choice(num_probs)
+
+    def run(self):
+        print("============")
+        print("Instructions")
+        print("============")
+        print("turn: arrow keys\npickup: P\ndrop: D\ndone: ENTER\nquit: ESC")
+        print("============")
+
+        self.env.reset(options={'problem_id': self.problem_id})
+
+        # Create the display window
+        self.env.render()
+
+        env = self.env
+
+        @env.unwrapped.window.event
+        def on_key_press(symbol, modifiers):
+            """
+            This handler processes keyboard commands that
+            control the simulation
+            """
+
+            if symbol == key.BACKSPACE or symbol == key.SLASH:
+                print("RESET")
+                self.env.reset(options={'problem_id': self.problem_id})
+                self.env.render()
+                return
+
+            if symbol == key.ESCAPE:
+                self.env.close()
+
+            if symbol == key.UP:
+                self.step(self.env.actions.pickup)
+            elif symbol == key.DOWN:
+                self.step(self.env.actions.drop)
+            elif symbol == key.LEFT:
+                self.step(self.env.actions.turn_left)
+            elif symbol == key.RIGHT:
+                self.step(self.env.actions.turn_right)
+            elif symbol == key.SPACE:
+                self.step(self.env.actions.toggle_row)
+            elif symbol == key.ENTER:
+                self.step(self.env.actions.done)
+
+        @env.unwrapped.window.event
+        def on_key_release(symbol, modifiers):
+            pass
+
+        @env.unwrapped.window.event
+        def on_draw():
+            self.env.render()
+
+        @env.unwrapped.window.event
+        def on_close():
+            pyglet.app.exit()
+
+        # Enter main event loop
+        pyglet.app.run()
+
+        self.env.close()
+
+    def step(self, action):
+        print(
+            "step {}/{}: {}".format(
+                self.env.step_count + 1,
+                self.env.max_episode_steps,
+                self.env.actions(action).name,
+            )
+        )
+
+        obs, reward, termination, truncation, info = self.env.step(action)
+
+        if reward > 0:
+            print(f"reward={reward:.2f}")
+
+        if termination or truncation:
+            print("done!")
+            self.env.reset(options={'problem_id': self.problem_id})
+
+        self.env.render()
+
+env = gym.make("BlocksWorld-v0", view="agent", render_mode="human")
+manual_control = ManualControl(env)
+manual_control.run()
