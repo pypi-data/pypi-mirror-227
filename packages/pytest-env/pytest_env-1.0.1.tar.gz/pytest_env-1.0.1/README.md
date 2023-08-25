a58794cee6b8879025cbefd64e17197eaa625854
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-25 03:07:17 +0000

    Add package pytest-env/1.0.1/pytest_env-1.0.1.tar.gz

---
+++ packages/pytest-env/pytest_env-1.0.1.tar.gz/pytest_env-1.0.1/README.md
@@ -0,0 +1,73 @@
+# pytest-env
+
+[![PyPI](https://img.shields.io/pypi/v/pytest-env?style=flat-square)](https://pypi.org/project/pytest-env/)
+[![Supported Python
+versions](https://img.shields.io/pypi/pyversions/pytest-env.svg)](https://pypi.org/project/pytest-env/)
+[![check](https://github.com/pytest-dev/pytest-env/actions/workflows/check.yml/badge.svg)](https://github.com/pytest-dev/pytest-env/actions/workflows/check.yml)
+[![Code style:
+black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black)
+[![Downloads](https://static.pepy.tech/badge/pytest-env/month)](https://pepy.tech/project/pytest-env)
+
+This is a `pytest` plugin that enables you to set environment variables in a `pytest.ini` or `pyproject.toml` file.
+
+## Installation
+
+Install with pip:
+
+```shell
+pip install pytest-env
+```
+
+## Usage
+
+In your pytest.ini file add a key value pair with `env` as the key and the environment variables as a line separated
+list of `KEY=VALUE` entries. The defined variables will be added to the environment before any tests are run:
+
+```ini
+[pytest]
+env =
+    HOME=~/tmp
+    RUN_ENV=test
+```
+
+Or with `pyproject.toml`:
+
+```toml
+[tool.pytest.ini_options]
+env = [
+    "HOME=~/tmp",
+    "RUN_ENV=test",
+]
+```
+
+### Only set if not already set
+
+You can use `D:` (default) as prefix if you don't want to override existing environment variables:
+
+```ini
+[pytest]
+env =
+    D:HOME=~/tmp
+    D:RUN_ENV=test
+```
+
+### Transformation
+
+You can use existing environment variables using a python-like format, these environment variables will be expended
+before setting the environment variable:
+
+```ini
+[pytest]
+env =
+    RUN_PATH=/run/path/{USER}
+```
+
+You can apply the `R:` prefix to keep the raw value and skip this transformation step (can combine with the `D:` flag,
+order is not important):
+
+```ini
+[pytest]
+env =
+    R:RUN_PATH=/run/path/{USER}
+    R:D:RUN_PATH_IF_NOT_SET=/run/path/{USER}
+```
---
+++ packages/pytest-env/pytest_env-1.0.1.tar.gz/pytest_env-1.0.1/tests/test_env.py
@@ -0,0 +1,118 @@
+from __future__ import annotations
+
+import os
+import re
+from pathlib import Path
+from unittest import mock
+
+import pytest
+
+
+@pytest.mark.parametrize(
+    ("env", "ini", "expected_env"),
+    [
+        pytest.param(
+            {},
+            "[pytest]\nenv = MAGIC=alpha",
+            {"MAGIC": "alpha"},
+            id="new key - add to env",
+        ),
+        pytest.param(
+            {},
+            "[pytest]\nenv = MAGIC=alpha\n SORCERY=beta",
+            {"MAGIC": "alpha", "SORCERY": "beta"},
+            id="two new keys - add to env",
+        ),
+        pytest.param(
+            # This test also tests for non-interference of env variables between this test and tests above
+            {},
+            "[pytest]\nenv = d:MAGIC=beta",
+            {"MAGIC": "beta"},
+            id="D flag - add to env",
+        ),
+        pytest.param(
+            {"MAGIC": "alpha"},
+            "[pytest]\nenv = MAGIC=beta",
+            {"MAGIC": "beta"},
+            id="key exists in env - overwrite",
+        ),
+        pytest.param(
+            {"MAGIC": "alpha"},
+            "[pytest]\nenv = D:MAGIC=beta",
+            {"MAGIC": "alpha"},
+            id="D exists - original val kept",
+        ),
+        pytest.param(
+            {"PLANET": "world"},
+            "[pytest]\nenv = MAGIC=hello_{PLANET}",
+            {"MAGIC": "hello_world"},
+            id="curly exist - interpolate var",
+        ),
+        pytest.param(
+            {"PLANET": "world"},
+            "[pytest]\nenv = R:MAGIC=hello_{PLANET}",
+            {"MAGIC": "hello_{PLANET}"},
+            id="R exists - not interpolate var",
+        ),
+        pytest.param(
+            {"MAGIC": "a"},
+            "[pytest]\nenv = R:MAGIC={MAGIC}b\n D:MAGIC={MAGIC}c\n MAGIC={MAGIC}d",
+            {"MAGIC": "{MAGIC}bd"},
+            id="incremental interpolation",
+        ),
+        pytest.param(
+            {"PLANET": "world"},
+            "[pytest]\nenv = D:R:RESULT=hello_{PLANET}",
+            {"RESULT": "hello_{PLANET}"},
+            id="two flags",
+        ),
+        pytest.param(
+            {"PLANET": "world"},
+            "[pytest]\nenv = R:D:RESULT=hello_{PLANET}",
+            {"RESULT": "hello_{PLANET}"},
+            id="two flags - reversed",
+        ),
+        pytest.param(
+            {"PLANET": "world"},
+            "[pytest]\nenv = d:r:RESULT=hello_{PLANET}",
+            {"RESULT": "hello_{PLANET}"},
+            id="lowercase flags",
+        ),
+        pytest.param(
+            {"PLANET": "world"},
+            "[pytest]\nenv =  D  :  R  :  RESULT  =  hello_{PLANET}",
+            {"RESULT": "hello_{PLANET}"},
+            id="whitespace is ignored",
+        ),
+        pytest.param(
+            {"MAGIC": "zero"},
+            "",
+            {"MAGIC": "zero"},
+            id="empty ini works",
+        ),
+    ],
+)
+def test_env(
+    testdir: pytest.Testdir,
+    env: dict[str, str],
+    ini: str,
+    expected_env: dict[str, str],
+    request: pytest.FixtureRequest,
+) -> None:
+    tmp_dir = Path(str(testdir.tmpdir))
+    test_name = re.sub(r"\W|^(?=\d)", "_", request.node.callspec.id).lower()
+    Path(str(tmp_dir / f"test_{test_name}.py")).symlink_to(Path(__file__).parent / "template.py")
+    (tmp_dir / "pytest.ini").write_text(ini, encoding="utf-8")
+
+    new_env = {
+        **env,
+        "_TEST_ENV": repr(expected_env),
+        "PYTEST_DISABLE_PLUGIN_AUTOLOAD": "1",
+        "PYTEST_PLUGINS": "pytest_env.plugin",
+    }
+
+    # monkeypatch persists env variables across parametrized tests, therefore using mock.patch.dict
+    with mock.patch.dict(os.environ, new_env, clear=True):
+        result = testdir.runpytest()
+
+    result.assert_outcomes(passed=1)
