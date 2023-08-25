5a583bd19d1fca81eee85603f93165e977e29424
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-25 04:09:13 +0000

    Add package sandi-python-toolbox/0.1.1/sandi_python_toolbox-0.1.1.tar.gz

---
+++ packages/sandi-python-toolbox/sandi_python_toolbox-0.1.1.tar.gz/sandi_python_toolbox-0.1.1/SANDI_python_toolbox/run.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import os
+
+from snakebids.app import SnakeBidsApp
+from snakebids.cli import add_dynamic_args
+
+
+def get_parser():
+    """Exposes parser for sphinx doc generation, cwd is the docs dir"""
+    app = SnakeBidsApp("../SANDI_python_toolbox")
+    add_dynamic_args(app.parser, app.config["parse_args"], app.config["pybids_inputs"])
+    return app.parser
+
+
+def main():
+    app = SnakeBidsApp(
+        os.path.abspath(os.path.dirname(__file__)),
+        configfile_path="config/snakebids.yml",
+    )
+    app.run_snakemake()
+
+
+if __name__ == "__main__":
+    main()
