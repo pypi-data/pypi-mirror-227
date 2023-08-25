e94e69664b8b33048c31c7e97bf0edf9c94be380
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-25 03:07:33 +0000

    Add package py-jaws-namespace/1.1.1/py_jaws_namespace-1.1.1-py3-none-any.whl

---
+++ packages/py-jaws-namespace/py_jaws_namespace-1.1.1-py3-none-any.whl/py_jaws_namespace/test.py
@@ -0,0 +1,17 @@
+class Test:
+
+    def __init__(self, client):
+        print("new")
+        pass
+
+    def get(self):
+        print("test")
+
+class Test2:
+
+    def __init__(self, client):
+        print("new2")
+        pass
+
+    def get(self):
+        print("test2")
