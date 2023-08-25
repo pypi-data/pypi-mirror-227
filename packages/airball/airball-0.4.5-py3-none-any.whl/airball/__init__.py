e31e669147717a50598296c32c19dc745fb6899f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-25 14:07:39 +0000

    Add package airball/0.4.5/airball-0.4.5-py3-none-any.whl

---
+++ packages/airball/airball-0.4.5-py3-none-any.whl/airball/__init__.py
@@ -0,0 +1,14 @@
+'''
+# Flybys in `REBOUND`
+- A package for implementing flybys in [hannorein/rebound](https://github.com/hannorein/rebound)
+'''
+from .flybys import *
+from .environments import *
+from .tools import *
+from .analytic import *
+from .imf import IMF
+from . import imf
+from .stars import Star,Stars
+from . import stars
+
+__version__ = 'v0.4.5'
