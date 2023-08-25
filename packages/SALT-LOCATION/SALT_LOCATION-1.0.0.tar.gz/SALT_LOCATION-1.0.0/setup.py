39e81a2b28e2151ee0d02217c9bd459bf80f67c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-25 15:07:26 +0000

    Add package SALT-LOCATION/1.0.0/SALT_LOCATION-1.0.0.tar.gz

---
+++ packages/SALT-LOCATION/SALT_LOCATION-1.0.0.tar.gz/SALT_LOCATION-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="SALT_LOCATION",
+    version="1.0.0",
+    description="Your package description",
+    author="Your Name",
+    packages=find_packages(),
+)
+
