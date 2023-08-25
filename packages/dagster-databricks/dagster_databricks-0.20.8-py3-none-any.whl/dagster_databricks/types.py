f687e8263973ac1d596ac6bc01668e506cd94c20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-25 03:09:13 +0000

    Add package dagster-databricks/0.20.8/dagster_databricks-0.20.8-py3-none-any.whl

---
+++ packages/dagster-databricks/dagster_databricks-0.20.8-py3-none-any.whl/dagster_databricks/types.py
@@ -0,0 +1,55 @@
+from enum import Enum
+from typing import NamedTuple, Optional
+
+
+class DatabricksRunResultState(str, Enum):
+    """See https://docs.databricks.com/dev-tools/api/2.0/jobs.html#runresultstate."""
+
+    SUCCESS = "SUCCESS"
+    FAILED = "FAILED"
+    TIMEDOUT = "TIMEDOUT"
+    CANCELED = "CANCELED"
+
+    def is_successful(self) -> bool:
+        return self == DatabricksRunResultState.SUCCESS
+
+
+class DatabricksRunLifeCycleState(str, Enum):
+    """See https://docs.databricks.com/dev-tools/api/2.0/jobs.html#jobsrunlifecyclestate."""
+
+    PENDING = "PENDING"
+    RUNNING = "RUNNING"
+    TERMINATING = "TERMINATING"
+    TERMINATED = "TERMINATED"
+    SKIPPED = "SKIPPED"
+    INTERNAL_ERROR = "INTERNAL_ERROR"
+
+    def has_terminated(self) -> bool:
+        return self in [
+            DatabricksRunLifeCycleState.TERMINATING,
+            DatabricksRunLifeCycleState.TERMINATED,
+            DatabricksRunLifeCycleState.INTERNAL_ERROR,
+            DatabricksRunLifeCycleState.SKIPPED,
+        ]
+
+    def is_skipped(self) -> bool:
+        return self == DatabricksRunLifeCycleState.SKIPPED
+
+
+class DatabricksRunState(NamedTuple):
+    """Represents the state of a Databricks job run."""
+
+    life_cycle_state: "DatabricksRunLifeCycleState"
+    result_state: Optional["DatabricksRunResultState"]
+    state_message: str
+
+    def has_terminated(self) -> bool:
+        """Has the job terminated?"""
+        return self.life_cycle_state.has_terminated()
+
+    def is_skipped(self) -> bool:
+        return self.life_cycle_state.is_skipped()
+
+    def is_successful(self) -> bool:
+        """Was the job successful?"""
+        return bool(self.result_state and self.result_state.is_successful())
