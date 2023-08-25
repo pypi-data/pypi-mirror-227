c630bbbf9a588b7656c86836ed1eecfae9816b34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-25 03:08:02 +0000

    Add package dup-utils/0.0.4/dup_utils-0.0.4-py3-none-any.whl

---
+++ packages/dup-utils/dup_utils-0.0.4-py3-none-any.whl/dup_utils/core/cli.py
@@ -0,0 +1,54 @@
+from __future__ import annotations
+
+import subprocess
+import sys
+from pathlib import Path
+
+import click
+
+from .scripts.git import cli_git
+from .scripts.version import cli_vs
+
+
+@click.group()
+def cli():
+    """A simple command line tool."""
+    pass  # pragma: no cover.
+
+
+@cli.command()
+@click.option(
+    "-p",
+    "--path",
+    type=click.Path(exists=True, resolve_path=True),
+    default=".",
+)
+def ls(path: str):
+    """List files in Current Path"""
+    for file in Path(path).glob("*"):
+        print(f"> {file.resolve()}")
+    sys.exit(0)
+
+
+@cli.command()
+def say():
+    """Say Hello World"""
+    sys.exit("Hello World")
+
+
+@cli.command()
+def cove():
+    """Run coverage"""
+    subprocess.run(["coverage", "run", "--m", "pytest", "tests"])
+    subprocess.run(["coverage", "combine"])
+    subprocess.run(["coverage", "report", "--show-missing"])
+
+
+def main() -> None:
+    cli.add_command(cli_git)
+    cli.add_command(cli_vs)
+    cli.main()
+
+
+if __name__ == "__main__":
+    main()
