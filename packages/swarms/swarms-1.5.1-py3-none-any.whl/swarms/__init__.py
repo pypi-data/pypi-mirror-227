1be52975926646167e5f8504ecde93a6d1122d8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-25 03:09:34 +0000

    Add package swarms/1.5.1/swarms-1.5.1-py3-none-any.whl

---
+++ packages/swarms/swarms-1.5.1-py3-none-any.whl/swarms/__init__.py
@@ -0,0 +1,19 @@
+#swarms
+from swarms.orchestrator.autoscaler import AutoScaler
+
+# worker
+# from swarms.workers.worker_node import WorkerNode
+from swarms.workers.worker import Worker
+from swarms.workers.autobot import AutoBot
+
+#boss
+from swarms.boss.boss_node import BossNode
+
+#models
+from swarms.agents.models.anthropic import Anthropic
+from swarms.agents.models.huggingface import HuggingFaceLLM
+# from swarms.agents.models.palm import GooglePalm
+from swarms.agents.models.petals import Petals
+from swarms.agents.models.openai import OpenAI
+
+
