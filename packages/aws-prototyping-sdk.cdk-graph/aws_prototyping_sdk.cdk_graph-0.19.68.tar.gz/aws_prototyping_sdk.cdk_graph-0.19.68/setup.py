4b9bc9251496ac740ae6ae9029aeba344d65367a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-25 14:08:50 +0000

    Add package aws-prototyping-sdk.cdk-graph/0.19.68/aws_prototyping_sdk.cdk_graph-0.19.68.tar.gz

---
+++ packages/aws-prototyping-sdk.cdk-graph/aws_prototyping_sdk.cdk_graph-0.19.68.tar.gz/aws_prototyping_sdk.cdk_graph-0.19.68/setup.py
@@ -0,0 +1,67 @@
+import json
+import setuptools
+
+kwargs = json.loads(
+    """
+{
+    "name": "aws_prototyping_sdk.cdk_graph",
+    "version": "0.19.68",
+    "description": "@aws-prototyping-sdk/cdk-graph",
+    "license": "Apache-2.0",
+    "url": "https://github.com/aws/aws-prototyping-sdk",
+    "long_description_content_type": "text/markdown",
+    "author": "AWS APJ COPE<apj-cope@amazon.com>",
+    "bdist_wheel": {
+        "universal": true
+    },
+    "project_urls": {
+        "Source": "https://github.com/aws/aws-prototyping-sdk"
+    },
+    "package_dir": {
+        "": "src"
+    },
+    "packages": [
+        "aws_prototyping_sdk.cdk_graph",
+        "aws_prototyping_sdk.cdk_graph._jsii"
+    ],
+    "package_data": {
+        "aws_prototyping_sdk.cdk_graph._jsii": [
+            "cdk-graph@0.19.68.jsii.tgz"
+        ],
+        "aws_prototyping_sdk.cdk_graph": [
+            "py.typed"
+        ]
+    },
+    "python_requires": "~=3.7",
+    "install_requires": [
+        "aws-cdk-lib>=2.81.0, <3.0.0",
+        "constructs>=10.2.39, <11.0.0",
+        "jsii>=1.82.0, <2.0.0",
+        "projen>=0.71.92, <0.72.0",
+        "publication>=0.0.3",
+        "typeguard~=2.13.3"
+    ],
+    "classifiers": [
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: JavaScript",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Typing :: Typed",
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved"
+    ],
+    "scripts": []
+}
+"""
+)
+
+with open("README.md", encoding="utf8") as fp:
+    kwargs["long_description"] = fp.read()
+
+
+setuptools.setup(**kwargs)
