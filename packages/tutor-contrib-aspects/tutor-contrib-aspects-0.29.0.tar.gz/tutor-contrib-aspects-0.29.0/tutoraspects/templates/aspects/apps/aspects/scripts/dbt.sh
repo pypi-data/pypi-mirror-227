d181b22ec6bfaa26b226658d60715c32385769e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-25 04:09:08 +0000

    Add package tutor-contrib-aspects/0.29.0/tutor-contrib-aspects-0.29.0.tar.gz

---
+++ packages/tutor-contrib-aspects/tutor-contrib-aspects-0.29.0.tar.gz/tutor-contrib-aspects-0.29.0/tutoraspects/templates/aspects/apps/aspects/scripts/dbt.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+## WARNING: If you modify this block, make sure to also update the
+##          corresponding block in the init-aspects.sh file.
+
+echo "Installing dbt packages..."
+
+pip install -r /app/aspects/dbt/requirements.txt
+
+rm -rf {{ DBT_REPOSITORY_PATH }}
+
+echo "Installing aspects-dbt"
+echo "git clone -b {{ DBT_BRANCH }} {{ DBT_REPOSITORY }}"
+git clone -b {{ DBT_BRANCH }} {{ DBT_REPOSITORY }}
+
+cd {{ DBT_REPOSITORY_PATH }} || exit
+
+{% if DBT_ENABLE_OVERRIDE %}
+echo /app/aspects/dbt/packages.yml > packages.yml
+echo /app/aspects/dbt/dbt_project.yml > dbt_project.yml
+
+cat {{ DBT_REPOSITORY_PATH }}/packages.yml
+cat {{ DBT_REPOSITORY_PATH }}/dbt_project.yml
+{% endif %}
+
+echo "Installing dbt dependencies"
+dbt deps --profiles-dir /app/aspects/dbt/
+
+echo "Running dbt $*"
+export ASPECTS_EVENT_SINK_DATABASE={{ASPECTS_EVENT_SINK_DATABASE}}
+dbt "$@" --profiles-dir /app/aspects/dbt/
+
+rm -rf {{ DBT_REPOSITORY_PATH }}
---
+++ packages/tutor-contrib-aspects/tutor-contrib-aspects-0.29.0.tar.gz/tutor-contrib-aspects-0.29.0/tutoraspects/templates/aspects/jobs/init/aspects/init-aspects.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+## WARNING: If you modify this block, make sure to also update the
+##          corresponding block in the alembic.sh file.
+
+cd /app/aspects/migrations
+
+alembic upgrade head
+
+cd -
+
+## WARNING: If you modify this block, make sure to also update the
+##          corresponding block in the dbt.sh file.
+
+echo "Installing dbt packages..."
+
+pip install -r /app/aspects/dbt/requirements.txt
+
+rm -rf aspects-dbt
+
+echo "Installing aspects-dbt"
+git clone -b {{ DBT_BRANCH }} {{ DBT_REPOSITORY }}
+
+cd {{ DBT_REPOSITORY_PATH }} || exit
+
+{% if DBT_ENABLE_OVERRIDE %}
+echo /app/aspects/dbt/packages.yml > packages.yml
+echo /app/aspects/dbt/dbt_project.yml > dbt_project.yml
+
+cat {{ DBT_REPOSITORY_PATH }}/packages.yml
+cat {{ DBT_REPOSITORY_PATH }}/dbt_project.yml
+{% endif %}
+
+export ASPECTS_EVENT_SINK_DATABASE={{ASPECTS_EVENT_SINK_DATABASE}}
+
+echo "Installing dbt dependencies"
+dbt deps --profiles-dir /app/aspects/dbt/
+
+echo "Running dbt $*"
+dbt run --profiles-dir /app/aspects/dbt/
+
+rm -rf {{ DBT_REPOSITORY_PATH }}
