0d3ddee75212c94917716d7656601d3f786a5b97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-25 03:09:02 +0000

    Add package temporl/0.0.1/temporl-0.0.1.tar.gz

---
+++ packages/temporl/temporl-0.0.1.tar.gz/temporl-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='temporl',  # Replace with the name you want to reserve
+    version='0.0.1',
+    packages=['temporl'],  # Replace with the name you want to reserve
+    description='Placeholder package',
+    long_description='This is a placeholder.',
+    author='Pedro Silvestre',
+    author_email='psilvestre@gmail.com',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+    ],
+)
