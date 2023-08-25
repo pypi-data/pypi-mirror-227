ecffde14bbd1ba2bf23f92bac393227a5fd7647f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-25 04:09:25 +0000

    Add package typed-argparse/0.3.0/typed_argparse-0.3.0-py3-none-any.whl

---
+++ packages/typed-argparse/typed_argparse-0.3.0-py3-none-any.whl/typed_argparse/__init__.py
@@ -0,0 +1,22 @@
+from .arg import Arg, arg
+from .choices import Choices, get_choices_from, get_choices_from_class
+from .parser import Binding, Bindings, Parser, SubParser, SubParserGroup
+from .typed_args import TypedArgs, WithUnionType, validate_type_union
+
+VERSION = "0.3.0"
+
+__all__ = [
+    "Choices",
+    "TypedArgs",
+    "WithUnionType",
+    "get_choices_from",
+    "get_choices_from_class",
+    "validate_type_union",
+    "Parser",
+    "SubParser",
+    "SubParserGroup",
+    "Arg",
+    "arg",
+    "Binding",
+    "Bindings",
+]
