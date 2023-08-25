f53ace60eb577fa2a9ffc85f71e1f1b50b59ea9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-25 03:09:13 +0000

    Add package physbiblio/2.1.1/physbiblio-2.1.1.tar.gz

---
+++ packages/physbiblio/physbiblio-2.1.1.tar.gz/physbiblio-2.1.1/physbiblio/version.py
@@ -0,0 +1,7 @@
+__version__ = "2.1.1"
+__version_date__ = "24/08/2023"
+
+__recent_changes__ = """<br>* possibility to call "Type"/"none" search in dictionary to allow noUpdate<br>
+* fixes at "Update/Reload bibtex"<br>
+* automatically update arxiv and doi information when modifying or updating bibtex, or cleaning the entries<br>
+"""
