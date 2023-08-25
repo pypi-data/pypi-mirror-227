80cc7786bb2b722bc8a4f2e76d261aad44944a30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-25 03:07:18 +0000

    Add package PyP6XerRemote/1.15.0/PyP6XerRemote-1.15.0.tar.gz

---
+++ packages/PyP6XerRemote/PyP6XerRemote-1.15.0.tar.gz/PyP6XerRemote-1.15.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(
+    name='PyP6XerRemote',
+    version='1.015.00',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    packages=['xerparser', 'xerparser.dcma14', 'xerparser.model', 'xerparser.model.classes'],
+    url='',
+    license='GNU GENERAL PUBLIC LICENSE',
+    author='Hassan',
+    author_email='hassan.emam@hotmail.com',
+    install_requires=[
+
+      ],
+    description='Parser for XER written in Python'
+)
