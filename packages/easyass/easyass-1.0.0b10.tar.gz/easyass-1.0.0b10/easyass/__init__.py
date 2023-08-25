2ee62c562e1622d81c7f13e557995c912270eadd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-25 03:09:08 +0000

    Add package easyass/1.0.0b10/easyass-1.0.0b10.tar.gz

---
+++ packages/easyass/easyass-1.0.0b10.tar.gz/easyass-1.0.0b10/easyass/__init__.py
@@ -0,0 +1,4 @@
+from . import scriptinfo, styles, events
+from .errors import Errors
+from .ass_types import AssColor, AssTime
+from .easyass import Ass
---
+++ packages/easyass/easyass-1.0.0b10.tar.gz/easyass-1.0.0b10/test/test.py
@@ -0,0 +1,17 @@
+from easyass import *
+
+with open(r'test.ass', 'r', encoding='utf8') as fp:
+    ass_str = fp.read()
+
+import easyass
+print(__name__)
+ass_obj = Ass()
+err = ass_obj.parse(ass_str)
+for e in err:
+    print(e['level'], e['message'])
+
+s, err = ass_obj.dump()
+with open(r'../op.ass', 'w', encoding='utf-8') as fp:
+    fp.write('\n'.join(s))
+for e in err:
+    print(e['level'], e['message'])
