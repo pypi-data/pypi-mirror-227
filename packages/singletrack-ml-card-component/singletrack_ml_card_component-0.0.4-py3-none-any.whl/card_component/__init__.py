1195cc8d4b26b492ec06aa8b530dcf803ca619ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-25 03:07:23 +0000

    Add package singletrack-ml-card-component/0.0.4/singletrack_ml_card_component-0.0.4-py3-none-any.whl

---
+++ packages/singletrack-ml-card-component/singletrack_ml_card_component-0.0.4-py3-none-any.whl/card_component/__init__.py
@@ -0,0 +1,29 @@
+import os
+import streamlit.components.v1 as components
+
+
+_RELEASE = False
+
+if not _RELEASE:
+    component_func = components.declare_component(
+        "card",
+        url="http://localhost:3001",
+    )
+else:
+    parent_dir = os.path.dirname(os.path.abspath(__file__))
+    build_dir = os.path.join(parent_dir, "frontend/build")
+    component_func = components.declare_component("card", path=build_dir)
+
+
+def card(title, content, tags, key=None):
+    """Create a new instance of "my_component".
+    """
+    return component_func(title=title, content=content, tags=tags, key=key)
+
+
+if not _RELEASE:
+    result = card(
+        title="I am a big title",
+        content="blah blah blah",
+        tags={"ticker": "VOD"},
+    )
