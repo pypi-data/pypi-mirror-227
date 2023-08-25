ec56f5f1e441aeb18f72c1399e07c52b82399270
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-25 15:07:31 +0000

    Add package squeal/0.0.12/squeal-0.0.12-py3-none-any.whl

---
+++ packages/squeal/squeal-0.0.12-py3-none-any.whl/squeal/backend/local.py
@@ -0,0 +1,214 @@
+import time
+from collections import Counter
+from typing import List, Tuple, Iterable, Optional, Collection
+
+from .base import Backend, Message, TopicLock
+
+
+class LocalBackend(Backend):
+    """
+    This is just a reference implementation for testing purposes.  Don't use it for anything else.  It's deliberately
+    unoptimized to make it easy to follow what it's doing.
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.messages = []
+        self.topic_locks = {}
+        self.rate_limits = {}
+        self.unique_constraint = set()
+        self.next_id = 0
+        self.created = False
+        self._max_payload_size = None
+        self._hash_size = 16
+
+    @property
+    def max_payload_size(self) -> Optional[int]:
+        return self._max_payload_size
+
+    @property
+    def hash_size(self) -> int:
+        return self._hash_size
+
+    def create(self) -> None:
+        self.created = True
+
+    def destroy(self) -> None:
+        self.created = False
+
+    def batch_put(
+        self,
+        data: Collection[Tuple[bytes, int, Optional[bytes]]],
+        priority: int,
+        delay: int,
+        failure_base_delay: int,
+    ) -> int:
+        assert self.created
+        for payload, topic, hsh in data:
+            if hsh is not None and len(hsh) != self.hash_size:
+                raise ValueError(
+                    f"hsh size is not HASH_SIZE ({len(hsh)} != {self.hash_size})"
+                )
+
+        constraint_violations = []
+        for payload, topic, hsh in data:
+            if hsh is not None:
+                if (topic, hsh) in self.unique_constraint:
+                    constraint_violations.append((payload, topic, hsh))
+                    continue
+                self.unique_constraint.add((topic, hsh))
+
+            self.messages.append(
+                {
+                    "id": self.next_id,
+                    "payload": payload,
+                    "topic": topic,
+                    "hsh": hsh,
+                    "priority": priority,
+                    "acquired": False,
+                    "delivery_time": time.time() + delay,
+                    "failure_base_delay": failure_base_delay,
+                    "failure_count": 0,
+                }
+            )
+            self.next_id += 1
+
+        return len(data) - len(constraint_violations)
+
+    def batch_get(
+        self, topic: int, size: int, visibility_timeout: int
+    ) -> List["Message"]:
+        assert self.created
+        now = time.time()
+        self.messages.sort(key=lambda x: [-x["priority"], x["id"]])
+
+        output = []
+        for msg in self.messages:
+            if len(output) >= size:
+                break
+            if msg["acquired"] and msg["expire_time"] > now:
+                continue
+            if msg["topic"] != topic:
+                continue
+            if msg["delivery_time"] > now:
+                continue
+
+            msg["acquired"] = True
+            msg["expire_time"] = now + visibility_timeout
+            output.append(Message(msg["payload"], msg["id"], self))
+        return output
+
+    def ack(self, task_id: int) -> None:
+        assert self.created
+        for idx, msg in enumerate(self.messages):
+            if msg["id"] != task_id:
+                continue
+            if not msg["acquired"]:
+                continue
+            break
+        else:
+            return
+        if self.messages[idx]["hsh"] is not None:
+            k = (self.messages[idx]["topic"], self.messages[idx]["hsh"])
+            self.unique_constraint.remove(k)
+        del self.messages[idx]
+
+    def batch_nack(self, task_ids: Collection[int]) -> None:
+        assert self.created
+        to_nack = set(task_ids)
+        for idx, msg in enumerate(self.messages):
+            if msg["id"] not in to_nack:
+                continue
+            if not msg["acquired"]:
+                continue
+
+            msg["acquired"] = False
+            delay = msg["failure_base_delay"] * (2 ** msg["failure_count"])
+            msg["failure_count"] += 1
+            msg["delivery_time"] = time.time() + delay
+            print(delay)
+
+    def batch_touch(self, task_ids: Collection[int], visibility_timeout: int) -> None:
+        assert self.created
+        to_touch = set(task_ids)
+        for msg in self.messages:
+            if msg["id"] in to_touch:
+                continue
+            if not msg["acquired"]:
+                continue
+            msg["expire_time"] = time.time() + visibility_timeout
+
+    def list_topics(self) -> List[Tuple[int, int]]:
+        assert self.created
+        counts = Counter()
+
+        now = time.time()
+        for msg in self.messages:
+            if msg["acquired"]:
+                continue
+            if msg["delivery_time"] > now:
+                continue
+
+            counts[msg["topic"]] += 1
+
+        return list(counts.items())
+
+    def get_topic_size(self, topic: int) -> int:
+        assert self.created
+        n = 0
+        now = time.time()
+        for msg in self.messages:
+            if msg["acquired"]:
+                continue
+            if msg["topic"] != topic:
+                continue
+            if msg["delivery_time"] > now:
+                continue
+
+            n += 1
+        return n
+
+    def acquire_topic(
+        self, topic_lock_visibility_timeout: int
+    ) -> Optional["TopicLock"]:
+        assert self.created
+        for topic, size in self.list_topics():
+            if topic not in self.topic_locks or self.topic_locks[topic] < time.time():
+                self.topic_locks[topic] = time.time() + topic_lock_visibility_timeout
+                return TopicLock(topic, self, topic_lock_visibility_timeout)
+        return None
+
+    def batch_release_topic(self, topics: Collection[int]) -> None:
+        assert self.created
+        for topic in topics:
+            if topic in self.topic_locks:
+                del self.topic_locks[topic]
+
+    def batch_touch_topic(
+        self, topics: Collection[int], topic_lock_visibility_timeout: int
+    ) -> None:
+        assert self.created
+        for topic in topics:
+            if topic not in self.topic_locks:
+                continue
+            self.topic_locks[topic] = time.time() + topic_lock_visibility_timeout
+
+    def rate_limit(self, key: bytes, interval_seconds: int) -> bool:
+        if len(key) != self.hash_size:
+            raise ValueError(
+                f"rate limit key size is not HASH_SIZE ({len(key)} != {self.hash_size})"
+            )
+
+        now = time.time()
+        if key not in self.rate_limits or self.rate_limits[key] <= now:
+            self.rate_limits[key] = now + interval_seconds
+            return True
+        return False
+
+    def rate_limit_forced(self, key: bytes, interval_seconds: int) -> None:
+        if len(key) != self.hash_size:
+            raise ValueError(
+                f"rate limit key size is not HASH_SIZE ({len(key)} != {self.hash_size})"
+            )
+
+        self.rate_limits[key] = time.time() + interval_seconds
---
+++ packages/squeal/squeal-0.0.12-py3-none-any.whl/squeal/queue.py
@@ -0,0 +1,158 @@
+import math
+from typing import Tuple, List, Dict, Iterable, Optional, Collection
+from squeal.backend.base import Backend, Message, TopicLock
+
+
+class Queue:
+    """
+    FIFO(-ish) queue backed by a SQL table
+    """
+
+    def __init__(
+        self,
+        backend: Backend,
+        new_message_delay: int = 0,
+        failure_base_delay: int = 1,
+        visibility_timeout: int = 60,
+        topic_lock_visibility_timeout: int = 60 * 15,
+        rate_limit_per_hour: Optional[float] = None,
+        auto_create: bool = True,
+    ):
+        self.backend = backend
+        self.new_message_delay = new_message_delay
+        self.failure_base_delay = failure_base_delay
+        self.visibility_timeout = visibility_timeout
+        self.topic_lock_visibility_timeout = topic_lock_visibility_timeout
+        self.rate_limit_per_hour = rate_limit_per_hour
+
+        self._held_messages: Dict[int, "Message"] = {}
+        self._held_topics: Dict[int, "TopicLock"] = {}
+
+        if self.new_message_delay < 0:
+            raise RuntimeError("Delay must be non-negative")
+        if self.failure_base_delay < 0:
+            raise RuntimeError("Delay must be non-negative")
+        if self.visibility_timeout <= 0:
+            raise RuntimeError("Visibility timeout must be positive")
+
+        if auto_create:
+            self.create()
+
+    def create(self) -> None:
+        self.backend.create()
+
+    def destroy(self) -> None:
+        self.backend.destroy()
+
+    @property
+    def _rate_limit_interval_seconds(self) -> Optional[int]:
+        if self.rate_limit_per_hour is None:
+            return None
+        return math.ceil(60 * 60 / self.rate_limit_per_hour)
+
+    def put(
+        self, item: bytes, topic: int, hsh: Optional[bytes] = None, priority: int = 0
+    ) -> int:
+        return self.batch_put(items=[(item, topic, hsh)], priority=priority)
+
+    def batch_put(
+        self, items: Collection[Tuple[bytes, int, Optional[bytes]]], priority: int = 0
+    ) -> int:
+        interval = self._rate_limit_interval_seconds
+        if interval is not None:
+            approved_items = []
+            for item, topic, hsh in items:
+                if hsh is None or self.backend.rate_limit(
+                    hsh, interval_seconds=interval
+                ):
+                    approved_items.append((item, topic, hsh))
+            items = approved_items
+
+        return self.backend.batch_put(
+            items,
+            priority,
+            self.new_message_delay,
+            self.failure_base_delay,
+        )
+
+    def get(self, topic: int) -> Optional["Message"]:
+        msgs = self.batch_get([(topic, 1)])
+        return msgs[0] if msgs else None
+
+    def batch_get(self, topics: Iterable[Tuple[int, int]]) -> List["Message"]:
+        out = []
+        for topic, size in topics:
+            if size <= 0:
+                continue
+            topic_msgs = self.backend.batch_get(topic, size, self.visibility_timeout)
+            out.extend(topic_msgs)
+        for msg in out:
+            self._held_messages[msg.idx] = msg
+        return out
+
+    def _prune_held_messages(self):
+        # XXX
+        released = [msg.idx for msg in self._held_messages.values() if msg.released]
+        for idx in released:
+            del self._held_messages[idx]
+
+    def touch_all(self) -> None:
+        # XXX
+        self._prune_held_messages()
+        self.backend.batch_touch(self._held_messages.keys(), self.visibility_timeout)
+
+    def nack_all(self) -> None:
+        # XXX
+        self._prune_held_messages()
+        self.backend.batch_nack(self._held_messages.keys())
+        for msg in self._held_messages.values():
+            if msg.status is None:
+                msg.status = False
+        self._held_messages.clear()
+
+    def list_topics(self) -> List[Tuple[int, int]]:
+        return self.backend.list_topics()
+
+    def get_topic_size(self, topic: int) -> int:
+        return self.backend.get_topic_size(topic)
+
+    def _prune_held_topics(self):
+        # XXX
+        released = [i for i, t in self._held_topics.items() if t.released]
+        for i in released:
+            del self._held_topics[i]
+
+    def acquire_topic(self) -> Optional["TopicLock"]:
+        topic = self.backend.acquire_topic(
+            topic_lock_visibility_timeout=self.topic_lock_visibility_timeout
+        )
+        if topic is None:
+            return None
+        self._held_topics[topic.idx] = topic
+        return topic
+
+    def release_topics(self) -> None:
+        self._prune_held_topics()
+        self.backend.batch_release_topic(self._held_topics.keys())
+        self._held_topics = {}
+
+    def release_topic(self, topic: int) -> None:
+        self._prune_held_topics()
+        if topic not in self._held_topics:
+            return
+        self._held_topics[topic].release()
+        self._prune_held_topics()
+
+    def touch_topics(self) -> None:
+        self._prune_held_topics()
+        self.backend.batch_touch_topic(
+            self._held_topics.keys(),
+            topic_lock_visibility_timeout=self.topic_lock_visibility_timeout,
+        )
+
+    def list_held_topics(self) -> List["TopicLock"]:
+        self._prune_held_topics()
+        return list(self._held_topics.values())
+
+
+__all__ = ["Queue"]
