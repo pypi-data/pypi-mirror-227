f98405bfd296501ab55dfd3e5efba91c6d36d395
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-25 03:08:04 +0000

    Add package kedro-datasets/1.6.0/kedro-datasets-1.6.0.tar.gz

---
+++ packages/kedro-datasets/kedro-datasets-1.6.0.tar.gz/kedro-datasets-1.6.0/kedro_datasets/polars/__init__.py
@@ -0,0 +1,11 @@
+"""``AbstractDataSet`` implementations that produce pandas DataFrames."""
+from typing import Any
+
+import lazy_loader as lazy
+
+# https://github.com/pylint-dev/pylint/issues/4300#issuecomment-1043601901
+CSVDataSet: Any
+
+__getattr__, __dir__, __all__ = lazy.attach(
+    __name__, submod_attrs={"csv_dataset": ["CSVDataSet"]}
+)
