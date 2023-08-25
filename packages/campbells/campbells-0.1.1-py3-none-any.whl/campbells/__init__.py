82dd8680d3614c7d096b1c98684c43fef1910a67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-25 14:08:38 +0000

    Add package campbells/0.1.1/campbells-0.1.1-py3-none-any.whl

---
+++ packages/campbells/campbells-0.1.1-py3-none-any.whl/campbells/__init__.py
@@ -0,0 +1,59 @@
+"""Campbells - A condensed web scraping library.
+
+https://www.github.com/lmmx/campbells
+
+Campbells is adapted from Beautiful Soup under the MIT license.
+
+Beautiful Soup uses a pluggable XML or HTML parser to parse a
+(possibly invalid) document into a tree representation. Beautiful Soup
+provides methods and Pythonic idioms that make it easy to navigate,
+search, and modify the parse tree.
+
+Like Beautiful Soup, Campbells works with Python 3.6 and up. It works
+better if lxml and/or html5lib is installed.
+
+For more than you ever wanted to know about Beautiful Soup, see their
+documentation: http://www.crummy.com/software/BeautifulSoup/bs4/doc/
+"""
+
+__version__ = "0.1.1"
+
+__all__ = ["BeautifulSoup"]
+
+import sys
+import warnings
+from collections import Counter
+
+from .builder import HTMLParserTreeBuilder
+from .builder.build import ParserRejectedMarkup, XMLParsedAsHTMLWarning
+from .dammit import UnicodeDammit
+from .element import (
+    CSS,
+    DEFAULT_OUTPUT_ENCODING,
+    PYTHON_SPECIFIC_ENCODINGS,
+    CData,
+    Comment,
+    Declaration,
+    Doctype,
+    NavigableString,
+    PageElement,
+    ProcessingInstruction,
+    ResultSet,
+    Script,
+    SoupStrainer,
+    Stylesheet,
+    Tag,
+    TemplateString,
+)
+from .main import (
+    BeautifulSoup,
+    FeatureNotFound,
+    GuessedAtParserWarning,
+    MarkupResemblesLocatorWarning,
+    StopParsing,
+)
+
+# If this file is run as a script, act as an HTML pretty-printer.
+if __name__ == "__main__":
+    soup = BeautifulSoup(sys.stdin)
+    print(soup.prettify())
