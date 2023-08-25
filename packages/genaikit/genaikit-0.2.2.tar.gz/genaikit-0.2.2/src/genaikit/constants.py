6dfcd3936404a259651019603de0683e20d1819b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-25 04:09:13 +0000

    Add package genaikit/0.2.2/genaikit-0.2.2.tar.gz

---
+++ packages/genaikit/genaikit-0.2.2.tar.gz/genaikit-0.2.2/src/genaikit/constants.py
@@ -0,0 +1,27 @@
+"""Constants for the whole project"""
+
+
+MODELS = (
+    'gpt-3.5-turbo',
+    'gpt-3.5-turbo-0613',
+    'gpt-4',
+    'gpt-4-0613',
+)
+MODELS_EMBEDDING = (
+    'text-embedding-ada-002',
+)
+
+MAX_TOKENS = (
+    (MODELS[0], 4096),
+    (MODELS[1], 4096),
+    (MODELS[2], 4096),
+    (MODELS[3], 4096)
+)
+
+ROLES = (  # roles for messages objects
+    'system',
+    'user',
+    'assistant'
+)  # see https://platform.openai.com/docs/guides/gpt/chat-completions-api
+
+DEBUG = True
