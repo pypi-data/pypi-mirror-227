a9582b19f7cb04240713508827ec6d16761c5788
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-25 03:07:20 +0000

    Add package EAScheduler/0.1.10/EAScheduler-0.1.10-py3-none-any.whl

---
+++ packages/EAScheduler/EAScheduler-0.1.10-py3-none-any.whl/eascheduler/errors/errors.py
@@ -0,0 +1,15 @@
+
+class JobAlreadyCanceledException(Exception):
+    pass
+
+
+class UnknownWeekdayError(Exception):
+    pass
+
+
+class FirstRunInThePastError(Exception):
+    pass
+
+
+class BoundaryFunctionError(Exception):
+    pass
