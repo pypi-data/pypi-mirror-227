b191b52081a5760dd0665d8d33ed17b1adc3ab3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-25 14:08:44 +0000

    Add package president/112/president-112.tar.gz

---
+++ packages/president/president-112.tar.gz/president-112/president/defines.py
@@ -0,0 +1,16 @@
+# This file is placed in the Public Domain.
+#
+# pylint: disable=C0116,W0611,W0401,W0614,E0402,E0611,E0603
+
+
+"object programming version"
+
+
+from opv import objects, methods, reactor, storage, threads
+
+
+from opv.objects import *
+from opv.methods import *
+#from opv.reactor import *
+from opv.storage import *
+#from opv.threads import *
