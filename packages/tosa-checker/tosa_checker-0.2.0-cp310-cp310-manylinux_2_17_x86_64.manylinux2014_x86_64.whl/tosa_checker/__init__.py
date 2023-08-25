06ae824972fa682d3f44bf0051a5593bb6379081
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-25 03:08:33 +0000

    Add package tosa-checker/0.2.0/tosa_checker-0.2.0-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/tosa-checker/tosa_checker-0.2.0-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/tosa_checker/__init__.py
@@ -0,0 +1,9 @@
+# SPDX-FileCopyrightText: Copyright 2022-2023, Arm Limited and/or its affiliates.
+# SPDX-License-Identifier: Apache-2.0
+
+"""the package provides a way to check if a TFLite model is compatible with the TOSA specification."""
+
+from _tosa_checker_wrapper import *
+
+__version__ = "0.2.0"
+__tensorflow_version__ = "v2.13.0"
