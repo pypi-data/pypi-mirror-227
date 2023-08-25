bb5e20295c8de7311b0161fe44671330f6777baf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-25 14:08:57 +0000

    Add package aws-prototyping-sdk.identity/0.19.68/aws_prototyping_sdk.identity-0.19.68.tar.gz

---
+++ packages/aws-prototyping-sdk.identity/aws_prototyping_sdk.identity-0.19.68.tar.gz/aws_prototyping_sdk.identity-0.19.68/src/aws_prototyping_sdk/identity/_jsii/__init__.py
@@ -0,0 +1,31 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+from typeguard import check_type
+
+import aws_cdk._jsii
+import aws_cdk.aws_cognito_identitypool_alpha._jsii
+import aws_prototyping_sdk.pdk_nag._jsii
+import cdk_nag._jsii
+import constructs._jsii
+import projen._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@aws-prototyping-sdk/identity",
+    "0.19.68",
+    __name__[0:-6],
+    "identity@0.19.68.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
