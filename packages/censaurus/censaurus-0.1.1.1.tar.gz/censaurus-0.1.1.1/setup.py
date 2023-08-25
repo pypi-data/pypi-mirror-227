a10442e7f71326b9535996bb90b3f276c84bc3bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-25 14:07:22 +0000

    Add package censaurus/0.1.1.1/censaurus-0.1.1.1.tar.gz

---
+++ packages/censaurus/censaurus-0.1.1.1.tar.gz/censaurus-0.1.1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="censaurus",
+    version="0.1.1.1",
+    description="A comprehensive, feature-rich, and user-oriented Python package that wraps the U.S. Census Bureau’s Data and Geographic APIs.",
+    long_description_content_type="text/markdown",
+    author="Ethan Singer",
+    author_email="singerep@bu.edu",
+    packages=['censaurus']
+)
