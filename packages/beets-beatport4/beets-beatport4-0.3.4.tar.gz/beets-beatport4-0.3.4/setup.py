3ebacaf536b6447a65cfbe24fad0b74b1dccfd77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-25 04:09:10 +0000

    Add package beets-beatport4/0.3.4/beets-beatport4-0.3.4.tar.gz

---
+++ packages/beets-beatport4/beets-beatport4-0.3.4.tar.gz/beets-beatport4-0.3.4/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+
+setup(
+    name='beets-beatport4',
+    version='0.3.4',
+    description='Plugin for beets (http://beets.io) to replace stock beatport plugin which is not yet compatible '
+                'with Beatport API v4.',
+    long_description=open('README.rst').read(),
+    author='Szymon "Samik" Tarasinski',
+    author_email='st.samik@gmail.com',
+    url='https://github.com/Samik081/beets-beatport4',
+    download_url='https://github.com/Samik081/beets-beatport4/releases/download/v0.3.3/beets-beatport4-0.3.4.tar.gz',
+    license='MIT',
+    platforms='ALL',
+
+    packages=['beetsplug'],
+
+    install_requires=[
+        'beets>=1.6.0',
+        'requests',
+        'confuse'
+    ],
+
+    classifiers=[
+        'Topic :: Multimedia :: Sound/Audio',
+        'Topic :: Multimedia :: Sound/Audio :: Players :: MP3',
+        'License :: OSI Approved :: MIT License',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
