69d0f375ae506b256946e5b9302baeb275cb8bfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-25 14:08:34 +0000

    Add package forks-sync/4.2.0/forks-sync-4.2.0.tar.gz

---
+++ packages/forks-sync/forks-sync-4.2.0.tar.gz/forks-sync-4.2.0/setup.py
@@ -0,0 +1,70 @@
+import re
+
+import setuptools
+
+
+with open('README.md', 'r') as readme_file:
+    long_description = readme_file.read()
+
+# Inspiration: https://stackoverflow.com/a/7071358/6064135
+with open('forks_sync/_version.py', 'r') as version_file:
+    version_groups = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file.read(), re.M)
+    if version_groups:
+        version = version_groups.group(1)
+    else:
+        raise RuntimeError('Unable to find version string!')
+
+
+REQUIREMENTS = [
+    'PyGithub == 1.*',
+    'woodchips == 1.*',
+]
+
+DEV_REQUIREMENTS = [
+    'bandit == 1.7.*',
+    'black == 23.*',
+    'build == 0.10.*',
+    'flake8 == 6.*',
+    'isort == 5.*',
+    'mypy == 1.5.*',
+    'pytest == 7.*',
+    'pytest-cov == 4.*',
+    'twine == 4.*',
+]
+
+setuptools.setup(
+    name='forks-sync',
+    version=version,
+    description='Keep all your git forks up to date with the remote default branch.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='http://github.com/justintime50/forks-sync',
+    author='Justintime50',
+    license='MIT',
+    packages=setuptools.find_packages(
+        exclude=[
+            'examples',
+            'test',
+        ]
+    ),
+    package_data={
+        'forks_sync': [
+            'py.typed',
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=REQUIREMENTS,
+    extras_require={
+        'dev': DEV_REQUIREMENTS,
+    },
+    entry_points={
+        'console_scripts': [
+            'forks-sync=forks_sync.cli:main',
+        ],
+    },
+    python_requires='>=3.8, <4',
+)
