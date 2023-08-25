7bdb325692ca6e151066366f68ec6c26a1626656
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-25 03:07:27 +0000

    Add package cubicweb-addressbook/2.0.1/cubicweb_addressbook-2.0.1-py3-none-any.whl

---
+++ packages/cubicweb-addressbook/cubicweb_addressbook-2.0.1-py3-none-any.whl/cubicweb_addressbook/__pkginfo__.py
@@ -0,0 +1,26 @@
+# pylint: disable-msg=W0622
+"""cubicweb-addressbook packaging information"""
+
+modname = "addressbook"
+distname = f"cubicweb-{modname}"
+
+numversion = (2, 0, 1)
+version = ".".join(str(num) for num in numversion)
+
+license = "LGPL"
+description = "address book component for the CubicWeb framework"
+author = "Logilab"
+author_email = "contact@logilab.fr"
+web = f"https://forge.extranet.logilab.fr/cubicweb/cubes/{distname}"
+classifiers = [
+    "Environment :: Web Environment",
+    "Framework :: CubicWeb",
+    "Programming Language :: Python",
+    "Programming Language :: JavaScript",
+]
+
+__depends__ = {
+    "cubicweb": ">= 4.0.0, < 5.0.0",
+    "cubicweb-web": ">= 1.0.0, < 2.0.0",
+    "cubicweb-geocoding": ">= 1.0.0, < 2.0.0",
+}
