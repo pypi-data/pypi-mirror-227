d7f302d51dbd9a205724fa90cfe9924f604f3f7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-25 03:09:08 +0000

    Add package datafed/2.0.2/datafed-2.0.2.tar.gz

---
+++ packages/datafed/datafed-2.0.2.tar.gz/datafed-2.0.2/setup.py
@@ -0,0 +1,36 @@
+import setuptools
+from os import path
+
+
+# read the contents of README file
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README"), encoding="utf-8") as f:
+    long_description = f.read()
+
+from datafed.VERSION import __version__
+
+setuptools.setup(
+    name="datafed",
+    version=__version__,
+    author="Dale Stansberry, Joshua Brown",
+    author_email="stansberrydv@ornl.gov, brownjs@ornl.gov",
+    description="DataFed CLI and API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ORNL/DataFed",
+    packages=setuptools.find_packages(),
+    setup_requires=["setuptools"],
+    install_requires=[
+        "protobuf>=3",
+        "pyzmq>=16",
+        "wget>=3",
+        "click>=7",
+        "prompt_toolkit>=2",
+    ],
+    entry_points={"console_scripts": ["datafed = datafed.CLI:run"]},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
