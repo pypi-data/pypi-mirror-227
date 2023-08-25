9d0c57c16902f791aaddf6487545e5925dbf84b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-25 03:07:35 +0000

    Add package hljm/1.1.12/hljm-1.1.12.tar.gz

---
+++ packages/hljm/hljm-1.1.12.tar.gz/hljm-1.1.12/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="hljm",
+    version="1.1.12",
+    author="黑龙",
+    author_email="2076293320@qq.com",
+    description="Description of your library",
+    packages=["hljm"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
