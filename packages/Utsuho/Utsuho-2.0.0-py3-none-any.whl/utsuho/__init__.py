f2ee88c4763f5823c8d1961aa2580408455281d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-25 15:08:27 +0000

    Add package Utsuho/2.0.0/Utsuho-2.0.0-py3-none-any.whl

---
+++ packages/Utsuho/Utsuho-2.0.0-py3-none-any.whl/utsuho/__init__.py
@@ -0,0 +1,5 @@
+""" Utsuho is an interconverter between Japanese half-width katakana and full-width katakana.
+"""
+from .converters import WidthConverterConfig, FullToHalfConverter, HalfToFullConverter
+
+__version__ = '2.0.0'
