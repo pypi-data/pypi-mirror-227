cbe918eef972eef0d2777cac3ab4115833d3bb94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-25 14:08:07 +0000

    Add package pint-pulsar/0.9.7/pint_pulsar-0.9.7-py3-none-any.whl

---
+++ packages/pint-pulsar/pint_pulsar-0.9.7-py3-none-any.whl/pint/models/binary_bt.py
@@ -0,0 +1,68 @@
+"""The BT (Blandford & Teukolsky) model."""
+
+from pint.models.parameter import floatParameter
+from pint.models.pulsar_binary import PulsarBinary
+from pint.models.stand_alone_psr_binaries.BT_model import BTmodel
+from pint.models.timing_model import MissingParameter
+
+
+class BinaryBT(PulsarBinary):
+    """Blandford and Teukolsky binary model.
+
+    This binary model is described in Blandford and Teukolshy 1976. It is
+    a relatively simple parametrized post-Keplerian model that does not
+    support Shapiro delay calculations.
+
+    The actual calculations for this are done in
+    :class:`pint.models.stand_alone_psr_binaries.BT_model.BTmodel`.
+
+    Parameters supported:
+
+    .. paramtable::
+        :class: pint.models.binary_bt.BinaryBT
+
+    Notes
+    -----
+    Because PINT's binary models all support specification of multiple orbital
+    frequency derivatives FBn, this is capable of behaving like the model called
+    BTX in tempo2. The model called BTX in tempo instead supports multiple
+    (non-interacting) companions, and that is not supported here. Neither can
+    PINT accept "BTX" as an alias for this model.
+
+    See Blandford & Teukolsky 1976, ApJ, 205, 580.
+    """
+
+    register = True
+
+    def __init__(self):
+        super().__init__()
+        self.binary_model_name = "BT"
+        self.binary_model_class = BTmodel
+
+        self.add_param(
+            floatParameter(
+                name="GAMMA",
+                value=0.0,
+                units="second",
+                description="Time dilation & gravitational redshift",
+            )
+        )
+        self.remove_param("M2")
+        self.remove_param("SINI")
+
+    def validate(self):
+        """Validate BT model parameters"""
+        super().validate()
+        for p in ("T0", "A1"):
+            if getattr(self, p).value is None:
+                raise MissingParameter("BT", p, f"{p} is required for BT")
+
+        # If any *DOT is set, we need T0
+        for p in ("PBDOT", "OMDOT", "EDOT", "A1DOT"):
+            if getattr(self, p).value is None:
+                getattr(self, p).value = "0"
+                getattr(self, p).frozen = True
+
+        if self.GAMMA.value is None:
+            self.GAMMA.value = "0"
+            self.GAMMA.frozen = True
---
+++ packages/pint-pulsar/pint_pulsar-0.9.7-py3-none-any.whl/pint/models/wave.py
@@ -0,0 +1,118 @@
+"""Delays expressed as a sum of sinusoids."""
+import astropy.units as u
+import numpy as np
+
+from pint.models.parameter import MJDParameter, floatParameter, prefixParameter
+from pint.models.timing_model import PhaseComponent, MissingParameter
+
+
+class Wave(PhaseComponent):
+    """Delays expressed as a sum of sinusoids.
+
+    Historically, used for decomposition of timing noise into a series of
+    sine/cosine components.
+
+    For consistency with the implementation in tempo2, this signal is treated
+    as a time series, but trivially converted into phase by multiplication by
+    F0, which could makes changes to PEPOCH fragile if there is strong spin
+    frequency evolution.
+
+    Parameters supported:
+
+    .. paramtable::
+        :class: pint.models.wave.Wave
+    """
+
+    register = True
+    category = "wave"
+
+    def __init__(self):
+        super().__init__()
+
+        self.add_param(
+            floatParameter(
+                name="WAVE_OM",
+                description="Base frequency of wave solution",
+                units="1/d",
+            )
+        )
+        self.add_param(
+            prefixParameter(
+                name="WAVE1",
+                units="s",
+                description="Wave components",
+                type_match="pair",
+                long_double=True,
+                parameter_type="pair",
+            )
+        )
+        self.add_param(
+            MJDParameter(
+                name="WAVEEPOCH",
+                description="Reference epoch for wave solution",
+                time_scale="tdb",
+            )
+        )
+        self.phase_funcs_component += [self.wave_phase]
+
+    def setup(self):
+        super().setup()
+        self.wave_terms = list(self.get_prefix_mapping_component("WAVE").keys())
+        self.num_wave_terms = len(self.wave_terms)
+
+    def validate(self):
+        super().validate()
+        self.setup()
+        if self.WAVEEPOCH.quantity is None:
+            if self.PEPOCH.quantity is None:
+                raise MissingParameter(
+                    "Wave",
+                    "WAVEEPOCH",
+                    "WAVEEPOCH or PEPOCH are required if " "WAVE_OM is set.",
+                )
+            else:
+                self.WAVEEPOCH = self.PEPOCH
+
+        if (not hasattr(self._parent, "F0")) or (self._parent.F0.quantity is None):
+            raise MissingParameter(
+                "Wave", "F0", "F0 is required if WAVE entries are present."
+            )
+        self.wave_terms.sort()
+        wave_in_order = list(range(1, max(self.wave_terms) + 1))
+        if self.wave_terms != wave_in_order:
+            diff = list(set(wave_in_order) - set(self.wave_terms))
+            raise MissingParameter("Wave", "WAVE%d" % diff[0])
+
+    def print_par(self, format="pint"):
+        result = ""
+        wave_terms = ["WAVE%d" % ii for ii in range(1, self.num_wave_terms + 1)]
+
+        result += self.WAVEEPOCH.as_parfile_line(format=format)
+        result += self.WAVE_OM.as_parfile_line(format=format)
+        for ft in wave_terms:
+            par = getattr(self, ft)
+            result += par.as_parfile_line(format=format)
+
+        return result
+
+    def wave_phase(self, toas, delays):
+        times = 0
+        wave_names = ["WAVE%d" % ii for ii in range(1, self.num_wave_terms + 1)]
+        wave_terms = [getattr(self, name) for name in wave_names]
+        wave_om = self.WAVE_OM.quantity
+        base_phase = (
+            wave_om
+            * (
+                toas.table["tdbld"] * u.day
+                - self.WAVEEPOCH.value * u.day
+                - delays.to(u.day)
+            )
+        ).value
+
+        for k, wave_term in enumerate(wave_terms):
+            wave_a, wave_b = wave_term.quantity
+            wave_phase = (k + 1) * base_phase
+            times += wave_a * np.sin(wave_phase)
+            times += wave_b * np.cos(wave_phase)
+
+        return ((times) * self._parent.F0.quantity).to(u.dimensionless_unscaled)
