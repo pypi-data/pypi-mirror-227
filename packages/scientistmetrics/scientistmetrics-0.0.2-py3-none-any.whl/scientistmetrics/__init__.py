c0b0f165d540ab4c63d97f1ca36abd06989fb108
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-25 14:07:39 +0000

    Add package scientistmetrics/0.0.2/scientistmetrics-0.0.2-py3-none-any.whl

---
+++ packages/scientistmetrics/scientistmetrics-0.0.2-py3-none-any.whl/scientistmetrics/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+@author: enfantbenidedieu
+"""
+from __future__ import absolute_import
+
+from .association import scientistmetrics
+from .association import ggheatmap
+from .model import powersetmodel
