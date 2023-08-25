8dde42c4c2c94273d5bae161834abbc388d668c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-25 03:07:20 +0000

    Add package Testing-sample2/0.1.8/Testing_sample2-0.1.8.tar.gz

---
+++ packages/Testing-sample2/Testing_sample2-0.1.8.tar.gz/Testing_sample2-0.1.8/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="Testing_sample2",
+    version="0.1.8",
+    author="venkateshwaran",
+    author_email="venkateshwaranns@gmail.com",
+    description="A short description of your package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/venkateshwaranns/testing.git",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'numpy>=1.18.0',
+        'requests>=2.22.0',
+        'pandas>=1.0.0'
+    ],
+)
