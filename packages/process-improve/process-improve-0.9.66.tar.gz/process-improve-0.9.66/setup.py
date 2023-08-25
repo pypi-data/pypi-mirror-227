37f2b0b7de4c16395e7ab9f40e9fd3352188d157
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-25 14:06:44 +0000

    Add package process-improve/0.9.66/process-improve-0.9.66.tar.gz

---
+++ packages/process-improve/process-improve-0.9.66.tar.gz/process-improve-0.9.66/setup.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+import pathlib
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open(pathlib.Path(".") / "process_improve" / "__init__.py") as fh:
+    version_number = [line.split("=")[1].strip() for line in fh.read().split("\n") if line.startswith("__version__")][
+        0
+    ].replace('"', "")
+
+setuptools.setup(
+    name="process-improve",
+    version=version_number,
+    author="Kevin Dunn",
+    author_email="kgdunn@gmail.com",
+    description=(
+        "Process Improvement using Data: Designed Experiments; Latent Variables (PCA, "
+        "PLS, multivariate methods with missing data); Process Monitoring; Batch data analysis."
+    ),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kgdunn/process_improve",
+    packages=setuptools.find_packages(exclude=["tests"]),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+    install_requires=[
+        "pandas",
+        "numpy",
+        "statsmodels",
+        "matplotlib",
+        "bokeh",
+        "scikit-learn",
+        "patsy",
+        "scikit-image",
+        "scikit-learn",
+        "plotly",
+        "numba",
+        "seaborn",
+        "pydantic",
+        "tqdm",
+    ],
+    # Include additional files into the package
+    include_package_data=True,
+)
