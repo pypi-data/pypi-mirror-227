f14d5a32ec96b069371f85f506c9ad6943575693
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-25 04:09:15 +0000

    Add package discoverable-tph-280/0.1.1/discoverable_tph_280-0.1.1.tar.gz

---
+++ packages/discoverable-tph-280/discoverable_tph_280-0.1.1.tar.gz/discoverable_tph_280-0.1.1/discoverable_tph_280/barometer.py
@@ -0,0 +1,42 @@
+from __future__ import annotations
+import logging
+import logging.config
+from typing import Optional
+
+from paho.mqtt.client import MQTTMessage
+from ha_mqtt_discoverable import EntityInfo, Subscriber, Settings, mqtt
+from .base import GuageInfo, Guage
+
+
+class BarometerInfo(GuageInfo):
+    """Special information for Barometer"""
+
+    component: str = "sensor"
+    name: str = "My Barometer"
+    object_id: Optional[str] = "my-barometer"
+    device_class: Optional[str] = "pressure"
+    unique_id: Optional[str] = "my-barometer"
+
+
+class Barometer(Guage):
+    """Implements an MQTT barometer:
+    https://www.home-assistant.io/integrations/sensor.mqtt/
+    """
+
+    value_name: str = "pressure"
+
+    def __init__(
+        cls, mqtt: MQTT, name: str = "Barometer", device_class="pressure"
+    ):
+        super(Barometer, cls).__init__(
+            mqtt=mqtt,
+            name=name,
+            device_class=device_class,
+            info_class=BarometerInfo,
+            callback=Barometer.command_callback,
+        )
+
+    @staticmethod
+    def command_callback(client: Client, user_data, message: MQTTMessage):
+        callback_payload = message.payload.decode()
+        logging.info(f"Barometer received {callback_payload} from HA")
