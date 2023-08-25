3ca3f22d96d71149c5c2d9a699e3aecc98a3b043
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-25 03:09:08 +0000

    Add package cast-vue/0.0.8/cast_vue-0.0.8-py3-none-any.whl

---
+++ packages/cast-vue/cast_vue-0.0.8-py3-none-any.whl/cast_vue/static/src/js/cast_vue/config.ts
@@ -0,0 +1,53 @@
+const {
+  blogUrl = "/",
+  csrfToken = "",
+  blogId = null,
+  paginationPageSize = "5",
+  wagtailApiPagesUrl = "/",
+  apiFacetCountsUrl = "/",
+  postCommentUrl = "/",
+  vueRouteName = "PostList",
+  postSlug = "",
+  pageType = "",
+} = document.getElementById("vue-configuration")?.dataset ?? {};
+
+let parsedBlogId = null;
+
+if (blogId) {
+  parsedBlogId = parseInt(blogId);
+  if (isNaN(parsedBlogId)) {
+    throw new Error(`Invalid blogId: ${blogId}`);
+  }
+} else {
+    throw new Error("Missing blogId");
+}
+
+const blogDetailUrl = new URL(`${wagtailApiPagesUrl}${parsedBlogId}/`);
+blogDetailUrl.searchParams.set("type", "cast.Blog");
+
+const postListUrl = new URL(wagtailApiPagesUrl);
+postListUrl.searchParams.set("child_of", parsedBlogId.toString());
+
+const parsedPaginationPageSize = parseInt(paginationPageSize);
+
+if (isNaN(parsedPaginationPageSize)) {
+    throw new Error(`Invalid paginationPageSize: ${paginationPageSize}`);
+}
+
+const parsedApiFacetCountsUrl = new URL(apiFacetCountsUrl);
+const parsedPostCommentUrl = new URL(postCommentUrl);
+
+export default {
+  blogUrl,
+  csrfToken,
+  blogId: parsedBlogId,
+  paginationPageSize: parsedPaginationPageSize,
+  wagtailApiPagesUrl,
+  apiFacetCountsUrl: parsedApiFacetCountsUrl,
+  postCommentUrl: parsedPostCommentUrl,
+  blogDetailUrl,
+  postListUrl,
+  vueRouteName,
+  postSlug,
+  pageType,
+};
