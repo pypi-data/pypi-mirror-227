87a775e5f0076550e8e181331b0177e7abfbc1d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-25 15:06:54 +0000

    Add package hactool0/0.11.0/hactool0-0.11.0.tar.gz

---
+++ packages/hactool0/hactool0-0.11.0.tar.gz/hactool0-0.11.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+ 
+ 
+setup(
+  name='hactool0',
+  version='0.11.0',
+  description='hactool',
+  long_description=open('README.md').read(),
+  url='',
+  author='GhostShadow',
+  license='GNU',
+  keywords='hactool',
+  packages=find_packages(),
+  install_requires=['colorama','keyboard','numpy','pick','playsound','pyautogui','pyperclip','tabulate']
+)
