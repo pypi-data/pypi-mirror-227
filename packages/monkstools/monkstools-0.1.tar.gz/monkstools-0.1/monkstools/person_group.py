d2a84e1c2a2c48642fd36a5aec9fff1bc5599422
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-25 14:07:57 +0000

    Add package monkstools/0.1/monkstools-0.1.tar.gz

---
+++ packages/monkstools/monkstools-0.1.tar.gz/monkstools-0.1/monkstools/person_group.py
@@ -0,0 +1,8 @@
+# monkstools/person_group.py
+class PersonGroup:
+    def __init__(self, tensor_data):
+        self.data = tensor_data
+
+    def analyze(self):
+        # 实现特定人群分析的方法
+        pass
