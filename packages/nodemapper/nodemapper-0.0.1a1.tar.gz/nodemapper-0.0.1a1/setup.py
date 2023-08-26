a33a71df56517d8cc4e571b2a654ab46454fa27c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-26 04:08:30 +0000

    Add package nodemapper/0.0.1a1/nodemapper-0.0.1a1.tar.gz

---
+++ packages/nodemapper/nodemapper-0.0.1a1.tar.gz/nodemapper-0.0.1a1/setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup, find_packages
+
+setup(name='nodemapper', version='0.0.1a1', packages=find_packages(), author="Sherjeel Shabih", author_email="sherjeelshabih@gmail.com")
