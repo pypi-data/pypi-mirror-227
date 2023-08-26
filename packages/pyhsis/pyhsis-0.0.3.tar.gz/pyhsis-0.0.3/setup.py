3bb046be225608d23744108a7519a5b704e1dbeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-26 04:08:28 +0000

    Add package pyhsis/0.0.3/pyhsis-0.0.3.tar.gz

---
+++ packages/pyhsis/pyhsis-0.0.3.tar.gz/pyhsis-0.0.3/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+#############################################
+# File Name: setup.py
+# Author: LiangjunFeng
+# Mail: zhumavip@163.com
+# Created Time:  2018-4-16 19:17:34
+#############################################
+
+from setuptools import setup, find_packages            #这个包没有的可以pip一下
+
+setup(
+    name = "pyhsis",      #这里是pip项目发布的名称
+    version = "0.0.3",  #版本号，数值大的会优先被pip
+    keywords = ("pip", "SICA","featureextraction"),
+    description = "An feature extraction algorithm",
+    long_description = "An feature extraction algorithm, improve the FastICA",
+    license = "MIT Licence",
+
+    url = "https://github.com/Zhangleyi/xxx",     #项目相关文件地址，一般是github
+    author = "Zhang Leyi",
+    author_email = "1903636706@qq.com",
+
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = "any",
+    install_requires = ["numpy"]          #这个项目需要的第三方库
+)
+
