57ee632d80720992d668a1f685344c954fb37154
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-26 04:10:17 +0000

    Add package mzt/1.2.0/mzt-1.2.0.tar.gz

---
+++ packages/mzt/mzt-1.2.0.tar.gz/mzt-1.2.0/moduleZoo/graphs/attention/__init__.py
@@ -0,0 +1,9 @@
+from .graph_linear_multi_head_attention import (
+    MultiHeadGraphAttentionLinear,
+    MultiHeadSelfGraphAttentionLinear,
+    SelfGraphAttentionLinear,
+)
+
+__all__ = ('MultiHeadGraphAttentionLinear',
+           'MultiHeadSelfGraphAttentionLinear',
+           'SelfGraphAttentionLinear')
