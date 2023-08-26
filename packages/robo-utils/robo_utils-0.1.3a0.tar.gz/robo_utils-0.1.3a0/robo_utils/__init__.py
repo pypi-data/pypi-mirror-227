e509a49b6e5845e50f6b1b1c0fcc19c93240a9db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-26 05:08:14 +0000

    Add package robo-utils/0.1.3a0/robo_utils-0.1.3a0.tar.gz

---
+++ packages/robo-utils/robo_utils-0.1.3a0.tar.gz/robo_utils-0.1.3a0/robo_utils/__init__.py
@@ -0,0 +1,19 @@
+
+"""
+robo-utils  - useful functions for mobile robotics
+"""
+
+# flit uses this as one source version #
+# note: pep whatever doesnt like > 'x.y.<number>c'
+# for example: 0.01.08d doesnt fly building from pyproject.toml
+
+__version__ = '0.01.03a'
+
+# import acts to export 
+
+from .roboutils import imap
+from .roboutils import fmap
+
+
+
+
