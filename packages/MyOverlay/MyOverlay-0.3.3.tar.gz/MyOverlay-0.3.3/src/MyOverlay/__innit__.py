30299ee7d4589c3fbb26e28386bd9b3f5baab350
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-26 03:07:09 +0000

    Add package MyOverlay/0.3.3/MyOverlay-0.3.3.tar.gz

---
+++ packages/MyOverlay/MyOverlay-0.3.3.tar.gz/MyOverlay-0.3.3/src/MyOverlay/__innit__.py
@@ -0,0 +1,21 @@
+from MyOverlay.funcs import myfunctions
+
+
+def InnitOverlay():
+    return myfunctions.StartOverlay()
+
+
+def settext(text: str, PosX: int, PosY: int, font: str = 'Segoe UI Semibold', size: int = 30, opx: int = 2):
+    myfunctions.settext(text, PosX, PosY, font, size, opx)
+
+
+def KillOverlay():
+    myfunctions.KillOverlay()
+
+
+def StatusOverlay():
+    return myfunctions.StatusOverlay()
+
+
+def GetJobDirectory(JobName: str):
+    return myfunctions.GetJobDirectory(JobName)
