342d686742794bc8f20e7ce95bb515b66c9e0193
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-26 04:08:53 +0000

    Add package aa-intel-tool/0.0.1a13/aa_intel_tool-0.0.1a13-py3-none-any.whl

---
+++ packages/aa-intel-tool/aa_intel_tool-0.0.1a13-py3-none-any.whl/aa_intel_tool/static/aa_intel_tool/javascript/aa-intel-tool-scan-result-common.js
@@ -0,0 +1,139 @@
+/* global aaIntelToolJsL10n, ClipboardJS */
+
+'use strict';
+
+/**
+ * Fetch data from an ajax URL
+ *
+ * @param url
+ * @returns {Promise<any>}
+ */
+const fetchAjaxData = async (url) => { // eslint-disable-line no-unused-vars
+    return await fetch(url)
+        .then(response => {
+            if (response.ok) {
+                return Promise.resolve(response);
+            } else {
+                return Promise.reject(new Error('Failed to load'));
+            }
+        })
+        .then(response => response.json())
+        .then(tableData => {
+            return tableData;
+        })
+        .catch(function (error) {
+            console.log(`Error: ${error.message}`);
+        });
+};
+
+
+jQuery(document).ready(($) => {
+    const elementCopyToClipboard = $('button#btn-copy-permalink-to-clipboard');
+
+
+    /**
+     * Remove copy buttons if the browser doesn't support it
+     */
+    if (!ClipboardJS.isSupported()) {
+        elementCopyToClipboard.remove();
+    }
+
+
+    /**
+     * Closing the message
+     *
+     * @param {string} element
+     * @param {int} closeAfter Close Message after given time in seconds (Default: 10)
+     */
+    const closeMessageElement = (element, closeAfter = 10) => {
+        $(element).fadeTo(closeAfter * 1000, 500).slideUp(500, (elm) => {
+            $(elm).slideUp(500, (el) => {
+                $(el).remove();
+            });
+        });
+    };
+
+
+    /**
+     * Show a message when copy action was successful
+     *
+     * @param {string} message
+     * @param {string} element
+     */
+    const showSuccess = (message, element) => {
+        $(element).html(
+            '<div class="alert alert-success alert-dismissable alert-message-success">' +
+            '<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + message +
+            '</div>'
+        );
+
+        closeMessageElement('.alert-message-success');
+    };
+
+
+    /**
+     * Show a message when copy action was not successful
+     *
+     * @param {string} message
+     * @param {string} element
+     */
+    const showError = (message, element) => {
+        $(element).html(
+            '<div class="alert alert-danger alert-dismissable alert-message-error">' +
+            '<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + message +
+            '</div>'
+        );
+
+        closeMessageElement('.alert-message-error', 9999);
+    };
+
+
+    /**
+     * Copy the scan link to clipboard
+     *
+     * @param elementId {string}
+     */
+    const copyScanLink = (elementId) => {
+        /**
+         * Copy text to clipboard
+         *
+         * @type Clipboard
+         */
+        const clipboardScanLink = new ClipboardJS(elementId);
+
+        /**
+         * Copy success
+         *
+         * @param {type} e
+         */
+        clipboardScanLink.on('success', (e) => {
+            showSuccess(
+                aaIntelToolJsL10n.copyToClipboard.permalink.text.success,
+                '.aa-intel-copy-result'
+            );
+
+            e.clearSelection();
+            clipboardScanLink.destroy();
+        });
+
+        /**
+         * Copy error
+         */
+        clipboardScanLink.on('error', () => {
+            showError(
+                aaIntelToolJsL10n.copyToClipboard.permalink.text.error,
+                '.aa-intel-copy-result'
+            );
+
+            clipboardScanLink.destroy();
+        });
+    };
+
+
+    /**
+     * Copy ping text
+     */
+    elementCopyToClipboard.click(() => {
+        copyScanLink('#' + elementCopyToClipboard.attr('id'));
+    });
+});
