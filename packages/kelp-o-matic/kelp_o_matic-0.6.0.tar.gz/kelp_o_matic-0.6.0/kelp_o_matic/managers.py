5cc47c28fd52e808cb50f59f24a526309808536a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-26 05:08:24 +0000

    Add package kelp-o-matic/0.6.0/kelp_o_matic-0.6.0.tar.gz

---
+++ packages/kelp-o-matic/kelp_o_matic-0.6.0.tar.gz/kelp_o_matic-0.6.0/kelp_o_matic/managers.py
@@ -0,0 +1,225 @@
+import warnings
+from pathlib import Path
+from typing import Union
+
+import numpy as np
+import rasterio
+from rich import print
+from rich.progress import Progress, SpinnerColumn, TimeElapsedColumn
+from torch.utils.data import DataLoader
+from torchvision import transforms
+
+from kelp_o_matic.geotiff_io import GeotiffReader, GeotiffWriter
+from kelp_o_matic.models import _Model
+from kelp_o_matic.utils import all_same
+
+
+class GeotiffSegmentationManager:
+    """Class for configuring data io and efficient segmentation of Geotiff imagery."""
+
+    def __init__(
+        self,
+        model: "_Model",
+        input_path: Union[str, "Path"],
+        output_path: Union[str, "Path"],
+        crop_size: int = 512,
+        padding: int = 256,
+        batch_size: int = 1,
+    ):
+        """Create the segmentation object.
+
+        Args:
+            model: A callable module that accepts a batch of torch.Tensor data and
+                returns classifications.
+            input_path: The path to the input geotiff image.
+            output_path: The destination file path for the output segmentation data.
+            crop_size: The size of image crop to classify iteratively until the entire
+                image is classified.
+            padding: The number of context pixels to add to each side of an image crop
+                to improve outputs.
+            batch_size: The number of crops to classify at a time using the model.
+        """
+        self.model = model
+
+        tran = transforms.Compose(
+            [
+                transforms.ToTensor(),
+                transforms.Lambda(lambda img: img[:3, :, :]),
+                transforms.Normalize(
+                    mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+                ),
+            ]
+        )
+        self.reader = GeotiffReader(
+            Path(input_path).expanduser().resolve(),
+            transform=tran,
+            crop_size=crop_size,
+            padding=padding,
+            filter_=self._should_keep,
+        )
+        self._dataloader = DataLoader(
+            self.reader,
+            shuffle=False,
+            batch_size=batch_size,
+            pin_memory=True,
+            num_workers=0,
+        )
+
+        self.writer = GeotiffWriter.from_reader(
+            Path(output_path).expanduser().resolve(),
+            self.reader,
+            count=1,
+            dtype="uint8",
+            nodata=0,
+        )
+
+    @staticmethod
+    def _should_keep(img: "np.ndarray") -> bool:
+        """Determines if an image crop should be classified or discarded.
+
+        Args:
+            img: The image crop, with padding removed.
+
+        Returns:
+             Flag to indicate if crop should be discarded.
+        """
+        _img = np.clip(img, 0, 255)
+        return bool(np.any(_img % 255))
+
+    def __call__(self):
+        """Run the segmentation task."""
+        self.on_start()
+        self._run_checks()
+
+        with rasterio.Env():
+            for batch_idx, batch in enumerate(self._dataloader):
+                self.on_batch_start(batch_idx)
+
+                crops, indices = batch
+                labels = self.model(crops).detach().cpu().numpy()
+
+                # Write outputs
+                for label, idx in zip(labels, indices):
+                    self.writer.write_index(label, int(idx))
+                    self.on_chip_write_end(int(idx))
+
+                del crops, indices, labels, batch
+
+                self.on_batch_end(batch_idx)
+        self.on_end()
+
+    def _no_data_check(self):
+        if self.reader.nodata is None:
+            warnings.warn(
+                "Define a nodata value on the input raster to speed up processing.",
+                UserWarning,
+            )
+
+    def _byte_type_check(self):
+        dtype = self.reader.profile["dtype"]
+        if dtype != "uint8":
+            raise AssertionError(
+                f"Input image has incorrect data type {dtype}. "
+                f"Only uint8 (aka Byte) images are supported."
+            )
+
+    def _band_count_check(self):
+        if self.reader.count < 3:
+            raise AssertionError(
+                "Input image has less than 3 bands. "
+                "The image should have at least 3 bands, with the first three in RGB "
+                "order."
+            )
+
+    def _block_tiles_check(self):
+        if not all_same(self.reader.block_shapes):
+            warnings.warn("Input image bands have different sized blocks.", UserWarning)
+
+        crop_shape = self.reader.crop_size + 2 * self.reader.padding
+        y_shape, x_shape = self.reader.block_shapes[0]
+        if y_shape == 1:
+            warnings.warn(
+                "The input image is not a tiled tif. Processing will be "
+                "significantly faster for tiled images.",
+                UserWarning,
+            )
+        elif crop_shape % y_shape != 0 or crop_shape % x_shape != 0:
+            warnings.warn(
+                "Suboptimal crop_size and padding were specified. Performance "
+                "will be degraded. The detected block shape for this band is "
+                "({y_shape}, {x_shape}). Faster performance may be achieved by setting "
+                "the crop_size and the padding such that (crop_size + 2*padding) is a "
+                "multiple of {y_shape}.",
+                UserWarning,
+            )
+
+    def _run_checks(self):
+        """Run image checks."""
+        self._no_data_check()
+        self._byte_type_check()
+        self._band_count_check()
+        self._block_tiles_check()
+
+    def on_start(self):
+        """Hook that runs before image processing."""
+        pass
+
+    def on_end(self):
+        """Hook that runs after image processing."""
+        pass
+
+    def on_batch_start(self, batch_idx: int):
+        """
+        Hook that runs for each batch of data, immediately before classification.
+
+        Args:
+            batch_idx: The batch index being processed.
+        """
+        pass
+
+    def on_batch_end(self, batch_idx: int):
+        """
+        Hook that runs for each batch of data, immediately after classification.
+
+        Args:
+            batch_idx: The batch index being processed.
+        """
+        pass
+
+    def on_chip_write_end(self, index: int):
+        """
+        Hook that runs for each crop of data, immediately after classification.
+
+        Args:
+            index: The index of the image crop that was processed.
+        """
+        pass
+
+
+class RichSegmentationManager(GeotiffSegmentationManager):
+    """Run the segmentation with Rich progress bars and logging."""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.progress = Progress(
+            SpinnerColumn("earth"), *Progress.get_default_columns(), TimeElapsedColumn()
+        )
+        self.processing_task = self.progress.add_task(
+            description="Processing", total=len(self.reader)
+        )
+
+    def __call__(self):
+        with self.progress:
+            super().__call__()
+
+    def on_start(self):
+        device_emoji = ":rocket:" if self.model.device.type == "cuda" else ":snail:"
+        print(f"Running with [magenta]{self.model.device} {device_emoji}")
+
+    def on_chip_write_end(self, index: int):
+        self.progress.update(self.processing_task, completed=index)
+
+    def on_end(self):
+        self.progress.update(self.processing_task, completed=len(self.reader))
+        print("[bold italic green]:tada: Segmentation complete! :tada:[/]")
---
+++ packages/kelp-o-matic/kelp_o_matic-0.6.0.tar.gz/kelp_o_matic-0.6.0/kelp_o_matic/models.py
@@ -0,0 +1,64 @@
+import gc
+import importlib.resources as importlib_resources
+from abc import ABC, abstractmethod
+
+import torch
+
+from kelp_o_matic.data import (
+    lraspp_kelp_presence_torchscript_path,
+    lraspp_kelp_species_torchscript_path,
+    lraspp_mussel_presence_torchscript_path,
+)
+
+
+class _Model(ABC):
+    def __init__(self, use_gpu: bool = True):
+        is_cuda = torch.cuda.is_available() and use_gpu
+        self.device = torch.device("cuda") if is_cuda else torch.device("cpu")
+        self.model = self.load_model()
+
+    @property
+    @abstractmethod
+    def torchscript_path(self):
+        raise NotImplementedError
+
+    def load_model(self) -> "torch.nn.Module":
+        with importlib_resources.as_file(self.torchscript_path) as ts:
+            model = torch.jit.load(ts, map_location=self.device)
+        model.eval()
+        return model
+
+    def reload(self):
+        del self.model
+        gc.collect()
+        self.model = self.load_model()
+
+    def __call__(self, batch: "torch.Tensor") -> "torch.Tensor":
+        with torch.no_grad():
+            return torch.argmax(self.model.forward(batch.to(self.device)), dim=1)
+
+
+class KelpPresenceSegmentationModel(_Model):
+    torchscript_path = lraspp_kelp_presence_torchscript_path
+
+
+class KelpSpeciesSegmentationModel(_Model):
+    torchscript_path = lraspp_kelp_species_torchscript_path
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.presence_model = KelpPresenceSegmentationModel(*args, **kwargs)
+
+    def __call__(self, batch: "torch.Tensor") -> "torch.Tensor":
+        with torch.no_grad():
+            batch = batch.to(self.device)
+            presence = self.presence_model(batch)  # 0: bg, 1: kelp
+
+            logits = self.model.forward(batch)
+            species = torch.add(torch.argmax(logits, dim=1), 2)  # 2: macro, 3: nereo
+
+            return torch.mul(presence, species)  # 0: bg, 2: macro, 3: nereo
+
+
+class MusselPresenceSegmentationModel(_Model):
+    torchscript_path = lraspp_mussel_presence_torchscript_path
