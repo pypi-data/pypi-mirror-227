54258db3848f5cb1eeb1a92dd1e126aa4d4708bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-26 03:10:49 +0000

    Add package airoboros/2.1.6/airoboros-2.1.6-py3-none-any.whl

---
+++ packages/airoboros/airoboros-2.1.6-py3-none-any.whl/airoboros/lmoe/api.py
@@ -0,0 +1,7 @@
+from airoboros.lmoe.flash import replace_llama_attn_with_flash_attn
+
+replace_llama_attn_with_flash_attn()
+from airoboros.lmoe.hf_api import main
+
+if __name__ == "__main__":
+    main()
