e3efe428904565e703557c899d1da8c9e7c6055c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-26 05:09:46 +0000

    Add package lvdb/0.0.2/lvdb-0.0.2.tar.gz

---
+++ packages/lvdb/lvdb-0.0.2.tar.gz/lvdb-0.0.2/setup.py
@@ -0,0 +1,40 @@
+"""A setuptools based setup module.
+
+See:
+https://packaging.python.org/guides/distributing-packages-using-setuptools/
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+
+# Get the long description from the README file
+long_description = (here / "README.md").read_text(encoding="utf-8")
+
+# Arguments marked as "Required" below must be included for upload to PyPI.
+# Fields marked as "Optional" may be commented out.
+
+setup(
+    name="lvdb",
+    version="1.0.1",
+    description="A package to locally shard vector data to conserve memory in memory-limited environments",  # Optional
+    author="TripIe",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3 :: Only",
+    ],
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    python_requires=">=3.7, <4",
+    install_requires=["numpy", "time", "matplotlib", "psutil"],
+)
