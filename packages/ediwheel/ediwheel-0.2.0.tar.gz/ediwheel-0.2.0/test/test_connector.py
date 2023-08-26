cd62074f8f400ab2458587dc5915c07f77b94423
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-26 05:08:14 +0000

    Add package ediwheel/0.2.0/ediwheel-0.2.0.tar.gz

---
+++ packages/ediwheel/ediwheel-0.2.0.tar.gz/ediwheel-0.2.0/test/test_connector.py
@@ -0,0 +1,52 @@
+import unittest
+
+from ediwheel.connector import *
+
+
+michelin = EdiConnectorConfig( # nullpointer
+            host="https://bibserve.com/MichelinSCEBE/AdhocA2XML25Servlet",
+            username="MWH9337",
+            password="AdHoc?ForiGrC931",
+            id="6069966",
+            timeout_s=10,
+        )
+conti = EdiConnectorConfig( # Invalid credentials
+    host="https://direct.conti.de/cgi-bin/easy-xml.cgi",
+    username="07319622",
+    password="Gr81st45Bg",
+    id="07319622",
+    timeout_s=10,
+)
+goodyear = EdiConnectorConfig( # NO BACKEND
+    host="",
+    username="50705723",
+    password="GREFRO10032021",
+    id="50705723",
+    timeout_s=10,
+)
+vredestein = EdiConnectorConfig( # WORKS
+    host="https://dealer.vredestein.com/adhocxml.dll",
+    username="50705723",
+    password="GREFRO10032021",
+    id="50705723",
+    timeout_s=10,
+)
+bridgestone = EdiConnectorConfig( # WORKS
+    host="https://adhoc.bridgestone.eu/prod/adhoc",
+    username="Bxp%A2",
+    password="A2*Bxp",
+    id="426009",
+    timeout_s=10,
+)
+
+class TestConnector(unittest.TestCase):
+
+    def test_vred(self):
+
+
+        connector = EdiConnector(vredestein)
+        connector.enquiry("8714692506864", "AP20565016WULAAB0")
+
+    def test_bridgestone(self):
+        connector = EdiConnector(bridgestone)
+        connector.enquiry("3286340729611", "7296")
