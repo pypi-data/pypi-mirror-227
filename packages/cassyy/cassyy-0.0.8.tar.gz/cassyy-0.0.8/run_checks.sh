5db0e6f0957c5a4f78bf848994d8780a53da5a61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-26 05:09:33 +0000

    Add package cassyy/0.0.8/cassyy-0.0.8.tar.gz

---
+++ packages/cassyy/cassyy-0.0.8.tar.gz/cassyy-0.0.8/run_checks.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -xe
+
+echo "CI is set to [${CI}]"
+if [[ $CI != "true" ]]; then
+    pre-commit run --all-files
+fi
+
+mypy --version
+mypy
+
+python -V
+python -m unittest
