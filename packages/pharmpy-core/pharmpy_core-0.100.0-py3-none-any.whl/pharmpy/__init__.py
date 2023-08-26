023278e638c2e9b98e15eec824042835f9239b35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-26 03:11:14 +0000

    Add package pharmpy-core/0.100.0/pharmpy_core-0.100.0-py3-none-any.whl

---
+++ packages/pharmpy-core/pharmpy_core-0.100.0-py3-none-any.whl/pharmpy/__init__.py
@@ -0,0 +1,16 @@
+"""
+=======
+Pharmpy
+=======
+
+Pharmpy is a python package for pharmacometrics modeling.
+
+Definitions
+===========
+"""
+
+__version__ = '0.100.0'
+
+import logging
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
