81041aeba7eb174f183753282127c0f38aab5a2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-26 05:09:34 +0000

    Add package codegpt-sdk/1.0.0/codegpt-sdk-1.0.0.tar.gz

---
+++ packages/codegpt-sdk/codegpt-sdk-1.0.0.tar.gz/codegpt-sdk-1.0.0/tests/test_response.py
@@ -0,0 +1,18 @@
+from codegpt.agent import Agent
+import asyncio
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+
+codegpt_api_key= os.getenv("CODEGPT_API_KEY")
+codegpt_agent_id= os.getenv("CODEGPT_AGENT_ID")
+
+agent_instance = Agent(api_key=codegpt_api_key, agent_id=codegpt_agent_id)
+
+prompt = "tell me short story about blue turtle"
+
+response = asyncio.run(agent_instance.completion(prompt, stream=False))
+
+print(response)
+
