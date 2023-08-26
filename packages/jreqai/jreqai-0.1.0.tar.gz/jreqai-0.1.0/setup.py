81b58a5ac4f2fb09ff263d63f88c5fdeb383c0e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-26 14:07:04 +0000

    Add package jreqai/0.1.0/jreqai-0.1.0.tar.gz

---
+++ packages/jreqai/jreqai-0.1.0.tar.gz/jreqai-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='jreqai',
+    version='0.1.0',
+    description='',
+    author='seekwe',
+    author_email='seekwe@email.com',
+    packages=[],
+    install_requires=['requests'],
+)
