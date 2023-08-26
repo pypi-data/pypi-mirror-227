feb6e11016bb6b68bb0ede012aa6e0fe2152b93b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-26 14:07:34 +0000

    Add package ColorPrintConsole/1.0.0/ColorPrintConsole-1.0.0.tar.gz

---
+++ packages/ColorPrintConsole/ColorPrintConsole-1.0.0.tar.gz/ColorPrintConsole-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='ColorPrintConsole',
+    version='1.0.0',
+    description='A utility for color printing in Python',
+    author='苏寅',
+    author_email='suyin_long@163.com',
+    packages=['ColorPrint'],
+)
