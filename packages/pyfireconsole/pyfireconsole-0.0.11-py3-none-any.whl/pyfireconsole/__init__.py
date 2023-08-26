9e6f62662b6dc79bdcf94bd3d46329ae9a23ac07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-26 14:06:13 +0000

    Add package pyfireconsole/0.0.11/pyfireconsole-0.0.11-py3-none-any.whl

---
+++ packages/pyfireconsole/pyfireconsole-0.0.11-py3-none-any.whl/pyfireconsole/__init__.py
@@ -0,0 +1,7 @@
+from pyfireconsole.console.pyfireconsole import PyFireConsole  # noqa: F401
+from pyfireconsole.db.connection import FirestoreConnection  # noqa: F401
+from pyfireconsole.models.pyfire_model import DocumentRef, PyfireCollection, PyfireDoc  # noqa: F401
+
+
+def hello_world():
+    print("Hello, world!")
