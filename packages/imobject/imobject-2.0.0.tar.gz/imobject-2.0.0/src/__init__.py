bac06fa6ae843565ba31cb1c8df0a0babb78721d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-26 03:09:34 +0000

    Add package imobject/2.0.0/imobject-2.0.0.tar.gz

---
+++ packages/imobject/imobject-2.0.0.tar.gz/imobject-2.0.0/src/__init__.py
@@ -0,0 +1,7 @@
+"""App module"""
+__version__ = "1.0.1"
+
+from .imobject.obj_dict import ObjDict
+from .imobject.improved_list import ImprovedList
+from .imobject.orm_collection import OrmCollection
+from .imobject.ioc import ObjectFactory
