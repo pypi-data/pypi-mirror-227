2f1334a291665feda67f5c4131810298170a927e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-26 05:09:43 +0000

    Add package dataproc-jupyter-plugin/0.1.9/dataproc_jupyter_plugin-0.1.9.tar.gz

---
+++ packages/dataproc-jupyter-plugin/dataproc_jupyter_plugin-0.1.9.tar.gz/dataproc_jupyter_plugin-0.1.9/src/index.ts
@@ -0,0 +1,321 @@
+/**
+ * @license
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin,
+  JupyterLab,
+  ILabShell
+} from '@jupyterlab/application';
+import { MainAreaWidget } from '@jupyterlab/apputils';
+import { ILauncher } from '@jupyterlab/launcher';
+import { LabIcon } from '@jupyterlab/ui-components';
+import { IMainMenu } from '@jupyterlab/mainmenu';
+import { Cluster } from './cluster/cluster';
+import { Batches } from './batches/batches';
+import clusterIcon from '../style/icons/cluster_icon.svg';
+import addRuntimeIcon from '../style/icons/add_runtime_template.svg';
+import serverlessIcon from '../style/icons/serverless_icon.svg';
+import { Panel, Title, Widget } from '@lumino/widgets';
+import { AuthLogin } from './login/authLogin';
+import { Kernel, KernelSpecAPI } from '@jupyterlab/services';
+import { iconDisplay } from './utils/utils';
+import { dpmsWidget } from './dpms/dpmsWidget';
+import dpmsIcon from '../style/icons/dpms_icon.svg';
+import { INotebookTracker, NotebookPanel } from '@jupyterlab/notebook';
+const iconDpms = new LabIcon({
+  name: 'launcher:dpms-icon',
+  svgstr: dpmsIcon
+});
+import { TITLE_LAUNCHER_CATEGORY } from './utils/const';
+import { RuntimeTemplate } from './runtime/runtimeTemplate';
+
+const extension: JupyterFrontEndPlugin<void> = {
+  id: 'dataproc_jupyter_plugin:plugin',
+  autoStart: true,
+  optional: [ILauncher, IMainMenu, ILabShell, INotebookTracker],
+  activate: async (
+    app: JupyterFrontEnd,
+    launcher: ILauncher,
+    mainMenu: IMainMenu,
+    labShell: ILabShell,
+    notebookTracker: INotebookTracker
+  ) => {
+    const { commands } = app;
+    const iconAddRuntime = new LabIcon({
+      name: 'launcher:add-runtime-icon',
+      svgstr: addRuntimeIcon
+    });
+    const iconCluster = new LabIcon({
+      name: 'launcher:clusters-icon',
+      svgstr: clusterIcon
+    });
+    const iconServerless = new LabIcon({
+      name: 'launcher:serverless-icon',
+      svgstr: serverlessIcon
+    });
+    window.addEventListener('beforeunload', () => {
+      localStorage.removeItem('notebookValue');
+    });
+    const panel = new Panel();
+    panel.id = 'dpms-tab';
+    panel.title.icon = iconDpms;
+    const loadDpmsWidget = (value: string) => {
+      const existingWidgets = panel.widgets;
+      existingWidgets.forEach(widget => {
+        if (widget instanceof dpmsWidget) {
+          widget.dispose();
+        }
+      });
+      const newWidget = new dpmsWidget(app as JupyterLab);
+      panel.addWidget(newWidget);
+    };
+
+    panel.addWidget(new dpmsWidget(app as JupyterLab));
+    const localStorageValue = localStorage.getItem('notebookValue');
+    if (localStorageValue) {
+      loadDpmsWidget(localStorageValue);
+    }
+    app.shell.add(panel, 'left', { rank: 1000 });
+    const onTitleChanged = async (title: Title<Widget>) => {
+      const widget = title.owner as NotebookPanel;
+      let localStorageValue = localStorage.getItem('notebookValue');
+      if (widget && widget instanceof NotebookPanel) {
+        const kernel = widget.sessionContext.session?.kernel;
+        if (kernel) {
+          const kernelName = kernel.name;
+          const kernelSpec = kernels[kernelName];
+          if (
+            kernelSpec?.resources.endpointParentResource.includes('/clusters/')
+          ) {
+            const parts =
+              kernelSpec?.resources.endpointParentResource.split('/');
+            const clusterValue = parts[parts.length - 1] + '/clusters';
+            if (localStorageValue === null) {
+              localStorage.setItem('notebookValue', clusterValue);
+              localStorageValue = localStorage.getItem('notebookValue');
+              loadDpmsWidget(localStorageValue || '');
+            } else if (localStorageValue !== clusterValue) {
+              localStorage.setItem('notebookValue', clusterValue);
+              localStorageValue = localStorage.getItem('notebookValue');
+              loadDpmsWidget(localStorageValue || '');
+            }
+          } else if (
+            kernelSpec?.resources.endpointParentResource.includes('/sessions')
+          ) {
+            const parts = kernelSpec?.name.split('-');
+            const sessionValue = parts.slice(1).join('-') + '/sessions';
+            if (localStorageValue === null) {
+              localStorage.setItem('notebookValue', sessionValue);
+              localStorageValue = localStorage.getItem('notebookValue');
+              loadDpmsWidget(localStorageValue || '');
+            } else if (localStorageValue !== sessionValue) {
+              localStorage.setItem('notebookValue', sessionValue);
+              localStorageValue = localStorage.getItem('notebookValue');
+              loadDpmsWidget(localStorageValue || '');
+            }
+          }
+        } else {
+          localStorage.removeItem('notebookValue');
+          loadDpmsWidget('');
+        }
+        document.title = title.label;
+      } else {
+        document.title = title.label;
+      }
+      console.log(Kernel);
+    };
+    labShell.currentChanged.connect(async (_, change) => {
+      const { oldValue, newValue } = change;
+      // Clean up after the old value if it exists,
+      // listen for changes to the title of the activity
+      if (oldValue) {
+        // Check if the old value is an instance of NotebookPanel
+        if (oldValue instanceof NotebookPanel) {
+          oldValue.title.changed.disconnect(onTitleChanged);
+        }
+      }
+      if (newValue) {
+        // Check if the new value is an instance of NotebookPanel
+        if (newValue instanceof NotebookPanel) {
+          newValue.title.changed.connect(onTitleChanged);
+        }
+      }
+    });
+
+    const kernelSpecs = await KernelSpecAPI.getSpecs();
+    const kernels = kernelSpecs.kernelspecs;
+
+    const createRuntimeTemplateComponentCommand =
+      'create-runtime-template-component';
+    commands.addCommand(createRuntimeTemplateComponentCommand, {
+      caption: 'Create a new runtime template',
+      label: 'New Runtime Template',
+      // @ts-ignore jupyter lab icon command issue
+      icon: args => (args['isPalette'] ? null : iconAddRuntime),
+      execute: () => {
+        const content = new RuntimeTemplate(app as JupyterLab);
+        const widget = new MainAreaWidget<RuntimeTemplate>({ content });
+        widget.title.label = 'Runtime template';
+        widget.title.icon = iconServerless;
+        app.shell.add(widget, 'main');
+      }
+    });
+
+    const createClusterComponentCommand = 'create-cluster-component';
+    commands.addCommand(createClusterComponentCommand, {
+      caption: 'Create a new Cluster Component',
+      label: 'Clusters',
+      // @ts-ignore jupyter lab icon command issue
+      icon: args => (args['isPalette'] ? null : iconCluster),
+      execute: () => {
+        const content = new Cluster();
+        const widget = new MainAreaWidget<Cluster>({ content });
+        widget.title.label = 'Clusters';
+        widget.title.icon = iconCluster;
+        app.shell.add(widget, 'main');
+      }
+    });
+
+    const createBatchesComponentCommand = 'create-batches-component';
+    commands.addCommand(createBatchesComponentCommand, {
+      caption: 'Create a new Serverless Component',
+      label: 'Serverless',
+      // @ts-ignore jupyter lab icon command issue
+      icon: args => (args['isPalette'] ? null : iconServerless),
+      execute: () => {
+        const content = new Batches();
+        const widget = new MainAreaWidget<Batches>({ content });
+        widget.title.label = 'Serverless';
+        widget.title.icon = iconServerless;
+        app.shell.add(widget, 'main');
+      }
+    });
+
+    const createAuthLoginComponentCommand = 'cloud-dataproc-settings:configure';
+    commands.addCommand(createAuthLoginComponentCommand, {
+      label: 'Cloud Dataproc Settings',
+      execute: () => {
+        const content = new AuthLogin();
+        const widget = new MainAreaWidget<AuthLogin>({ content });
+        widget.title.label = 'Config Setup';
+        widget.title.icon = iconCluster;
+        app.shell.add(widget, 'main');
+      }
+    });
+
+    let serverlessIndex = -1;
+
+    if (launcher) {
+      Object.values(kernels).forEach((kernelsData, index) => {
+        if (
+          kernelsData?.resources.endpointParentResource &&
+          kernelsData?.resources.endpointParentResource.includes('/sessions')
+        ) {
+          const commandNotebook = `notebook:create-${kernelsData?.name}`;
+          commands.addCommand(commandNotebook, {
+            caption: kernelsData?.display_name,
+            label: kernelsData?.display_name,
+            icon: iconDisplay(kernelsData),
+            execute: async () => {
+              const model = await app.commands.execute(
+                'docmanager:new-untitled',
+                {
+                  type: 'notebook',
+                  path: '',
+                  kernel: { name: kernelsData?.name }
+                }
+              );
+              await app.commands.execute('docmanager:open', {
+                kernel: { name: kernelsData?.name },
+                path: model.path,
+                factory: 'notebook'
+              });
+            }
+          });
+
+          serverlessIndex = index;
+
+          launcher.add({
+            command: commandNotebook,
+            category: 'Dataproc Serverless Notebooks',
+            //@ts-ignore jupyter lab Launcher type issue
+            metadata: kernelsData?.metadata,
+            rank: index + 1,
+            //@ts-ignore jupyter lab Launcher type issue
+            args: kernelsData?.argv
+          });
+        }
+      });
+      Object.values(kernels).forEach((kernelsData, index) => {
+        if (
+          kernelsData?.resources.endpointParentResource &&
+          !kernelsData?.resources.endpointParentResource.includes('/sessions')
+        ) {
+          const commandNotebook = `notebook:create-${kernelsData?.name}`;
+          commands.addCommand(commandNotebook, {
+            caption: kernelsData?.display_name,
+            label: kernelsData?.display_name,
+            icon: iconDisplay(kernelsData),
+            execute: async () => {
+              const model = await app.commands.execute(
+                'docmanager:new-untitled',
+                {
+                  type: 'notebook',
+                  path: '',
+                  kernel: { name: kernelsData?.name }
+                }
+              );
+              await app.commands.execute('docmanager:open', {
+                kernel: { name: kernelsData?.name },
+                path: model.path,
+                factory: 'notebook'
+              });
+            }
+          });
+
+          launcher.add({
+            command: commandNotebook,
+            category: 'Dataproc Cluster Notebooks',
+            //@ts-ignore jupyter lab Launcher type issue
+            metadata: kernelsData?.metadata,
+            rank: index + 1,
+            //@ts-ignore jupyter lab Launcher type issue
+            args: kernelsData?.argv
+          });
+        }
+      });
+      launcher.add({
+        command: createRuntimeTemplateComponentCommand,
+        category: 'Dataproc Serverless Notebooks',
+        rank: serverlessIndex + 2
+      });
+      launcher.add({
+        command: createClusterComponentCommand,
+        category: TITLE_LAUNCHER_CATEGORY,
+        rank: 1
+      });
+      launcher.add({
+        command: createBatchesComponentCommand,
+        category: TITLE_LAUNCHER_CATEGORY,
+        rank: 2
+      });
+    }
+  }
+};
+
+export default extension;
---
+++ packages/dataproc-jupyter-plugin/dataproc_jupyter_plugin-0.1.9.tar.gz/dataproc_jupyter_plugin-0.1.9/src/login/configSelection.tsx
@@ -0,0 +1,413 @@
+/**
+ * @license
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React, { useState, useEffect } from 'react';
+import { LabIcon } from '@jupyterlab/ui-components';
+import settingsIcon from '../../style/icons/settings_icon.svg';
+import {
+  API_HEADER_BEARER,
+  API_HEADER_CONTENT_TYPE,
+  REGION_URL,
+  USER_INFO_URL
+} from '../utils/const';
+import { authApi } from '../utils/utils';
+import { Select } from 'semantic-ui-react';
+import 'semantic-ui-css/semantic.min.css';
+import { requestAPI } from '../handler/handler';
+import ClipLoader from 'react-spinners/ClipLoader';
+import { ToastContainer, ToastOptions, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import THIRD_PARTY_LICENSES from '../../third-party-licenses.txt';
+import ListRuntimeTemplates from '../runtime/listRuntimeTemplates';
+import expandLessIcon from '../../style/icons/expand_less.svg';
+import expandMoreIcon from '../../style/icons/expand_more.svg';
+import CreateRuntime from '../runtime/createRunTime';
+import { SessionTemplate } from '../utils/listRuntimeTemplateInterface';
+import { ProjectIDDropdown } from './projectIdDropdown';
+
+const iconExpandLess = new LabIcon({
+  name: 'launcher:expand-less-icon',
+  svgstr: expandLessIcon
+});
+const iconExpandMore = new LabIcon({
+  name: 'launcher:expand-more-icon',
+  svgstr: expandMoreIcon
+});
+
+function ConfigSelection({ loginState, configError, setConfigError }: any) {
+  const Iconsettings = new LabIcon({
+    name: 'launcher:settings_icon',
+    svgstr: settingsIcon
+  });
+
+  const [projectId, setProjectId] = useState('');
+  const [region, setRegion] = useState('');
+  const [regionList, setRegionList] = useState([{}]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isLoadingUser, setIsLoadingUser] = useState(true);
+  const [isLoadingRegion, setIsLoadingRegion] = useState(true);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [regionEmpty, SetRegionEmpty] = useState(true);
+  const [isSaveDisabled, setIsSaveDisabled] = useState(true);
+  const [userInfo, setUserInfo] = useState({
+    email: '',
+    picture: ''
+  });
+  const [expandRuntimeTemplate, setExpandRuntimeTemplate] = useState(true);
+  const [openCreateTemplate, setOpenCreateTemplate] = useState(false);
+
+  const [selectedRuntimeClone, setSelectedRuntimeClone] =
+    useState<SessionTemplate>();
+
+  const handleProjectIdChange = (projectId: string) => {
+    setRegionList([]);
+    SetRegionEmpty(true);
+    if (projectId.length !== 0) {
+      regionListAPI(projectId);
+      setProjectId(projectId);
+      setIsSaveDisabled(false);
+    }
+  };
+
+  const handleRegionChange = (event: any, data: any) => {
+    setRegion(data.value);
+    if (projectId) {
+      setIsSaveDisabled(false);
+    }
+    setIsDropdownOpen(false);
+  };
+
+  const handleSave = async () => {
+    setIsLoading(true);
+    setIsSaveDisabled(true);
+    const dataToSend = { projectId: projectId, region: region };
+    try {
+      const data = await requestAPI<any>('configuration', {
+        body: JSON.stringify(dataToSend),
+        method: 'POST'
+      });
+      if (typeof data === 'object' && data !== null) {
+        const configStatus = (data as { config: string }).config;
+        setIsLoading(false);
+        if (configStatus && !toast.isActive('custom-toast')) {
+          const toastifyCustomStyle: ToastOptions<{}> = {
+            hideProgressBar: true,
+            autoClose: false,
+            theme: 'dark',
+            position: toast.POSITION.BOTTOM_CENTER,
+            toastId: 'custom-toast'
+          };
+          if (configStatus.includes('Failed')) {
+            toast.error(configStatus, toastifyCustomStyle);
+          } else {
+            toast.success(
+              `${configStatus} - You will need to restart Jupyter in order for the new project and region to fully take effect.`,
+              toastifyCustomStyle
+            );
+          }
+        }
+      }
+    } catch (reason) {
+      console.error(`Error on POST {dataToSend}.\n${reason}`);
+    }
+  };
+
+  const displayUserInfo = async () => {
+    const credentials = await authApi();
+    if (credentials) {
+      fetch(USER_INFO_URL, {
+        method: 'GET',
+        headers: {
+          'Content-Type': API_HEADER_CONTENT_TYPE,
+          Authorization: API_HEADER_BEARER + credentials.access_token
+        }
+      })
+        .then((response: any) => {
+          response
+            .json()
+            .then((responseResult: any) => {
+              setUserInfo(responseResult);
+              setIsLoadingUser(false);
+            })
+            .catch((e: any) => console.log(e));
+        })
+        .catch((err: any) => {
+          setIsLoadingUser(false);
+          console.error('Error displaying user info', err);
+          toast.error('Failed to fetch user information');
+        });
+    }
+  };
+
+  const regionListAPI = async (projectId: any) => {
+    try {
+      const credentials = await authApi();
+      if (credentials) {
+        fetch(`${REGION_URL}${projectId}/regions`, {
+          method: 'GET',
+          headers: {
+            'Content-Type': API_HEADER_CONTENT_TYPE,
+            Authorization: API_HEADER_BEARER + credentials.access_token
+          }
+        })
+          .then((response: any) => {
+            if (response.ok) {
+              SetRegionEmpty(true);
+              return response.json();
+            } else {
+              setIsDropdownOpen(false);
+              setIsSaveDisabled(true);
+              SetRegionEmpty(false);
+              if (!toast.isActive('custom-toast-error')) {
+                toast.error(response.status + ' Permission Denied', {
+                  position: toast.POSITION.BOTTOM_CENTER,
+                  toastId: 'custom-toast-error'
+                });
+              }
+              throw new Error(`Request failed with status ${response.status}`);
+            }
+          })
+          .then((responseResult: any) => {
+            let transformedRegionList = [];
+            transformedRegionList = responseResult.items.map((data: any) => {
+              return {
+                value: data.name,
+                key: data.name,
+                text: data.name
+              };
+            });
+            setRegionList(transformedRegionList);
+            setIsDropdownOpen(false);
+            SetRegionEmpty(false);
+            setIsLoadingRegion(false);
+          })
+          .catch((error: any) => {
+            setIsLoadingRegion(false);
+            console.error('Error fetching region list:', error.message);
+          });
+      }
+    } catch (error) {
+      setIsLoadingRegion(false);
+      console.error('Error fetching region list:');
+      toast.error('Failed to fetch the regions');
+    }
+  };
+  const handleDropdownOpen = () => {
+    if (regionEmpty) {
+      setIsDropdownOpen(true);
+    }
+  };
+
+  /**
+   * onClick handler for when user's click on the "license" link in
+   * the user info box.
+   */
+  const handleLicenseClick = async () => {
+    const licenseWindow = window.open('about:blank');
+    if (licenseWindow) {
+      const preEle = licenseWindow.document.createElement('pre');
+      preEle.textContent = THIRD_PARTY_LICENSES;
+      licenseWindow.document.body.appendChild(preEle);
+    }
+  };
+
+  const fetchProjectRegion = async () => {
+    const credentials = await authApi();
+    if (credentials && credentials.project_id && credentials.region_id) {
+      handleProjectIdChange(credentials.project_id);
+      setProjectId(credentials.project_id);
+      setRegion(credentials.region_id);
+      setConfigError(false);
+    } else if (credentials && credentials.config_error === 1) {
+      setConfigError(true);
+    }
+  };
+
+  const handleRuntimeExpand = () => {
+    let runTimeMode = !expandRuntimeTemplate;
+    setExpandRuntimeTemplate(runTimeMode);
+  };
+
+  useEffect(() => {
+    if (loginState) {
+      fetchProjectRegion();
+      displayUserInfo();
+    } else {
+      displayUserInfo();
+    }
+    setSelectedRuntimeClone(undefined);
+  }, []);
+  return (
+    <div>
+      <ToastContainer />
+      {isLoadingUser && isLoadingRegion && !configError ? (
+        <div className="spin-loaderMain">
+          <ClipLoader
+            color="#8A8A8A"
+            loading={true}
+            size={20}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          />
+          Loading Config Setup
+        </div>
+      ) : !configError && openCreateTemplate ? (
+        <CreateRuntime
+          setOpenCreateTemplate={setOpenCreateTemplate}
+          selectedRuntimeClone={selectedRuntimeClone}
+        />
+      ) : (
+        <div className="settings-component">
+          <div className="settings-overlay">
+            <div>
+              <Iconsettings.react tag="div" />
+            </div>
+            <div className="settings-text">Settings</div>
+          </div>
+          <div className="settings-seperator"></div>
+          <div className="config-overlay">
+            <div className="config-form">
+              <div className="project-overlay">
+                <label className="project-text" htmlFor="project-id">
+                  Project ID
+                </label>
+                <ProjectIDDropdown
+                  projectId={projectId}
+                  onProjectIdChange={handleProjectIdChange}
+                />
+              </div>
+
+              <div className="region-overlay">
+                <label className="region-text" htmlFor="region-id">
+                  Region
+                </label>
+
+                <Select
+                  search
+                  onClick={handleDropdownOpen}
+                  placeholder={region}
+                  className="region-select"
+                  value={region}
+                  onChange={handleRegionChange}
+                  options={regionList}
+                  isDisabled={isDropdownOpen}
+                />
+              </div>
+              <div className="save-overlay">
+                <button
+                  className={
+                    isSaveDisabled ? 'save-button disabled' : 'save-button'
+                  }
+                  disabled={isSaveDisabled}
+                  onClick={handleSave}
+                >
+                  Save
+                </button>
+                {isLoading && (
+                  <div className="save-loader">
+                    <ClipLoader
+                      loading={true}
+                      size={25}
+                      aria-label="Loading Spinner"
+                      data-testid="loader"
+                    />
+                  </div>
+                )}
+              </div>
+            </div>
+            {isDropdownOpen && (
+              <div className="region-loader">
+                <ClipLoader
+                  loading={true}
+                  size={15}
+                  aria-label="Loading Spinner"
+                  data-testid="loader"
+                />
+              </div>
+            )}
+            <div className="user-info-card">
+              <div className="google-header">
+                This account is managed by google.com
+              </div>
+              <div className="seperator"></div>
+              <div className="user-overlay">
+                <div className="user-image-overlay">
+                  <img
+                    src={userInfo.picture}
+                    alt="User Image"
+                    className="user-image"
+                  />
+                </div>
+                <div className="user-details">
+                  <div className="user-email">{userInfo.email}</div>
+                </div>
+              </div>
+              <div className="seperator"></div>
+              <div className="google-header">
+                <a
+                  href="https://policies.google.com/privacy?hl=en"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Privacy Policy
+                </a>
+                <span className="privacy-terms"> • </span>
+                <a
+                  href="https://policies.google.com/terms?hl=en"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Terms of Service
+                </a>
+                <span className="footer-divider"> • </span>
+                <a onClick={handleLicenseClick} href="#">
+                  Licenses
+                </a>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div className="runtime-title-section">
+              <div className="runtime-title-part">
+                Serverless Runtime Templates
+              </div>
+              <div
+                className="expand-icon"
+                onClick={() => handleRuntimeExpand()}
+              >
+                {expandRuntimeTemplate ? (
+                  <iconExpandLess.react tag="div" />
+                ) : (
+                  <iconExpandMore.react tag="div" />
+                )}
+              </div>
+            </div>
+            {expandRuntimeTemplate && (
+              <ListRuntimeTemplates
+                openCreateTemplate={openCreateTemplate}
+                setOpenCreateTemplate={setOpenCreateTemplate}
+                setSelectedRuntimeClone={setSelectedRuntimeClone}
+              />
+            )}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default ConfigSelection;
