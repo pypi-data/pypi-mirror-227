5554c5c7b7ffc61fb58c58128fb061d68ef101c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-26 04:08:28 +0000

    Add package example-pkg-1756907975/1.7.0/example_pkg_1756907975-1.7.0-py3-none-any.whl

---
+++ packages/example-pkg-1756907975/example_pkg_1756907975-1.7.0-py3-none-any.whl/pg_message/add.py
@@ -0,0 +1,14 @@
+from cffi import FFI
+import sys
+sys.path.append("../lib")
+
+def add():
+    ffi = FFI()
+    ffi.cdef("""
+      int add(int a, int b);
+    """)
+
+    lib = ffi.dlopen("../lib/libffi_test.dll")
+    print(lib.add(3, 4))
+
+add()
