0ef2bfa13956263e0629e11766bf3a4011f85c88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-26 04:08:38 +0000

    Add package iroh/0.0.1/iroh-0.0.1.tar.gz

---
+++ packages/iroh/iroh-0.0.1.tar.gz/iroh-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="iroh",
+    description="Bytes. Distributed.",
+    version="0.0.1",
+    author="n0 team",
+    url="https://github.com/pytest-dev/pytest-timeout",
+    license="MIT OR Apache-2.0",
+    install_requires=["setuptools"],
+)
