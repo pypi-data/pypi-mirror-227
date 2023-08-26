0da78e39bb602f4e02f7199b75ea1e5b439848d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-26 14:06:38 +0000

    Add package CURSEWORK/0.3.0/CURSEWORK-0.3.0.tar.gz

---
+++ packages/CURSEWORK/CURSEWORK-0.3.0.tar.gz/CURSEWORK-0.3.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='CURSEWORK',
+    version='0.3.0',    
+    description='A Framework For Building 2d Games With Python+Pygame.',
+    url='https://github.com/setoyuma/CURSEWORK',
+    author='Setoichi',
+    author_email='setoichi.dev@gmail.com',
+    license='MIT',
+    packages=['CURSEWORK'],
+    install_requires=['pygame-ce',                     
+                      ],
+
+    classifiers=[
+        'Development Status :: 3 - Alpha'
+    ],
+)
