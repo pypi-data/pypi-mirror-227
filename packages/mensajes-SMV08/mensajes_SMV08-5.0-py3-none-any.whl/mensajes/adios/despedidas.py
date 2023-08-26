84751f9e2658fb873187221266507c4ac272c3a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-26 05:09:56 +0000

    Add package mensajes-SMV08/5.0/mensajes_SMV08-5.0-py3-none-any.whl

---
+++ packages/mensajes-SMV08/mensajes_SMV08-5.0-py3-none-any.whl/mensajes/adios/despedidas.py
@@ -0,0 +1,7 @@
+def despedir():
+    print("Me despedido desde despedidas.despedir()")
+    
+class Despedida:
+     def __init__(self):
+         print("Me despido desde Despedida.__init__")
+             
