20d84f2491ec8ee157f56181d8d1f065b11e2b6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-26 03:07:34 +0000

    Add package readfcs/1.1.6/readfcs-1.1.6-py2.py3-none-any.whl

---
+++ packages/readfcs/readfcs-1.1.6-py2.py3-none-any.whl/readfcs/datasets/_datasets.py
@@ -0,0 +1,15 @@
+from urllib.request import urlretrieve
+
+
+def example() -> str:
+    """An example fcs file.
+
+    With a spill matrix.
+
+    Originally from:
+    https://github.com/whitews/FlowIO/blob/master/examples/fcs_files/100715.fcs
+    """
+    url = "https://lamindb-test.s3.amazonaws.com/example.fcs"
+    path_data, _ = urlretrieve(url, "example.fcs")
+
+    return path_data
