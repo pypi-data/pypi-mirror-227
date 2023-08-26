f5bf93fb3165e3148bdb7e579de2cc505385046b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-26 14:06:54 +0000

    Add package PRomancer/0.2/PRomancer-0.2.tar.gz

---
+++ packages/PRomancer/PRomancer-0.2.tar.gz/PRomancer-0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="PRomancer",
+    version="0.2",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    install_requires=[
+        'openai>=0.0,<1.0',
+    ],
+    entry_points={
+        'console_scripts': [
+            'promancer = main:main',
+        ],
+    },
+)
