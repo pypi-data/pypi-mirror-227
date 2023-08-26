937e30bf4735678761bdc5e9f3ba75d6f845dcba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-26 04:10:12 +0000

    Add package skscope/0.1.5/skscope-0.1.5-cp39-cp39-win_amd64.whl

---
+++ packages/skscope/skscope-0.1.5-cp39-cp39-win_amd64.whl/skscope/__init__.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    :
+# @Author  :
+# @Site    :
+# @File    : __init__.py
+
+__version__ = "0.1.5"
+__author__ = "Zezhi Wang, Jin Zhu," "Peng Chen," "Junxian Zhu, Xueqin Wang"
+
+
+from .solver import (
+    ScopeSolver,
+    HTPSolver,
+    GraspSolver,
+    IHTSolver,
+    FobaSolver,
+    ForwardSolver,
+    OMPSolver,
+)
+from .base_solver import BaseSolver
+from .numeric_solver import convex_solver_nlopt
+
+__all__ = [
+    "ScopeSolver",
+    "HTPSolver",
+    "GraspSolver",
+    "IHTSolver",
+    "BaseSolver",
+    "FobaSolver",
+    "ForwardSolver",
+    "OMPSolver",
+    "convex_solver_nlopt",
+]
