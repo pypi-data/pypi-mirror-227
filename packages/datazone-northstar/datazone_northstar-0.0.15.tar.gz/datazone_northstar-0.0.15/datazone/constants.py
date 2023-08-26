7d43cf132a3565934393d1d72ed5f0fe28b8c087
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-26 04:10:44 +0000

    Add package datazone-northstar/0.0.15/datazone_northstar-0.0.15.tar.gz

---
+++ packages/datazone-northstar/datazone_northstar-0.0.15.tar.gz/datazone_northstar-0.0.15/datazone/constants.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+
+class Constants:
+    DEFAULT_CONFIG_FILE_NAME = "config.yml"
+    DEFAULT_SESSION_DURATION = "P90D"
+    DEFAULT_KEYRING_APP_NAME = "datazone"
+    DEFAULT_OAUTH2_CLIENT_ID = "front_end_client"
