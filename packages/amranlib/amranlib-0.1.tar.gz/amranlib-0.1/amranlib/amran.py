c197ece34aec1cdfe807f6f537219549fed60f66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-26 04:10:49 +0000

    Add package amranlib/0.1/amranlib-0.1.tar.gz

---
+++ packages/amranlib/amranlib-0.1.tar.gz/amranlib-0.1/amranlib/amran.py
@@ -0,0 +1,36 @@
+def Name(n):
+    m = input()
+    print("Welcome Back:",m)
+Name("")
+
+def Sum(n,m):
+    n= input("Enter First Num:")
+    n =int(n)
+    m= input("Enter Second Num:")
+    m =int(m)
+    print (n+m)
+Sum(0,0)
+
+def Mul(n,m):
+    n= input("Enter First Num:")
+    n =int(n)
+    m= input("Enter Second Num:")
+    m =int(m)
+    print (n*m)
+Mul(0,0)
+
+def Min(n,m):
+    n= input("Enter First Num:")
+    n =int(n)
+    m= input("Enter Second Num:")
+    m =int(m)
+    print (n-m)
+Min(0,0)
+
+def Kes(n,m):
+    n= input("Enter First Num:")
+    n =int(n)
+    m= input("Enter Second Num:")
+    m =int(m)
+    print (n/m)
+Kes(0,0)
---
+++ packages/amranlib/amranlib-0.1.tar.gz/amranlib-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='amranlib',
+      version='0.1',
+      description='test',
+      packages=['amranlib'],
+      author_email='sjahnahhfrasad@gmail.com',
+      zip_safe=False)
