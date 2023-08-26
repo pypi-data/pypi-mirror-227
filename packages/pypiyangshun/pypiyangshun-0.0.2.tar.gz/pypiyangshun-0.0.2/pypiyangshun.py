6c621a27a1664c499d5b259ae215fc0683e0cdc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-26 03:10:39 +0000

    Add package pypiyangshun/0.0.2/pypiyangshun-0.0.2.tar.gz

---
+++ packages/pypiyangshun/pypiyangshun-0.0.2.tar.gz/pypiyangshun-0.0.2/pypiyangshun.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+def PY():
+    print("pypi yangshun")
+
+
+if __name__ == '__main__':
+    PY()
---
+++ packages/pypiyangshun/pypiyangshun-0.0.2.tar.gz/pypiyangshun-0.0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pypiyangshun',
+    version='0.0.2',
+    description='pypi yangshun  Linux test',
+    # url='https://github.com/andyljones/coolgpus',
+    author='yangshun',
+    author_email='2095686947@qq.com',
+    license='MIT',
+    scripts=['pypiyangshun.py']
+    )
