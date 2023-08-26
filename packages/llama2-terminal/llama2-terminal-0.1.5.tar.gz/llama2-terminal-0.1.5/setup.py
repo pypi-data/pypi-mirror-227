2853039364854a7825356c5eb7c6986c2d98d887
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-26 05:09:07 +0000

    Add package llama2-terminal/0.1.5/llama2-terminal-0.1.5.tar.gz

---
+++ packages/llama2-terminal/llama2-terminal-0.1.5.tar.gz/llama2-terminal-0.1.5/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="llama2-terminal",
+    version="0.1.5",
+    packages=find_packages(),
+    install_requires=[
+        "inquirer",
+        "cmd2",
+        "farm-haystack",
+        "torch",
+        "transformers>=4.0.0",
+        "bitsandbytes>=0.3.9",
+        "pyyaml"
+    ],
+    include_package_data=True,
+    author="SamthinkGit",
+    author_email="sebastianmayorquin@gmail.com",
+    description="Descripción breve del proyecto",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/SamthinkGit/llama2-terminal",
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
