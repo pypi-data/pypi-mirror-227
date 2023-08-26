4efb46889c31e32e3de47f9437b5fd5574394353
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-26 14:07:34 +0000

    Add package ironmill-oxide/0.1.0/ironmill_oxide-0.1.0-py3-none-any.whl

---
+++ packages/ironmill-oxide/ironmill_oxide-0.1.0-py3-none-any.whl/ironmill_oxide/settings.py
@@ -0,0 +1,7 @@
+MANIFEST = 'circuits.toml'
+DEPENDENCIES = '[dependencies]'
+DEP_DIR = 'circuits'
+
+_POSTPEND = '/v1/repo'
+BUNDLER_URL = 'https://bundler.oxide.is' + _POSTPEND
+REGISTRY_URL = 'https://registry.oxide.is' + _POSTPEND
