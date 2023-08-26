289689d5b10166677b75ef2ec4f74a336cbaf39d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-26 03:07:31 +0000

    Add package cdonut/0.1/cdonut-0.1.tar.gz

---
+++ packages/cdonut/cdonut-0.1.tar.gz/cdonut-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='cdonut',
+    version='0.1',
+    description='A package to bring the c language donut meme to python, just import it to your code, then type: cdonut()',
+    author='Omar',
+    packages=['cdonut'],
+    install_requires=[
+        'numpy',
+    ],
+)
