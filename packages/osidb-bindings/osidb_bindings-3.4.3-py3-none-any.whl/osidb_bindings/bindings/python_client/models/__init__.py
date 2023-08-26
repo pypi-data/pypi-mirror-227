97d31b4eeab56a059992e33515524851f85f4587
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-26 03:08:01 +0000

    Add package osidb-bindings/3.4.3/osidb_bindings-3.4.3-py3-none-any.whl

---
+++ packages/osidb-bindings/osidb_bindings-3.4.3-py3-none-any.whl/osidb_bindings/bindings/python_client/models/__init__.py
@@ -0,0 +1,324 @@
+""" Contains all the data models used in inputs/outputs """
+
+from .affect import Affect
+from .affect_meta_attr import AffectMetaAttr
+from .affect_post import AffectPost
+from .affect_post_meta_attr import AffectPostMetaAttr
+from .affect_report_data import AffectReportData
+from .affect_type import AffectType
+from .affectedness_enum import AffectednessEnum
+from .auth_token_create_response_200 import AuthTokenCreateResponse200
+from .auth_token_refresh_create_response_200 import AuthTokenRefreshCreateResponse200
+from .auth_token_retrieve_response_200 import AuthTokenRetrieveResponse200
+from .auth_token_verify_create_response_200 import AuthTokenVerifyCreateResponse200
+from .blank_enum import BlankEnum
+from .collectors_api_v1_status_retrieve_response_200 import (
+    CollectorsApiV1StatusRetrieveResponse200,
+)
+from .collectors_api_v1_status_retrieve_response_200_collectors_item import (
+    CollectorsApiV1StatusRetrieveResponse200CollectorsItem,
+)
+from .collectors_api_v1_status_retrieve_response_200_collectors_item_data import (
+    CollectorsApiV1StatusRetrieveResponse200CollectorsItemData,
+)
+from .collectors_api_v1_status_retrieve_response_200_collectors_item_error import (
+    CollectorsApiV1StatusRetrieveResponse200CollectorsItemError,
+)
+from .collectors_api_v1_status_retrieve_response_200_collectors_item_state import (
+    CollectorsApiV1StatusRetrieveResponse200CollectorsItemState,
+)
+from .collectors_healthy_retrieve_response_200 import (
+    CollectorsHealthyRetrieveResponse200,
+)
+from .collectors_retrieve_response_200 import CollectorsRetrieveResponse200
+from .comment import Comment
+from .comment_meta_attr import CommentMetaAttr
+from .cv_ev_5_package_versions import CVEv5PackageVersions
+from .cv_ev_5_versions import CVEv5Versions
+from .epss import EPSS
+from .erratum import Erratum
+from .exploit_only_report_data import ExploitOnlyReportData
+from .exploit_only_report_data_source_enum import ExploitOnlyReportDataSourceEnum
+from .exploits_api_v1_collect_update_response_200 import (
+    ExploitsApiV1CollectUpdateResponse200,
+)
+from .exploits_api_v1_cve_map_retrieve_response_200 import (
+    ExploitsApiV1CveMapRetrieveResponse200,
+)
+from .exploits_api_v1_cve_map_retrieve_response_200_cves import (
+    ExploitsApiV1CveMapRetrieveResponse200Cves,
+)
+from .exploits_api_v1_epss_list_response_200 import ExploitsApiV1EpssListResponse200
+from .exploits_api_v1_flaw_data_list_response_200 import (
+    ExploitsApiV1FlawDataListResponse200,
+)
+from .exploits_api_v1_report_data_list_response_200 import (
+    ExploitsApiV1ReportDataListResponse200,
+)
+from .exploits_api_v1_report_date_retrieve_response_200 import (
+    ExploitsApiV1ReportDateRetrieveResponse200,
+)
+from .exploits_api_v1_report_date_retrieve_response_200_action_required_item import (
+    ExploitsApiV1ReportDateRetrieveResponse200ActionRequiredItem,
+)
+from .exploits_api_v1_report_date_retrieve_response_200_no_action_item import (
+    ExploitsApiV1ReportDateRetrieveResponse200NoActionItem,
+)
+from .exploits_api_v1_report_date_retrieve_response_200_not_relevant_item import (
+    ExploitsApiV1ReportDateRetrieveResponse200NotRelevantItem,
+)
+from .exploits_api_v1_report_explanations_retrieve_response_200 import (
+    ExploitsApiV1ReportExplanationsRetrieveResponse200,
+)
+from .exploits_api_v1_report_explanations_retrieve_response_200_explanations_item import (
+    ExploitsApiV1ReportExplanationsRetrieveResponse200ExplanationsItem,
+)
+from .exploits_api_v1_report_pending_retrieve_response_200 import (
+    ExploitsApiV1ReportPendingRetrieveResponse200,
+)
+from .exploits_api_v1_report_pending_retrieve_response_200_pending_actions_item import (
+    ExploitsApiV1ReportPendingRetrieveResponse200PendingActionsItem,
+)
+from .exploits_api_v1_status_retrieve_response_200 import (
+    ExploitsApiV1StatusRetrieveResponse200,
+)
+from .exploits_api_v1_supported_products_list_response_200 import (
+    ExploitsApiV1SupportedProductsListResponse200,
+)
+from .flaw import Flaw
+from .flaw_acknowledgment import FlawAcknowledgment
+from .flaw_acknowledgment_post import FlawAcknowledgmentPost
+from .flaw_acknowledgment_put import FlawAcknowledgmentPut
+from .flaw_classification import FlawClassification
+from .flaw_classification_state import FlawClassificationState
+from .flaw_comment import FlawComment
+from .flaw_comment_post import FlawCommentPost
+from .flaw_comment_post_meta_attr import FlawCommentPostMetaAttr
+from .flaw_comment_type import FlawCommentType
+from .flaw_meta_attr import FlawMetaAttr
+from .flaw_meta_type import FlawMetaType
+from .flaw_post import FlawPost
+from .flaw_post_classification import FlawPostClassification
+from .flaw_post_classification_state import FlawPostClassificationState
+from .flaw_post_meta_attr import FlawPostMetaAttr
+from .flaw_reference import FlawReference
+from .flaw_reference_post import FlawReferencePost
+from .flaw_reference_type import FlawReferenceType
+from .flaw_report_data import FlawReportData
+from .flaw_type import FlawType
+from .impact_enum import ImpactEnum
+from .jira_comment import JiraComment
+from .jira_issue import JiraIssue
+from .jira_issue_fields import JiraIssueFields
+from .jira_issue_query_result import JiraIssueQueryResult
+from .jira_issue_type import JiraIssueType
+from .jira_user import JiraUser
+from .major_incident_state_enum import MajorIncidentStateEnum
+from .maturity_preliminary_enum import MaturityPreliminaryEnum
+from .meta import Meta
+from .meta_meta_attr import MetaMetaAttr
+from .nist_cvss_validation_enum import NistCvssValidationEnum
+from .osidb_api_v1_affects_create_response_201 import OsidbApiV1AffectsCreateResponse201
+from .osidb_api_v1_affects_destroy_response_204 import (
+    OsidbApiV1AffectsDestroyResponse204,
+)
+from .osidb_api_v1_affects_list_affectedness import OsidbApiV1AffectsListAffectedness
+from .osidb_api_v1_affects_list_flaw_impact import OsidbApiV1AffectsListFlawImpact
+from .osidb_api_v1_affects_list_flaw_source import OsidbApiV1AffectsListFlawSource
+from .osidb_api_v1_affects_list_flaw_type import OsidbApiV1AffectsListFlawType
+from .osidb_api_v1_affects_list_impact import OsidbApiV1AffectsListImpact
+from .osidb_api_v1_affects_list_order_item import OsidbApiV1AffectsListOrderItem
+from .osidb_api_v1_affects_list_resolution import OsidbApiV1AffectsListResolution
+from .osidb_api_v1_affects_list_response_200 import OsidbApiV1AffectsListResponse200
+from .osidb_api_v1_affects_list_trackers_type import OsidbApiV1AffectsListTrackersType
+from .osidb_api_v1_affects_list_type import OsidbApiV1AffectsListType
+from .osidb_api_v1_affects_retrieve_response_200 import (
+    OsidbApiV1AffectsRetrieveResponse200,
+)
+from .osidb_api_v1_affects_update_response_200 import OsidbApiV1AffectsUpdateResponse200
+from .osidb_api_v1_flaws_acknowledgments_create_response_201 import (
+    OsidbApiV1FlawsAcknowledgmentsCreateResponse201,
+)
+from .osidb_api_v1_flaws_acknowledgments_destroy_response_204 import (
+    OsidbApiV1FlawsAcknowledgmentsDestroyResponse204,
+)
+from .osidb_api_v1_flaws_acknowledgments_list_response_200 import (
+    OsidbApiV1FlawsAcknowledgmentsListResponse200,
+)
+from .osidb_api_v1_flaws_acknowledgments_retrieve_response_200 import (
+    OsidbApiV1FlawsAcknowledgmentsRetrieveResponse200,
+)
+from .osidb_api_v1_flaws_acknowledgments_update_response_200 import (
+    OsidbApiV1FlawsAcknowledgmentsUpdateResponse200,
+)
+from .osidb_api_v1_flaws_comments_create_response_201 import (
+    OsidbApiV1FlawsCommentsCreateResponse201,
+)
+from .osidb_api_v1_flaws_comments_list_response_200 import (
+    OsidbApiV1FlawsCommentsListResponse200,
+)
+from .osidb_api_v1_flaws_comments_retrieve_response_200 import (
+    OsidbApiV1FlawsCommentsRetrieveResponse200,
+)
+from .osidb_api_v1_flaws_create_response_201 import OsidbApiV1FlawsCreateResponse201
+from .osidb_api_v1_flaws_list_affects_affectedness import (
+    OsidbApiV1FlawsListAffectsAffectedness,
+)
+from .osidb_api_v1_flaws_list_affects_impact import OsidbApiV1FlawsListAffectsImpact
+from .osidb_api_v1_flaws_list_affects_resolution import (
+    OsidbApiV1FlawsListAffectsResolution,
+)
+from .osidb_api_v1_flaws_list_affects_trackers_type import (
+    OsidbApiV1FlawsListAffectsTrackersType,
+)
+from .osidb_api_v1_flaws_list_affects_type import OsidbApiV1FlawsListAffectsType
+from .osidb_api_v1_flaws_list_impact import OsidbApiV1FlawsListImpact
+from .osidb_api_v1_flaws_list_major_incident_state import (
+    OsidbApiV1FlawsListMajorIncidentState,
+)
+from .osidb_api_v1_flaws_list_nist_cvss_validation import (
+    OsidbApiV1FlawsListNistCvssValidation,
+)
+from .osidb_api_v1_flaws_list_order_item import OsidbApiV1FlawsListOrderItem
+from .osidb_api_v1_flaws_list_requires_summary import OsidbApiV1FlawsListRequiresSummary
+from .osidb_api_v1_flaws_list_response_200 import OsidbApiV1FlawsListResponse200
+from .osidb_api_v1_flaws_list_source import OsidbApiV1FlawsListSource
+from .osidb_api_v1_flaws_list_type import OsidbApiV1FlawsListType
+from .osidb_api_v1_flaws_references_create_response_201 import (
+    OsidbApiV1FlawsReferencesCreateResponse201,
+)
+from .osidb_api_v1_flaws_references_destroy_response_204 import (
+    OsidbApiV1FlawsReferencesDestroyResponse204,
+)
+from .osidb_api_v1_flaws_references_list_response_200 import (
+    OsidbApiV1FlawsReferencesListResponse200,
+)
+from .osidb_api_v1_flaws_references_retrieve_response_200 import (
+    OsidbApiV1FlawsReferencesRetrieveResponse200,
+)
+from .osidb_api_v1_flaws_references_update_response_200 import (
+    OsidbApiV1FlawsReferencesUpdateResponse200,
+)
+from .osidb_api_v1_flaws_retrieve_response_200 import OsidbApiV1FlawsRetrieveResponse200
+from .osidb_api_v1_flaws_update_response_200 import OsidbApiV1FlawsUpdateResponse200
+from .osidb_api_v1_manifest_retrieve_response_200 import (
+    OsidbApiV1ManifestRetrieveResponse200,
+)
+from .osidb_api_v1_schema_retrieve_format import OsidbApiV1SchemaRetrieveFormat
+from .osidb_api_v1_schema_retrieve_lang import OsidbApiV1SchemaRetrieveLang
+from .osidb_api_v1_schema_retrieve_response_200 import (
+    OsidbApiV1SchemaRetrieveResponse200,
+)
+from .osidb_api_v1_status_retrieve_response_200 import (
+    OsidbApiV1StatusRetrieveResponse200,
+)
+from .osidb_api_v1_status_retrieve_response_200_osidb_data import (
+    OsidbApiV1StatusRetrieveResponse200OsidbData,
+)
+from .osidb_api_v1_status_retrieve_response_200_osidb_service import (
+    OsidbApiV1StatusRetrieveResponse200OsidbService,
+)
+from .osidb_api_v1_trackers_list_affects_affectedness import (
+    OsidbApiV1TrackersListAffectsAffectedness,
+)
+from .osidb_api_v1_trackers_list_affects_flaw_impact import (
+    OsidbApiV1TrackersListAffectsFlawImpact,
+)
+from .osidb_api_v1_trackers_list_affects_flaw_source import (
+    OsidbApiV1TrackersListAffectsFlawSource,
+)
+from .osidb_api_v1_trackers_list_affects_flaw_type import (
+    OsidbApiV1TrackersListAffectsFlawType,
+)
+from .osidb_api_v1_trackers_list_affects_impact import (
+    OsidbApiV1TrackersListAffectsImpact,
+)
+from .osidb_api_v1_trackers_list_affects_resolution import (
+    OsidbApiV1TrackersListAffectsResolution,
+)
+from .osidb_api_v1_trackers_list_affects_type import OsidbApiV1TrackersListAffectsType
+from .osidb_api_v1_trackers_list_order_item import OsidbApiV1TrackersListOrderItem
+from .osidb_api_v1_trackers_list_response_200 import OsidbApiV1TrackersListResponse200
+from .osidb_api_v1_trackers_list_type import OsidbApiV1TrackersListType
+from .osidb_api_v1_trackers_retrieve_response_200 import (
+    OsidbApiV1TrackersRetrieveResponse200,
+)
+from .osidb_healthy_retrieve_response_200 import OsidbHealthyRetrieveResponse200
+from .osidb_whoami_retrieve_response_200 import OsidbWhoamiRetrieveResponse200
+from .osidb_whoami_retrieve_response_200_profile import (
+    OsidbWhoamiRetrieveResponse200Profile,
+)
+from .osim_api_v1_workflows_adjust_create_response_200 import (
+    OsimApiV1WorkflowsAdjustCreateResponse200,
+)
+from .osim_api_v1_workflows_retrieve_2_response_200 import (
+    OsimApiV1WorkflowsRetrieve2Response200,
+)
+from .osim_api_v1_workflows_retrieve_response_200 import (
+    OsimApiV1WorkflowsRetrieveResponse200,
+)
+from .osim_healthy_retrieve_response_200 import OsimHealthyRetrieveResponse200
+from .osim_retrieve_response_200 import OsimRetrieveResponse200
+from .paginated_affect_list import PaginatedAffectList
+from .paginated_epss_list import PaginatedEPSSList
+from .paginated_exploit_only_report_data_list import PaginatedExploitOnlyReportDataList
+from .paginated_flaw_acknowledgment_list import PaginatedFlawAcknowledgmentList
+from .paginated_flaw_comment_list import PaginatedFlawCommentList
+from .paginated_flaw_list import PaginatedFlawList
+from .paginated_flaw_reference_list import PaginatedFlawReferenceList
+from .paginated_flaw_report_data_list import PaginatedFlawReportDataList
+from .paginated_supported_products_list import PaginatedSupportedProductsList
+from .paginated_tracker_list import PaginatedTrackerList
+from .requires_summary_enum import RequiresSummaryEnum
+from .resolution_enum import ResolutionEnum
+from .source_666_enum import Source666Enum
+from .status_enum import StatusEnum
+from .supported_products import SupportedProducts
+from .taskman_api_v1_group_create_response_200 import TaskmanApiV1GroupCreateResponse200
+from .taskman_api_v1_group_retrieve_response_200 import (
+    TaskmanApiV1GroupRetrieveResponse200,
+)
+from .taskman_api_v1_group_update_response_200 import TaskmanApiV1GroupUpdateResponse200
+from .taskman_api_v1_task_assignee_retrieve_response_200 import (
+    TaskmanApiV1TaskAssigneeRetrieveResponse200,
+)
+from .taskman_api_v1_task_assignee_update_response_200 import (
+    TaskmanApiV1TaskAssigneeUpdateResponse200,
+)
+from .taskman_api_v1_task_comment_create_response_200 import (
+    TaskmanApiV1TaskCommentCreateResponse200,
+)
+from .taskman_api_v1_task_comment_update_response_200 import (
+    TaskmanApiV1TaskCommentUpdateResponse200,
+)
+from .taskman_api_v1_task_flaw_create_response_200 import (
+    TaskmanApiV1TaskFlawCreateResponse200,
+)
+from .taskman_api_v1_task_flaw_retrieve_response_200 import (
+    TaskmanApiV1TaskFlawRetrieveResponse200,
+)
+from .taskman_api_v1_task_flaw_update_response_200 import (
+    TaskmanApiV1TaskFlawUpdateResponse200,
+)
+from .taskman_api_v1_task_retrieve_response_200 import (
+    TaskmanApiV1TaskRetrieveResponse200,
+)
+from .taskman_api_v1_task_status_update_reason import TaskmanApiV1TaskStatusUpdateReason
+from .taskman_api_v1_task_status_update_resolution import (
+    TaskmanApiV1TaskStatusUpdateResolution,
+)
+from .taskman_api_v1_task_status_update_response_200 import (
+    TaskmanApiV1TaskStatusUpdateResponse200,
+)
+from .taskman_api_v1_task_status_update_status import TaskmanApiV1TaskStatusUpdateStatus
+from .taskman_api_v1_task_unassigned_retrieve_response_200 import (
+    TaskmanApiV1TaskUnassignedRetrieveResponse200,
+)
+from .taskman_healthy_retrieve_response_200 import TaskmanHealthyRetrieveResponse200
+from .token_obtain_pair import TokenObtainPair
+from .token_refresh import TokenRefresh
+from .token_verify import TokenVerify
+from .tracker import Tracker
+from .tracker_meta_attr import TrackerMetaAttr
+from .tracker_report_data import TrackerReportData
+from .tracker_type import TrackerType
