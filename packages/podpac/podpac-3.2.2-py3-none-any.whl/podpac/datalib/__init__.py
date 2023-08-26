d2cc7623c366997597f9198db29b56a17a2b22cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-26 04:09:05 +0000

    Add package podpac/3.2.2/podpac-3.2.2-py3-none-any.whl

---
+++ packages/podpac/podpac-3.2.2-py3-none-any.whl/podpac/datalib/__init__.py
@@ -0,0 +1,24 @@
+"""
+Datalib Public API
+
+This module gets imported in the root __init__.py
+and exposed its contents to podpac.datalib
+"""
+
+import sys
+
+from podpac.datalib.cosmos_stations import COSMOSStations
+from podpac.datalib.drought_monitor import DroughtCategory, DroughtMonitorCategory
+from podpac.datalib.egi import EGI
+from podpac.datalib.gfs import GFS, GFSLatest
+from podpac.datalib.modis_pds import MODIS
+from podpac.datalib.satutils import Landsat8, Sentinel2
+from podpac.datalib.smap_egi import SMAP
+from podpac.datalib.terraintiles import TerrainTiles
+from podpac.datalib.weathercitizen import WeatherCitizen
+from podpac.datalib.soilscape import SoilSCAPE20min
+from podpac.datalib import soilgrids
+
+# intake requires python >= 3.6
+if sys.version >= "3.6":
+    from podpac.datalib.intake_catalog import IntakeCatalog
