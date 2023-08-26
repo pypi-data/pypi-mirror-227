3b2f6d61f85121974239e05df729594100d384f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-26 04:10:03 +0000

    Add package substrate-ai/0.0.1/substrate_ai-0.0.1-py3-none-any.whl

---
+++ packages/substrate-ai/substrate_ai-0.0.1-py3-none-any.whl/clients/AWS_Client.py
@@ -0,0 +1,105 @@
+import boto3
+import base64
+from utils.env import config_data
+import typer
+import time
+import time
+import boto3
+from utils.env import config_data
+import boto3
+import time
+from utils.utils import get_user_id
+from utils.console import console
+
+class AWS_Client:
+    def __init__(self):
+        access_key_id = config_data["AWS_ACCESS_KEY_ID"]
+        secret_access_key = config_data["AWS_SECRET_ACCESS_KEY"]
+        self.ecr_client = boto3.client('ecr', aws_access_key_id=access_key_id, aws_secret_access_key=secret_access_key, region_name='us-east-1')
+        # self.kinesis_client = boto3.client('kinesis', aws_access_key_id=access_key_id, aws_secret_access_key=secret_access_key, region_name='us-east-1')
+        self.logs_client = boto3.client('logs', aws_access_key_id=config_data["AWS_ACCESS_KEY_ID"], aws_secret_access_key=config_data["AWS_SECRET_ACCESS_KEY"], region_name='us-east-1')
+        self.sagemaker_client = boto3.client('sagemaker', aws_access_key_id=config_data["AWS_ACCESS_KEY_ID"], aws_secret_access_key=config_data["AWS_SECRET_ACCESS_KEY"], region_name='us-east-1')
+
+        token = self.ecr_client.get_authorization_token()
+        self.username, self.password = base64.b64decode(token['authorizationData'][0]['authorizationToken']).decode().split(':')
+        self.registry = token['authorizationData'][0]['proxyEndpoint']    
+
+        # create a docker repository for the user (should be project repository in the future)
+    def get_or_create_user_repo(self):
+        repo_name = f"repo.{get_user_id()}"
+
+        # check if repo already exists
+        try:
+            
+            response = self.ecr_client.describe_repositories(repositoryNames=[repo_name])
+            repositoryUri = response["repositories"][0]["repositoryUri"]
+            return repositoryUri
+        except self.ecr_client.exceptions.RepositoryNotFoundException:
+            response = self.ecr_client.create_repository(repositoryName=repo_name)
+            repositoryUri = response["repository"]["repositoryUri"]
+            return repositoryUri
+        
+
+    def is_job_running(self, job_name):
+        response = self.sagemaker_client.describe_training_job(TrainingJobName=job_name)
+        return response['TrainingJobStatus'] in ['InProgress', 'Stopping']
+    
+    def loop_until_job_running(self, job_name):
+        while not self.is_job_running(job_name):
+            time.sleep(5)
+
+    def is_job_terminated(self, job_name):
+        response = self.sagemaker_client.describe_training_job(TrainingJobName=job_name)
+        return response['TrainingJobStatus'] in ['Failed', 'Completed', 'Stopped']
+    
+    def stop_job(self, job_name):
+        self.sagemaker_client.stop_training_job(TrainingJobName=job_name)
+
+    def stream_logs(self, job_name, wait):
+
+        # if wait:
+        #     console.print("Waiting for logs to be available")
+
+        while True:
+            response = self.logs_client.describe_log_streams(
+                logGroupName='/aws/sagemaker/TrainingJobs',
+                logStreamNamePrefix=job_name,
+                limit=1
+            )
+
+            if len(response['logStreams']) > 0:
+                log_stream_name = response['logStreams'][0]['logStreamName']
+                break
+
+            if not wait:
+                print("No log stream found for job: %s" % job_name)
+                raise typer.Exit(code=1)
+            
+            time.sleep(5)
+
+
+        response = self.logs_client.get_log_events(
+            logGroupName='/aws/sagemaker/TrainingJobs',
+            logStreamName=log_stream_name,
+            startFromHead=True, 
+        )
+
+        for event in response['events']:
+            print(event['message'])
+
+        while 'nextForwardToken' in response:
+            time.sleep(1)
+            response = self.logs_client.get_log_events(
+                logGroupName='/aws/sagemaker/TrainingJobs',
+                logStreamName=log_stream_name,
+                startFromHead=True, 
+                nextToken=response['nextForwardToken'],
+            )
+
+            for event in response['events']:
+                print(event['message'])
+
+            if self.is_job_terminated(job_name):
+                break
+                
+
---
+++ packages/substrate-ai/substrate_ai-0.0.1-py3-none-any.whl/clients/JobClient.py
@@ -0,0 +1,43 @@
+from utils.env import config_data
+import typer
+from utils.utils import get_cli_token, get_project_config
+from clients.AWS_Client import AWS_Client
+from clients.DockerClient import DockerClient
+import requests
+from utils.console import console
+from yaspin import yaspin
+
+
+class JobClient:
+    def __init__(self):
+        self.aws_client = AWS_Client()
+        access_key_id = config_data["AWS_ACCESS_KEY_ID"]
+        secret_access_key = config_data["AWS_SECRET_ACCESS_KEY"]
+        self.docker_client2 = DockerClient(access_key_id, secret_access_key, 'us-east-1')
+
+    def start_job(self):
+        self.docker_client2.build()
+        console.print("Image built", style="bold green")
+        repo_uri = self.aws_client.get_or_create_user_repo()
+        console.print("Uploading your code to the cloud", style="bold green")
+        self.docker_client2.push(repo_uri)
+        console.print("Code uploaded", style="bold green")
+
+        job_name = self.run_container_from_backend(repo_uri)
+        console.print(f"Job is starting, your job name is {job_name}", style="bold green")
+
+        spinner = yaspin()
+        spinner.start()
+        self.aws_client.loop_until_job_running(job_name)
+        spinner.stop()
+
+        console.print(f"Job {job_name} has started", style="bold green")
+        console.print("Streaming your logs", style="bold green")
+
+        try:
+            self.aws_client.stream_logs(job_name, True)
+        except KeyboardInterrupt:
+            console.print(f"Stopping streaming logs due to keyboard interrupt")
+            console.print("Your job is still running in the cloud")
+
+        console.print(f"Job {job_name} terminated", style="bold green")
