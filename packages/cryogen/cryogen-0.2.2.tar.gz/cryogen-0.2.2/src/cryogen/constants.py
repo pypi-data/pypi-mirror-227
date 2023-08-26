ee9e5683c7a947c34ccdd845283ea2736590c375
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-26 04:11:47 +0000

    Add package cryogen/0.2.2/cryogen-0.2.2.tar.gz

---
+++ packages/cryogen/cryogen-0.2.2.tar.gz/cryogen-0.2.2/src/cryogen/constants.py
@@ -0,0 +1,21 @@
+from enum import Enum
+
+PARTITION_SIZES = [1_000_000, 100_000, 10_000]
+FAR_AWAY_BLOCK = 1_000_000_000
+DEFAULT_RANGE = range(0, FAR_AWAY_BLOCK)
+BLOCK_CHUNK = 1000
+BATCH_SIZE = 2**29  # 512 mb uncompressed chunks
+
+
+class Dataset(Enum):
+    blocks = "blocks"
+    transactions = "transactions"
+    logs = "logs"
+    contracts = "contracts"
+    traces = "traces"
+    state_diffs = "state_diffs"
+    balance_diffs = "balance_diffs"
+    code_diffs = "code_diffs"
+    nonce_diffs = "nonce_diffs"
+    storage_diffs = "storage_diffs"
+    vm_traces = "vm_traces"
