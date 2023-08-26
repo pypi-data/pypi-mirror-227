6ec3cbfdb15b970cee8f61e0fff39c02713d8ddb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-26 03:10:35 +0000

    Add package openllm/0.2.27/openllm-0.2.27-cp38-cp38-macosx_10_16_universal2.whl

---
+++ packages/openllm/openllm-0.2.27-cp38-cp38-macosx_10_16_universal2.whl/openllm/models/auto/modeling_flax_auto.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+import typing as t
+from collections import OrderedDict
+from .factory import BaseAutoLLMClass, _LazyAutoMapping
+from openllm_core.config import CONFIG_MAPPING_NAMES
+MODEL_FLAX_MAPPING_NAMES = OrderedDict([('flan_t5', 'FlaxFlanT5'), ('opt', 'FlaxOPT')])
+MODEL_FLAX_MAPPING = _LazyAutoMapping(CONFIG_MAPPING_NAMES, MODEL_FLAX_MAPPING_NAMES)
+class AutoFlaxLLM(BaseAutoLLMClass):
+  _model_mapping: t.ClassVar = MODEL_FLAX_MAPPING
