c4b1b426bbf7d776e3ef7c6a3b69f58728cfb662
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-26 04:08:41 +0000

    Add package iroh/0.0.3/iroh-0.0.3.tar.gz

---
+++ packages/iroh/iroh-0.0.3.tar.gz/iroh-0.0.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+with open("README.md", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="iroh",
+    description="Bytes. Distributed.",
+    version="0.0.3",
+    author="n0 team",
+    url="https://github.com/n0-computer/iroh",
+    license="MIT OR Apache-2.0",
+    install_requires=["setuptools"],
+)
