23d535f48d5323801d606d98db63e15aab98a2a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-26 03:09:09 +0000

    Add package littletree/0.1.0/littletree-0.1.0-py3-none-any.whl

---
+++ packages/littletree/littletree-0.1.0-py3-none-any.whl/littletree/node.py
@@ -0,0 +1,96 @@
+import copy
+from typing import Mapping, Optional, Iterator, TypeVar, Generic, Hashable, Union, Iterable
+
+from .basenode import BaseNode
+from .exporters.dotexporter import DotExporter
+from .exporters.stringexporter import StringExporter
+from .serializers.dictserializer import DictSerializer
+from .serializers.rowserializer import RowSerializer
+
+TNode = TypeVar("TNode", bound="Node")
+TIdentifier = TypeVar("TIdentifier", bound=Hashable)
+TData = TypeVar("TData")
+
+
+class Node(BaseNode[TIdentifier], Generic[TIdentifier, TData]):
+    __slots__ = "data"
+
+    def __init__(
+        self,
+        data: TData = None,
+        identifier: TIdentifier = "node",
+        children: Union[Mapping[TIdentifier, TNode], Iterable[TNode]] = (),
+        parent: Optional[TNode] = None,
+    ):
+        """
+        Create a Node
+
+        :param identifier: Identifier for this node
+        :param parent: Immediately assign a parent
+        :param children: Children to add
+        """
+        super().__init__(identifier, children, parent)
+        self.data = data
+
+    def __repr__(self) -> str:
+        output = [self.__class__.__name__, f"({self.data!r}, identifier={self.identifier!r})"]
+        return "".join(output)
+
+    def __str__(self):
+        if self.data is None:
+            return f"{self.identifier}"
+        else:
+            return f"{self.identifier}\n{self.data}"
+
+    def __eq__(self: TNode, other: TNode):
+        if self is other:
+            return True
+        return ((self.identifier, self.data, self._cdict)
+                == (other.identifier, other.data, other._cdict))
+
+    def __copy__(self):
+        return self.__class__(self.data,
+                              identifier=self.identifier,
+                              children={k: v.__copy__() for (k, v) in self._cdict.items()})
+
+    def __deepcopy__(self, memo):
+        return self.__class__(copy.deepcopy(self.data, memo),
+                              identifier=self.identifier,
+                              children={k: v.__deepcopy__(memo) for (k, v) in self._cdict.items()})
+
+    copy = __copy__
+
+    @classmethod
+    def from_dict(cls, data, **kwargs) -> TNode:
+        """
+        Load tree from Dict.
+        :param data: Dict in which tree is stored
+        :return: Root node of new tree
+        """
+        return DictSerializer(cls, fields=["data"], **kwargs).from_dict(data)
+
+    @classmethod
+    def from_paths(cls, rows, **kwargs) -> TNode:
+        return RowSerializer(cls, fields=["data"], **kwargs).from_rows(rows)
+
+    def to_string(self, file=None, keep=None, str_factory=None, **kwargs) -> Optional[str]:
+        exporter = StringExporter(str_factory=str_factory, **kwargs)
+        return exporter.to_string(self, file, keep=keep)
+
+    def to_image(self, file=None, keep=None, node_attributes=None, **kwargs):
+        if node_attributes is None:
+            node_attributes = {"label": str}
+        exporter = DotExporter(node_attributes=node_attributes, **kwargs)
+        return exporter.to_image(self, file, keep=keep)
+
+    def to_dot(self, file=None, keep=None, node_attributes=None, **kwargs) -> Optional[str]:
+        if node_attributes is None:
+            node_attributes = {"label": str}
+        exporter = DotExporter(node_attributes=node_attributes, **kwargs)
+        return exporter.to_dot(self, file, keep=keep)
+
+    def to_dict(self, fields=("data",), **kwargs) -> Mapping:
+        return DictSerializer(self.__class__, fields=fields, **kwargs).to_dict(self)
+
+    def to_rows(self, fields=("data",), **kwargs) -> Iterator[Mapping]:
+        return RowSerializer(self.__class__, fields=fields, **kwargs).to_rows(self)
---
+++ packages/littletree/littletree-0.1.0-py3-none-any.whl/littletree/nodepath.py
@@ -0,0 +1,103 @@
+import itertools
+from fnmatch import fnmatchcase
+from typing import Optional, TypeVar, Iterable
+
+TNode = TypeVar("TNode", bound="BaseNode")
+
+
+class NodePath:
+    __slots__ = "_node"
+
+    # Can be overriden by child classes
+    separator = "/"
+
+    def __init__(self, node: TNode):
+        self._node = node
+
+    def __call__(self, path) -> TNode:
+        if isinstance(path, str):
+            path = path.split(self.separator)
+        node = self._node
+        for segment in path:
+            node = node._cdict[segment]
+        return node
+
+    def __len__(self):
+        return 1 + sum(1 for _ in self._node.iter_ancestors())
+
+    def __iter__(self) -> Iterable[TNode]:
+        node = self._node
+        return itertools.chain(reversed(tuple(node.iter_ancestors())), [node])
+
+    def __str__(self) -> str:
+        separator = self.separator
+        return separator + separator.join([node.identifier for node in self])
+
+    def get(self, path) -> Optional[TNode]:
+        try:
+            return self(path)
+        except KeyError:
+            return None
+
+    def create(self, path) -> TNode:
+        if isinstance(path, str):
+            path = path.split(self.separator)
+
+        node = self._node
+        path = iter(path)
+
+        try:
+            for segment in path:
+                node = node[segment]
+        except KeyError:
+            node = self._create_node(identifier=segment, parent=node)
+
+            for segment in path:
+                node = self._create_node(identifier=segment, parent=node)
+
+        return node
+
+    def _create_node(self, identifier, parent: TNode) -> TNode:
+        """Subclasses can overwrite this if the construction of a Node is different."""
+        node = self._node.__class__()
+        node.identifier = identifier
+        node.parent = parent
+        return node
+
+    def glob(self, path) -> Iterable[TNode]:
+        """
+        Find nodes by globbing patterns.
+
+        For example to find all nodes that start with 'a':
+            node.path.glob("**/a*")
+        """
+
+        if isinstance(path, str):
+            path = path.split(self.separator)
+
+        nodes = {id(self._node): self._node}
+        for segment in path:
+            if segment == "**":
+                nodes = {id(node): node
+                         for candidate in nodes.values()
+                         for node in candidate.iter_tree()}
+            elif segment == "":
+                nodes = {id(node): node
+                         for node in nodes.values()
+                         if not node.is_leaf}
+            elif self._is_pattern(segment):
+                nodes = {id(node): node
+                         for candidate in nodes.values()
+                         for node in candidate.children
+                         if fnmatchcase(str(node.identifier), segment)}
+            else:
+                nodes = {id(candidate[segment]): candidate[segment]
+                         for candidate in nodes.values()
+                         if segment in candidate}
+
+        return nodes.values()
+
+    @staticmethod
+    def _is_pattern(segment) -> bool:
+        """Check if segment is a pattern. If not direct access is much faster."""
+        return isinstance(segment, str) and any(char in segment for char in "*?[")
