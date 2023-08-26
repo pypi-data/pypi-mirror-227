baffc459d9566b1e43d6e844f23fa587199b3e14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-26 05:09:30 +0000

    Add package gpt-json/0.4.0/gpt_json-0.4.0-py3-none-any.whl

---
+++ packages/gpt-json/gpt_json-0.4.0-py3-none-any.whl/gpt_json/tests/shared.py
@@ -0,0 +1,41 @@
+from enum import Enum
+
+from pydantic import BaseModel, Field
+
+
+class MySubSchema(BaseModel):
+    name: str
+
+
+class MySchema(BaseModel):
+    text: str
+    items: list[str]
+    numerical: int | float
+    sub_element: MySubSchema
+    reason: bool = Field(description="Explanation")
+
+
+class UnitType(Enum):
+    CELSIUS = "celsius"
+    FAHRENHEIT = "fahrenheit"
+
+
+class GetCurrentWeatherRequest(BaseModel):
+    location: str = Field(description="The city and state, e.g. San Francisco, CA")
+    unit: UnitType | None = None
+
+
+def get_current_weather(request: GetCurrentWeatherRequest):
+    """Test description"""
+
+
+async def get_current_weather_async(request: GetCurrentWeatherRequest):
+    """Test description"""
+
+
+def get_weather_additional_args(request: GetCurrentWeatherRequest, other_args: str):
+    pass
+
+
+def get_weather_no_pydantic(other_args: str):
+    pass
