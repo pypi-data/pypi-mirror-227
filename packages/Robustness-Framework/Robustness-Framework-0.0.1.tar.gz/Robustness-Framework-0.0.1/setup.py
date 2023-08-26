e322664f28507b12667fc5661a2b16feb32b6132
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-26 05:11:02 +0000

    Add package Robustness-Framework/0.0.1/Robustness-Framework-0.0.1.tar.gz

---
+++ packages/Robustness-Framework/Robustness-Framework-0.0.1.tar.gz/Robustness-Framework-0.0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Robustness-Framework',
+    version='0.0.1',
+    author='Mohammad KHalooei',
+    url='https://github.com/khalooei/robustness-framework',
+    author_email='mkhalooei90@gmail.com',
+    description='An efficient framework for establishing a baseline for standard and adversarial machine learning training projects ',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.8',
+)
