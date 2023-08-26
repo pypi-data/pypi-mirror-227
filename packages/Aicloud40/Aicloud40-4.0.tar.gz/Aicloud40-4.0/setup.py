2d022640120551d4a2d84d22b3c963b447a81511
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-26 03:07:35 +0000

    Add package Aicloud40/4.0/Aicloud40-4.0.tar.gz

---
+++ packages/Aicloud40/Aicloud40-4.0.tar.gz/Aicloud40-4.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Aicloud40',
+    version='4.0',
+    description='My open-source package',
+    author='sreehari',
+    author_email='your@email.com',
+    packages=find_packages(),
+)
