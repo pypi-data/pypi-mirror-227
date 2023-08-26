d0c8a2b3c8a7d26486ed78ceb417c9d586958b1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-26 04:09:21 +0000

    Add package simon-search/0.0.1.post0/simon-search-0.0.1.post0.tar.gz

---
+++ packages/simon-search/simon-search-0.0.1.post0.tar.gz/simon-search-0.0.1.post0/simon/__init__.py
@@ -0,0 +1,9 @@
+from .search import Search
+from .store import Datastore
+from .start import create_context
+from .models import AgentContext
+from .models import ParsedDocument
+from .components.documents import index_document, parse_web, parse_text, parse_tika, bulk_index
+
+
+
