c7f6f733f4aa8282e12a7efab5be09480ba3b158
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-26 04:09:09 +0000

    Add package manyworlds/0.3.0/manyworlds-0.3.0.tar.gz

---
+++ packages/manyworlds/manyworlds-0.3.0.tar.gz/manyworlds-0.3.0/docs/index.rst
@@ -0,0 +1,38 @@
+Manyworlds
+==========
+
+.. toctree::
+   :maxdepth: 5
+   :caption: Contents:
+
+manyworlds.scenario\_forest module
+----------------------------------
+
+.. automodule:: manyworlds.scenario_forest
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+manyworlds.scenario module
+--------------------------
+
+.. automodule:: manyworlds.scenario
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+manyworlds.step module
+----------------------
+
+.. automodule:: manyworlds.step
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+manyworlds.data\_table module
+-----------------------------
+
+.. automodule:: manyworlds.data_table
+   :members:
+   :undoc-members:
+   :show-inheritance:
---
+++ packages/manyworlds/manyworlds-0.3.0.tar.gz/manyworlds-0.3.0/manyworlds/__main__.py
@@ -0,0 +1,42 @@
+# __main__.py
+import argparse
+import manyworlds as mw
+from termcolor import colored  # type: ignore
+
+
+def main():
+    parser = argparse.ArgumentParser(prog="manyworlds")
+    parser.add_argument("--input", "-i", help="input scenario file")
+    parser.add_argument("--output", "-o", help="output scenario file")
+    parser.add_argument(
+        "--mode",
+        "-m",
+        choices=["strict", "relaxed"],
+        default="strict",
+        help="flattening mode",
+    )
+    parser.add_argument(
+        "--comments", "-c", default=False, action="store_true", help="output comments"
+    )
+    args = parser.parse_args()
+
+    # read hierarchical feature file:
+    tree = mw.ScenarioForest.from_file(args.input)
+
+    # print scenario forest outline to terminal:
+    for sc in tree.scenarios():
+        level = sc.level()
+        if level > 1:
+            indentation_string = "   " * (level - 2) + colored("└─ ", "blue")
+        else:
+            indentation_string = ""
+        optional_colon = ":" if sc.is_organizational() else ""
+        print(indentation_string + sc.name + optional_colon)
+
+    # write flat feature file:
+    if args.output:
+        tree.flatten(args.output, mode=args.mode, comments=args.comments)
+
+
+if __name__ == "__main__":
+    main()
