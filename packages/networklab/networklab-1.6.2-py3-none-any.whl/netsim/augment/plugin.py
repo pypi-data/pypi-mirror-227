8f4d61d7fef719977511d51cc3934cae407c9fcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-26 14:07:09 +0000

    Add package networklab/1.6.2/networklab-1.6.2-py3-none-any.whl

---
+++ packages/networklab/networklab-1.6.2-py3-none-any.whl/netsim/augment/plugin.py
@@ -0,0 +1,87 @@
+'''
+plugin - implement custom topology transformation plugins
+'''
+
+import os
+import sys
+import typing
+import importlib
+import importlib.util
+
+from box import Box
+from ..utils import log
+from ..utils.files import get_moddir
+from .. import data
+
+def load_plugin_from_path(path: str, plugin: str) -> typing.Optional[object]:
+  module_path = path+'/'+plugin
+  if not os.path.exists(module_path):
+    if os.path.exists(module_path+'.py'):
+      module_path = module_path+'.py'
+    else:
+      return None
+
+  module: typing.Optional[object] = None
+  config_name = None
+
+  if os.path.isdir(module_path):
+    module_path = module_path + '/plugin.py'
+    config_name = plugin
+    if not os.path.exists(module_path):
+      return None
+
+  module_name = plugin.replace('.py','')
+  module_name = f'netlab.plugin.{module_name}'
+
+  try:
+    modspec  = importlib.util.spec_from_file_location(module_name,module_path)
+    assert(modspec is not None)
+    pymodule = importlib.util.module_from_spec(modspec)
+    sys.modules[module_name] = pymodule
+    assert(modspec.loader is not None)
+    modspec.loader.exec_module(pymodule)
+  except:
+    print(f"Cannot load plugin {module_name} from {module_path}\n{str(sys.exc_info()[1])}")
+    log.fatal('Aborting the transformation process','plugin')
+
+  if config_name:
+    setattr(pymodule,'config_name',config_name)
+
+  if log.VERBOSE:
+    print(f"loaded plugin {module_name}")
+  return pymodule
+
+def init(topology: Box) -> None:
+  data.types.must_be_list(parent=topology,key='defaults.plugin',path='',create_empty=True)    # defaults.plugin must be a list (if present)
+  if topology.defaults.plugin:                                                                # If we have default plugins...
+    data.types.must_be_list(parent=topology,key='plugin',path='',create_empty=True)           # ... make sure the plugin attribute is a list
+    topology.plugin.extend(topology.defaults.plugin)                                          # ... and extend it with default plugins
+
+  if not 'plugin' in topology:
+    return
+
+  topology.Plugin = []
+  for pname in topology.plugin:
+    plugin = None
+    search_path = ('.',str(get_moddir() / 'extra'))
+    for path in search_path:
+      if not plugin:
+        plugin = load_plugin_from_path(path,pname)
+    if plugin:
+      topology.Plugin.append(plugin)
+    else:
+      log.error(f"Cannot find plugin {pname} in {search_path}",log.IncorrectValue,'plugin')
+
+  if log.debug_active('plugin'):
+    print(f'plug INIT: {topology.Plugin}')
+
+def execute(action: str, topology: Box) -> None:
+  if not 'Plugin' in topology:
+    return
+
+  for plugin in topology.Plugin:
+    if hasattr(plugin,action):
+      func = getattr(plugin,action)
+      if log.debug_active('plugin'):
+        print(f'plug INIT: {topology.Plugin}')
+      func(topology)
---
+++ packages/networklab/networklab-1.6.2-py3-none-any.whl/netsim/cli/test.py
@@ -0,0 +1,111 @@
+#
+# netlab config command
+#
+# Deploy custom configuration template to network devices
+#
+import typing
+import argparse
+import os
+import glob
+import subprocess
+import shutil
+
+from box import Box
+from pathlib import Path
+
+from .. import utils
+from ..utils import read as _read
+from . import external_commands
+
+#
+# CLI parser for 'netlab config' command
+#
+def test_parse(args: typing.List[str], settings: Box) -> argparse.Namespace:
+  choices = ( p for p in settings.providers.keys() if 'start' in settings.providers[p] )
+  parser = argparse.ArgumentParser(
+    prog='netlab test',
+    description='Test virtual lab installation')
+  parser.add_argument(
+    '-w','--work-directory',
+    dest='workdir',
+    action='store',
+    default='test',
+    help='Working directory (default: test)')
+  parser.add_argument(
+    '-v','--verbose',
+    dest='verbose',
+    action='count',
+    default=0,
+    help='Verbose logging')
+  parser.add_argument(
+    dest='provider',
+    action='store',
+    choices=list(choices),
+    help='Run tests for the specified provider')
+
+  return parser.parse_args(args)
+
+def copy_topology(args: argparse.Namespace) -> None:
+  if args.verbose:
+    print("Creating test topology file in %s" % args.workdir)
+  os.mkdir(args.workdir)
+  os.chdir(args.workdir)
+  moddir = Path(__file__).resolve().parent.parent
+
+  src_topo = moddir / "templates/tests" / (args.provider + ".yml")
+  try:
+    shutil.copy(src_topo,"topology.yml")
+  except:
+    utils.log.fatal("Cannot copy test topology file %s into %s" % (src_topo,args.workdir))
+  if args.verbose:
+    print("... done, moving on\n")
+
+def create_configs() -> None:
+  external_commands.print_step(2,"creating configuration files")
+  if not external_commands.run_command(["netlab","create"],True):
+    utils.log.fatal("netlab create failed, aborting...","netlab test")
+
+def cleanup_working_directory(args: argparse.Namespace) -> None:
+  if args.verbose:
+    print("\nCleaning up -- removing %s and its contents" % args.workdir)
+  os.chdir("..")
+  try:
+    shutil.rmtree(args.workdir)
+  except:
+    print("... cannot clean up working directory, trying to run 'sudo rm -fr'")
+    try:
+      subprocess.run(["sudo","rm","-fr",args.workdir],check=True)
+      print("... that worked ;)")
+    except:
+      print("... rm -fr failed, please clean up %s manually" % args.workdir)
+      return
+
+  if args.verbose:
+    print("... done, test completed\n")
+
+def run(cli_args: typing.List[str]) -> None:
+  settings = _read.read_yaml('package:topology-defaults.yml')
+  if not cli_args:
+    utils.log.fatal("Specify the virtualization environment to test or use -h to get help","test")
+
+  if not settings:
+    utils.log.fatal("Cannot read the global defaults","test")
+
+  args = test_parse(cli_args,settings)
+  if os.path.exists(args.workdir):
+    utils.log.fatal("Directory %s already exists, aborting" % args.workdir,"test")
+
+  if args.verbose:
+    utils.log.set_logging_flags(args)
+
+  external_commands.run_probes(settings,args.provider,1)
+  copy_topology(args)
+  create_configs()
+  if not external_commands.run_command('netlab up'):
+    utils.log.fatal('netlab up failed, aborting','test')
+  elif not external_commands.run_command('netlab down'):
+    utils.log.fatal('netlab down failed','test')
+#  external_commands.start_lab(settings,args.provider,3)
+#  external_commands.deploy_configs(4)
+#  external_commands.stop_lab(settings,args.provider,5)
+  cleanup_working_directory(args)
