3919c54bdbae4fc255441900f6ac91fcf7226788
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-26 05:08:38 +0000

    Add package dash-chat-components/0.1.0/dash_chat_components-0.1.0.tar.gz

---
+++ packages/dash-chat-components/dash_chat_components-0.1.0.tar.gz/dash_chat_components-0.1.0/dash_chat_components/_imports_.py
@@ -0,0 +1,15 @@
+from .Chat import Chat
+from .ChatInput import ChatInput
+from .ChatMessage import ChatMessage
+from .ChatMessageList import ChatMessageList
+from .ChatMessageTyping import ChatMessageTyping
+from .ChatSimple import ChatSimple
+
+__all__ = [
+    "Chat",
+    "ChatInput",
+    "ChatMessage",
+    "ChatMessageList",
+    "ChatMessageTyping",
+    "ChatSimple"
+]
