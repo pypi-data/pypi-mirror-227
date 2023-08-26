34038a71d8bb287007f2528732e2d24ff1d3cb62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-26 04:10:02 +0000

    Add package rtasr/0.0.4/rtasr-0.0.4.tar.gz

---
+++ packages/rtasr/rtasr-0.0.4.tar.gz/rtasr-0.0.4/src/rtasr/__main__.py
@@ -0,0 +1,53 @@
+"""Define the entry point for the command line interface of rtasr."""
+
+import argparse
+
+from rich import print
+from rich.traceback import install
+from rich_argparse import RichHelpFormatter
+
+from rtasr.cli import (
+    DownloadDatasetCommand,
+    EvaluationCommand,
+    ListItemsCommand,
+    TranscriptionASRCommand,
+)
+from rtasr.cli_messages import ascii_art
+
+install(show_locals=True)
+
+
+def main() -> None:
+    """Define the entry point for the command line interface of rtasr."""
+    parser = argparse.ArgumentParser(
+        prog="rtasr",
+        description="🏆 Run benchmarks against the most common ASR tools on the market.",
+        usage="rtasr <command> [<args>]",
+        formatter_class=RichHelpFormatter,
+    )
+    commands_parser = parser.add_subparsers(dest="command")
+
+    # Register subcommands
+    DownloadDatasetCommand.register_subcommand(commands_parser)
+    EvaluationCommand.register_subcommand(commands_parser)
+    ListItemsCommand.register_subcommand(commands_parser)
+    TranscriptionASRCommand.register_subcommand(commands_parser)
+
+    args = parser.parse_args()
+
+    print(ascii_art)
+
+    if not hasattr(args, "func"):
+        print(
+            "[bold red]Oops something went wrong. Please check the command you"
+            f" entered.[/bold red]\n👉 {args}\n"
+        )
+        parser.print_help()
+        exit(1)
+
+    args.func(args).run()
+    exit(0)
+
+
+if __name__ == "__main__":
+    main()
