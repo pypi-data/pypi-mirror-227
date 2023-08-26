b6d135fb7a526c0ea33fd0c25ad2d2615f9e5caa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-26 14:06:44 +0000

    Add package IslandOfMisfitToys/0.2.10/IslandOfMisfitToys-0.2.10-py2.py3-none-any.whl

---
+++ packages/IslandOfMisfitToys/IslandOfMisfitToys-0.2.10-py2.py3-none-any.whl/misfit_toys/acoustic_fwi/__init__.py
@@ -0,0 +1,10 @@
+__all__ = [
+    "input_dictionaries",
+    "fwi2",
+    "deploy",
+    "custom_losses"
+]
+from .input_dictionaries import *
+from .fwi2 import *
+from .deploy import *
+from .custom_losses import *
