f982cf855f5b5ab78b1c1d99731adc2a9b8b7bf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-26 03:10:49 +0000

    Add package evannester/1.0.3/evannester-1.0.3.tar.gz

---
+++ packages/evannester/evannester-1.0.3.tar.gz/evannester-1.0.3/evannester.py
@@ -0,0 +1,14 @@
+"""这是一个打印出全部数组内容的模块.
+我们可以通过设置数组去调用该模块中的打印函数"""
+
+
+def print_lol(the_first, indent=False, level=0):
+    # 通过循环递归去判断是否是list数组去无限打印每个结果
+    for each_item in the_first:
+        if isinstance(each_item, list):
+            print_lol(each_item, indent, level+1)
+        else:
+            if indent:
+                for tab_stop in range(level):
+                    print("\t", end='')
+            print(each_item)
