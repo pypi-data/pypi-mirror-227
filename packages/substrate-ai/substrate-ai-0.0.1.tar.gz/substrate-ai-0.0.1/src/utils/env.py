75928e402a3d8f13c4bd466b322aeb33426d3f41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-26 04:10:03 +0000

    Add package substrate-ai/0.0.1/substrate-ai-0.0.1.tar.gz

---
+++ packages/substrate-ai/substrate-ai-0.0.1.tar.gz/substrate-ai-0.0.1/src/utils/env.py
@@ -0,0 +1,13 @@
+config_data = {
+    "SUPABASE_URL": "https://wahqlmyfzxxydjnhovfg.supabase.co",
+    "SUPABASE_ANON_KEY": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndhaHFsbXlmenh4eWRqbmhvdmZnIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODUzNzgyMDEsImV4cCI6MjAwMDk1NDIwMX0.ZN-WaS4DRO5-2Q7xYYnoVTYSg0eHh9xt2oZQAfFarIE",
+    # todo change key before release
+    "AWS_ACCESS_KEY_ID": "AKIAQSAVYKZKG3ZHBKOG",
+    "AWS_SECRET_ACCESS_KEY": "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",
+    "AWS_DEFAULT_REGION": "us-east-1",
+    # todo to remove
+    "AWS_ECR_REPOSITORY_NAME": "substrate",
+    "PYTHON_BACKEND_URL": "https://fastapi.substrateai.com",
+}
+
+# todo transform to .env file
