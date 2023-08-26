3e08386c5f3cac780b674106556cb58e24062b60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-26 03:09:24 +0000

    Add package SpiralFilm/0.1.12/SpiralFilm-0.1.12.tar.gz

---
+++ packages/SpiralFilm/SpiralFilm-0.1.12.tar.gz/SpiralFilm-0.1.12/spiralfilm/__init__.py
@@ -0,0 +1,9 @@
+# spiral_film/__init__.py
+
+# Import main classes and functions from the relevant modules
+from .core import FilmCore
+from .config import FilmConfig
+from .embed import FilmEmbed, FilmEmbedConfig
+
+# Specify the version of the package
+__version__ = "0.1.12"
