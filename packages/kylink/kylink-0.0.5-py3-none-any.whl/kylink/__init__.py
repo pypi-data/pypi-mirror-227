ef388ee12b9cc603bef0336072554859e1cd55d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-26 14:07:10 +0000

    Add package kylink/0.0.5/kylink-0.0.5-py3-none-any.whl

---
+++ packages/kylink/kylink-0.0.5-py3-none-any.whl/kylink/__init__.py
@@ -0,0 +1,1 @@
+from .gql import Kylink
---
+++ packages/kylink/kylink-0.0.5-py3-none-any.whl/kylink/kylink.py
@@ -0,0 +1,16 @@
+from kylink.defi import DeFiProvider
+from kylink.market import MarketProvider
+from kylink.gql import GraphQLProvider
+from kylink.resolve import ResolveProvider
+from kylink.token import TokenProvider
+from kylink.wallet import WalletProvider
+
+
+class Kylink:
+    def __init__(self, api=None) -> None:
+        self.raw = GraphQLProvider(api)
+        self.market = MarketProvider(self.raw)
+        self.defi = DeFiProvider(self.raw)
+        self.resolve = ResolveProvider(self.raw)
+        self.wallet = WalletProvider(self.raw)
+        self.token = TokenProvider(self.raw)
