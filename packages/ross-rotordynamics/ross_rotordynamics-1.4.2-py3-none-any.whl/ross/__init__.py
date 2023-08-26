ce8699bf0d335092c4a02be269699f07d417b6b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-26 04:09:27 +0000

    Add package ross-rotordynamics/1.4.2/ross_rotordynamics-1.4.2-py3-none-any.whl

---
+++ packages/ross-rotordynamics/ross_rotordynamics-1.4.2-py3-none-any.whl/ross/__init__.py
@@ -0,0 +1,18 @@
+__version__ = "1.4.2"
+from plotly import io as _pio
+
+import ross.plotly_theme
+
+from .bearing_seal_element import *
+from .defects import *
+from .disk_element import *
+from .materials import *
+from .point_mass import *
+from .probe import *
+from .results import *
+from .rotor_assembly import *
+from .shaft_element import *
+from .units import Q_
+from .utils import get_data_from_figure, visualize_matrix
+
+_pio.templates.default = "ross"
