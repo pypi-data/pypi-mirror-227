4f824f1fe8a77fce0d99355f092e1c2d64f08e20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-26 05:09:56 +0000

    Add package mensajes-SMV08/5.0/mensajes-SMV08-5.0.tar.gz

---
+++ packages/mensajes-SMV08/mensajes-SMV08-5.0.tar.gz/mensajes-SMV08-5.0/mensajes/__init__.py
@@ -0,0 +1,1 @@
+print("Cargando paquete mensjaes.....")
---
+++ packages/mensajes-SMV08/mensajes-SMV08-5.0.tar.gz/mensajes-SMV08-5.0/tests/test_hola.py
@@ -0,0 +1,8 @@
+import unittest
+import numpy as np
+from mensajes.hola.saludos import generar_array
+
+class PruebasHola(unittest.TestCase):
+    def test_generar_array(self):
+        np.array([0,1,2,3,4,5])
+        generar_array(6)
