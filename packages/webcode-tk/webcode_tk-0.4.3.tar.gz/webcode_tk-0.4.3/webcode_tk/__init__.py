af2cedaa77f01cf3b0159284798b175419289a78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-26 05:10:52 +0000

    Add package webcode-tk/0.4.3/webcode_tk-0.4.3.tar.gz

---
+++ packages/webcode-tk/webcode_tk-0.4.3.tar.gz/webcode_tk-0.4.3/webcode_tk/__init__.py
@@ -0,0 +1,15 @@
+# __init__.py
+"""Deals with html and css documents to check their code.
+
+Modules exported by this package:
+- `color_keywords`: helper library to align color keywords with their
+          properties and hex and rgb values.
+- `colortools`: processes CSS color related properties and values.
+- `css`: creates Stylesheet objects that store CSS information.
+- `html`: gets html files from a project folder, gets the HTML code
+          from files, gets number of a particular element in a file
+          or folder, gets elements as tags, and much more.
+- `validator`: sends HTML or CSS code to the W3C Validator to check
+          for errors.
+"""
+__version__ = "0.4.3"
