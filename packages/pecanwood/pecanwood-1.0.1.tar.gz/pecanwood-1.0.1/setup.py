e53c8c458aea48da9652f928d80660154871911f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-26 03:11:02 +0000

    Add package pecanwood/1.0.1/pecanwood-1.0.1.tar.gz

---
+++ packages/pecanwood/pecanwood-1.0.1.tar.gz/pecanwood-1.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pecanwood",
+    version="1.0.1",
+    description="telegramme bor",
+    author="eugene",
+    author_email="eugene.langedyk@draglobal.com",
+    url="https://github.com/yourusername/your-package-name",
+    packages=find_packages(),
+    install_requires=[
+        # List your package dependencies here
+    ],
+)
