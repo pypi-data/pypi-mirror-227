d38d6bf9093fcd8fea1dadfb7f9b6d1d6f5135b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-26 05:09:14 +0000

    Add package ploft/0.0.1/ploft-0.0.1-py3-none-any.whl

---
+++ packages/ploft/ploft-0.0.1-py3-none-any.whl/ploft/__init__.py
@@ -0,0 +1,19 @@
+import pkg_resources
+
+#from pkg_resources import resource_stream
+
+
+import sys
+if sys.version_info >= (3, 9):
+    import importlib.resources as importlib_resources
+else:
+    import importlib_resources
+
+data_path = pkg_resources.resource_filename('ploft', 'jsc/main2.js')
+
+f = importlib_resources.files('ploft')
+
+
+__version__ = "0.0.1"
+
+__all__ = ["__version__"]
