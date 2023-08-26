89ba03c11a8c5ea920d365ad5b80eea331ef8b77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-26 03:10:11 +0000

    Add package structlog-gcp/0.2.1/structlog_gcp-0.2.1.tar.gz

---
+++ packages/structlog-gcp/structlog_gcp-0.2.1.tar.gz/structlog_gcp-0.2.1/structlog_gcp/processors.py
@@ -0,0 +1,134 @@
+# https://cloud.google.com/functions/docs/monitoring/logging#writing_structured_logs
+# https://cloud.google.com/logging/docs/agent/logging/configuration#process-payload
+# https://cloud.google.com/logging/docs/structured-logging#special-payload-fields
+
+
+import json
+from typing import Any
+
+import structlog.processors
+from structlog.typing import EventDict, Processor, WrappedLogger
+
+from .types import CLOUD_LOGGING_KEY, SOURCE_LOCATION_KEY
+
+
+class CoreCloudLogging:
+    """Initialize the Google Cloud Logging event message"""
+
+    def setup(self) -> list[Processor]:
+        return [
+            # If some value is in bytes, decode it to a unicode str.
+            structlog.processors.UnicodeDecoder(),
+            # Add a timestamp in ISO 8601 format.
+            structlog.processors.TimeStamper(fmt="iso"),
+            self,
+        ]
+
+    def __call__(
+        self, logger: WrappedLogger, method_name: str, event_dict: EventDict
+    ) -> EventDict:
+        value = {
+            "message": event_dict.pop("event"),
+            "time": event_dict.pop("timestamp"),
+        }
+
+        event_dict[CLOUD_LOGGING_KEY] = value
+        return event_dict
+
+
+class FormatAsCloudLogging:
+    """Finalize the Google Cloud Logging event message and replace the logging event"""
+
+    def __init__(self) -> None:
+        self.renderer = structlog.processors.JSONRenderer()
+        self.label = "logging.googleapis.com/labels"
+
+    def setup(self) -> list[Processor]:
+        return [self, structlog.processors.JSONRenderer()]
+
+    def _serialize(self, value: Any) -> str:
+        if isinstance(value, str):
+            return value
+        return json.dumps(value, default=repr)
+
+    def __call__(
+        self, logger: WrappedLogger, method_name: str, event_dict: EventDict
+    ) -> EventDict:
+        event: EventDict = event_dict.pop(CLOUD_LOGGING_KEY)
+
+        if event_dict:
+            event[self.label] = {}
+
+            for key, item in event_dict.items():
+                value = self._serialize(item)
+                event[self.label][key] = value
+
+        return event
+
+
+class LogSeverity:
+    """Set the severity using the Google Cloud Logging severities"""
+
+    def __init__(self) -> None:
+        self.default = "notset"
+
+        # From Python's logging level to Google level
+        self.mapping = {
+            "notset": "DEFAULT",  # The log entry has no assigned severity level.
+            "debug": "DEBUG",  # Debug or trace information.
+            "info": "INFO",  # Routine information, such as ongoing status or performance.
+            # "notice": "NOTICE", # Normal but significant events, such as start up, shut down, or a configuration change.
+            "warn": "WARNING",  # Warning events might cause problems.
+            "warning": "WARNING",  # Warning events might cause problems.
+            "error": "ERROR",  # Error events are likely to cause problems.
+            "critical": "CRITICAL",  # Critical events cause more severe problems or outages.
+            # "alert": "ALERT", # A person must take an action immediately.
+            # "emergency": "EMERGENCY", #	One or more systems are unusable.
+        }
+
+    def setup(self) -> list[Processor]:
+        # Add log level to event dict.
+        return [structlog.processors.add_log_level, self]
+
+    def __call__(
+        self, logger: WrappedLogger, method_name: str, event_dict: EventDict
+    ) -> EventDict:
+        """Format a Python log level value as a GCP log severity.
+
+        See: https://cloud.google.com/logging/docs/reference/v2/rest/v2/LogEntry#LogSeverity
+        """
+
+        log_level = event_dict.pop("level")
+        severity = self.mapping.get(log_level, self.default)
+
+        event_dict[CLOUD_LOGGING_KEY]["severity"] = severity
+        return event_dict
+
+
+class CodeLocation:
+    """Inject the location of the logging message into the logs"""
+
+    def setup(self) -> list[Processor]:
+        # Add callsite parameters.
+        call_site_proc = structlog.processors.CallsiteParameterAdder(
+            parameters=[
+                structlog.processors.CallsiteParameter.PATHNAME,
+                structlog.processors.CallsiteParameter.MODULE,
+                structlog.processors.CallsiteParameter.FUNC_NAME,
+                structlog.processors.CallsiteParameter.LINENO,
+            ]
+        )
+        return [call_site_proc, self]
+
+    def __call__(
+        self, logger: WrappedLogger, method_name: str, event_dict: EventDict
+    ) -> EventDict:
+        location = {
+            "file": event_dict.pop("pathname"),
+            "line": str(event_dict.pop("lineno")),
+            "function": f"{event_dict.pop('module')}:{event_dict.pop('func_name')}",
+        }
+
+        event_dict[CLOUD_LOGGING_KEY][SOURCE_LOCATION_KEY] = location
+
+        return event_dict
---
+++ packages/structlog-gcp/structlog_gcp-0.2.1.tar.gz/structlog_gcp-0.2.1/tests/test_log.py
@@ -0,0 +1,156 @@
+import datetime
+import json
+from unittest.mock import patch
+
+import structlog
+
+import structlog_gcp
+
+
+def test_normal(capsys, logger):
+    logger.info("test")
+
+    output = capsys.readouterr()
+
+    assert "" == output.err
+
+    msg = json.loads(output.out)
+
+    expected = {
+        "logging.googleapis.com/sourceLocation": {
+            "file": "/app/test.py",
+            "function": "test:test123",
+            "line": "42",
+        },
+        "message": "test",
+        "severity": "INFO",
+        "time": "2023-04-01T08:00:00.000000Z",
+    }
+    assert expected == msg
+
+
+def test_error(capsys, logger):
+    try:
+        1 / 0
+    except ZeroDivisionError:
+        logger.exception("oh noes", foo="bar")
+
+    output = capsys.readouterr()
+
+    assert "" == output.err
+
+    msg = json.loads(output.out)
+
+    expected = {
+        "@type": "type.googleapis.com/google.devtools.clouderrorreporting.v1beta1.ReportedErrorEvent",
+        "context": {
+            "reportLocation": {
+                "file": "/app/test.py",
+                "function": "test:test123",
+                "line": "42",
+            },
+        },
+        "logging.googleapis.com/sourceLocation": {
+            "file": "/app/test.py",
+            "function": "test:test123",
+            "line": "42",
+        },
+        "serviceContext": {
+            "service": "unknown service",
+            "version": "unknown version",
+        },
+        "logging.googleapis.com/labels": {"foo": "bar"},
+        "severity": "CRITICAL",
+        "message": "oh noes",
+        "stack_trace": "oh noes\nTraceback blabla",
+        "time": "2023-04-01T08:00:00.000000Z",
+    }
+    assert expected == msg
+
+
+def test_service_context_default(capsys, logger):
+    try:
+        1 / 0
+    except ZeroDivisionError:
+        logger.exception("oh noes")
+
+    msg = json.loads(capsys.readouterr().out)
+
+    assert msg["serviceContext"] == {
+        "service": "unknown service",
+        "version": "unknown version",
+    }
+
+
+@patch.dict("os.environ", {"K_SERVICE": "test-service", "K_REVISION": "test-version"})
+def test_service_context_envvar(capsys, mock_logger_env):
+    processors = structlog_gcp.build_processors()
+    structlog.configure(processors=processors)
+    logger = structlog.get_logger()
+
+    try:
+        1 / 0
+    except ZeroDivisionError:
+        logger.exception("oh noes")
+
+    msg = json.loads(capsys.readouterr().out)
+
+    assert msg["serviceContext"] == {
+        "service": "test-service",
+        "version": "test-version",
+    }
+
+
+def test_service_context_custom(capsys, mock_logger_env):
+    processors = structlog_gcp.build_processors(
+        service="my-service",
+        version="deadbeef",
+    )
+    structlog.configure(processors=processors)
+    logger = structlog.get_logger()
+
+    try:
+        1 / 0
+    except ZeroDivisionError:
+        logger.exception("oh noes")
+
+    msg = json.loads(capsys.readouterr().out)
+
+    assert msg["serviceContext"] == {
+        "service": "my-service",
+        "version": "deadbeef",
+    }
+
+
+def test_labels_string(capsys, logger):
+    logger.info(
+        "test",
+        test1="test1",
+        test2=2,
+        test3={"foo": "bar"},
+        test4={"date": datetime.date(2023, 1, 1)},
+    )
+
+    output = capsys.readouterr()
+
+    assert "" == output.err
+
+    msg = json.loads(output.out)
+
+    expected = {
+        "logging.googleapis.com/sourceLocation": {
+            "file": "/app/test.py",
+            "function": "test:test123",
+            "line": "42",
+        },
+        "message": "test",
+        "severity": "INFO",
+        "time": "2023-04-01T08:00:00.000000Z",
+        "logging.googleapis.com/labels": {
+            "test1": "test1",
+            "test2": "2",
+            "test3": '{"foo": "bar"}',
+            "test4": '{"date": "datetime.date(2023, 1, 1)"}',
+        },
+    }
+    assert expected == msg
