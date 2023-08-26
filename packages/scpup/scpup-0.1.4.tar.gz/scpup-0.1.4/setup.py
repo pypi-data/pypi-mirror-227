fb67c8ae0205370cf53f613a5be37ffef54d9b1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-26 03:08:13 +0000

    Add package scpup/0.1.4/scpup-0.1.4.tar.gz

---
+++ packages/scpup/scpup-0.1.4.tar.gz/scpup-0.1.4/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='scpup',
+  description='AramEau SCPUP Lib',
+  author='Ivan Aram Gonzalez Su',
+  author_email='ivan.ags.95@gmail.com',
+  packages=['src/scpup', 'src/scpup/abc', 'src/scpup/utils'],
+)
