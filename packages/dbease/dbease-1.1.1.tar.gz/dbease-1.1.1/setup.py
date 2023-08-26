b639782ccb987ea76730b700be40cf00cf46d2f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-26 14:06:58 +0000

    Add package dbease/1.1.1/dbease-1.1.1.tar.gz

---
+++ packages/dbease/dbease-1.1.1.tar.gz/dbease-1.1.1/setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Author: 梁开孟
+# date: 2023/8/24
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='dbease',
+    version='1.1.1',
+    author='梁开孟',
+    author_email='519281809@qq.com',
+    packages=find_packages(),
+    include_package_data=True,
+    python_requires='>=3.6',
+    install_requires=[
+        'cx_Oracle>=8.3.0',
+        'pandas>=1.5.3',
+        'configparser',
+        'base64',
+        're>=2.2.1',
+        'prettytable>=3.5.0',
+        'time',
+    ],
+)
