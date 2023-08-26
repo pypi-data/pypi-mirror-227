f5fd81d6bbdceef5bbb08314ecfe78acba13777b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-26 04:10:14 +0000

    Add package abstra/1.8.31/abstra-1.8.31-py3-none-any.whl

---
+++ packages/abstra/abstra-1.8.31-py3-none-any.whl/tests/fixtures.py
@@ -0,0 +1,31 @@
+import os, json, tempfile
+from pathlib import Path
+
+abstra_json = {"version": "0.2"}
+
+
+def rm_tree(pth: Path):
+    pth = Path(pth)
+    if not pth.exists():
+        return
+    for child in pth.glob("*"):
+        if child.is_file():
+            child.unlink()
+        else:
+            rm_tree(child)
+    pth.rmdir()
+
+
+def init_dir():
+    path = Path(tempfile.mkdtemp())
+    abstra_json_path = path / "abstra.json"
+    path.mkdir(exist_ok=True)
+    if not abstra_json_path.exists():
+        abstra_json_path.write_text(json.dumps(abstra_json))
+    os.environ["ABSTRA_DATABASE_URL"] = str(Path(path, "db.sqlite3"))
+    os.chdir(path)
+    return path
+
+
+def clear_dir(path: Path):
+    rm_tree(path)
