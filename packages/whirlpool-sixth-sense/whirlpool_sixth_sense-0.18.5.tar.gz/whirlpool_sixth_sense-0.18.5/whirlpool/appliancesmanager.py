32b1a0f87be7aac9c8381a7a0c3ae57b14acb788
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-26 05:10:54 +0000

    Add package whirlpool-sixth-sense/0.18.5/whirlpool_sixth_sense-0.18.5.tar.gz

---
+++ packages/whirlpool-sixth-sense/whirlpool_sixth_sense-0.18.5.tar.gz/whirlpool_sixth_sense-0.18.5/whirlpool/appliancesmanager.py
@@ -0,0 +1,97 @@
+import json
+import logging
+
+import aiohttp
+
+from .auth import Auth
+from .backendselector import BackendSelector
+
+LOGGER = logging.getLogger(__name__)
+
+
+class AppliancesManager:
+    def __init__(
+        self,
+        backend_selector: BackendSelector,
+        auth: Auth,
+        session: aiohttp.ClientSession,
+    ):
+        self._backend_selector = backend_selector
+        self._auth = auth
+        self._aircons = None
+        self._washer_dryers = None
+        self._ovens = None
+        self._session: aiohttp.ClientSession = session
+
+    def _create_headers(self):
+        return {
+            "Authorization": "Bearer " + self._auth.get_access_token(),
+            "Content-Type": "application/json",
+            # "Host": "api.whrcloud.eu",
+            "User-Agent": "okhttp/3.12.0",
+            "Pragma": "no-cache",
+            "Cache-Control": "no-cache",
+        }
+
+    async def fetch_appliances(self):
+        account_id = None
+        async with self._session.get(
+            f"{self._backend_selector.base_url}/api/v1/getUserDetails",
+            headers=self._create_headers(),
+        ) as r:
+            if r.status != 200:
+                LOGGER.error(f"Failed to get account id: {r.status}")
+                return False
+            account_id = json.loads(await r.text())["accountId"]
+
+        async with self._session.get(
+            f"{self._backend_selector.base_url}/api/v2/appliance/all/account/{account_id}",
+            headers=self._create_headers(),
+        ) as r:
+            if r.status != 200:
+                LOGGER.error(f"Failed to get appliances: {r.status}")
+                return False
+
+            self._aircons = []
+            self._washer_dryers = []
+            self._ovens = []
+
+            locations = json.loads(await r.text())[str(account_id)]
+            for appliances in locations.values():
+                for appliance in appliances:
+                    appliance_data = {
+                        "SAID": appliance["SAID"],
+                        "NAME": appliance["APPLIANCE_NAME"],
+                        "DATA_MODEL": appliance["DATA_MODEL_KEY"],
+                        "CATEGORY": appliance["CATEGORY_NAME"],
+                        "MODEL_NUMBER": appliance.get("MODEL_NO"),
+                        "SERIAL_NUMBER": appliance.get("SERIAL"),
+                    }
+                    data_model = appliance["DATA_MODEL_KEY"].lower()
+                    if "airconditioner" in data_model:
+                        self._aircons.append(appliance_data)
+                    elif "dryer" in data_model or "washer" in data_model:
+                        self._washer_dryers.append(appliance_data)
+                    elif (
+                        "cooking_minerva" in data_model
+                        or "cooking_vsi" in data_model
+                        or "cooking_u2" in data_model
+                    ):
+                        self._ovens.append(appliance_data)
+                    else:
+                        LOGGER.warning(
+                            "Unsupported appliance data model %s", data_model
+                        )
+        return True
+
+    @property
+    def aircons(self):
+        return self._aircons
+
+    @property
+    def washer_dryers(self):
+        return self._washer_dryers
+
+    @property
+    def ovens(self):
+        return self._ovens
---
+++ packages/whirlpool-sixth-sense/whirlpool_sixth_sense-0.18.5.tar.gz/whirlpool_sixth_sense-0.18.5/whirlpool/backendselector.py
@@ -0,0 +1,59 @@
+import logging
+from enum import Enum
+
+LOGGER = logging.getLogger(__name__)
+
+
+class Brand(Enum):
+    Whirlpool = 0
+    Maytag = 1
+    KitchenAid = 2
+
+
+class Region(Enum):
+    EU = 0
+    US = 1
+
+
+BACKEND_DATA = {
+    Brand.Whirlpool: {
+        "client_id": "whirlpool_android",
+        "client_secret": "i-eQ8MD4jK4-9DUCbktfg-t_7gvU-SrRstPRGAYnfBPSrHHt5Mc0MFmYymU2E2qzif5cMaBYwFyFgSU6NTWjZg",
+    },
+    Brand.Maytag: {
+        "client_id": "maytag_ios",
+        "client_secret": "OfTy3A3rV4BHuhujkPThVDE9-SFgOymJyUrSbixjViATjCGviXucSKq2OxmPWm8DDj9D1IFno_mZezTYduP-Ig",
+    },
+    Brand.KitchenAid: {
+        "client_id": "Kitchenaid_iOS",
+        "client_secret": "kkdPquOHfNH-iIinccTdhAkJmaIdWBhLehhLrfoXRWbKjEpqpdu92PISF_yJEWQs72D2yeC0PdoEKeWgHR9JRA",
+    },
+    Region.EU: {"base_url": "https://prod-api.whrcloud.eu"},
+    Region.US: {"base_url": "https://api.whrcloud.com"},
+}
+
+
+class BackendSelector:
+    def __init__(self, brand: Brand, region: Region):
+        self._brand = brand
+        self._region = region
+
+    @property
+    def brand(self):
+        return self._brand
+
+    @property
+    def region(self):
+        return self._region
+
+    @property
+    def base_url(self):
+        return BACKEND_DATA[self._region].get("base_url")
+
+    @property
+    def client_id(self):
+        return BACKEND_DATA[self._brand].get("client_id")
+
+    @property
+    def client_secret(self):
+        return BACKEND_DATA[self._brand].get("client_secret")
