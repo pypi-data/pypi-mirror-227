fdb322f9cf14e5a2abf9271edae07fa4a95e53db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-26 05:09:09 +0000

    Add package renput/0.0.3/renput-0.0.3.tar.gz

---
+++ packages/renput/renput-0.0.3.tar.gz/renput-0.0.3/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.3'
+DESCRIPTION = 'Tab-completion for inputs'
+with open('README.md', encoding='utf-8') as readme:
+    LONG_DESCRIPTION = readme.read()
+    
+# Setting up
+setup(
+    name="renput",
+    version=VERSION,
+    author="Jaegerwald (JaegerwaldDev)",
+    author_email="<jaegerwald.dev@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['tab', 'completion', 'auto-completion', 'tab-completion', 'input', 'modified input', 'windows', 'easy-to-use'],
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
