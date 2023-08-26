b6913ea0ea994ccda066a1f1dc6693d566a7d248
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-26 05:09:48 +0000

    Add package ZaidPy/0.1/ZaidPy-0.1.tar.gz

---
+++ packages/ZaidPy/ZaidPy-0.1.tar.gz/ZaidPy-0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+setup(
+    name= 'ZaidPy',
+    version='0.1',
+    author= 'Zaid',
+    description='Zaid program',
+    packages= ['ZaidPy'],
+    author_email='fhdhasd39@gmail.com',
+    zip_safe=False,
+    classifiers=[
+    "Programming Language :: Python :: 3",
+    "Operating System :: OS Independent",
+    "License :: OSI Approved :: MIT License"
+    ]
+)
