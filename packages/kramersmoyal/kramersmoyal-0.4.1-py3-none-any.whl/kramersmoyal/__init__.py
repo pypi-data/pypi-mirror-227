9f524bbd11cfc9ab58dc23a54a1f75bd4da1a45e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-26 04:08:32 +0000

    Add package kramersmoyal/0.4.1/kramersmoyal-0.4.1-py3-none-any.whl

---
+++ packages/kramersmoyal/kramersmoyal-0.4.1-py3-none-any.whl/kramersmoyal/__init__.py
@@ -0,0 +1,7 @@
+from .kernels import epanechnikov, silvermans_rule
+from .kmc import km
+
+__name__ = 'kramersmoyal'
+__version__ = '0.4.1'
+__author__ = 'Leonardo Rydin Gorjão and Francisco Meirinhos'
+__copyright__ = 'Copyright 2019–2023 Leonardo Rydin Gorjão, MIT License'
