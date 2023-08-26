29deb6306dc46535355f010d0649852d245f51bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-26 04:08:53 +0000

    Add package idem-azure/2.1.2/idem-azure-2.1.2.tar.gz

---
+++ packages/idem-azure/idem-azure-2.1.2.tar.gz/idem-azure-2.1.2/idem_azure/tool/azure/result_utils.py
@@ -0,0 +1,43 @@
+from typing import Any
+from typing import Dict
+
+
+def update_result_for_unsuccessful_operation(
+    hub,
+    result: Dict[str, Any],
+    resource_type: str,
+    name: str,
+    response: Dict[str, Any],
+    operation: str,
+):
+    result["result"] = False
+    result["comment"].append(
+        hub.tool.azure.comment_utils.could_not_do_operation_comment(
+            resource_type, name, operation
+        )
+    )
+
+    if response.get("comment"):
+        comment = response["comment"]
+        if isinstance(comment, str):
+            result["comment"].append(comment)
+        elif isinstance(comment, list):
+            result["comment"].extend(comment)
+
+    if response.get("ret"):
+        result["comment"].append(str(response["ret"]))
+
+    hub.log.debug(f"{result['comment']}")
+    return result
+
+
+def absent_implemented_through_wrapper_result(hub, resource_type: str, name: str):
+    return {
+        "comment": [
+            f"Invalid invocation! Absent method for {resource_type} is implemented by wrapper."
+        ],
+        "old_state": None,
+        "new_state": None,
+        "name": name,
+        "result": False,
+    }
