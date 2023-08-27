5499a0b927894c24cb64afb078822be2dcbd8359
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-27 03:10:28 +0000

    Add package example-pkg-1756907975/2.2.0/example_pkg_1756907975-2.2.0-py3-none-any.whl

---
+++ packages/example-pkg-1756907975/example_pkg_1756907975-2.2.0-py3-none-any.whl/pg_message/add.py
@@ -0,0 +1,18 @@
+import os
+from cffi import FFI
+
+def add():
+    ffi = FFI()
+    ffi.cdef("""
+        int add(int a, int b);
+    """)
+
+    # 获取当前模块的绝对路径
+    module_dir = os.path.dirname(os.path.abspath(__file__))
+
+    # 计算库文件的路径
+    dll_filename = "libffi_test.dll" if os.name == 'nt' else "libffi_test.so"
+    dll_path = os.path.join(module_dir, "..", "lib", dll_filename)
+
+    lib = ffi.dlopen(dll_path)
+    return lib.add(3, 4)
