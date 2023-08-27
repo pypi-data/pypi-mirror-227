62e0fa1d46a81415e24e283e967821ee31565a22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-27 03:09:00 +0000

    Add package herodotus-logger/1.1.1/herodotus_logger-1.1.1-py3-none-any.whl

---
+++ packages/herodotus-logger/herodotus_logger-1.1.1-py3-none-any.whl/herodotus/handlers/stream_handlers.py
@@ -0,0 +1,25 @@
+import copy
+from logging import StreamHandler, Formatter, LogRecord
+from typing import Callable
+
+
+class EnhancedStreamHandler(StreamHandler):
+    def __init__(
+            self,
+            stream=None,
+            level: int = 0,
+            strict_level: bool = False,
+            formatter: Formatter | None = None,
+            msg_func: Callable[[str], str] | None = None):
+        super().__init__(stream)
+        self.setFormatter(formatter)
+        self.setLevel(level)
+        self.strict_level = strict_level
+        self.msg_func = msg_func
+
+    def emit(self, record: LogRecord) -> None:
+        if not self.strict_level or record.levelno == self.level:
+            modified_record = copy.deepcopy(record)
+            if self.msg_func:
+                modified_record.msg = self.msg_func(modified_record.msg)
+            super().emit(modified_record)
