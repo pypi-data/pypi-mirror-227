d733b441c3a2c443277091bc7d345824ed1a0fb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-27 14:07:02 +0000

    Add package burla/0.3.1/burla-0.3.1.tar.gz

---
+++ packages/burla/burla-0.3.1.tar.gz/burla-0.3.1/src/burla/__init__.py
@@ -0,0 +1,14 @@
+from fire import Fire
+
+# Imported here to avoid cyclic imports
+_BURLA_SERVICE_URL = "https://burla-webservice-0-3-0-zqhes3whbq-uc.a.run.app"
+
+# _BURLA_SERVICE_URL = "http://127.0.0.1:5000"
+# _BURLA_SERVICE_URL = "https://burla-webservice-y66ufvpuua-uc.a.run.app"
+
+from burla._auth import login
+from burla._remote_parallel_map import remote_parallel_map
+
+
+def init_cli():
+    Fire({"login": login})
