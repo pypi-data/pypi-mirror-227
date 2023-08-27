e059ecc9861a4e1e0dd30a3b36ee885375d767c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-27 03:10:24 +0000

    Add package xia-composer/0.0.7/xia_composer-0.0.7-cp39-none-macosx_11_0_x86_64.whl

---
+++ packages/xia-composer/xia_composer-0.0.7-cp39-none-macosx_11_0_x86_64.whl/xia_composer/__init__.py
@@ -0,0 +1,23 @@
+from xia_composer.template import Template
+from xia_composer.target import Target
+from xia_composer.task import Task, Produce
+from xia_composer.mission import Mission
+from xia_composer.knowledge import KnowledgeNode
+from xia_composer.actor import Actor, MockActor
+from xia_composer.dialog import Dialog, Turn
+from xia_composer.campaign import Campaign
+from xia_composer.target import Target
+
+__all__ = [
+    "Template",
+    "Target",
+    "Task", "Produce",
+    "Mission",
+    "KnowledgeNode",
+    "Actor", "MockActor",
+    "Dialog", "Turn",
+    "Campaign",
+    "Target"
+]
+
+__version__ = "0.0.7"
