22645db9c89449cbe9e0d40ecf8f4f3b1fa4bb68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-27 14:06:58 +0000

    Add package monopyly/1.4.2/monopyly-1.4.2.tar.gz

---
+++ packages/monopyly/monopyly-1.4.2.tar.gz/monopyly-1.4.2/monopyly/static/js/modules/update-display-ajax.js
@@ -0,0 +1,26 @@
+/*
+ * Update an object with the results of an AJAX request.
+ *
+ * An object (`$display`) is updated using the response from an AJAX
+ * request. The request delivers a set of data (`rawData`) to the given
+ * endpoint. If nothing is returned, an error is logged. Various
+ * functions are defined below to peform different actions using the
+ * resuls of the request.
+ */
+
+import { executeAjaxRequest } from './ajax.js';
+
+
+function replaceDisplayContentsAjaxRequest(endpoint, rawData, $display) {
+
+	// The action is to replace the display's contents
+	function action(response) {
+		$display.html(response);
+	}
+	// Assign the response to the display object
+	executeAjaxRequest(endpoint, rawData, action);
+
+}
+
+
+export { replaceDisplayContentsAjaxRequest };
