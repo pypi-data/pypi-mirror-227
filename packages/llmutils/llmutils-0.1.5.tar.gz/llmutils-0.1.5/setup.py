d9b2407777b2335b887a55fa8f12cb6acd76a1bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-27 14:07:04 +0000

    Add package llmutils/0.1.5/llmutils-0.1.5.tar.gz

---
+++ packages/llmutils/llmutils-0.1.5.tar.gz/llmutils-0.1.5/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+import os
+
+setup(
+    name='llmutils',
+    version='0.1.5',
+    author='ttthree',
+    author_email='tongjie@gmail.com',
+    description='A few utility classes for working with LLMs',
+    packages=["llmutils"],
+    install_requires=[
+        'faiss-cpu',
+        'openai',
+        'jinja2',
+        'python-dotenv',
+        'tiktoken'
+    ],
+)
+
+print(find_packages(os.path.dirname(__file__)))
