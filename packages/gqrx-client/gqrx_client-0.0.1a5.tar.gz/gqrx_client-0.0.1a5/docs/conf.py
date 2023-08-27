339412436265bb94598526ea45c126fa483ca700
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-27 14:06:50 +0000

    Add package gqrx-client/0.0.1a5/gqrx_client-0.0.1a5.tar.gz

---
+++ packages/gqrx-client/gqrx_client-0.0.1a5.tar.gz/gqrx_client-0.0.1a5/docs/conf.py
@@ -0,0 +1,32 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Gqrx Client'
+copyright = "2023, Daniel 'Vector' Kerr"
+author = "Daniel 'Vector' Kerr"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['sphinx.ext.autodoc']
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'src'))
