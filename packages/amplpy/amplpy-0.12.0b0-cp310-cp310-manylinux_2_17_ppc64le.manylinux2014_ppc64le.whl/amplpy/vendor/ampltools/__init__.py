b72ba547a6d1cdef482773d46e1c8381d1ef3b7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-27 04:09:00 +0000

    Add package amplpy/0.12.0b0/amplpy-0.12.0b0-cp310-cp310-manylinux_2_17_ppc64le.manylinux2014_ppc64le.whl

---
+++ packages/amplpy/amplpy-0.12.0b0-cp310-cp310-manylinux_2_17_ppc64le.manylinux2014_ppc64le.whl/amplpy/vendor/ampltools/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+__version__ = "0.5.6"
+from .notebooks import (
+    ampl_notebook,
+)
+
+from .modules import (
+    activate as activate_license,
+)
+
+from .utils import (
+    cloud_platform_name,
+    register_magics,
+    add_to_path,
+)
+
+_SUPPORT_MESSAGE = """
+
+Please report any bugs at: https://github.com/ampl/amplpy
+
+For support/feedback go to https://discuss.ampl.com or e-mail <support@ampl.com>
+"""
