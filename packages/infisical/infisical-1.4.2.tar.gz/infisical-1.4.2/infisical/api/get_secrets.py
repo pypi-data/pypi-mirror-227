3f11afd0e5b149ca71db2ed61e88320ee11f4a0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-27 14:06:50 +0000

    Add package infisical/1.4.2/infisical-1.4.2.tar.gz

---
+++ packages/infisical/infisical-1.4.2.tar.gz/infisical-1.4.2/infisical/api/get_secrets.py
@@ -0,0 +1,24 @@
+from infisical.models.api import GetSecretsDTO, SecretsResponse
+from requests import Session
+
+
+def get_secrets_req(api_request: Session, options: GetSecretsDTO) -> SecretsResponse:
+    """Send request again Infisical API to fetch secrets.
+    See more information on https://infisical.com/docs/api-reference/endpoints/secrets/read
+
+    :param api_request: The :class:`requests.Session` instance used to perform the request
+    :param workspace_id: The ID of the workspace
+    :param environment: The environment
+    :return: Returns the API response as-is
+    """
+
+    response = api_request.get(
+        "/api/v3/secrets",
+        params={
+            "environment": options.environment,
+            "workspaceId": options.workspace_id,
+            "secretPath": options.path,
+        },
+    )
+
+    return SecretsResponse.parse_obj(response.json())
