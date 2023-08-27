36b98fb22e26c23e6128d0f3936cfec3c52341df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-27 14:07:03 +0000

    Add package isTrue/0.0.20/isTrue-0.0.20.tar.gz

---
+++ packages/isTrue/isTrue-0.0.20.tar.gz/isTrue-0.0.20/isTrue/isTrue.py
@@ -0,0 +1,12 @@
+def isTrue(var):
+    if var:
+        return True
+    return False
+
+    print("WHAT?")
+
+# this is for a test
+
+# I know its dumb
+
+# 0.0.13-1???????????????
---
+++ packages/isTrue/isTrue-0.0.20.tar.gz/isTrue-0.0.20/setup.py
@@ -0,0 +1,21 @@
+
+import setuptools
+
+
+setuptools.setup(
+    name="isTrue",
+    version="0.0.20",
+    description="WHAAAAAAAAAAAAAAAT???",
+    keywords="IDWHAAAAAAAAAAAAAAATWHAAAAAAAAAAAAAAATK???",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.4',
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: Other/Proprietary License",
+        "License :: Free To Use But Restricted",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+
+    ],
+)
