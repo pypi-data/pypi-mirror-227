8a861c917a3358912bb00ba03003a4f5e1ebbb52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-27 03:09:03 +0000

    Add package streamdeck-linux-gui/3.1.0/streamdeck_linux_gui-3.1.0-py3-none-any.whl

---
+++ packages/streamdeck-linux-gui/streamdeck_linux_gui-3.1.0-py3-none-any.whl/streamdeck_ui/config.py
@@ -0,0 +1,12 @@
+"""Defines shared configuration variables for the streamdeck_ui project"""
+import os
+
+PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
+LOGO = os.path.join(PROJECT_PATH, "logo.png")
+FONTS_PATH = os.path.join(PROJECT_PATH, "fonts", "roboto")
+DEFAULT_FONT = "Roboto-Regular.ttf"
+DEFAULT_FONT_SIZE = 14
+DEFAULT_FONT_COLOR = "white"
+DEFAULT_BACKGROUND_COLOR = "#000000"
+STATE_FILE = os.environ.get("STREAMDECK_UI_CONFIG", os.path.expanduser("~/.streamdeck_ui.json"))
+CONFIG_FILE_VERSION = 1  # Update only if backward incompatible changes are made to the config file
