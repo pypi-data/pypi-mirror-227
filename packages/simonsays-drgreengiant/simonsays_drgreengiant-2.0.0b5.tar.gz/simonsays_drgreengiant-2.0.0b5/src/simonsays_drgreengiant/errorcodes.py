9df9fab6718105f3bb6396dd5c13d17e4bf8ab75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-27 03:10:13 +0000

    Add package simonsays-drgreengiant/2.0.0b5/simonsays_drgreengiant-2.0.0b5.tar.gz

---
+++ packages/simonsays-drgreengiant/simonsays_drgreengiant-2.0.0b5.tar.gz/simonsays_drgreengiant-2.0.0b5/src/simonsays_drgreengiant/errorcodes.py
@@ -0,0 +1,44 @@
+#!./.venv/bin/python3
+import enum
+from collections.abc import Iterable
+
+
+@enum.unique
+class ErrorCode(enum.IntEnum):
+    """Error codes"""
+    OK = 0
+    UNKNOWN = enum.auto()
+    NOT_IMPLEMENTED = enum.auto()
+    LOOKUP_FAILURE = enum.auto()
+    DISABLED = enum.auto()
+    ON_COOLDOWN = enum.auto()
+    RANDOM_CHANCE = enum.auto()
+    BLOCKED_USER = enum.auto()
+    BLOCKED_CHANNEL = enum.auto()
+
+
+ErrorSet = set[ErrorCode]
+
+
+def flatten(xs):
+    """Flatten an iterable"""
+    for x in xs:
+        if isinstance(x, Iterable) and not isinstance(x, (str, bytes)):
+            yield from flatten(x)
+        else:
+            yield x
+
+
+def errorset(errors: Iterable[ErrorCode] | ErrorCode) -> ErrorSet:
+    """Make an error set"""
+    if isinstance(errors, Iterable):
+        return set(flatten(errors))
+    elif isinstance(errors, ErrorCode):
+        return set([errors])
+    else:
+        raise TypeError(f"Expected Iterable[ErrorCode] | ErrorCode, got {type(errors)}")
+
+
+def success(errors: ErrorSet) -> bool:
+    """Whether the error set is a success"""
+    return ErrorCode.OK in errors and len(errors) == 1
