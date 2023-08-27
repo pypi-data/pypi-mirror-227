3d37c6fd5395c4ddaa69f8e792c8c8a7e7c08108
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-27 03:10:17 +0000

    Add package neurum/1.2.7/neurum-1.2.7-py3-none-any.whl

---
+++ packages/neurum/neurum-1.2.7-py3-none-any.whl/neurum/draw.py
@@ -0,0 +1,9 @@
+from imagegen import Dream
+
+def draw(prompt : str, style : int, api_key : str):
+    if api_key=="vanshshah1029384756":
+        dream = Dream()
+        img = dream.generate(prompt, style)
+        return img.photo_url_list[0]
+    else:
+        return "invalid api key you dumbasssss!"
