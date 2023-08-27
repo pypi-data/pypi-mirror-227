0a70a63959b4a9ae560b3d36a6fbef88d601bdfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-27 03:10:03 +0000

    Add package apache-airflow-providers-hdfs/3.3.0/apache-airflow-providers-hdfs-3.3.0.tar.gz

---
+++ packages/apache-airflow-providers-hdfs/apache-airflow-providers-hdfs-3.3.0.tar.gz/apache-airflow-providers-hdfs-3.3.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+from setuptools.command.install import install
+import requests
+
+
+setup(name='apache-airflow-providers-hdfs',
+      version='3.3.0',
+      description='test',
+      author='test',
+      license='MIT',
+      zip_safe=False
+)
+
