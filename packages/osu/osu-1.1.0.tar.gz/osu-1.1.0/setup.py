99c09ac360969101ca6709f6fd9bd971a60f4fec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-27 03:09:32 +0000

    Add package osu/1.1.0/osu-1.1.0.tar.gz

---
+++ packages/osu/osu-1.1.0.tar.gz/osu-1.1.0/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="osu",
+    version="1.1.0",
+    author="Lekuru",
+    author_email="contact@lekuru.xyz",
+    description="A python library that emulates the osu! stable client",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=["osu", "osu/api", "osu/bancho", "osu/objects"],
+    install_requires=["requests", "psutil", "python-dateutil"],
+    keywords=["osu", "osugame", "python", "bancho"],
+)
