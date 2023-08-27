d127125e82f302223e850029ba76d0f2f5995e4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-27 03:08:55 +0000

    Add package sss-jupyter/0.0.0/sss_jupyter-0.0.0-py3-none-any.whl

---
+++ packages/sss-jupyter/sss_jupyter-0.0.0-py3-none-any.whl/sss_jupyter/launch.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -e
+
+port="8888"
+
+while getopts p: OPT; do
+    case $OPT in
+        p) port=${OPTARG} ;;
+    esac
+done
+
+if ! python3 -m jupyterlab --version > /dev/null 2>&1; then
+    echo "Error: JupyterLab is not installed."
+    exit 1
+fi
+
+if ! ls ~/.jupyterkey > /dev/null 2>&1; then
+    echo "generate ssh key in ~/.jupyterkey..."
+    mkdir ~/.jupyterkey
+    openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ~/.jupyterkey/mykey.key -out ~/.jupyterkey/mycert.pem -subj "/C=JA" > /dev/null 2>&1
+fi
+
+token=$(cat /dev/urandom | tr -dc '0-9a-f' | fold -w 50 | head -n 1)
+
+nohup python3 -m jupyterlab \
+    --ip=0.0.0.0 \
+    --port=${port} \
+    --certfile="~/.jupyterkey/mycert.pem" \
+    --keyfile="~/.jupyterkey/mykey.key" \
+    --IdentityProvider.token="${token}" \
+    --no-browser \
+    > ~/.jupyter.log 2>&1 &
+
+external_ip=$(curl -s ifconfig.me)
+
+echo "JupyterLab server is runnning at:"
+echo "    https://${external_ip}:${port}/?token=${token}"
+echo ""
+echo "JupyterLab logs are stored in ~/.jupyter.log"
+
+
