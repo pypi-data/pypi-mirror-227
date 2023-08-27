23b0d56b1d410b04b28f5998e97016e694835aa3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-27 14:06:40 +0000

    Add package chem-templates/0.0.7/chem_templates-0.0.7-py3-none-any.whl

---
+++ packages/chem-templates/chem_templates-0.0.7-py3-none-any.whl/chem_templates/imports.py
@@ -0,0 +1,16 @@
+import os
+from typing import Union, Optional, Callable, Tuple, Any
+from collections import OrderedDict, defaultdict
+from itertools import permutations, product, chain, islice
+import re
+import random
+import warnings
+import time
+import pickle
+import json
+from multiprocessing import Pool
+from functools import partial
+
+# external
+import numpy as np
+import pandas as pd
