555f28dee52b74844a47911ae75506d443b9f5af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-27 03:09:51 +0000

    Add package arcade/3.0.0.dev25/arcade-3.0.0.dev25.tar.gz

---
+++ packages/arcade/arcade-3.0.0.dev25.tar.gz/arcade-3.0.0.dev25/arcade/gui/ui_manager.py
@@ -0,0 +1,402 @@
+"""
+The better gui for arcade
+
+- Improved events, now fully typed
+- UIElements are now called Widgets (like everywhere else)
+- Widgets render into a FrameBuffer, which supports in memory drawings with less memory usage
+- Support for animated widgets
+- Texts are now rendered with pyglet, open easier support for text areas with scrolling
+- TextArea with scroll support
+"""
+from __future__ import annotations
+
+from collections import defaultdict
+from typing import List, Dict, TypeVar, Iterable, Optional, Type, Union
+from typing_extensions import TypeGuard
+
+from pyglet.event import EventDispatcher, EVENT_HANDLED, EVENT_UNHANDLED
+
+import arcade
+from arcade.gui.events import (
+    UIMouseMovementEvent,
+    UIMousePressEvent,
+    UIMouseReleaseEvent,
+    UIMouseScrollEvent,
+    UITextEvent,
+    UIMouseDragEvent,
+    UITextMotionEvent,
+    UITextMotionSelectEvent,
+    UIKeyPressEvent,
+    UIKeyReleaseEvent,
+    UIOnUpdateEvent,
+)
+from arcade.gui.surface import Surface
+from arcade.gui.widgets import UIWidget, Rect
+from arcade.camera import SimpleCamera
+
+W = TypeVar("W", bound=UIWidget)
+
+
+class UIManager(EventDispatcher):
+    """
+    UIManager is the central component within Arcade's GUI system.
+    Handles window events, layout process and rendering.
+
+    To process window events, :py:meth:`UIManager.enable()` has to be called,
+    which will inject event callbacks for all window events and redirects them through the widget tree.
+
+    If used within a view :py:meth:`UIManager.enable()` should be called from :py:meth:`View.on_show_view()` and
+    :py:meth:`UIManager.disable()` should be called from :py:meth:`View.on_hide_view()`
+
+    Supports `size_hint` to grow/shrink direct children dependent on window size.
+    Supports `size_hint_min` to ensure size of direct children (e.g. UIBoxLayout).
+    Supports `size_hint_max` to ensure size of direct children (e.g. UIBoxLayout).
+
+    .. code:: py
+
+        class MyView(arcade.View):
+            def __init__():
+                super().__init__()
+                manager = UIManager()
+
+                manager.add(Dummy())
+
+            def on_show_view(self):
+                # Set background color
+                self.window.background_color = arcade.color.DARK_BLUE_GRAY
+
+                # Enable UIManager when view is shown to catch window events
+                self.ui.enable()
+
+            def on_hide_view(self):
+                # Disable UIManager when view gets inactive
+                self.ui.disable()
+
+            def on_draw():
+                self.clear()
+
+                ...
+
+                manager.draw() # draws the UI on screen
+
+    """
+
+    _enabled = False
+
+    OVERLAY_LAYER = 10
+
+    def __init__(self, window: Optional[arcade.Window] = None):
+        super().__init__()
+        self.window = window or arcade.get_window()
+        self._surfaces: Dict[int, Surface] = {}
+        self.children: Dict[int, List[UIWidget]] = defaultdict(list)
+        self._rendered = False
+        #: Camera used when drawing the UI
+        self.camera = SimpleCamera()
+        self.register_event_type("on_event")
+
+    def add(self, widget: W, *, index=None, layer=0) -> W:
+        """
+        Add a widget to the :class:`UIManager`.
+        Added widgets will receive ui events and be rendered.
+
+        By default the latest added widget will receive ui events first and will be rendered on top of others.
+
+        The UIManager supports layered setups, widgets added to a higher layer are drawn above lower layers
+        and receive events first.
+        The layer 10 is reserved for overlaying components like dropdowns or tooltips.
+
+        :param widget: widget to add
+        :param index: position a widget is added, None has the highest priority
+        :param layer: layer which the widget should be added to, higher layer are above
+        :return: the widget
+        """
+        if index is None:
+            self.children[layer].append(widget)
+        else:
+            self.children[layer].insert(max(len(self.children), index), widget)
+        widget.parent = self
+        self.trigger_render()
+        return widget
+
+    def remove(self, child: UIWidget):
+        """
+        Removes the given widget from UIManager.
+
+        :param child: widget to remove
+        """
+        for children in self.children.values():
+            if child in children:
+                children.remove(child)
+                child.parent = None
+                self.trigger_render()
+
+    def walk_widgets(self, *, root: Optional[UIWidget] = None, layer=0) -> Iterable[UIWidget]:
+        """
+        walks through widget tree, in reverse draw order (most top drawn widget first)
+
+        :param root: root widget to start from, if None, the layer is used
+        :param layer: layer to search, None will search through all layers
+        """
+        if layer is None:
+            layers = sorted(self.children.keys(), reverse=True)
+        else:
+            layers = [layer]
+
+        for layer in layers:
+
+            children = root.children if root else self.children[layer]
+            for child in reversed(children):
+                yield from self.walk_widgets(root=child)
+                yield child
+
+    def clear(self):
+        """
+        Remove all widgets from UIManager
+        """
+        for layer in self.children.values():
+            for widget in layer[:]:
+                self.remove(widget)
+
+    def get_widgets_at(self, pos, cls: Type[W] = UIWidget, layer=0) -> Iterable[W]:
+        """
+        Yields all widgets containing a position, returns first top laying widgets which is instance of cls.
+
+        :param pos: Pos within the widget bounds
+        :param cls: class which the widget should be an instance of
+        :param layer: layer to search, None will search through all layers
+        :return: iterator of widgets of given type at position
+        """
+        def check_type(widget) -> TypeGuard[W]:
+            return isinstance(widget, cls)
+
+        for widget in self.walk_widgets(layer=layer):
+            if check_type(widget) and widget.rect.collide_with_point(*pos):
+                yield widget
+
+    def _get_surface(self, layer: int) -> Surface:
+        if layer not in self._surfaces:
+            if len(self._surfaces) > 2:
+                raise Exception("Don't use too much layers!")
+
+            self._surfaces[layer] = Surface(
+                size=self.window.get_size(),
+                pixel_ratio=self.window.get_pixel_ratio(),
+            )
+
+        return self._surfaces[layer]
+
+    def trigger_render(self):
+        """
+        Request rendering of all widgets
+        """
+        self._rendered = False
+
+    def _do_layout(self):
+        layers = sorted(self.children.keys())
+        for layer in layers:
+            surface = self._get_surface(layer)
+            if not surface:
+                raise ValueError("No surface exists for this layer.")
+            surface_width, surface_height = surface.size
+
+            for child in self.children[layer]:
+
+                if child.size_hint:
+                    sh_x, sh_y = child.size_hint
+                    nw = surface_width * sh_x if sh_x else None
+                    nh = surface_height * sh_y if sh_y else None
+                    child.rect = child.rect.resize(nw, nh)
+
+                if child.size_hint_min:
+                    shm_w, shm_h = child.size_hint_min
+                    child.rect = child.rect.min_size(shm_w or 0, shm_h or 0)
+
+                if child.size_hint_max:
+                    shm_w, shm_h = child.size_hint_max
+                    child.rect = child.rect.max_size(
+                        shm_w or child.width, shm_h or child.height
+                    )
+
+                child._do_layout()
+
+    def _do_render(self, force=False):
+        layers = sorted(self.children.keys())
+        force = force or not self._rendered
+        for layer in layers:
+            surface = self._get_surface(layer)
+
+            if surface is None:
+                raise ValueError("Surface is None for layer, can't render.")
+
+            with surface.activate():
+                if force:
+                    surface.clear()
+
+                for child in self.children[layer]:
+                    child._do_render(surface, force)
+
+        self._rendered = True
+
+    def enable(self) -> None:
+        """
+        Registers handler functions (`on_...`) to :py:attr:`arcade.gui.UIElement`
+
+        on_draw is not registered, to provide full control about draw order,
+        so it has to be called by the devs themselves.
+
+        Within a view, this method should be called from :py:meth:`arcade.View.on_show_view()`.
+        """
+        if not self._enabled:
+            self._enabled = True
+            self.window.push_handlers(
+                self.on_resize,
+                self.on_update,
+                self.on_mouse_drag,
+                self.on_mouse_motion,
+                self.on_mouse_press,
+                self.on_mouse_release,
+                self.on_mouse_scroll,
+                self.on_key_press,
+                self.on_key_release,
+                self.on_text,
+                self.on_text_motion,
+                self.on_text_motion_select,
+            )
+
+    def disable(self) -> None:
+        """
+        Remove handler functions (`on_...`) from :py:attr:`arcade.Window`
+
+        If every :py:class:`arcade.View` uses its own :py:class:`arcade.gui.UIManager`,
+        this method should be called in :py:meth:`arcade.View.on_hide_view()`.
+        """
+        if self._enabled:
+            self._enabled = False
+            self.window.remove_handlers(
+                self.on_resize,
+                self.on_update,
+                self.on_mouse_drag,
+                self.on_mouse_motion,
+                self.on_mouse_press,
+                self.on_mouse_release,
+                self.on_mouse_scroll,
+                self.on_key_press,
+                self.on_key_release,
+                self.on_text,
+                self.on_text_motion,
+                self.on_text_motion_select,
+            )
+
+    def on_update(self, time_delta):
+        return self.dispatch_ui_event(UIOnUpdateEvent(self, time_delta))
+
+    def draw(self) -> None:
+        # Request Widgets to prepare for next frame
+        self._do_layout()
+
+        ctx = self.window.ctx
+        with ctx.enabled(ctx.BLEND):
+            self._do_render()
+
+        # Draw layers
+        self.camera.use()
+        with ctx.enabled(ctx.BLEND):
+            layers = sorted(self.children.keys())
+            for layer in layers:
+                self._get_surface(layer).draw()
+
+    def adjust_mouse_coordinates(self, x, y):
+        """
+        This method is used, to translate mouse coordinates to coordinates
+        respecting the viewport and projection of cameras.
+        The implementation should work in most common cases.
+
+        If you use scrolling in the :py:class:`arcade.Camera` you have to reset scrolling
+        or overwrite this method using the camera conversion::
+
+            ui_manager.adjust_mouse_coordinates = camera.mouse_coordinates_to_world
+        """
+        # NOTE: Only support scrolling until cameras support transforming
+        #       mouse coordinates
+        px, py = self.camera.position
+        return x + px, y + py
+
+    def on_event(self, event) -> Union[bool, None]:
+        layers = sorted(self.children.keys(), reverse=True)
+        for layer in layers:
+            for child in reversed(self.children[layer]):
+                if child.dispatch_event("on_event", event):
+                    # child can consume an event by returning True
+                    return EVENT_HANDLED
+        return EVENT_UNHANDLED
+
+    def dispatch_ui_event(self, event):
+        return self.dispatch_event("on_event", event)
+
+    def on_mouse_motion(self, x: float, y: float, dx: float, dy: float):
+        x, y = self.adjust_mouse_coordinates(x, y)
+        return self.dispatch_ui_event(UIMouseMovementEvent(self, x, y, dx, dy))  # type: ignore
+
+    def on_mouse_press(self, x: float, y: float, button: int, modifiers: int):
+        x, y = self.adjust_mouse_coordinates(x, y)
+        return self.dispatch_ui_event(UIMousePressEvent(self, x, y, button, modifiers))  # type: ignore
+
+    def on_mouse_drag(
+        self, x: float, y: float, dx: float, dy: float, buttons: int, modifiers: int
+    ):
+        x, y = self.adjust_mouse_coordinates(x, y)
+        return self.dispatch_ui_event(UIMouseDragEvent(self, x, y, dx, dy, buttons, modifiers))  # type: ignore
+
+    def on_mouse_release(self, x: float, y: float, button: int, modifiers: int):
+        x, y = self.adjust_mouse_coordinates(x, y)
+        return self.dispatch_ui_event(UIMouseReleaseEvent(self, x, y, button, modifiers))  # type: ignore
+
+    def on_mouse_scroll(self, x, y, scroll_x, scroll_y):
+        x, y = self.adjust_mouse_coordinates(x, y)
+        return self.dispatch_ui_event(
+            UIMouseScrollEvent(self, x, y, scroll_x, scroll_y)
+        )
+
+    def on_key_press(self, symbol: int, modifiers: int):
+        return self.dispatch_ui_event(UIKeyPressEvent(self, symbol, modifiers))  # type: ignore
+
+    def on_key_release(self, symbol: int, modifiers: int):
+        return self.dispatch_ui_event(UIKeyReleaseEvent(self, symbol, modifiers))  # type: ignore
+
+    def on_text(self, text):
+        return self.dispatch_ui_event(UITextEvent(self, text))
+
+    def on_text_motion(self, motion):
+        return self.dispatch_ui_event(UITextMotionEvent(self, motion))
+
+    def on_text_motion_select(self, motion):
+        return self.dispatch_ui_event(UITextMotionSelectEvent(self, motion))
+
+    def on_resize(self, width, height):
+        scale = self.window.get_pixel_ratio()
+        self.camera.resize(width, height)
+
+        for surface in self._surfaces.values():
+            surface.resize(size=(width, height), pixel_ratio=scale)
+
+        self.trigger_render()
+
+    @property
+    def rect(self) -> Rect:  # type: ignore
+        return Rect(0, 0, *self.window.get_size())
+
+    def debug(self):
+        """Walks through all widgets of a UIManager and prints out the rect"""
+        for index, layer in self.children.items():
+            print(f"Layer {index}")
+            for child in reversed(layer):
+                self._debug(child, prefix="  ")
+        return
+
+    @staticmethod
+    def _debug(element, prefix=""):
+        print(f"{prefix}{element.__class__}:{element.rect}")
+        if isinstance(element, UIWidget):
+            for child in element.children:
+                UIManager._debug(child, prefix=prefix + "  ")
---
+++ packages/arcade/arcade-3.0.0.dev25.tar.gz/arcade-3.0.0.dev25/arcade/gui/widgets/slider.py
@@ -0,0 +1,262 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import Optional, Tuple, Union, Mapping
+
+from pyglet.event import EVENT_UNHANDLED
+
+import arcade
+from arcade.math import get_distance
+from arcade.types import Color, RGBA255
+from arcade.gui import (
+    Surface,
+    UIEvent,
+    UIMouseMovementEvent,
+    UIMouseDragEvent,
+    UIMousePressEvent,
+    UIMouseReleaseEvent,
+)
+from arcade.gui.events import UIOnChangeEvent
+from arcade.gui.property import Property, bind
+from arcade.gui.style import UIStyleBase, UIStyledWidget
+
+@dataclass
+class UISliderStyle(UIStyleBase):
+    """
+    Used to style the slider for different states. Below is its use case.
+
+    .. code:: py
+
+        button = UITextureButton(style={"normal": UITextureButton.UIStyle(...),})
+    """
+    bg: RGBA255 = Color(94, 104, 117)
+    border: RGBA255 = Color(77, 81, 87)
+    border_width: int = 1
+    filled_bar: RGBA255 = Color(50, 50, 50)
+    unfilled_bar: RGBA255 = Color(116, 125, 123)
+
+
+class UISlider(UIStyledWidget[UISliderStyle]):
+    """
+    A simple horizontal slider. The value of the slider can be set by moving the cursor(indicator).
+
+    There are four states of the UISlider i.e normal, hovered, pressed and disabled.
+
+    :param value: Current value of the curosr of the slider.
+    :param min_value: Minimum value of the slider.
+    :param max_value: Maximum value of the slider.
+    :param x: x coordinate of bottom left.
+    :param y: y coordinate of bottom left.
+    :param width: Width of the slider.
+    :param height: Height of the slider.
+    :param Mapping[str, "UISlider.UIStyle"] | None style: Used to style the slider for different states.
+
+    """
+
+    value = Property(0)
+    hovered = Property(False)
+    pressed = Property(False)
+    disabled = Property(False)
+
+    UIStyle = UISliderStyle
+
+    DEFAULT_STYLE = {
+        "normal": UIStyle(),
+        "hover": UIStyle(
+            bg=Color(96, 103, 112),
+            border=Color(77, 81, 87),
+            border_width=2,
+            filled_bar=Color(50, 50, 50),
+            unfilled_bar=Color(116, 125, 123),
+        ),
+        "press": UIStyle(
+            bg=Color(96, 103, 112),
+            border=Color(77, 81, 87),
+            border_width=3,
+            filled_bar=Color(50, 50, 50),
+            unfilled_bar=Color(116, 125, 123),
+        ),
+        "disabled": UIStyle(
+            bg=Color(94, 104, 117),
+            border=Color(77, 81, 87),
+            border_width=1,
+            filled_bar=Color(50, 50, 50),
+            unfilled_bar=Color(116, 125, 123),
+        )
+    }
+
+    def __init__(
+        self,
+        *,
+        value: float = 0,
+        min_value: float = 0,
+        max_value: float = 100,
+        x: float = 0,
+        y: float = 0,
+        width: float = 300,
+        height: float = 20,
+        size_hint=None,
+        size_hint_min=None,
+        size_hint_max=None,
+        style: Union[Mapping[str, UISliderStyle], None] = None,
+        **kwargs,
+    ):
+        super().__init__(
+            x=x,
+            y=y,
+            width=width,
+            height=height,
+            size_hint=size_hint,
+            size_hint_min=size_hint_min,
+            size_hint_max=size_hint_max,
+            style=style or UISlider.DEFAULT_STYLE,
+            **kwargs,
+        )
+
+        self.value = value
+        self.vmin = min_value
+        self.vmax = max_value
+
+        self.cursor_radius = self.height // 3
+
+        # trigger render on value changes
+        bind(self, "value", self.trigger_full_render)
+        bind(self, "hovered", self.trigger_render)
+        bind(self, "pressed", self.trigger_render)
+        bind(self, "disabled", self.trigger_render)
+
+        self.register_event_type("on_change")
+
+    def get_current_state(self) -> str:
+        """Returns the current state of the slider i.e disabled, press, hover or normal."""
+        if self.disabled:
+            return "disabled"
+        elif self.pressed:
+            return "press"
+        elif self.hovered:
+            return "hover"
+        else:
+            return "normal"
+
+    def _x_for_value(self, value: float):
+        x = self.content_rect.x
+        nval = (value - self.vmin) / self.vmax
+        return (
+            x
+            + self.cursor_radius
+            + nval * (self.content_width - 2 * self.cursor_radius)
+        )
+
+    @property
+    def norm_value(self):
+        """Normalized value between 0.0 and 1.0"""
+        return (self.value - self.vmin) / self.vmax
+
+    @norm_value.setter
+    def norm_value(self, value):
+        """Normalized value between 0.0 and 1.0"""
+        self.value = min(value * (self.vmax - self.vmin) + self.vmin, self.vmax)
+
+    @property
+    def value_x(self):
+        """Returns the current value of the cursor of the slider."""
+        return self._x_for_value(self.value)
+
+    @value_x.setter
+    def value_x(self, nx):
+        cr = self.content_rect
+
+        x = min(cr.right - self.cursor_radius, max(nx, cr.x + self.cursor_radius))
+        if self.width == 0:
+            self.norm_value = 0
+        else:
+            self.norm_value = (x - cr.x - self.cursor_radius) / float(
+                self.content_width - 2 * self.cursor_radius
+            )
+
+    def do_render(self, surface: Surface):
+        style = self.get_current_style()
+
+        self.prepare_render(surface)
+
+        # TODO accept constructor params
+        slider_height = self.content_height // 4
+        cursor_radius = self.cursor_radius
+
+        slider_left_x = self._x_for_value(self.vmin)
+        slider_right_x = self._x_for_value(self.vmax)
+        cursor_center_x = self.value_x
+
+        slider_bottom = (self.content_height - slider_height) // 2
+        slider_center_y = self.content_height // 2
+
+        # slider
+        bg_slider_color = style.get("unfilled_bar", UISlider.UIStyle.unfilled_bar)
+        fg_slider_color = style.get("filled_bar", UISlider.UIStyle.filled_bar)
+
+        arcade.draw_xywh_rectangle_filled(
+            slider_left_x - self.content_rect.x,
+            slider_bottom,
+            slider_right_x - slider_left_x,
+            slider_height,
+            bg_slider_color,
+        )
+        arcade.draw_xywh_rectangle_filled(
+            slider_left_x - self.content_rect.x,
+            slider_bottom,
+            cursor_center_x - slider_left_x,
+            slider_height,
+            fg_slider_color,
+        )
+
+        # cursor
+        border_width = style.get("border_width", UISlider.UIStyle.border_width)
+        cursor_color = style.get("bg", UISlider.UIStyle.bg)
+        cursor_outline_color = style.get("border", UISlider.UIStyle.border)
+
+        rel_cursor_x = cursor_center_x - self.content_rect.x
+        arcade.draw_circle_filled(
+            rel_cursor_x, slider_center_y, cursor_radius, cursor_color
+        )
+        arcade.draw_circle_filled(
+            rel_cursor_x, slider_center_y, cursor_radius // 4, cursor_outline_color
+        )
+        arcade.draw_circle_outline(
+            rel_cursor_x,
+            slider_center_y,
+            cursor_radius,
+            cursor_outline_color,
+            border_width,
+        )
+
+    def _cursor_pos(self) -> Tuple[float, float]:
+        return self.value_x, int(self.y + self.height // 2)
+
+    def _is_on_cursor(self, x: float, y: float) -> bool:
+        cursor_center_x, cursor_center_y = self._cursor_pos()
+        cursor_radius = self.cursor_radius
+        distance_to_cursor = get_distance(x, y, cursor_center_x, cursor_center_y)
+        return distance_to_cursor <= cursor_radius
+
+    def on_event(self, event: UIEvent) -> Optional[bool]:
+        if isinstance(event, UIMouseMovementEvent):
+            self.hovered = self._is_on_cursor(event.x, event.y)
+
+        if isinstance(event, UIMouseDragEvent):
+            if self.pressed:
+                old_value = self.value
+                self.value_x = event.x
+                self.dispatch_event("on_change", UIOnChangeEvent(self, old_value, self.value))  # type: ignore
+
+        if isinstance(event, UIMousePressEvent):
+            if self._is_on_cursor(event.x, event.y):
+                self.pressed = True
+
+        if isinstance(event, UIMouseReleaseEvent):
+            self.pressed = False
+
+        return EVENT_UNHANDLED
+
+    def on_change(self, event: UIOnChangeEvent):
+        """To be implemented by the user, triggered when the cursor's value is changed."""
+        pass
