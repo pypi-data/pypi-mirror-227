326b11ac89e390289c0f47b98326203b89a73d36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-27 03:09:53 +0000

    Add package sumonet/0.0.5/sumonet-0.0.5.tar.gz

---
+++ packages/sumonet/sumonet-0.0.5.tar.gz/sumonet-0.0.5/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='sumonet',
+    version='0.0.5',
+    license='Academic Free License ("AFL") v. 3.0',
+    author="Berke Dilekoglu",
+    author_email='dilekogluberke@gmail.com',
+    packages=find_packages(),
+    url='https://github.com/berkedilekoglu/SUMOnet',
+    download_url = 'https://github.com/berkedilekoglu/SUMOnet/archive/refs/tags/v0.0.5tar.gz', 
+    keywords='sumoylation machine-learning deep-learning',
+    install_requires=[
+        'numpy',
+        'scikit-learn',
+        'joblib',
+        'pandas',
+        'epitopepredict',
+        'tensorflow',
+        'keras',
+      ],
+
+)
