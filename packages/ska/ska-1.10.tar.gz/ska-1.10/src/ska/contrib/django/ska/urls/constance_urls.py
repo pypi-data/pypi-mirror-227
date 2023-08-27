60674e2727da5ef41c2b7615338c51dd5f4f26e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-27 04:09:14 +0000

    Add package ska/1.10/ska-1.10.tar.gz

---
+++ packages/ska/ska-1.10.tar.gz/ska-1.10/src/ska/contrib/django/ska/urls/constance_urls.py
@@ -0,0 +1,13 @@
+from django.urls import re_path as url
+
+from ..views import constance_login
+
+__author__ = "Artur Barseghyan"
+__copyright__ = "2013-2021 Artur Barseghyan"
+__license__ = "GPL 2.0/LGPL 2.1"
+__all__ = ("urlpatterns",)
+
+
+urlpatterns = [
+    url(r"^login/", constance_login, name="ska.login"),
+]
