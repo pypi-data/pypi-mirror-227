3dfe3613d5b49da4d932b894ac0de8348cd6e8fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-27 03:10:16 +0000

    Add package formal-sdk/1.0.14/formal-sdk-1.0.14.tar.gz

---
+++ packages/formal-sdk/formal-sdk-1.0.14.tar.gz/formal-sdk-1.0.14/src/formal_sdk/gen/admin/v1/permissions_pb2.pyi
@@ -0,0 +1,17 @@
+from google.protobuf import any_pb2 as _any_pb2
+from .types.v1 import policy_pb2 as _policy_pb2
+from .types.v1 import list_metadata_pb2 as _list_metadata_pb2
+from ...validate.v1 import validate_pb2 as _validate_pb2
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from typing import ClassVar as _ClassVar
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class GetPermissionsRequest(_message.Message):
+    __slots__ = []
+    def __init__(self) -> None: ...
+
+class GetPermissionsResponse(_message.Message):
+    __slots__ = []
+    def __init__(self) -> None: ...
