232b86600e3a646c752ac53c1989c6ccb8c4df77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-27 14:07:02 +0000

    Add package nonebot-plugin-simple-httpcat/0.0.1/nonebot-plugin-simple-httpcat-0.0.1.tar.gz

---
+++ packages/nonebot-plugin-simple-httpcat/nonebot-plugin-simple-httpcat-0.0.1.tar.gz/nonebot-plugin-simple-httpcat-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="nonebot-plugin-simple-httpcat",
+    version="0.0.1",
+    packages=["nonebot_plugin_simple_httpcat"],
+    install_requires=["nonebot2","nonebot-adapter-onebot"],
+    description="适用于nonebot2 v11、简单粗暴的httpcat插件 调用https://http.cat的图片",
+    author="NightWind",
+    author_email="2125714976@qq.com",
+    url="https://github.com/XTxiaoting14332/nonebot-plugin-simple-httpcat",
+)
