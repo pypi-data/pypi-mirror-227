edd8b8c8ef2459020a41936a3756564db4ae546e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-27 14:06:59 +0000

    Add package pyncos-module/0.0.13/pyncos-module-0.0.13.tar.gz

---
+++ packages/pyncos-module/pyncos-module-0.0.13.tar.gz/pyncos-module-0.0.13/setup.py
@@ -0,0 +1,28 @@
+#-*- coding:utf-8 -*-
+
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+ 
+setuptools.setup(
+    name="pyncos-module", 
+    version="0.0.13",   
+    author="xuwh",  
+    author_email="xuwhdev@gmail.com", 
+    description="", 
+    long_description=long_description, 
+    long_description_content_type="text/markdown",
+    url="https://gitee.com/zwsjz/carbot-module-lib", 
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.10',
+    install_requires=[
+      'pyserial>=3.5',
+      'paho-mqtt>=1.6.1'
+    ]
+)
