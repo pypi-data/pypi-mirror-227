ead3ab57978171cdc7fd98431a590d85eb621a25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-27 04:09:04 +0000

    Add package Rawan-aq11/0.1/Rawan-aq11-0.1.tar.gz

---
+++ packages/Rawan-aq11/Rawan-aq11-0.1.tar.gz/Rawan-aq11-0.1/setup.py
@@ -0,0 +1,8 @@
+import setuptools
+setuptools.setup(
+    name='Rawan-aq11', 
+    version='0.1',
+    description='simble calculator ',
+    author='Rawan aqlan',
+    packages=setuptools.find_packages()
+)
