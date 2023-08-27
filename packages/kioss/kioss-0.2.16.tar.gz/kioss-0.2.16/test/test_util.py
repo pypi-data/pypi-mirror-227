30106706f64be1c3546ac2381290ba98065c01eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-27 03:09:12 +0000

    Add package kioss/0.2.16/kioss-0.2.16.tar.gz

---
+++ packages/kioss/kioss-0.2.16.tar.gz/kioss-0.2.16/test/test_util.py
@@ -0,0 +1,25 @@
+import unittest
+from queue import Queue
+
+from kioss.util import QueueIterator, sidify
+
+
+class TestUtil(unittest.TestCase):
+    def test_sidify(self):
+        f = lambda x: x**2
+        self.assertEqual(f(2), 4)
+        self.assertEqual(sidify(f)(2), 2)
+
+        @sidify
+        def f(x):
+            return x**2
+
+        self.assertEqual(f(2), 2)
+
+    def test_queue_iterator(self):
+        queue = Queue()
+        it = QueueIterator(queue, None)
+        for i in range(8):
+            queue.put(i)
+        queue.put(None)
+        self.assertListEqual(list(it), list(range(8)))
