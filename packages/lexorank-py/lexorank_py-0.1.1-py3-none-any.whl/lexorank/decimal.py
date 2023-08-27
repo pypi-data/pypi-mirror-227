f00897e2f7c83780748d390309caf905269a2217
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-27 03:09:07 +0000

    Add package lexorank-py/0.1.1/lexorank_py-0.1.1-py3-none-any.whl

---
+++ packages/lexorank-py/lexorank_py-0.1.1-py3-none-any.whl/lexorank/decimal.py
@@ -0,0 +1,175 @@
+from typing_extensions import Self
+
+from lexorank import integer
+from lexorank.base import Base, Base36
+from lexorank.integer import Integer, Sign
+
+DECIMAL_POINT = ":"
+
+
+class Decimal:
+    def __init__(
+        self, significant_figures: Integer, exponent: int, *, decimal_point: str = DECIMAL_POINT
+    ) -> None:
+        self._integer, self._exponent = self._rstrip(significant_figures, exponent)
+        self._decimal_point = decimal_point
+
+    @property
+    def base(self) -> Base:
+        return self._integer.base
+
+    @property
+    def decimal_point(self) -> str:
+        return self._decimal_point
+
+    def __neg__(self) -> Self:
+        return self.__class__(-self._integer, self._exponent)
+
+    def __abs__(self) -> Self:
+        return self.__class__(abs(self._integer), self._exponent)
+
+    def __add__(self, other: object) -> Self:
+        other = self._type_guard(other)
+
+        if self._exponent == other._exponent:
+            return self.__class__(self._integer + other._integer, self._exponent)
+
+        lv: Decimal = self
+        rv = other
+        if self._exponent < other._exponent:
+            lv, rv = rv, lv
+
+        diff = lv._exponent - rv._exponent
+        lv_integer = lv._integer << diff
+        lv_exponent = lv._exponent - diff
+
+        return self.__class__(lv_integer + rv._integer, lv_exponent)
+
+    def __sub__(self, other: object) -> Self:
+        other = self._type_guard(other)
+
+        return self + (-other)
+
+    def __mul__(self, other: object) -> Self:
+        other = self._type_guard(other)
+
+        return self.__class__(self._integer * other._integer, self._exponent + other._exponent)
+
+    def __eq__(self, other: object) -> bool:
+        other = self._type_guard(other)
+
+        return self._integer == other._integer and self._exponent == other._exponent
+
+    def __gt__(self, other: object) -> bool:
+        other = self._type_guard(other)
+
+        if self._exponent == other._exponent:
+            return self._integer > other._integer
+        if self._exponent > other._exponent:
+            return self._integer << (self._exponent - other._exponent) > other._integer
+        return self._integer > other._integer << (other._exponent - self._exponent)
+
+    def __ge__(self, other: object):
+        return self.__gt__(other) or self.__eq__(other)
+
+    def __lt__(self, other: object):
+        return not self.__ge__(other)
+
+    def __le__(self, other: object):
+        return not self.__gt__(other)
+
+    def whole_number(self) -> Integer:
+        if self._exponent < 0:
+            return self._integer >> abs(self._exponent)
+        return self._integer << self._exponent
+
+    def decimal(self) -> "Decimal":
+        return self - self.whole_number()
+
+    def __str__(self) -> str:
+        sign = "" if self._integer.sign == Sign.POSITIVE else "-"
+        intger_str = str(abs(self._integer))
+        if self._exponent < 0:
+            if len(intger_str) <= abs(self._exponent):
+                intger_str = "0" * (abs(self._exponent) - len(intger_str) + 1) + intger_str
+            return (
+                sign
+                + intger_str[: self._exponent]
+                + self._decimal_point
+                + intger_str[self._exponent :]
+            )
+        if self._exponent > 0:
+            return sign + intger_str + "0" * self._exponent + self._decimal_point
+        return sign + intger_str + self._decimal_point
+
+    def __repr__(self) -> str:
+        return f"<{self.__class__.__name__} value={self}, base={self.base.base()}>"
+
+    def __int__(self) -> int:
+        return int(self.whole_number())
+
+    def __float__(self) -> float:
+        return float(int(self._integer) * (self.base.base() ** self._exponent))
+
+    def __len__(self) -> int:
+        return len(self._integer)
+
+    @staticmethod
+    def _rstrip(value: Integer, exponent: int) -> tuple[Integer, int]:
+        index = 0
+        for digit in reversed(value.digits):
+            if digit != 0:
+                break
+            index += 1
+
+        return value >> index, exponent + index
+
+    def _type_guard(self, other: object) -> "Decimal":
+        if isinstance(other, (int, str, float, Integer)):
+            return parse(other, self.base)
+        if isinstance(other, Decimal):
+            return other
+        raise ValueError(f"unsupported operand type(s) {type(other)}")
+
+
+def parse(
+    value: str | int | float | Integer, base: Base = Base36, *, decimal_point: str = DECIMAL_POINT
+) -> Decimal:
+    if isinstance(value, int):
+        return Decimal(integer.parse(value, base), 0, decimal_point=decimal_point)
+
+    if isinstance(value, float):
+        # WARN: This is not accurate.
+        whole = integer.parse(int(value), base)
+        if len(whole) >= 10 or value == 0:
+            return Decimal(whole, 0, decimal_point=decimal_point)
+        if abs(value) >= 1:
+            exponent = len(whole) - 10
+        else:
+            # TODO: more accurate
+            exponent = -9
+            v = abs(value)
+            while True:
+                v *= base.base()
+                exponent -= 1
+                if v >= 1:
+                    break
+                if exponent <= -150:
+                    break
+        return Decimal(
+            integer.parse(int(value * (base.base() ** -exponent)), base),
+            exponent,
+            decimal_point=decimal_point,
+        )
+
+    if isinstance(value, Integer):
+        return Decimal(value, 0, decimal_point=decimal_point)
+
+    try:
+        index = value.index(decimal_point)
+    except ValueError:
+        return Decimal(integer.parse(value, base), 0, decimal_point=decimal_point)
+
+    integer_str = value[:index] + value[index + 1 :]
+    exponent = index - len(value) + 1
+    return Decimal(integer.parse(integer_str, base), exponent, decimal_point=decimal_point)
---
+++ packages/lexorank-py/lexorank_py-0.1.1-py3-none-any.whl/lexorank/lexorank.py
@@ -0,0 +1,199 @@
+from enum import IntEnum
+from functools import lru_cache
+
+from typing_extensions import Self
+
+from lexorank import decimal
+from lexorank.base import Base, Base36
+from lexorank.decimal import DECIMAL_POINT, Decimal
+from lexorank.integer import Integer
+
+BUCKET_SEPARATOR = "|"
+WHOLE_NUMBER_SIZE = 6
+
+
+class Bucket(IntEnum):
+    BUCEKT_0 = 0
+    BUCKET_1 = 1
+    BUCKET_2 = 2
+
+    def next(self) -> Self:
+        return self.__class__((self.value + 1) % 3)
+
+
+class LexoRank:
+    def __init__(
+        self,
+        bucekt: Bucket,
+        rank: Decimal,
+        *,
+        bucket_separator: str = BUCKET_SEPARATOR,
+        whole_number_size: int = WHOLE_NUMBER_SIZE,
+    ) -> None:
+        self._bucket = bucekt
+        self._rank = rank
+        self._bucket_separator = bucket_separator
+        self._whole_number_size = whole_number_size
+
+    @property
+    def bucket(self) -> Bucket:
+        return self._bucket
+
+    @property
+    def rank(self) -> Decimal:
+        return self._rank
+
+    def prev(self, step: int = 16) -> Self:
+        return self.next(-step)
+
+    def next(self, step: int = 16) -> Self:
+        return self.__class__(
+            self._bucket,
+            Decimal(self._rank.whole_number(), 0, decimal_point=self._rank.decimal_point) + step,
+            bucket_separator=self._bucket_separator,
+            whole_number_size=self._whole_number_size,
+        )
+
+    def __add__(self, other: object) -> Self:
+        other = self._type_guard(other)
+
+        return self.__class__(
+            self._bucket,
+            self._rank + other._rank,
+            bucket_separator=self._bucket_separator,
+            whole_number_size=self._whole_number_size,
+        )
+
+    def __sub__(self, other: object) -> Self:
+        other = self._type_guard(other)
+
+        return self.__class__(
+            self._bucket,
+            self._rank - other._rank,
+            bucket_separator=self._bucket_separator,
+            whole_number_size=self._whole_number_size,
+        )
+
+    def __mul__(self, other: object) -> Self:
+        other = self._type_guard(other)
+
+        return self.__class__(
+            self._bucket,
+            self._rank * other._rank,
+            bucket_separator=self._bucket_separator,
+            whole_number_size=self._whole_number_size,
+        )
+
+    def __eq__(self, other: object) -> bool:
+        other = self._type_guard(other)
+
+        return self._bucket == other._bucket and self._rank == other._rank
+
+    def __gt__(self, other: object) -> bool:
+        other = self._type_guard(other)
+
+        if self._bucket.value > other._bucket.value:
+            return True
+        if self._bucket.value < other._bucket.value:
+            return False
+        return self._rank > other._rank
+
+    def __ge__(self, other: object):
+        return self.__gt__(other) or self.__eq__(other)
+
+    def __lt__(self, other: object):
+        return not self.__ge__(other)
+
+    def __le__(self, other: object):
+        return not self.__gt__(other)
+
+    def __str__(self) -> str:
+        rank = str(self._rank)
+        index = rank.index(self._rank.decimal_point)
+
+        whole = rank[:index].zfill(self._whole_number_size)
+        rank = rank[index + 1 :]
+
+        return (
+            f"{self._bucket.value}{self._bucket_separator}{whole}{self._rank.decimal_point}{rank}"
+        )
+
+    def __repr__(self) -> str:
+        return f"<{self.__class__.__name__} value={self} base={self._rank.base.base()}>"
+
+    def __float__(self) -> float:
+        return float(self._rank)
+
+    def __int__(self) -> int:
+        return int(self._rank)
+
+    def _type_guard(self, other: object) -> "LexoRank":
+        if isinstance(other, LexoRank):
+            return other
+        if isinstance(other, str):
+            return parse(
+                other,
+                self._rank.base,
+                decimal_point=self._rank.decimal_point,
+                bucket_separator=self._bucket_separator,
+                whole_number_size=self._whole_number_size,
+            )
+        if isinstance(other, (int, float, Integer)):
+            return LexoRank(self._bucket, decimal.parse(other, self._rank.base))
+        if isinstance(other, Decimal):
+            return LexoRank(self._bucket, other)
+        raise ValueError(f"unsupported operand type(s) {type(other)}")
+
+
+def parse(
+    value: str,
+    base: Base = Base36,
+    *,
+    decimal_point: str = DECIMAL_POINT,
+    bucket_separator: str = BUCKET_SEPARATOR,
+    whole_number_size: int = WHOLE_NUMBER_SIZE,
+) -> LexoRank:
+    if value[1] != bucket_separator or value[2 + whole_number_size] != decimal_point:
+        raise ValueError(f"invalid lexorank format: {value}")
+
+    bucket = Bucket(int(value[0]))
+    decimal_ = decimal.parse(value[2:], base, decimal_point=decimal_point)
+    return LexoRank(
+        bucket, decimal_, bucket_separator=bucket_separator, whole_number_size=whole_number_size
+    )
+
+
+@lru_cache()
+def middle(
+    bucket: Bucket,
+    base: Base = Base36,
+    *,
+    decimal_point: str = DECIMAL_POINT,
+    bucket_separator: str = BUCKET_SEPARATOR,
+    whole_number_size: int = WHOLE_NUMBER_SIZE,
+) -> LexoRank:
+    max_decimal = decimal.parse("1" + "0" * whole_number_size, base, decimal_point=decimal_point)
+    return LexoRank(
+        bucket,
+        max_decimal * 0.5,
+        bucket_separator=bucket_separator,
+        whole_number_size=whole_number_size,
+    )
+
+
+def between(a: LexoRank | None, b: LexoRank | None) -> LexoRank:
+    if a is None:
+        if b is None:
+            raise ValueError("a and b cannot be None at the same time")
+        return b.prev()
+    if b is None:
+        return a.next()
+
+    if a > b:
+        a, b = b, a
+    mid = (a + b) * 0.5
+    if int(a) < int(mid) < int(b):
+        return LexoRank(a.bucket, decimal.parse(mid.rank.whole_number()))
+
+    # TODO: shorten the length of the decimal part
+    return mid
