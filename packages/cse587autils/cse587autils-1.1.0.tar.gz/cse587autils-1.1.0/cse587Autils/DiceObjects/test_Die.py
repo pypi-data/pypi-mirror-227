0922bab80504aea3d761c5b17e41059aa802942c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-27 03:09:59 +0000

    Add package cse587autils/1.1.0/cse587autils-1.1.0.tar.gz

---
+++ packages/cse587autils/cse587autils-1.1.0.tar.gz/cse587autils-1.1.0/cse587Autils/DiceObjects/test_Die.py
@@ -0,0 +1,83 @@
+import pytest
+import numpy as np
+from numpy.testing import assert_allclose
+from cse587Autils.DiceObjects.Die import Die
+
+
+def test_die_constructor():
+    face_probs = [1 / 6] * 6
+    my_die = Die(face_probs)
+    assert my_die.face_probs == face_probs
+
+
+def test_die_face_probs_setter():
+    face_probs = [1 / 6] * 6
+    my_die = Die()
+    my_die.face_probs = face_probs
+    assert my_die.face_probs == face_probs
+
+
+def test_die_repr():
+    face_probs = [1 / 6] * 6
+    my_die = Die(face_probs)
+    assert repr(my_die) == f"Die({face_probs})"
+
+
+def test_die_len():
+    face_probs = [1 / 6] * 6
+    my_die = Die(face_probs)
+    assert len(my_die) == 6
+
+
+def test_die_getitem():
+    face_probs = [1 / 6] * 6
+    my_die = Die(face_probs)
+    for i in range(len(my_die)):
+        assert my_die[i] == 1 / 6
+
+
+def test_die_getitem_raises_index_error():
+    face_probs = [1 / 6] * 6
+    my_die = Die(face_probs)
+    with pytest.raises(IndexError):
+        my_die[6]
+
+
+def test_die_getitem_raises_type_error():
+    face_probs = [1 / 6] * 6
+    my_die = Die(face_probs)
+    with pytest.raises(TypeError):
+        my_die["0"]
+
+
+def test_die_roll():
+    np.random.seed(42)
+    face_probs = [1 / 6] * 6
+    my_die = Die(face_probs)
+    result = my_die.roll()
+    assert result in set(range(6))
+
+
+def test_expectation():
+    # Define the observed data
+    observed_data = np.array([1, 0, 0, 0, 0, 0])
+
+    # Create a die with equal face probabilities (face_probs)
+    face_probs = [1/6] * len(observed_data)
+    my_die = Die(face_probs)
+
+    # Calculate the expectation
+    actual = my_die.expectation(observed_data)
+
+    # Assert the expected values
+    expected = [0.16666667, 1.0, 1.0, 1.0, 1.0]
+    assert all(round(x, 8) == round(y, 8) for x, y in zip(actual, expected))
+
+
+def test_die_likelihood():
+    face_counts = np.array([1, 0, 0, 0, 0, 0])
+    face_probs = [1 / 6] * len(face_counts)
+    my_die = Die(face_probs)
+    likelihood = my_die.likelihood(face_counts)
+    expected_likelihood = (1 / 6) ** 1 * (5 / 6) ** 0
+    assert_allclose(likelihood, expected_likelihood)
---
+++ packages/cse587autils/cse587autils-1.1.0.tar.gz/cse587autils-1.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['cse587Autils', 'cse587Autils.DiceObjects', 'cse587Autils.utils']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['numpy>=1.25.1,<2.0.0', 'pandas>=2.0.3,<3.0.0']
+
+setup_kwargs = {
+    'name': 'cse587autils',
+    'version': '1.1.0',
+    'description': 'A set of classes and functions to be used in CSE 587A',
+    'long_description': "[![Build](https://github.com/Chase-Mateusiak/cse587Autils/actions/workflows/python-package.yml/badge.svg)](https://github.com/Chase-Mateusiak/cse587Autils/actions/workflows/python-package.yml)\n\n# Introduction\n\nThis package stores objects, functions and at some point possibly data for\nMichael Brent's CSE 587A: Algorithms for Computational Biology at Washington\nUniversity.  \n\n[See the documentation here](https://chase-mateusiak.github.io/cse587Autils/)\n\n# Installation\n\n## Typical User\n\nThis is on PyPI and may be installed with pip:\n\n```bash\npip install cse587Autils\n```\n\n## Cutting edge version\n\nIt is possible that the `dev` branch is ahead of `main` and the `pypi` version.\nHowever, `dev` is not necessarily as stable. Install the `dev` version at your\nown risk:\n\n```bash\npip install https://github.com/Chase-Mateusiak/cse587Autils.git@dev\n```\n\n# Developers\n\nIf you want to participate in improving this repository, then fork the repo\ninto your own profile. Issue pull requests against `dev`.  \n\n## codespace\n\nA development container is configured such that after you fork the repo, you\ncan launch a codespace with a pre-configured environment and start coding\nright away. Changes you make will be pushed to your fork, and from there you \ncan issue pull requests against this repo.",
+    'author': 'chase mateusiak',
+    'author_email': 'chase.mateusiak@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
