39d07dc64f3ce9067c7866d84606dd118e340bab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-27 14:07:04 +0000

    Add package media-jk/0.1.0/media_jk-0.1.0.tar.gz

---
+++ packages/media-jk/media_jk-0.1.0.tar.gz/media_jk-0.1.0/media_jk/media_jk.py
@@ -0,0 +1,50 @@
+#! /usr/bin/python
+import sys
+from .lib.Argument import Argument
+import os
+import json
+
+def print_help():
+    print("Usage: "+sys.argv[0]+" [track --type=<query>] --file=<filename> ")
+    exit(-1)
+
+def main():
+    a = Argument(sys.argv)
+
+    if a.hasOptions(['-h', '--help']):
+        print_help()
+
+    if(a.hasOptionValue(['--file' ,'f'])):
+        data = json.loads(os.popen('mediainfo --Output=JSON '+a.getOptionValue(['--file', 'f'])).read())
+        if(a.hasCommand('track') and a.hasOptionValue('--type')):
+            print("Checking tracks...")
+            err = 0
+            for track in data["media"]["track"]:
+                if(track['@type'] == a.getOptionValue('--type')):
+                    d = json.dumps(track, indent=4)
+                    if(a.hasOptions(['--list-keys', '-l'])):
+                        print(track.keys())
+                        err=0
+                        break
+                    if(a.hasOptionValue('--key')):
+                        print(track[a.getOptionValue('--key')])
+                    else:
+                        print(d)
+                    err=0
+                    break   
+                else:
+                    err=1
+            if(err):
+                print("Unable to fetch track")
+        else:
+            json.dumps(data, indent=4)
+            
+    else:
+        print_help()
+
+
+if __name__ == "__main__" :
+     main()
+
+
+
