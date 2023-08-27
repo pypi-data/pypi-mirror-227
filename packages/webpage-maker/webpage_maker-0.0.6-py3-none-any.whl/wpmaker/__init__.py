2602213c0017592765d8a5fcc6386495f7867d5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-27 14:06:58 +0000

    Add package webpage-maker/0.0.6/webpage_maker-0.0.6-py3-none-any.whl

---
+++ packages/webpage-maker/webpage_maker-0.0.6-py3-none-any.whl/wpmaker/__init__.py
@@ -0,0 +1,23 @@
+class newHtmlObj():
+    def __init__(self,htmlObj={"head":"<title>Hello world!</title>","body":"<h1>It's a HTML webpage!<body>"}):
+        self.code="<DOCTYPE html>\n<html>\n<head>\n"+htmlObj["head"]+"\n</head>\n<body>\n"+htmlObj["body"]+"\n</body>\n</html>"
+    def getHtmlCode(self):
+        return(self.code)
+
+def title(c):
+    return("<title>%s</title>")
+
+def h(c,l=1):
+    return("<h%d>"%(l)+c+"</h%d>"%(l))
+
+def p(c):
+    return("<p>"+c+"</p>")
+
+def br():
+    return("<br />")
+
+def img(src,alt="An image"):
+    return("<img src=\"%s\" alt=\"%s\" />"%(src,alt))
+
+def a(c="",href="#"):
+    return("<a href=%s>%s</a>"%(href,c))
