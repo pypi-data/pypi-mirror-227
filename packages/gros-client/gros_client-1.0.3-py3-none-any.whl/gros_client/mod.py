7c211d2f05a55a9b941b2ffef2459e90e28554cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-27 03:08:59 +0000

    Add package gros-client/1.0.3/gros_client-1.0.3-py3-none-any.whl

---
+++ packages/gros-client/gros_client-1.0.3-py3-none-any.whl/gros_client/mod.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+
+class Mod(Enum):
+    MOD_ORIGINAL = "ORIGINAL",
+    MOD_ACTION = 'ACTION',
+    MOD_HOME = 'HOME',
+    MOD_FIX = 'FIX',
+
+    MOD_4_WHEEL = "WHEEL_4",
+    MOD_3_WHEEL = "WHEEL_3",
+    MOD_2_WHEEL = "WHEEL_2",
