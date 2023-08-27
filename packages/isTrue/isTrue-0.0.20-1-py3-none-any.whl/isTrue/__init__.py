584824642c980c7adc07c945ee40cac6c2256616
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-27 14:07:03 +0000

    Add package isTrue/0.0.20/isTrue-0.0.20-1-py3-none-any.whl

---
+++ packages/isTrue/isTrue-0.0.20-1-py3-none-any.whl/isTrue/__init__.py
@@ -0,0 +1,1 @@
+from isTrue import isTrue
---
+++ packages/isTrue/isTrue-0.0.20-1-py3-none-any.whl/isTrue/isTrue.py
@@ -0,0 +1,12 @@
+def is_true(var):
+    if var:
+        return True
+    return False
+
+    print("WHAT?")
+
+# this is for a test
+
+# I know its dumb
+
+# 0.0.20-1???????????????
