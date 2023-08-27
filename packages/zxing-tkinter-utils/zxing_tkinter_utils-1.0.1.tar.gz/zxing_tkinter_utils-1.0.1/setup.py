c3cb6e80020a7cbf6611f85e485730d99a907dd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-27 03:09:20 +0000

    Add package zxing-tkinter-utils/1.0.1/zxing_tkinter_utils-1.0.1.tar.gz

---
+++ packages/zxing-tkinter-utils/zxing_tkinter_utils-1.0.1.tar.gz/zxing_tkinter_utils-1.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+	long_description = fh.read()
+"""
+项目名称，版本，描述。
+主页markdown内容。
+需要上传的模块。
+开源协议。
+"""
+setup(
+	name='zxing_tkinter_utils',
+	version='1.0.1',
+	description='tkinter实用工具',
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	packages=['zxing_tkinter_utils'],
+	license="MIT"
+)
