f5c26e8824a9cefa268880b941c28de0593a2552
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-27 03:10:10 +0000

    Add package ETSformer-pytorch/0.1.1/ETSformer-pytorch-0.1.1.tar.gz

---
+++ packages/ETSformer-pytorch/ETSformer-pytorch-0.1.1.tar.gz/ETSformer-pytorch-0.1.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'ETSformer-pytorch',
+  packages = find_packages(exclude=[]),
+  version = '0.1.1',
+  license='MIT',
+  description = 'ETSTransformer - Exponential Smoothing Transformer for Time-Series Forecasting - Pytorch',
+  long_description_content_type = 'text/markdown',
+  author = 'Phil Wang',
+  author_email = 'lucidrains@gmail.com',
+  url = 'https://github.com/lucidrains/ETSformer-pytorch',
+  keywords = [
+    'artificial intelligence',
+    'deep learning',
+    'transformers',
+    'time-series',
+    'forecasting'
+  ],
+  install_requires=[
+    'einops>=0.4',
+    'scipy',
+    'torch>=1.6',
+  ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
