9cb7f0fba55666bc03b26e00d37c21f064f7872c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-27 14:07:03 +0000

    Add package url-local/0.0.8/url-local-0.0.8.tar.gz

---
+++ packages/url-local/url-local-0.0.8.tar.gz/url-local-0.0.8/src/action_name_enum.py
@@ -0,0 +1,16 @@
+from enum import Enum
+
+class ActionName(Enum):
+    GRAPHQL = "graphql"
+    GENDER_DETECTION = "process"
+    # TODO: We need to change it
+    EVENT = "event"
+    ADD_LOG = "add"
+    GET_ALL_GROUPS = "getAllGroups"
+    GET_GROUP_BY_NAME = "getGroupByName"
+    GET_GROUP_BY_ID = "getGroupById"
+    CREATE_USER = "createUser"
+    UPDATE_USER = "updateUser"
+    LOGIN = "login"
+    TIMELINE = "timeline"
+
