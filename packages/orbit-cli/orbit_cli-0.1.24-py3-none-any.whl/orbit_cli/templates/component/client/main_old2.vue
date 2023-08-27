e5b7bf5945ab15060680c09732d70d8bc478e94f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-27 03:10:01 +0000

    Add package orbit-cli/0.1.24/orbit_cli-0.1.24-py3-none-any.whl

---
+++ packages/orbit-cli/orbit_cli-0.1.24-py3-none-any.whl/orbit_cli/templates/component/client/main_old2.vue
@@ -0,0 +1,30 @@
+<template>
+    <n-dialog-provider>
+        <n-message-provider placement="bottom-right">
+            <{{ project }}Component :router="router"/>
+        </n-message-provider>
+    </n-dialog-provider>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue';
+import { NDialogProvider, NMessageProvider } from 'naive-ui';
+import {{ project }}Component from './{{ project }}Component.vue';
+const router = ref(null)
+
+export default defineComponent({
+    name: '{{ project }}',
+    components: { {{ project }}Component, NDialogProvider, NMessageProvider },
+    install (app, options) {
+        app.component('{{ project }}', this);
+        (options.buttons||[]).forEach((button) => {
+            options.router.addRoute(button)
+            options.menu.push(button)
+        })
+        router.value = options.router
+    },
+    computed: {
+        router () { return router }
+    },
+})
+</script>
