8b402d4f960f6ce76db42a3e7e01d65bda59226d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-27 03:08:53 +0000

    Add package Wafacul/0.1/Wafacul-0.1.tar.gz

---
+++ packages/Wafacul/Wafacul-0.1.tar.gz/Wafacul-0.1/cod/__init__.py
@@ -0,0 +1,8 @@
+def gamma(a,b):
+    return   a + b
+def tarah(a,b):
+    return a - b
+def darab(a,b):
+    return a * b
+def qsma(a,b):
+    return a / b 
---
+++ packages/Wafacul/Wafacul-0.1.tar.gz/Wafacul-0.1/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+setuptools.setup(
+    name='Wafacul',
+    version='0.1',
+    author= 'Wafa alhammadi',
+    description='Simple library in python for test',
+    packages=setuptools.find_packages(),
+
+)
