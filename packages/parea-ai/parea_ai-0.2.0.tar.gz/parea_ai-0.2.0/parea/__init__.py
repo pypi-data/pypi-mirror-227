4b657e0b11a25136e98de1ce985814b44e652953
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-27 04:09:23 +0000

    Add package parea-ai/0.2.0/parea_ai-0.2.0.tar.gz

---
+++ packages/parea-ai/parea_ai-0.2.0.tar.gz/parea_ai-0.2.0/parea/__init__.py
@@ -0,0 +1,24 @@
+# type: ignore[attr-defined]
+# flake8: noqa
+
+"""
+    Parea API SDK
+
+    The Parea SDK allows you to interact with Parea from your product or service.
+    To install the official [Python SDK](https://pypi.org/project/parea/),
+    run the following command:  ```bash pip install parea ```.
+"""
+
+from importlib import metadata as importlib_metadata
+
+from parea.client import Parea
+
+
+def get_version() -> str:
+    try:
+        return importlib_metadata.version(__name__)
+    except importlib_metadata.PackageNotFoundError:  # pragma: no cover
+        return "unknown"
+
+
+version: str = get_version()
