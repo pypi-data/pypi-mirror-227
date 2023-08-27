3bca07e6e77005e88f0423a5d482ab2e871f92dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-27 03:10:46 +0000

    Add package turtle-utils/1.0.2/turtle_utils-1.0.2.tar.gz

---
+++ packages/turtle-utils/turtle_utils-1.0.2.tar.gz/turtle_utils-1.0.2/setup.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+'''
+@Name:  setup
+@IDE：PyCharm
+@Author:qtclm
+@Date：2023/8/26 16:23
+'''
+
+
+import codecs
+import os
+from setuptools import setup, find_packages
+
+# these things are needed for the README.md show on pypi (if you dont need delete it)
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+# 获取项目依赖
+with open('requirements.txt','r',encoding='utf-8') as file:
+    projects_depends = file.read()
+
+projects_depends = projects_depends.split('\n')
+
+# you need to change all these
+VERSION = '1.0.2'
+DESCRIPTION = '个人项目常用工具类'
+
+setup(
+    # name="要显示的唯一标识（用于pip install xxx）",
+    name="turtle_utils",
+    version=VERSION,
+    author="qtclm",
+    author_email="248313385@qq.com",
+    description=DESCRIPTION,
+    # 长描述内容的类型设置为markdown
+    long_description_content_type="text/markdown",
+    # 长描述设置为README.md的内容
+    long_description=long_description,
+    # 使用find_packages()自动发现项目中的所有包
+    packages=find_packages(),
+    # 许可协议
+    license='MIT',
+    # 要安装的依赖包
+    install_requires=projects_depends,
+    keywords=['python', 'qtclm','windows','mac','linux'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
---
+++ packages/turtle-utils/turtle_utils-1.0.2.tar.gz/turtle_utils-1.0.2/turtle_utils/mysql_connect.py
@@ -0,0 +1,64 @@
+# _*_ coding: UTF-8 _*_
+"""
+@file            : mysql_connect
+@Author          : qtclm
+@Date            : 2023/1/29 19:44
+@Desc            : 自动生成接口测试用例：支持正交实验，等价类，边界值
+"""
+
+import time
+from turtle_utils.mysql_public import operationMysql
+from turtle_utils.operation_yaml import operationYaml
+import pymysql
+
+class Config(object):
+    def __init__(self,file_path='config',file_name='config.yaml',env='qa'):
+        if not env in('qa','dev'):
+            raise Exception("不支持的环境配置")
+        config=operationYaml(file_path=file_path,file_name=file_name).read_data()
+        self.dataBaseConfig=config['config'][env]
+
+class mysql_qa(operationMysql,Config):
+    def __init__(self,db_name='hope_saas_bms'):
+        Config.__init__(self)
+        tencent_cloud_mysql_conf=self.dataBaseConfig['mysql_conf_qa']
+        tencent_cloud_mysql_conf['db'] = db_name
+        tencent_cloud_mysql_conf["cursorclass"]=pymysql.cursors.DictCursor
+        # print(tencent_cloud_mysql_conf)
+        super(mysql_qa,self).__init__(**tencent_cloud_mysql_conf)
+
+class mysql_reg(operationMysql,Config):
+    def __init__(self,db_name='hope_saas_bms'):
+        Config.__init__(self)
+        tencent_cloud_mysql_conf=self.dataBaseConfig['mysql_conf_reg']
+        tencent_cloud_mysql_conf['db'] = db_name
+        tencent_cloud_mysql_conf["cursorclass"]=pymysql.cursors.DictCursor
+        super(mysql_reg,self).__init__(**tencent_cloud_mysql_conf)
+
+
+class mysql_prod(operationMysql,Config):
+    def __init__(self):
+        Config.__init__(self)
+        tencent_cloud_mysql_conf=self.dataBaseConfig['mysql_conf_prod']
+        tencent_cloud_mysql_conf['db'] = 'hope_saas_oms'
+        tencent_cloud_mysql_conf["cursorclass"]=pymysql.cursors.DictCursor
+        super(mysql_prod,self).__init__(**tencent_cloud_mysql_conf)
+
+class mysql_prod_bms(operationMysql,Config):
+    def __init__(self):
+        Config.__init__(self)
+        tencent_cloud_mysql_conf=self.dataBaseConfig['mysql_conf_prod_bms']
+        tencent_cloud_mysql_conf['db'] = 'hope_saas_bms'
+        tencent_cloud_mysql_conf["cursorclass"]=pymysql.cursors.DictCursor
+        # print(tencent_cloud_mysql_conf)
+        super(mysql_prod_bms,self).__init__(**tencent_cloud_mysql_conf)
+
+if __name__=="__main__":
+    pd=mysql_qa(db_name='hope_saas_audit')
+
+    start_time=time.time()
+    sql_str=''' INSERT INTO `hope_saas_audit`.`aud_shipping_order_audit_process_detail`( `shipping_order_process_no`,`shipping_order_no`, `shipping_status`, `weight`, `volumn`, `quantity`, `value`, `unit`, `create_time`, `update_time`, `create_user`, `update_user`, `user_id`, `del_flag`, `tenant_code`, `back_reason`, `back_explain`, `status`, `back_status`, `signing_time`, `vehicle_owner_ship`, `original_value`, `plan_original_value`, `plan_value`, `reassignment_type`, `estimate_detail`, `dispatch_person`, `contract_code`)
+VALUES ( 'ASOP20220706_%s','test_20220706_%s', 4, 30.0000, 30.0000, 30.0000, 700.0000, NULL, '2020-04-22 16:30:54', '2021-06-11 22:25:10', '超级用户', '秦敏', 'T10', 1, 'YLGYS20042100000000002', '3232', NULL, 3, 1, '2020-04-22 16:30:54', NULL, 0.00000, 0.00000, 0.00000, 'NORMAL', '{}', '', '') ; '''
+    list1=[(i,i) for i in range(1000)]
+    pd.sql_batch(sql_operation=sql_str,data_list=list1)
+    print(f"执行耗时：{time.time()-start_time}")
