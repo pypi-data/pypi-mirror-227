39e66c24367844a81279f488da6c4ea28890d07c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-27 04:09:25 +0000

    Add package llamabot/0.0.71/llamabot-0.0.71.tar.gz

---
+++ packages/llamabot/llamabot-0.0.71.tar.gz/llamabot-0.0.71/llamabot/prompt_library/zotero.py
@@ -0,0 +1,43 @@
+"""Zotero prompt library."""
+try:
+    from outlines import text
+except ImportError:
+    import warnings
+
+    warnings.warn(
+        "Please install the `outlines` package to use the llamabot prompt library."
+    )
+
+
+@text.prompt
+def retrieverbot_sysprompt():
+    """You are an expert in retrieving information from JSON files."""
+
+
+@text.prompt
+def docbot_sysprompt():
+    """You are an expert in answering questions about any paper.
+
+    Your responses are like how Richard Feynman would answer questions.
+    That is to say, you will explain yourself and the paper (where relevant)
+    at a level that an undergraduate student would understand.
+    """
+
+
+@text.prompt
+def get_key(query: str = ""):
+    """Return for me keys that correspond to my Zotero library entry associated with the query:
+
+    {{ query }}
+
+    Return this as JSON:
+
+    {
+        "key": [<key1>, <key2>, ...]
+    }
+
+    Do not return anything else except JSON.
+    Ensure that you do not return any explanations of your response.
+
+    # noqa: DAR101
+    """
