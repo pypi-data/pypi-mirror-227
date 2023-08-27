52576beba05fca334e9d86820991c8fb65d4eacd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-27 04:09:03 +0000

    Add package lfsdata/0.0.1/lfsdata-0.0.1.tar.gz

---
+++ packages/lfsdata/lfsdata-0.0.1.tar.gz/lfsdata-0.0.1/setup.py
@@ -0,0 +1,30 @@
+"""Install packages as defined in this file into the Python environment."""
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf8") as fh:
+    long_description = fh.read()
+setup(
+    name="lfsdata",
+    version="0.0.1",
+    author="Arusha Developers",
+    author_email="info@arusha.dev",
+    maintainer="Hamed Khademi Khaledi",
+    maintainer_email="khaledihkh@gmail.com",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/arushadev/lfsdata",
+    packages=find_packages(),
+    namespace_packages=['lfsdata'],
+    install_requires=[
+        "setuptools>=45.0",
+        "coloredlogs",
+        "gitlab",
+    ],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Programming Language :: Python :: 3.0",
+        "Topic :: Utilities",
+    ],
+    python_requires='>=3.11',
+)
