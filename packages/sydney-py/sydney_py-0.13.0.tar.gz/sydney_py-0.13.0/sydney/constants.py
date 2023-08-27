07705fbd016d962eb82f9c075940c99f162973a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-27 03:09:58 +0000

    Add package sydney-py/0.13.0/sydney_py-0.13.0.tar.gz

---
+++ packages/sydney-py/sydney_py-0.13.0.tar.gz/sydney_py-0.13.0/sydney/constants.py
@@ -0,0 +1,12 @@
+HEADERS = {
+    "Accept": "application/json",
+    "Accept-Encoding": "gzip, deflate",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Content-Type": "application/json",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36 Edg/116.0.1938.62",
+}
+
+BING_CREATE_CONVESATION_URL = "https://www.bing.com/turing/conversation/create"
+BING_CHATHUB_URL = "wss://sydney.bing.com/sydney/ChatHub"
+
+DELIMETER = "\x1e"  # Record separator character.
