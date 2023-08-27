d29ffac30080181dbb6d8c89174df2239f9aa2fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-27 14:07:04 +0000

    Add package media-jk/0.2.0/media_jk-0.2.0.tar.gz

---
+++ packages/media-jk/media_jk-0.2.0.tar.gz/media_jk-0.2.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open('/home/jevaa_kharthik/pythoncli/mediaquery_pkg/media_jk/requirments', 'r') as f:
+    requirments = f.read().splitlines()
+
+setup(
+
+    name='media_jk',
+    version='0.2.0',
+    packages=find_packages(),
+    author='Jevaa Kharthik N',
+    author_email='jevaasureka@gmail.com',
+    description='Media_jk is a Wrapper of Mediainfo',
+    url= 'https://git.selfmade.ninja/jevaa_kharthik/media_jk',
+    install_requires=[
+            requirments
+    ],
+    entry_points={
+        'console_scripts': [
+            'media_jk = media_jk.media_jk:main'
+        ],
+    },
+    
+)
+    
