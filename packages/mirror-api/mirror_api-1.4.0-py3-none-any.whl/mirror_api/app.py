d2848479ec81ff98e00688c7b5fbcf7d02e9e6a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-28 04:12:32 +0000

    Add package mirror-api/1.4.0/mirror_api-1.4.0-py3-none-any.whl

---
+++ packages/mirror-api/mirror_api-1.4.0-py3-none-any.whl/mirror_api/app.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+
+def create_app() -> FastAPI:
+    from mirror_api._router import router
+
+    app = FastAPI()
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+    app.include_router(router, tags=["sample"])
+
+    return app
