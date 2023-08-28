b3535973334b1d0d49a0098277650513032e3f11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-28 04:11:55 +0000

    Add package mes-proto-python/0.0.15/mes_proto_python-0.0.15.tar.gz

---
+++ packages/mes-proto-python/mes_proto_python-0.0.15.tar.gz/mes_proto_python-0.0.15/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mes_proto_python",
+    version="0.0.15",
+    packages=find_packages(),
+    install_requires=[
+        # List your library's dependencies here
+    ],
+    author="Emil Sroka",
+    # author_email="your.email@example.com",
+    # description="A simple library example",
+    # long_description=open('README.md').read(),
+    # long_description_content_type="text/markdown",
+    # url="https://github.com/yourusername/my-library",  # Optional
+)
