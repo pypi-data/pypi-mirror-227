5a114458e5adaad89d2e4cad16d465048c10d3ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-28 14:19:45 +0000

    Add package ascend-deployer/5.0.0b24/ascend_deployer-5.0.0b24-py3-none-any.whl

---
+++ packages/ascend-deployer/ascend_deployer-5.0.0b24-py3-none-any.whl/ascend_deployer/install.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+readonly BASE_DIR=$(
+    cd "$(dirname $0)" >/dev/null 2>&1
+    pwd -P
+)
+
+export ANSIBLE_CACHE_PLUGIN_CONNECTION=$BASE_DIR/facts_cache
+export ANSIBLE_CONFIG=$BASE_DIR/ansible.cfg
+
+function get_specified_python() {
+    if [ ! -z ${ASCEND_PYTHON_VERSION} ]; then
+        echo ${ASCEND_PYTHON_VERSION}
+    else
+        echo $(grep -oP "^ascend_python_version=\K.*" ${BASE_DIR}/downloader/config.ini | sed 's/\r$//')
+    fi
+}
+
+readonly specified_python=$(get_specified_python)
+readonly PYTHON_VERSION=$(echo ${specified_python} | sed 's/P/p/;s/-//')
+readonly PYTHON_PREFIX=${HOME}/.local/${PYTHON_VERSION}
+export PATH=${PYTHON_PREFIX}/bin:$PATH
+export LD_LIBRARY_PATH=${PYTHON_PREFIX}/lib:$LD_LIBRARY_PATH
+
+
+main() {
+    python3 -V > /dev/null 2>&1
+    if [[ $? != 0 ]]; then
+      python ${BASE_DIR}/ascend_deployer.py $*
+    else
+      python3 ${BASE_DIR}/ascend_deployer.py $*
+    fi
+}
+
+main $*
+main_status=$?
+exit ${main_status}
