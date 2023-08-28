27f921438a33c51d00d891be0ca9faa54daa26d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-28 03:10:19 +0000

    Add package FLAVORS/0.1/FLAVORS-0.1.tar.gz

---
+++ packages/FLAVORS/FLAVORS-0.1.tar.gz/FLAVORS-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='FLAVORS',
+    version='0.1',
+    author='Michael Mech',
+    description='Fast and Lightweight Assessment of Variables for zOptimally-Reduced Subsets towards Feature Learning Automation with Variable-Objective, Resource Scheduling',
+    packages=find_packages())
+
+
+    
