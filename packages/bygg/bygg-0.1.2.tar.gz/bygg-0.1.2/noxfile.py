f2e5aab061288c13dad0e040d39bb2b263084b6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-28 04:12:04 +0000

    Add package bygg/0.1.2/bygg-0.1.2.tar.gz

---
+++ packages/bygg/bygg-0.1.2.tar.gz/bygg-0.1.2/noxfile.py
@@ -0,0 +1,55 @@
+import nox
+
+nox.options.error_on_external_run = True
+
+
+@nox.session(python=["3.11"])
+def tests(session):
+    session.install("-r", "requirements.txt", "-r", "requirements-dev.txt")
+    session.install(".")
+    session.run("pytest")
+
+
+@nox.session(python=["3.11"])
+def basics(session):
+    session.install(".")
+    session.run("bygg", success_codes=[1], silent=True)
+    session.run("bygg", "--help", silent=True)
+
+
+@nox.session(python=["3.11"])
+def examples(session):
+    session.install(".")
+
+    with session.chdir("examples/only_python"):
+        session.run("bygg")
+        session.run("bygg", "hello")
+        session.run("bygg", "--clean")
+
+    with session.chdir("examples/parametric"):
+        session.run("bygg")
+        session.run("bygg", "--clean")
+
+    with session.chdir("examples/taskrunner"):
+        session.run("bygg")
+        session.run("bygg", "--clean")
+
+    with session.chdir("examples/trivial"):
+        session.run("bygg")
+        session.run("bygg", "--clean")
+
+    with session.chdir("examples/environments"):
+        session.run("bygg")
+        session.run("bygg", "clean_environments")
+
+        session.run("bygg", "default_action")
+        session.run("bygg", "clean_environments")
+
+        session.run("bygg", "action1")
+        session.run("bygg", "clean_environments")
+
+        session.run("bygg", "action2")
+        session.run("bygg", "clean_environments")
+
+        session.run("bygg", "action1", "default_action", "action2")
+        session.run("bygg", "clean_environments")
