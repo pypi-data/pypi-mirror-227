a91b6443af43dae9b900d5303d7ff2269f1a8ad3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-28 04:11:37 +0000

    Add package caddy-python/0.0.4/caddy-python-0.0.4.tar.gz

---
+++ packages/caddy-python/caddy-python-0.0.4.tar.gz/caddy-python-0.0.4/caddy/tools.py
@@ -0,0 +1,28 @@
+from caddy.base import BaseAPI
+
+
+class Tools(BaseAPI):
+    def __init__(self, api_key=None, local=False):
+        super().__init__(api_key, local=local)
+        self.api_key = api_key
+        self.route = "/tools"
+
+    def request(self, method: str, path: str, body: object = None) -> object:
+        path = f"{self.route}/{path}/run"
+        response = self._request(
+            method=method,
+            path=path,
+            body=body
+        )
+        return response
+
+    def search_tools(self, query):
+        path = f'{self.route}/search'
+        body = dict()
+        body['query'] = query
+        response = self._request(
+            method='POST',
+            path=path,
+            body=body
+        )
+        return response['items']
