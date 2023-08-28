cccf37896b88e02cd3cd6873c5e83b4ae8d1d427
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-28 03:10:56 +0000

    Add package swiftly-sys/0.0.108/swiftly_sys-0.0.108-py3-none-any.whl

---
+++ packages/swiftly-sys/swiftly_sys-0.0.108-py3-none-any.whl/swiftly_sys-0.0.108.data/scripts/swiftly-utils.bat
@@ -0,0 +1,26 @@
+@echo off
+setlocal enabledelayedexpansion
+
+:read_cli_result
+:: Get the system's temporary directory using Python
+for /f %%i in ('python3 -c "import tempfile; print(tempfile.gettempdir())"') do set temp_dir=%%i    
+echo Temp Directory: %temp_dir%
+
+:: Set the result file path
+set result_file_path=%temp_dir%\swiftly_cli_result.txt
+
+:: Check if the result file exists
+if not exist "%result_file_path%" (
+    echo Error: Result file not found!
+    exit /b 1
+)
+
+:: Read the result
+for /f "delims=" %%a in ("%result_file_path%") do set result=%%a
+
+:: Remove the temporary file
+del "%result_file_path%"
+
+:: Return the result
+echo !result!
+goto :eof
