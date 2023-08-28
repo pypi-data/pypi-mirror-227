91f678f419c57db9ebc6c09282f17016037352ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-28 03:11:43 +0000

    Add package fastapi-pubsub/0.2.0/fastapi_pubsub-0.2.0-py3-none-any.whl

---
+++ packages/fastapi-pubsub/fastapi_pubsub-0.2.0-py3-none-any.whl/fastapi_pubsub/base.py
@@ -0,0 +1,68 @@
+from abc import ABC, abstractmethod
+from typing import Dict
+
+import json
+
+from fastapi import WebSocket, WebSocketDisconnect
+
+from fastapi_pubsub.connection_manager import MemoryConnectionManager, RedisConnectionManager
+
+
+class RedisCredentials:
+    host: str
+    port: int
+    username: str
+    password: str
+    db: int
+
+
+class BasePubSubWebSocket(ABC):
+    def __init__(self, websocket: WebSocket, channel_id: str, redis_credentials: RedisCredentials = None):
+        self.websocket = websocket
+        self.channel_id = channel_id
+
+        self.connection_manager = self._initialize_connection_manager(
+            redis_credentials)
+
+    def _initialize_connection_manager(self, redis_credentials: RedisCredentials):
+        if redis_credentials is not None:
+            return RedisConnectionManager(**redis_credentials.__dict__)
+        else:
+            return MemoryConnectionManager()
+
+    async def send_json(self, data: Dict):
+        await self.send_message(json.dumps(data))
+
+    async def send_message(self, message: str):
+        await self.websocket.send_text(message)
+
+    async def receive_message(self) -> str:
+        return await self.websocket.receive_text()
+
+    async def close(self):
+        await self.websocket.close()
+
+    async def run(self, send_message_on_exception=True):
+        await self.websocket.accept()
+        self.connection_manager.add(self.channel_id, self)
+
+        try:
+            await self.process()
+
+            while True:
+                await self.websocket.receive_text()
+
+        except WebSocketDisconnect:
+            self.connection_manager.remove(self.channel_id, self)
+
+            if send_message_on_exception:
+                await self.process()
+
+    async def process(self):
+        for client in self.connection_manager.get(self.channel_id):
+            response = await self.generate_object_to_send()
+            await client.send_json(response)
+
+    @abstractmethod
+    async def generate_object_to_send(self):
+        pass
