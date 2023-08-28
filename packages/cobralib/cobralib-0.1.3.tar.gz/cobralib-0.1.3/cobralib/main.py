065d56893fb26e7968bf1dae891406adb3405c70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-28 04:12:53 +0000

    Add package cobralib/0.1.3/cobralib-0.1.3.tar.gz

---
+++ packages/cobralib/cobralib-0.1.3.tar.gz/cobralib-0.1.3/cobralib/main.py
@@ -0,0 +1,23 @@
+# Import necessary packages here
+
+# - If a package and a module within the package is to be imported
+#   uncomment the following lines where dir is the directory containing
+#   the source files.  These lines should go above the module imports
+# import sys
+# import os
+# sys.path.insert(1, os.path.abspath(dir))
+# ==========================================================================================
+# ==========================================================================================
+
+# File:    main.py
+# Date:    July 09, 2023
+# Author:  Jonathan A. Webb
+# Purpose: Describe the purpose of functions of this file
+# ==========================================================================================
+# ==========================================================================================
+# Insert Code here
+
+
+# ==========================================================================================
+# ==========================================================================================
+# eof
