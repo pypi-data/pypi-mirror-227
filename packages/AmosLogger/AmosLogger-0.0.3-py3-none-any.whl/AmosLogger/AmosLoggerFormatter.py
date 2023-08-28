250034e530d59a9ebefd7002c9e0d5c7738704dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-28 14:19:49 +0000

    Add package AmosLogger/0.0.3/AmosLogger-0.0.3-py3-none-any.whl

---
+++ packages/AmosLogger/AmosLogger-0.0.3-py3-none-any.whl/AmosLogger/AmosLoggerFormatter.py
@@ -0,0 +1,19 @@
+from . import *
+import getpass
+
+
+class AmosLogFormatter(logging.Formatter):
+    def __init__(self, fmt, datefmt=None, style='%'):
+        super().__init__(fmt, datefmt, style)
+
+    def format(self, record):
+        # 设置颜色
+        level_color = {
+            logging.INFO: '\033[0;32m',
+            logging.WARNING: '\033[0;33m',
+            logging.ERROR: '\033[0;31m',
+            logging.CRITICAL: '\033[0;35m'
+        }
+        record.user = getpass.getuser()
+        record.levelname = level_color.get(record.levelno) + record.levelname + '\033[0m'
+        return super().format(record)
