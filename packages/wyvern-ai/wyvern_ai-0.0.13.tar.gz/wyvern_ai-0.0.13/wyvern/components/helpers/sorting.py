83c12e0ab53673fd651bf3a2416dee4ff9313d4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-28 04:12:55 +0000

    Add package wyvern-ai/0.0.13/wyvern_ai-0.0.13.tar.gz

---
+++ packages/wyvern-ai/wyvern_ai-0.0.13.tar.gz/wyvern_ai-0.0.13/wyvern/components/helpers/sorting.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from typing import List
+
+from wyvern.components.component import Component
+from wyvern.entities.candidate_entities import (
+    GENERALIZED_WYVERN_ENTITY,
+    ScoredCandidate,
+)
+
+
+class SortingComponent(
+    Component[
+        List[ScoredCandidate[GENERALIZED_WYVERN_ENTITY]],
+        List[ScoredCandidate[GENERALIZED_WYVERN_ENTITY]],
+    ],
+):
+    async def execute(
+        self,
+        input: List[ScoredCandidate[GENERALIZED_WYVERN_ENTITY]],
+        descending=True,
+        **kwargs
+    ) -> List[ScoredCandidate[GENERALIZED_WYVERN_ENTITY]]:
+        return sorted(input, key=lambda candidate: candidate.score, reverse=descending)
