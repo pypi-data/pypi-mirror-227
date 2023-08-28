e94805dc0a68ad7ca14faeb4df4c611210dc347e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-28 04:12:33 +0000

    Add package pyfileflow/0.1.2/pyfileflow-0.1.2-py3-none-any.whl

---
+++ packages/pyfileflow/pyfileflow-0.1.2-py3-none-any.whl/pyfileflow/__init__.py
@@ -0,0 +1,14 @@
+"""PyFileFlow, the file system organizer module."""
+
+# type: ignore
+try:
+    from importlib.metadata import PackageNotFoundError  # type: ignore
+    from importlib.metadata import version
+except ImportError:  # pragma: no cover
+    from importlib_metadata import PackageNotFoundError  # type: ignore
+    from importlib_metadata import version
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "unknown"
