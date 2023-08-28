6b577f79312b412bbc05090bedfe9a462a2e541f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-28 04:12:00 +0000

    Add package primecheckergbr/1.0.0/primecheckergbr-1.0.0.tar.gz

---
+++ packages/primecheckergbr/primecheckergbr-1.0.0.tar.gz/primecheckergbr-1.0.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+
+    name='primecheckergbr',
+
+    version='1.0.0',
+
+    author='GuillaumeBR',
+
+    author_email='ghirguy@hotmail.com',
+
+    description='A package for checking prime numbers',
+
+    packages=find_packages(),
+
+)
