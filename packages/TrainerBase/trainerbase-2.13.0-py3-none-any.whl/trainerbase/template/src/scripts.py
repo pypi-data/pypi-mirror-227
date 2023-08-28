c6657517889469cc95dcf6ab0707d366a5878cb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-28 03:12:01 +0000

    Add package TrainerBase/2.13.0/trainerbase-2.13.0-py3-none-any.whl

---
+++ packages/TrainerBase/trainerbase-2.13.0-py3-none-any.whl/trainerbase/template/src/scripts.py
@@ -0,0 +1,10 @@
+from trainerbase.scriptengine import ScriptEngine, system_script_engine
+
+
+script_engine = ScriptEngine()
+
+
+# # Example:
+# @script_engine.simple_script
+# def my_simple_script():
+#     ...
