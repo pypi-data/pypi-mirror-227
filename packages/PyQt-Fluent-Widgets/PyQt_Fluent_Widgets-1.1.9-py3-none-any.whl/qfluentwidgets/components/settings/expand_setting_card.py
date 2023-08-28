b91d1087e8a3a20e8d69270bc6f80a6679ba8b15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-28 03:11:43 +0000

    Add package PyQt-Fluent-Widgets/1.1.9/PyQt_Fluent_Widgets-1.1.9-py3-none-any.whl

---
+++ packages/PyQt-Fluent-Widgets/PyQt_Fluent_Widgets-1.1.9-py3-none-any.whl/qfluentwidgets/components/settings/expand_setting_card.py
@@ -0,0 +1,247 @@
+# coding:utf-8
+from typing import Union
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import QEvent, Qt, QPropertyAnimation, pyqtProperty, QEasingCurve, QRectF
+from PyQt5.QtGui import QColor, QPainter
+from PyQt5.QtWidgets import QFrame, QWidget, QAbstractButton, QApplication, QScrollArea, QVBoxLayout
+
+from ...common.config import isDarkTheme
+from ...common.icon import FluentIcon as FIF
+from ...common.style_sheet import FluentStyleSheet
+from .setting_card import SettingCard
+from ..layout.v_box_layout import VBoxLayout
+
+
+class ExpandButton(QAbstractButton):
+    """ Expand button """
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setFixedSize(30, 30)
+        self.__angle = 0
+        self.isHover = False
+        self.isPressed = False
+        self.rotateAni = QPropertyAnimation(self, b'angle', self)
+        self.clicked.connect(self.__onClicked)
+
+    def paintEvent(self, e):
+        painter = QPainter(self)
+        painter.setRenderHints(QPainter.Antialiasing |
+                               QPainter.SmoothPixmapTransform)
+        painter.setPen(Qt.NoPen)
+
+        # draw background
+        r = 255 if isDarkTheme() else 0
+        if self.isPressed:
+            color = QColor(r, r, r, 10)
+        elif self.isHover:
+            color = QColor(r, r, r, 14)
+        else:
+            color = Qt.transparent
+
+        painter.setBrush(color)
+        painter.drawRoundedRect(self.rect(), 4, 4)
+
+        # draw icon
+        painter.translate(self.width()//2, self.height()//2)
+        painter.rotate(self.__angle)
+        FIF.ARROW_DOWN.render(painter, QRectF(-5, -5, 9.6, 9.6))
+
+    def enterEvent(self, e):
+        self.setHover(True)
+
+    def leaveEvent(self, e):
+        self.setHover(False)
+
+    def mousePressEvent(self, e):
+        super().mousePressEvent(e)
+        self.setPressed(True)
+
+    def mouseReleaseEvent(self, e):
+        super().mouseReleaseEvent(e)
+        self.setPressed(False)
+
+    def setHover(self, isHover: bool):
+        self.isHover = isHover
+        self.update()
+
+    def setPressed(self, isPressed: bool):
+        self.isPressed = isPressed
+        self.update()
+
+    def __onClicked(self):
+        self.rotateAni.setEndValue(180 if self.angle < 180 else 0)
+        self.rotateAni.setDuration(200)
+        self.rotateAni.start()
+
+    def getAngle(self):
+        return self.__angle
+
+    def setAngle(self, angle):
+        self.__angle = angle
+        self.update()
+
+    angle = pyqtProperty(float, getAngle, setAngle)
+
+
+class SpaceWidget(QWidget):
+    """ Spacing widget """
+
+    def __init__(self, parent=None):
+        super().__init__(parent=parent)
+        self.setAttribute(Qt.WA_TranslucentBackground)
+        self.setFixedHeight(1)
+
+
+class ExpandSettingCard(QScrollArea):
+    """ Expandable setting card """
+
+    def __init__(self, icon: Union[str, QIcon, FIF], title: str, content: str = None, parent=None):
+        super().__init__(parent=parent)
+        self.isExpand = False
+        self.expandButton = ExpandButton(self)
+        self.scrollWidget = QFrame(self)
+        self.view = QFrame(self.scrollWidget)
+        self.card = SettingCard(icon, title, content, self)
+
+        self.scrollLayout = QVBoxLayout(self.scrollWidget)
+        self.viewLayout = QVBoxLayout(self.view)
+        self.spaceWidget = SpaceWidget(self.scrollWidget)
+
+        # expand animation
+        self.expandAni = QPropertyAnimation(self.verticalScrollBar(), b'value', self)
+
+        self.__initWidget()
+
+    def __initWidget(self):
+        """ initialize widgets """
+        self.setWidget(self.scrollWidget)
+        self.setWidgetResizable(True)
+        self.setFixedHeight(self.card.height())
+        self.setViewportMargins(0, self.card.height(), 0, 0)
+        self.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+
+        # initialize layout
+        self.scrollLayout.setContentsMargins(0, 0, 0, 0)
+        self.scrollLayout.setSpacing(0)
+        self.scrollLayout.addWidget(self.view)
+        self.scrollLayout.addWidget(self.spaceWidget)
+        
+        self.card.hBoxLayout.addWidget(self.expandButton, 0, Qt.AlignRight)
+        self.card.hBoxLayout.addSpacing(8)
+
+        # initialize expand animation
+        self.expandAni.setEasingCurve(QEasingCurve.OutQuad)
+        self.expandAni.setDuration(200)
+
+        # initialize style sheet
+        self.view.setObjectName('view')
+        self.scrollWidget.setObjectName('scrollWidget')
+        self.setProperty('isExpand', False)
+        FluentStyleSheet.EXPAND_SETTING_CARD.apply(self.card)
+        FluentStyleSheet.EXPAND_SETTING_CARD.apply(self)
+
+        self.card.installEventFilter(self)
+        self.expandAni.valueChanged.connect(self._onExpandValueChanged)
+        self.expandButton.clicked.connect(self.toggleExpand)
+
+    def addWidget(self, widget: QWidget):
+        """ add widget to tail """
+        self.card.hBoxLayout.removeItem(self.card.hBoxLayout.itemAt(-1))
+        self.card.hBoxLayout.addWidget(widget, 0, Qt.AlignRight)
+        self.card.hBoxLayout.addSpacing(19)
+        self.card.hBoxLayout.addWidget(self.expandButton, 0, Qt.AlignRight)
+        self.card.hBoxLayout.addSpacing(8)
+
+    def wheelEvent(self, e):
+        pass
+
+    def setExpand(self, isExpand: bool):
+        """ set the expand status of card """
+        if self.isExpand == isExpand:
+            return
+
+        # update style sheet
+        self.isExpand = isExpand
+        self.setProperty('isExpand', isExpand)
+        self.setStyle(QApplication.style())
+
+        # start expand animation
+        if isExpand:
+            h = self.viewLayout.sizeHint().height()
+            self.verticalScrollBar().setValue(h)
+            self.expandAni.setStartValue(h)
+            self.expandAni.setEndValue(0)
+        else:
+            self.expandAni.setStartValue(0)
+            self.expandAni.setEndValue(self.verticalScrollBar().maximum())
+
+        self.expandAni.start()
+
+    def toggleExpand(self):
+        """ toggle expand status """
+        self.setExpand(not self.isExpand)
+
+    def resizeEvent(self, e):
+        self.card.resize(self.width(), self.card.height())
+
+    def eventFilter(self, obj, e):
+        if obj is self.card:
+            if e.type() == QEvent.Enter:
+                self.expandButton.setHover(True)
+            elif e.type() == QEvent.Leave:
+                self.expandButton.setHover(False)
+            elif e.type() == QEvent.MouseButtonPress and e.button() == Qt.LeftButton:
+                self.expandButton.setPressed(True)
+            elif e.type() == QEvent.MouseButtonRelease and e.button() == Qt.LeftButton:
+                self.expandButton.setPressed(False)
+                self.expandButton.click()
+
+        return super().eventFilter(obj, e)
+
+    def _onExpandValueChanged(self):
+        vh = self.viewLayout.sizeHint().height()
+        h = self.viewportMargins().top()
+        self.setFixedHeight(max(h + vh - self.verticalScrollBar().value(), h))
+
+    def _adjustViewSize(self):
+        """ adjust view size """
+        h = self.viewLayout.sizeHint().height()
+        self.spaceWidget.setFixedHeight(h)
+
+        if self.isExpand:
+            self.setFixedHeight(self.card.height()+h)
+
+    def setValue(self, value):
+        """ set the value of config item """
+        pass
+
+
+
+class GroupSeparator(QWidget):
+    """ group separator """
+
+    def __init__(self, parent=None):
+        super().__init__(parent=parent)
+        self.setFixedHeight(3)
+
+    def paintEvent(self, e):
+        painter = QPainter(self)
+        c = 35 if isDarkTheme() else 230
+        painter.setPen(QColor(c, c, c))
+        painter.drawLine(0, 1, self.width(), 1)
+
+
+class ExpandGroupSettingCard(ExpandSettingCard):
+    """ Expand group setting card """
+
+    def addGroupWidget(self, widget: QWidget):
+        """ add widget to group """
+        # add separator
+        if self.viewLayout.count() >= 1:
+            self.viewLayout.addWidget(GroupSeparator(self.view))
+
+        widget.setParent(self.view)
+        self.viewLayout.addWidget(widget)
+        self._adjustViewSize()
