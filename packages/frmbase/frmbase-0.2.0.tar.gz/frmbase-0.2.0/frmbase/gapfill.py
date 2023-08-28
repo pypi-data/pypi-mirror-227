cb978554e04ef17d465a598ff10c77b7d77b7233
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-28 04:12:24 +0000

    Add package frmbase/0.2.0/frmbase-0.2.0.tar.gz

---
+++ packages/frmbase/frmbase-0.2.0.tar.gz/frmbase-0.2.0/frmbase/gapfill.py
@@ -0,0 +1,314 @@
+
+from  ipdb import set_trace as idebug 
+import scipy.interpolate as spinterp
+import numpy as np
+
+from frmbase.fitter.lsf import Lsf
+import frmbase.plateau as plateau
+
+
+
+def fill_gaps(y, small_size=5, bad_value=np.nan):
+    """Fill gaps in a 1d array
+    
+    Gap filling is a useful step if you intend to smooth data. By filling in gaps
+    with "reasonable" values you can mitigate edge effects around the gaps.
+    Be careful to remove the gap filled data after smoothing to avoid analysing fake data!
+
+    Assumes that any dc-offset has been removed from the signal (e.g by mean-zeroing
+    or median-zeroing the data)
+
+    Based on an idea I first saw in the TPS unit of Kepler SOC Pipeline, although 
+    the algorithm is of an independent design  See Section 9.2.3.2 of
+    https://archive.stsci.edu/kepler/manuals/KSCI-19081-001_Data_Processing_Handbook.pdf
+
+    This implementation is not concerned with preserving noise properties in the gap.
+
+    Inputs
+    ---------
+    y
+        (1d np array) Data to be gap filled. The data is assumed to be sequential
+        in time, with gaps indicated by Nans. The characteristic value of the data
+        (i.e the mean or median) is assumed to be close to zero. If not, edge effects
+        will be evident near the start and end of the data.
+    
+    Optional Inputs
+    --------------------
+    bad_value
+        (float) Use this to set the value for elements of y that are in gaps. Default
+        is Nan, but you can set to any floating point value.
+    small_size
+        (int) Controls the threshold between small and large gaps (see below)
+    
+    The algorithm proceeds in 3 steps.
+    1. Gaps are identifed as sequences in y where the values are equal to `bad_value`
+    2. Small gaps (less than small_size) are replaced by the average of nearby points.
+    3. Large gaps are infilled with a spline interpolation across the gaps. 
+
+
+    Returns
+    ----------
+    y
+        (1d np array) Original array with gaps filled 
+    idx
+        (1d np array bool) True if that element is interpolated
+    """
+    y = y.copy()
+
+    if np.isfinite(bad_value):
+        idx = y == bad_value
+    else:
+        idx = np.isnan(y)
+
+    y = fix_ends(y, idx)
+
+    gaps = plateau.plateau(idx.astype(float), .5)
+    if len(gaps) == 0:
+        return y, idx
+
+    y = fill_small_gaps(y, gaps, small_size)
+    y = fill_large_gaps_cubic(y, gaps, small_size)
+
+    idx = plateau.convert_plateau_to_index(gaps, len(y))
+    return y, idx
+
+
+def fix_ends(y, idx):
+    """Back fill a gap at the start of the array with the first good value.
+    
+    Forward fill a gap at the end of the array with the last good value
+    """
+    #Back fill empty first elts
+    gaps = plateau.plateau(idx.astype(float), .5)
+    if len(gaps) == 0:
+        return y
+
+    if gaps[0,0] == 0:
+        first = gaps[0,1] +1
+        fill_val = y[first]
+        y[:first] = fill_val
+    
+    #Forward fill empty last elts 
+    if gaps[-1, 1] == len(y):
+        last = gaps[-1, 0] - 1
+        fill_val = y[last]
+        y[last:] = fill_val
+    return y
+
+
+def fill_small_gaps(y, gaps, max_size):
+    if len(gaps) == 0:  #No gaps to fill
+        return y 
+
+    gap_size = gaps[:,1] - gaps[:,0]
+    gaps = gaps[gap_size <= max_size]
+
+    for g in gaps:
+        left, right = g[0], g[1]
+        lwr = max(left - max_size, 0)
+        upr = min(right + max_size + 1, len(y))
+        snippet = y[lwr:upr]
+        assert len(snippet) > 0
+        fill_value = np.nanmean(snippet)
+        y[left:right] = fill_value 
+    return y
+
+
+def fill_large_gaps(y, gaps, min_size):
+    """Deprecated. Use fill_large_gaps_cubic instead"""
+    if len(gaps) == 0:  #No gaps to fill
+        return y 
+
+    gap_size = gaps[:,1] - gaps[:,0]
+    gaps = gaps[gap_size > min_size]
+
+    t = np.arange(len(y))
+
+    #This works better. In fill with old data
+    #TODO in fill with old data from left and right (not just left)
+    #then make them match up in the middle
+    for g in gaps:
+        pad_size = int( (g[1] - g[0]))
+        left = np.max([g[0] - pad_size, 0])
+        # right = np.min([g[1] + pad, len(y)])
+        # slice_l = slice(left, g[0])
+        range_to_interp = np.arange(g[0], g[1])
+        
+        fill_data = y[left:g[0]]
+        if len(fill_data) < pad_size:
+            #resize automatically fills the new array with copies of old data
+            fill_data = np.resize(fill_data, pad_size)
+
+        y[range_to_interp] = fill_data
+
+    return y
+
+
+def fill_large_gaps_cubic(y, gaps, min_size):
+    if len(gaps) == 0:
+        return y
+        
+    gap_size = gaps[:,1] - gaps[:,0]
+    gaps = gaps[gap_size > min_size]
+    y_out = y.copy()
+    for g in gaps:
+        pad_size = int( (g[1] - g[0]))
+        y_tmp = fill_single_large_gap_cubic(y, g[0], g[1], pad_size)
+        y_out[ g[0]:g[1] ] = y_tmp #fill the gap
+
+        #TODO Add noise?
+    return y_out
+
+
+def fill_single_large_gap_cubic(y, y1, y2, pad_size):
+    """Fill a large gap in a timeseries using cubic interpolation.
+    
+    Fit a cubic polynomial to a sequence before a gap, and again
+    to a sequency after the gap. The fit a cubic polynomial over the gap
+    that has the same value and slope as the anchoring polynomials on either side
+
+    Works, but not well tempered in production.
+
+    Inputs
+    --------
+    y
+        (1d np array) Data set in which to fill the gap.
+    y1, y2 
+        (ints) indices of start and end of gap 
+    pad_size
+        (int) Number of indices of data before and after to fit to. 
+
+
+    Returns
+    -----------
+    A numpy array of length (y2-y1) representing the interpolated polynomial.
+    You may want to add some noise to the interpolation before applying smoothing
+
+    Explanation
+    ------------
+    A polynomial is of the form :math:`y = ax^3 + bx^2 + cx + d`. The slope 
+    of the polynomial is is :math:`y' = 3ax^3 + 2bx + c`.
+
+    At x=0, (y, y') has the same values as the polynomial fit to the "before" data.
+    At x=n, they have the same values as the polynomial fit to the "after" data. 
+    Working through the simultaneous equations gives us the values of a, b, c and d
+    for the interpolating polynomial.
+
+    """
+
+    #Order of indices: 0..left..start (gap) end ..right...
+    y0 = np.max([y1 - pad_size, 0])
+    y3 = np.min([y2 + pad_size, len(y)])
+    
+    #import ipdb; ipdb.set_trace()
+    #if y1 > y0:
+        #left_anchor = y[y0:y1]
+    #else:
+        #left_anchor = [y[y0]]
+
+    #if y3 > y2:
+        #right_anchor = y[y2:y3]
+    #else:
+        #right_anchor = y[y3-1]
+
+    if y0 == y1 or y2 == y3:
+        #One of our anchor points is at the end of the array
+        #Not 100% sure of this what happens if first data point is good?
+        return y[y1:y2]
+    
+    left_anchor = y[y0:y1]
+    left_anchor = left_anchor[ np.isfinite(left_anchor)]
+    
+    right_anchor = y[y2:y3]
+    right_anchor = right_anchor[ np.isfinite(right_anchor)]
+    
+        
+    ys, ms = get_params_of_anchor_section(left_anchor, left=True)
+    ye, me = get_params_of_anchor_section(right_anchor, left=False)
+
+    c = ms
+    d = ys 
+    dy = ye - ys 
+    dm = me - ms 
+    size = y2 - y1
+    assert size > 3, "Not enough points in anchors to do fit"
+
+    Amat = np.array([size**3, size**2, 3*size**2, 2*size]).reshape((2,2))
+    bVec = np.array([dy - ms*size, dm]).reshape((2,1))
+    res = np.linalg.solve(Amat, bVec)
+    a, b = res[0], res[1]
+
+    x = np.arange(size)
+    y = (((a * x) + b) * x + c) * x + d
+    assert np.all(np.isfinite(y))
+    return y 
+
+    
+def get_params_of_anchor_section(y, left=True):
+    #TODO Robust fit?
+    x = np.arange(len(y))
+    fobj = Lsf(x, y, 1, 4)
+    
+    pars = fobj.getParams()
+    d, c, b, a = pars
+
+    if left:
+        x0 = x[-1]
+    else:
+        x0 = x[0]
+
+    offset = fobj.getBestFitModel(x0)[0]
+    slope = ((3*a * x0) + 2*b)*x0 + c
+    return offset, slope
+
+
+
+import matplotlib.pyplot as plt 
+def test_single_large_gap():
+    x = np.arange(1000)    
+    y = np.sin(2*np.pi*x/250)
+    
+    t1, t2 = 200, 340
+    y[t1:t2] = np.nan
+    y[t2:] -= 2
+
+    plt.clf()
+    plt.plot(x, y, 'r.-')
+
+    gaps = np.array( [ [t1, t2]])
+    y0 = y.copy()
+    y_gap = fill_large_gaps_cubic(y, gaps, 3)
+    y[ gaps[0,0]:gaps[0,1]] = y_gap
+    
+    plt.plot(x[t1:t2], y_gap, 'b.-')
+
+# def test_fill_large_gaps1():
+#     x = np.arange(1000)    
+#     y = np.sin(2*np.pi*x/250)
+#     y[200:300] = np.nan
+
+#     plt.clf()
+#     plt.plot(x, y, 'r-')
+
+#     gaps = np.array( [ [200, 300]])
+#     y0 = y.copy()
+#     y = fill_large_gaps(y, gaps, 3)
+
+#     plt.plot(x, y, 'b--')
+
+
+# def test_fill_large_gaps2():
+#     x = np.arange(1000)    
+#     y = np.sin(2*np.pi*x/250)
+#     y[500:] = np.sin( - 2* np.pi*x[500:]/250 + 125 ) - 1.5
+
+
+#     y[475:575] = np.nan
+#     plt.clf()
+#     plt.plot(x, y, 'r-')
+
+#     gaps = np.array( [ [475, 575]])
+#     y0 = y.copy()
+#     y = fill_large_gaps(y, gaps, 3)
+
+#     plt.plot(x, y, 'b--')
---
+++ packages/frmbase/frmbase-0.2.0.tar.gz/frmbase-0.2.0/frmbase/support.py
@@ -0,0 +1,474 @@
+"""
+Created on Mon Nov 28 14:27:46 2016
+
+Convenience functions to speed development
+
+Inventory:
+---------------
+
+Decorators
+-------------
+@timer
+    Print name of function being run, then time elapsed while it ran
+
+@cache
+    Save results of running a function with one argument to a cache file
+
+
+Other functions:
+Timer()
+    Time blocks of code instead of a single function.
+
+
+@author: fergal
+"""
+
+from ipdb import set_trace as idebug
+from glob import glob
+import pandas as pd
+import numpy as np
+import datetime
+import inspect
+import math
+import os
+import re
+
+
+#Work around for windows
+if 'HOME' not in os.environ:
+    try:
+        os.environ['HOME'] = os.environ['USERPROFILE']
+    except KeyError:
+        pass 
+
+
+
+def check_cols_in_df(df: pd.DataFrame, cols: list) -> bool:
+    """Because I always get this wrong"""
+    return check_columns_in_df(df, cols)
+
+def check_columns_in_df(df: pd.DataFrame, cols: list) -> bool:
+    """Check that every element of cols in a column in the dataframe
+
+    Inputs
+    --------
+    df
+        A dataframe
+    cols
+        (list or iterable) Columns you expect in dataframe
+
+
+    Returns
+    -------
+    **True** if all `cols` are columns in dataframes
+    """
+    return len(set(cols) - set(df.columns)) <= 0
+
+
+
+def count_duplicates(data):
+    """Count the number of each occurence of an element in a list
+
+    Input
+    ----------
+    data
+        (list, array, or any 1d iterable)
+
+
+    Returns
+    ------------
+    A dictionary
+    """
+
+    out = dict()
+    for elt in data:
+        try:
+            out[elt] += 1
+        except KeyError:
+            out[elt] = 1
+
+    return out
+
+
+def chop(string):
+    return string[:-1]
+
+def chomp(string):
+    if string[-1] == '\n':
+        return string[:-1]
+    return string
+
+
+def int_to_bin_str(arr):
+    """Convert a 32bit int (or numpy array of ints) to a bit string
+
+    Inputs
+    -------
+    arr
+        (int or np array of ints). Values to convert
+
+    Returns
+    --------
+    string, or array of strings
+
+
+    Example
+    ----------
+    12 ---> "0000 0000 0000 0000 0000 0000 0000 1100"
+    """
+
+    is_int = False
+    if isinstance(arr, int) or isinstance(arr, np.integer):
+        arr = np.array([arr])
+        is_int = True
+
+    vals = []
+    for n in arr:
+        strr = format(np.uint32(n), '032b')
+        out = []
+        for i in range(0, 32, 4):
+            out.append(strr[i:i+4])
+        vals.append(" ".join(out))
+
+    if is_int:
+        return vals[0]
+    else:
+        return np.array(vals)
+
+
+def is_evenly_spaced(y):
+    diff = np.diff(y)
+    return np.all(diff == diff[0])
+
+    
+from typing import Union, Optional, Callable
+def load_df_from_pattern(pattern, loader: Optional[Union[str, Callable]]=None, **kwargs) -> pd.DataFrame:
+    """Load a set of files whose paths match pattern
+
+    This only works for local files. Results are 
+    stored in a dataframe
+
+    Inputs
+    --------
+    pattern (str)
+        A string that is globbed to find all appropriate files on disk
+    loader (func, or string)
+        Function that loads individual files. Must return a dataframe.
+        Eg pd.read_csv, pd.read_parquet, etc
+        If this is a string (eg csv), or None, function will try to guess the
+        correction loading function. 
+
+    All optional inputs are passed to `loader`
+
+    Returns
+    ----------
+    A Pandas dataframe
+    """
+    #Just because function exists, doesn't mean required packages
+    #are installed
+    opts = {
+        'csv': pd.read_csv,
+        'parquet': pd.read_parquet,
+        'json': pd.read_json,
+        'xls': pd.read_excel,
+    }
+
+    add_src = kwargs.pop('source', False)
+    flist = glob(pattern)
+
+    if len(flist) == 0:
+        raise ValueError("No files found matching %s" %(pattern))
+
+    #Choose a loading function
+    if not hasattr(loader, '__call__'):
+        if isinstance(loader, str):
+            ext = loader
+        elif loader is None:
+            ext = os.path.splitext(flist[0])[-1]
+            ext = ext[1:]  #Remove . at start
+        else:
+            raise ValueError("loader should be a string or a function")
+
+        try:
+            loader = opts[ext]
+        except KeyError:
+            raise ValueError("Unrecognised file type %s" %(ext))
+
+    def f(fn):
+        try:
+            df = loader(fn, **kwargs)
+        except Exception as e:
+            raise IOError("Error parsings %s" %fn) from e
+
+        if add_src:
+            df['_source'] = fn
+        return df
+
+    # f = lambda x: loader(x, **kwargs)
+    dflist = list(map(f, flist))
+    return pd.concat(dflist)
+
+
+def npregex(pattern, array):
+    """Search an numpy array of strings to a pattern
+
+    Inputs
+    ---------
+    pattern
+        string or regex object
+    array
+        Numpy array to search
+
+    Returns
+    -----------
+    A boolean array of shape equal to pattern
+    """
+
+    f = np.vectorize( lambda x: bool(re.search(pattern, x)))
+    return f(array)
+
+
+def npmap(function, *array):
+    """Perform a map, and package the result into an np array"""
+    return np.array( lmap(function, *array))
+
+def lmap(function, *array):
+    """Perform a map, and package the result into a list, like what map did in Py2.7"""
+    return list(map(function, *array))    
+
+def orderOfMag(val):
+    """Return the order of magnitude of value
+
+    e.g orderOfMag(9999) is 3
+
+    Input
+    val (float)
+
+    Return
+    (int)
+    """
+
+    return int(math.floor(math.log10(math.fabs(float(val)))))
+
+
+
+def printse(val, err, sigdig=2, pretty=True):
+    """Return a value and its uncertainty in SI format, and
+    scientific notation, eg 12.345(67)e7
+
+    Input
+    val, err (float) The value and it's associated uncertainty
+    sigdig (int)     Number of significant digits to print
+    pretty (bool)    If true print the number as 12.345(67).
+                     If false, return 12.345 0.0067
+                     pretty option not implemented yet
+
+    Returns:
+    A string
+
+    Note
+    ------
+    If your number has no uncertainty, consider printh instead.
+
+    """
+
+    vex=orderOfMag(float(val))
+    eex=orderOfMag(float(err))
+
+    val /= 10**vex
+    err /= 10**eex
+
+    big = max(vex, eex)
+    val /= 10**(big-vex)
+    err /= 10**(big-eex)
+
+    strr = printe(val, err, sigdig=sigdig, pretty=pretty)
+    if pretty:
+        strr = "%se%02i" %(strr, big)
+    else:
+        tmp = strr.split()
+        strr= "%se%02i %se%02i" %(tmp[0], big, tmp[1], big)
+
+    return strr
+
+
+def printe(val, err, sigdig=2, pretty=True):
+    """Return a value and its uncertainty in SI format, 12.345(67)
+
+    Input
+    val, err (float) The value and it's associated uncertainty
+    sigdig (int)     Number of significant digits to print
+    pretty (bool)    If true print the number as 12.345(67).
+                     If false, return 12.345 0.0067
+
+    Returns:
+    A string
+
+    Note
+    ------
+    If your number has no uncertainty, consider printh instead.
+    """
+
+    val = float(val)
+    err = float(err)
+
+    if err == 0:
+        strr = "%.6f 0" %( val)
+        return strr
+
+    #Significant digits in the value
+    sd = int(max(0, sigdig-1-orderOfMag(err)))
+
+
+    if pretty:
+        if err > 1 and err < 10:
+            strr = " %.*f(%.1f)" %(sd, val, err)
+        else:
+            if sd !=0:
+                err /= pow(10, orderOfMag(err)-1)
+            #err /= pow(10, orderOfMag(err)-2)
+            strr = "%.*f(%g)" %(sd,val,round(err))
+    else:
+        if err > 1 and err < 10:
+            strr = " %.*f %.1f" %(sd, val, err)
+        else:
+            #err /= pow(10, orderOfMag(err)-1)
+            #strr = " %.*f(%g)", sd, val, math.rint(err)
+            strr = "%.*f %.*f" %(sd,val,sd,err)
+
+    return strr
+
+
+def printh(num:float, unit:str = None) -> str:
+    """Convert a number into a human readable format by adding a suffix
+
+    Like k for thousands, M for millions, etc.
+
+    Input
+    -------------
+    num
+        Number to parse
+    unit
+        (Optional) Units the number is in like metres or Watts. Beware
+        of numbers that already have suffixes like kg or MW.
+
+    Note
+    -----
+    If you number has an associated uncertainty, consider using printe,
+    or printse instead.
+    """
+    postfix = {
+         0:"",
+         3:'k',
+         6:'M',
+         9:'G',
+        12:'T',
+        -3:'m',
+        -6:'μ',
+        -9:'n',
+       -12:'p',
+    }
+
+    if unit is None:
+        unit = ""
+
+    order = np.floor(np.log10(num))
+    order -= int(order % 3)  #Round to the nearest multiple of three
+    modifier = postfix[order]
+    val = num * 10**(-order)
+    text = "%.2f %s%s" %(val, modifier, unit)
+    return text
+
+
+
+class Timer(object):
+    """Time a section of code.
+
+    Usage:
+    ----------------------
+    .. code-block:: python
+
+        with Timer("Some text"):
+            a = long_computation()
+            b = even_longer_computation()
+
+    Prints "Some text" to the screen, then the time taken once the computation
+    is complete.
+    """
+
+    def __init__(self, msg=None):
+        self.msg = msg
+
+    def __enter__(self):
+        self.t0 = datetime.datetime.now()
+        print (self.msg)
+
+    def __exit__(self, type, value, traceback):
+        t1 = datetime.datetime.now()
+
+        if self.msg is None:
+            self.msg = "Elapsed Time"
+
+        print ("%s %s" %(self.msg, t1 - self.t0))
+
+
+
+def scale_to_range(y, mn, mx):
+    """Linearly scale *y* so it's spans the range [mn,mx]"""
+    mny = np.min(y)
+    mxy = np.max(y)
+
+    assert mny < mxy, "y is single valued"
+
+    out = (y - mny) / (mxy - mny)    #Scale to [0,1]
+    out = out * (mx-mn) + mn
+    return out
+
+
+def wmean(values, unc):
+    """Computed the weighted average and uncertainty
+
+    Inputs
+    ---------
+    values
+        (array of floats)
+    unc
+        (array of floats) 1 sigma uncertainty on values
+
+    Returns
+    ---------
+    (mean, unc), two floats
+    """
+
+    assert len(values) == len(unc)
+    values = np.array(values)
+    unc = np.array(unc)
+
+    #Need a least one positive value and no negative values
+    assert np.any(unc > 0)
+    assert np.all(unc >= 0)
+
+    weights = 1 / unc**2
+    numer = np.sum(values * weights)
+    denom = np.sum(weights)
+
+    wmean = numer / denom
+    wunc = 1/np.sqrt(denom)
+
+    return wmean, wunc
+
+
+#
+# Decorators
+#
+
+
+def timer(func):
+    """A decorator to time your function"""
+    def wrapperForFunc(*args, **kwargs):
+         with Timer(func.__name__):
+             return func(*args, **kwargs)
+
+    return wrapperForFunc
+
+
