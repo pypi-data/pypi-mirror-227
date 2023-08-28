381989094f37bf50056764ebd4724f1158740170
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-28 04:12:23 +0000

    Add package frmbase/0.1.0/frmbase-0.1.0.tar.gz

---
+++ packages/frmbase/frmbase-0.1.0.tar.gz/frmbase-0.1.0/frmbase/flogger.py
@@ -0,0 +1,187 @@
+from colorama import Fore
+import traceback
+import datetime
+import inspect
+import logging
+import os
+
+"""Simpler replacement to python's logging infrastructure
+
+I can never understand how to get Python's logging module to work how I want. For example,
+why doesn't this code print anything?::
+
+    import logging
+    logging.info("Hello")  #Default log level is WARN, don't expect anything
+    logging.basicConfig(level=logging.INFO)
+    logging.info("Goodbye")  #Also prints nothing
+
+
+This simplifies things by
+
+* Handles the non-intuitive requirements of setting up a logging object
+* Makes it easy to change log level on the fly
+* Only sends log messages to stderr. No multiple handlers
+* Fixed format for prefix: Looks something like
+    INFO 2021-06-23T13:43:41 predict_peak.py:save_result:38 Your message here
+
+
+Usage
+------------::
+
+    logger = flogger.Logger()
+    logger.setLevel(flogger.DEBUG)
+    logger.warn("This is a warning...")
+
+
+The module also creates a singleton object that can be used without initialisation. Changes to verbosity
+in this singleton object are felt globally. Usage::
+
+    flogger.log.warn("This is a warning")
+
+Notes
+---------
+The thread-safeness of this code has not been tested, or thought about too much. That said, it
+uses the CPython logger under the hood, so it should work.
+
+TODO
+--------------
+x Add backtrace method
+x Better colours?
+o Ensure threadsafeness
+o A MultiLogger with different log levels and different outputs
+o Avoid the duplication inherent in get_level_str()
+"""
+
+ERROR = 10
+WARN = 20
+INFO = 30
+DEBUG = 40
+
+
+class BaseStyle:
+    """Defines the API for a stylist object
+
+    A stylist constructs a string to be logged based on the input level and messagge.
+    It is similar in intent to the Formatter class in the Cpython logging module, but
+    given a different name to avoid confusion. The API is quite different
+    """
+
+    def __call__(self, level, msg):
+        return self.format_msg(level, msg)
+
+    def format_msg(self, level, msg):
+        """Reimplement this in the daughter classes"""
+        return msg
+
+
+class DefaultStyle(BaseStyle):
+    def format_msg(self, level, msg):
+        date_fmt = "%Y-%m-%dT%H:%M:%S"
+
+        level_str, level_clr = self.get_level_str(level)
+        location_str = self.get_location_str()
+        date = datetime.datetime.now().strftime(date_fmt)
+        output = level_clr + "%s %s %s " % (level_str, date, location_str)
+        output = output + Fore.RESET + msg
+        return output
+
+    def get_level_str(self, level):
+        names = {
+            10: ("ERROR", Fore.RED),
+            20: ("WARN", Fore.MAGENTA),
+            30: ("INFO", Fore.CYAN),
+            40: ("DEBUG", Fore.RESET),
+        }
+        return names.get(level, "L%02i" % (level))
+
+    def get_location_str(self):
+        # Get the calling frame
+        frame = inspect.currentframe()
+        for i in range(5):
+            frame = frame.f_back
+
+        (filename, lineno, funcname, _, _) = inspect.getframeinfo(frame)
+        filename = filename.split(os.path.sep)[-1]
+        return "%s:%s:%s" % (filename, funcname, lineno)
+
+
+class MonochromeStyle(DefaultStyle):
+    """Prints everything in black and white"""
+
+    def get_level_str(self, level):
+        names = {
+            10: ("ERROR", ""),
+            20: ("WARN", ""),
+            30: ("INFO", ""),
+            40: ("DEBUG", ""),
+        }
+        return names.get(level, "L%02i" % (level))
+
+
+class Logger:
+    def __init__(self, level=INFO, style=DefaultStyle()):
+        # Sometimes I pass a class instead of an object. Intent is the same
+        if isinstance(style, type):
+            style = style()
+
+        self.stylist = style
+        self.level = level
+
+        # Create the python logger
+        self.log = logging.Logger("Flogger")
+        if len(self.log.handlers) == 0:
+            format = logging.Formatter("%(message)s")
+            handler = logging.StreamHandler()
+            handler.setFormatter(format)
+            self.log.addHandler(handler)
+        assert len(self.log.handlers) == 1
+
+    def debug(self, msg):
+        self.write(DEBUG, msg)
+
+    def info(self, msg):
+        self.write(INFO, msg)
+
+    def warning(self, msg):   #Mneumonic for warn
+        return self.warn(msg)  
+
+    def warn(self, msg):
+        self.write(WARN, msg)
+
+    def error(self, msg):
+        self.write(ERROR, msg)
+
+
+    def setLevel(self, level):
+        self.level = level
+
+    def getLevel(self):
+        return self.level
+
+    def log_exception(self, exc, level=ERROR):
+        """Log an exception and its backtrace as an error message
+
+        Example::
+
+            try:
+                1/0
+            except ZeroDivisionError as e:
+                log.log_exception(e)
+        """
+        msg = str(exc)
+        msg = msg + "\n" + traceback.format_exc()
+        self.write(level, msg)
+
+    #Mneumonc for log_exception
+    def exception(self, exc):
+        self.log_exception(exc)
+
+    def write(self, level, msg):
+        if level > self.level:
+            return
+        msg = self.stylist(level, msg)
+        self.log.log(1, msg)
+
+
+# Call this object directly if you don't want to muck about with creating multiple log objects
+log = Logger()
---
+++ packages/frmbase/frmbase-0.1.0.tar.gz/frmbase-0.1.0/frmbase/tstools.py
@@ -0,0 +1,140 @@
+
+import scipy.ndimage as spimg
+import numpy as np
+
+#Functions included as a convenience
+from frmbase.plateau import plateau, convert_plateau_to_index
+from frmbase.gapfill import fill_gaps
+
+def median_detrend(y, filtersize):
+    """Apply a median-detrend high pass filter to y
+    
+    Inputs
+    ------
+    y
+        (numpy array) Array to be detrended
+    filtersize
+        (int) Length of filter in same units as y
+
+    Returns
+    ----------
+    An array the same shape as y ,with long term trends removed, and short term signal preserved.
+    """
+    if filtersize %2 == 0:
+        filtersize += 1
+
+    filt = spimg.median_filter(y, filtersize, mode='reflect')
+    return y - filt
+
+
+def identify_single_point_outliers(y_orig):
+    tol = 4
+    y = np.diff(y_orig)
+    rms = 1.4826 * mad(y)  #A robust estimate of mad, assuming Gaussian scatter
+    roll = np.roll(y, 1)
+
+    #Maybe a hypotenuse here?
+    #y**2 + roll**2 > (tol*rms)**2
+    idx = (np.fabs(y) > tol * rms) & (np.fabs(roll) > tol * rms) & (y * roll < 0) 
+    idx = np.concatenate([idx, np.array([False])])
+    return idx
+
+
+def mad(y):
+    """Compute median absolute deviation of a timeseries.
+    
+    MAD is a robust estimator of the noise. For purely Gaussian data,
+    std(y) = 1.4826 * mad(y)
+    
+    """
+    assert len(y) > 1
+    return np.median(np.fabs(y))
+
+
+def find_peaks(y: np.ndarray) -> np.ndarray:
+    """Find values in y that are larger than values immediately before or after
+
+    Note, by this definition of a peak, the zeroth element of an array
+    can't be a peak, neither can the last.
+
+    Note: If searching for peaks in noisy data, apply some smoothing first
+    to prevent every second point being declared a peak
+
+
+    Inputs
+    ----------
+    y
+        Data to search 
+    
+    Returns
+    ----------
+    A 1d boolean array of length `len(y)`. Values are **True** if that
+    index corresponds to a peak
+    """
+
+
+    diff = np.diff(y, axis=0)
+    idx = (diff >= 0) & (np.roll(diff, -1, axis=0) < 0)
+
+    #If y[0] > y[-1], y[-1] will be set to True, even though
+    #it's not a peak by our defintion. We force the last element to False here,
+    #and pad the array with a leading zero to ensure the corrent length
+    idx = np.concatenate( [[False], idx[:-1], [False]])
+    assert len(idx) == len(y)
+    return idx 
+
+
+def sigma_clip(y, nSigma, maxIter=1e4, initialClip=None):
+    """Iteratively find and remove outliers
+    Find outliers by identifiny all points more than **nSigma** from
+    the mean value. The recalculate the mean and std and repeat until
+    no more outliers found.
+    Inputs:
+    ----------
+    y
+        (1d numpy array) Array to be cleaned
+    nSigma
+        (float) Threshold to cut at. 5 is typically a good value for
+        most arrays found in practice.
+    Optional Inputs:
+    -------------------
+    maxIter
+        (int) Maximum number of iterations
+    initialClip
+        (1d boolean array) If an element of initialClip is set to True,
+        that value is treated as a bad value in the first iteration, and
+        not included in the computation of the mean and std.
+    Returns:
+    ------------
+    1d numpy array. Where set to True, the corresponding element of y
+    is an outlier.
+    """
+    #import matplotlib.pyplot as mp
+    idx = initialClip
+    if initialClip is None:
+        idx = np.zeros( len(y), dtype=bool)
+
+    assert(len(idx) == len(y))
+
+    #x = np.arange(len(y))
+    #mp.plot(x, y, 'k.')
+
+    oldNumClipped = np.sum(idx)
+    for i in range(int(maxIter)):
+        mean = np.nanmean(y[~idx])
+        std = np.nanstd(y[~idx])
+
+        newIdx = np.fabs(y-mean) > nSigma*std
+        newIdx = np.logical_or(idx, newIdx)
+        newNumClipped = np.sum(newIdx)
+
+        #print "Iter %i: %i (%i) clipped points " \
+            #%(i, newNumClipped, oldNumClipped)
+
+        if newNumClipped == oldNumClipped:
+            return newIdx
+
+        oldNumClipped = newNumClipped
+        idx = newIdx
+        i+=1
+    return idx
