091b93d45375b2a6d7991812dddb608c88d5339b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-28 03:10:39 +0000

    Add package opendeliverybot/0.2.3/opendeliverybot-0.2.3.tar.gz

---
+++ packages/opendeliverybot/opendeliverybot-0.2.3.tar.gz/opendeliverybot-0.2.3/opendeliverybot/__init__.py
@@ -0,0 +1,15 @@
+import os
+import sys
+from console_ui import consoleui
+
+mode = 1
+try:
+    if sys.stdin.isatty():
+        mode = 0
+except AttributeError:  # stdin is NoneType if not in terminal mode
+    pass
+
+if mode == 0:
+    consoleui()
+else:
+    os.system("streamlit run web.py")
