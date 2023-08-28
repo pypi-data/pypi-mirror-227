aad228101457f83ab811348a1429a48cb83cca4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-28 03:12:03 +0000

    Add package bovine-pubsub/0.3.2/bovine_pubsub-0.3.2.tar.gz

---
+++ packages/bovine-pubsub/bovine_pubsub-0.3.2.tar.gz/bovine_pubsub-0.3.2/bovine_pubsub/amqp_iterator.py
@@ -0,0 +1,35 @@
+import json
+import logging
+from bovine.types import ServerSentEvent
+
+logger = logging.getLogger(__name__)
+
+
+class AmqpIterator:
+    def __init__(self, queue, clients):
+        self.clients = clients
+        self.queue = queue.iterator()
+
+    async def __aenter__(self):
+        await self.queue.__aenter__()
+        return self
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        await self.queue.__aexit__(exc_type, exc_val, exc_tb)
+
+    def __aiter__(self):
+        return self
+
+    async def __anext__(self):
+        try:
+            message = await self.queue.__anext__()
+
+            async with message.process():
+                client = self.clients[message.routing_key]
+                body = message.body.decode("utf-8")
+                event = ServerSentEvent.parse_utf8(body)
+
+            return client, json.loads(event.data)
+        except Exception as e:
+            logger.exception(e)
+            return None, None
