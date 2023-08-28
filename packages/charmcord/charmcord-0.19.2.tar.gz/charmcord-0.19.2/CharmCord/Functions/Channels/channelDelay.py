a21c65dfbea88f51e59a8a72b1f7e9ce571ab414
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-28 04:11:58 +0000

    Add package charmcord/0.19.2/charmcord-0.19.2.tar.gz

---
+++ packages/charmcord/charmcord-0.19.2.tar.gz/charmcord-0.19.2/CharmCord/Functions/Channels/channelDelay.py
@@ -0,0 +1,20 @@
+import CharmCord.CharmErrorHandling as ErrorHandling
+
+EH = ErrorHandling.CharmErrorHandling()
+
+
+async def channelDelay(ID, Context):
+    """
+    Ex. $channelDelay[ChannelID]
+    Returns the channel delay for the given channel ID
+    """
+    if len(ID) < 1:
+        raise EH.Errors(4, "No parameter provided for '$channelDelay'")
+    from CharmCord.Classes.CharmCord import bots
+
+    try:
+        int(ID)
+        channel = await bots.fetch_channel(ID)
+        return channel.slowmode_delay
+    except ValueError:
+        EH.Errors(2, ID)
