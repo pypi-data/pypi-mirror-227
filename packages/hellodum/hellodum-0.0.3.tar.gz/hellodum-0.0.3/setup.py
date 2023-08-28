d86d623525664c37ab27ab59f8dd3faddee48e49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-28 03:10:27 +0000

    Add package hellodum/0.0.3/hellodum-0.0.3.tar.gz

---
+++ packages/hellodum/hellodum-0.0.3.tar.gz/hellodum-0.0.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.3'
+DESCRIPTION = 'A Simple Hello package, bro!'
+LONG_DESCRIPTION = "Hasnain ne task diys tha kr liya"
+
+setup(
+    name='hellodum',
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    author='Huzaifa',
+    author_email='huzaifat65@outlook.com',
+    install_requires=['numpy'],
+    classifiers=[
+        # "Development State :: Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        ""
+    ]
+)
---
+++ packages/hellodum/hellodum-0.0.3.tar.gz/hellodum-0.0.3/zimzim/hello.py
@@ -0,0 +1,7 @@
+import numpy
+
+class hello:
+    def __init__(self) -> None:
+        pass
+    def oHello(self):
+        print('Hello Boy!!')
