d71c8e48aab8b01f851aaad0a6b0caea79d5f87d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-28 03:10:22 +0000

    Add package deskaone-requests/0.0.3/deskaone_requests-0.0.3.tar.gz

---
+++ packages/deskaone-requests/deskaone_requests-0.0.3.tar.gz/deskaone_requests-0.0.3/src/deskaone_requests/Utils/Typer.py
@@ -0,0 +1,69 @@
+from typing import Optional
+import os, sys, datetime, time
+from .Color import Color
+from deskaone_requests.Exceptions import Error, Stoper
+
+class Typer:
+    
+    @staticmethod
+    def Parse_Text(message: str):
+        Size = os.get_terminal_size()
+        Length = Size.columns
+        if len(message) > Length:message = message[:Length]
+        elif len(message) < Length:message  = message + ' ' * (Length - len(message))
+        return message
+    
+    @staticmethod
+    def Parse_Text_Line(message: str, Normal: Optional[bool] = None, Bold: Optional[bool] = None):
+        Size = os.get_terminal_size()
+        Length = int((Size.columns - 22) / 2)
+        if len(message) > Length:message = message[:Length]
+        elif len(message) < Length:
+            if Normal is True:message  = message + '-' * (Length - len(message))
+            elif Bold is True:message  = message + '=' * (Length - len(message))
+        return message
+    
+    @staticmethod
+    def Print(message: any, Refresh: Optional[bool] = None, Enter: Optional[bool] = None, isTimer: Optional[bool] = None): # type: ignore
+        if Refresh is None and Enter is None:raise Error('Refresh True or False / Enter True or False')
+        if Enter is True:
+            sys.stdout.write(f'{Typer.Parse_Text(" ")}\r')
+            sys.stdout.write(f'{message}{Color.WHITE}\n')
+            sys.stdout.write(f'{Typer.Parse_Text(" ")}\r')
+            sys.stdout.flush()
+        if Refresh is True:
+            sys.stdout.write(f'{message}{Color.WHITE}\r')
+            if isTimer is False:sys.stdout.write(f'{Typer.Parse_Text(" ")}\r')
+
+    @staticmethod
+    def Line(Normal: Optional[bool] = None, Bold: Optional[bool] = None):
+        if Normal is None and Bold is None:raise Error('Normal True or False / Bold True or False')
+        if Normal is True:sys.stdout.write(
+            f'{Typer.Parse_Text_Line("-", Normal=True)}{Color.BLUE}[{Color.YELLOW}{datetime.datetime.now().strftime("%d/%m/%Y, %H:%M:%S")}{Color.BLUE}]{Color.WHITE}{Typer.Parse_Text_Line("-", Normal=True)}{Color.WHITE}\n')
+        if Bold is True:sys.stdout.write(f'{Typer.Parse_Text_Line("=", Bold=True)}{Color.BLUE}[{Color.YELLOW}{datetime.datetime.now().strftime("%d/%m/%Y, %H:%M:%S")}{Color.BLUE}]{Color.WHITE}{Typer.Parse_Text_Line("=", Bold=True)}{Color.WHITE}\n')
+
+    @staticmethod
+    def Run(message: any, Refresh: Optional[bool] = None, Enter: Optional[bool] = None): # type: ignore
+        if Refresh is None and Enter is None:raise Error('Refresh True or False / Enter True or False')
+        if Refresh is True:
+            for c in str(message) + "\r":
+                sys.stdout.write(c)
+                sys.stdout.flush()
+                time.sleep(2 * 0.001)
+        if Enter is True:
+            for c in str(message) + "\n":
+                sys.stdout.write(c)
+                sys.stdout.flush()
+                time.sleep(2 * 0.001)
+
+    @staticmethod
+    def Input(message: any, Integer: Optional[bool] = None, String: Optional[bool] = None): # type: ignore
+        if Integer is None and String is None:raise Error('Integer True or False / String True or False')
+        if Integer is True:
+            while True:
+                try:return int(input(f'{Color.WHITE}{message}{Color.WHITE}'))
+                except KeyboardInterrupt:raise Stoper('Exit')
+        if String is True:
+            while True:
+                try:return str(input(f'{Color.WHITE}{message}{Color.WHITE}'))
+                except KeyboardInterrupt:raise Stoper('Exit')
---
+++ packages/deskaone-requests/deskaone_requests-0.0.3.tar.gz/deskaone_requests-0.0.3/src/deskaone_requests/__init__.py
@@ -0,0 +1,12 @@
+from .Client import Client
+from .Utils import *
+from .Firebase import Firebase
+from .Exceptions import *
+from .GAuth import GAuth
+
+from sqlalchemy.dialects.sqlite import *
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.sql import func
+from sqlalchemy.exc import IntegrityError
+import sqlalchemy
+Reset()
