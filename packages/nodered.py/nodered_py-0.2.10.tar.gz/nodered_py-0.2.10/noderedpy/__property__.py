c8233f047d9435559bde3a06afd633e7e53c3d61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-28 14:20:18 +0000

    Add package nodered.py/0.2.10/nodered_py-0.2.10.tar.gz

---
+++ packages/nodered.py/nodered_py-0.2.10.tar.gz/nodered_py-0.2.10/noderedpy/__property__.py
@@ -0,0 +1,114 @@
+# -*- coding: utf-8 -*-
+import json, pandas as pd
+from typing import Any, Union, List
+
+
+
+class Property:
+    def __init__(self, name:str, default:Any = None, required:bool = False, display_icon:str = None):
+        self.name, self.default, self.required, self.display_icon =\
+            name, default, required, display_icon
+        
+    @property
+    def display_name(self) -> str:
+        return " ".join([
+            item.capitalize()
+            for item in self.name.split("_")
+        ])
+    
+    @property
+    def var_name(self) -> str:
+        return f"np-var_{self.name}"
+
+class CodeProperty(Property):
+    def __init__(self, name:str, default:str = "", language:str = None, height:int = 250, required:bool = False, display_icon:str = None):
+        if not isinstance(default, str):
+            raise TypeError("CodeProperty can accept types: [ 'dict', 'json string' ]")
+        
+        super().__init__(name, default, required, display_icon if display_icon else "fa fa-code")
+        self.language, self.height =\
+            language, height
+
+class InputProperty(Property):
+    def __init__(self, name:str, default:Union[int, float, str] = None, required:bool = False, display_icon:str = None):
+        if default is None:
+            if isinstance(default, int):
+                default = 0
+            elif isinstance(default, float):
+                default = 0.0
+            else:
+                default = ""
+        elif not isinstance(default, (int, float, str)):
+            raise TypeError("InputProperty can accept types: [ 'int', 'float', 'str' ]")
+
+        super().__init__(name, default, required, display_icon)
+
+        if display_icon is None:
+            if isinstance(default, (int, float)):
+                self.display_icon = "fa fa-sort-numeric-asc"
+            else:
+                self.display_icon = "fa fa-font"
+        else:
+            self.display_icon = display_icon
+
+class ListProperty(Property):
+    def __init__(self, name:str, default:list = [], height:int = 250, required:bool = False, display_icon:str = None):
+        if not isinstance(default, list):
+            raise TypeError("ListProperty can accept type: 'list'")
+
+        super().__init__(name, default, required, display_icon if display_icon else "fa fa-list-ul")
+        self.height = height
+
+class DictProperty(CodeProperty):
+    def __init__(self, name:str, default:Union[dict, str] = {}, height:int = 250, required:bool = False, display_icon:str = None):
+        if not isinstance(default, ( dict, str )):
+            raise TypeError("DictProperty can accept types: [ 'dict', 'json string' ]")
+        
+        if isinstance(default, str):
+            if not default.strip().startswith("{") or not default.strip().endswith("}"):
+                raise ValueError("DictProperty value must be 'dict' or 'json string'!")
+
+            try:
+                json.loads(default)
+            except:
+                raise ValueError("DictProperty value must be 'dict' or 'json string'!")
+
+        if isinstance(default, dict):
+            default = json.dumps(default, indent = 4)
+
+        super().__init__(name, default, "json", height, required, display_icon if display_icon else "fa fa-file-code-o")
+
+class SpinnerProperty(Property):
+    def __init__(self, name:str, default:float = 0, step:float = None, min:float = None, max:float = None, required:bool = False, display_icon:str = None):
+        if not isinstance(default, (int, float)):
+            raise TypeError("SpinnerProperty can accept types: [ 'int', 'float' ]")
+
+        super().__init__(name, default, required, display_icon if display_icon else "fa fa-random")
+        self.step, self.min, self.max =\
+            step, min, max
+
+class CheckBoxProperty(Property):
+    def __init__(self, name:str, default:bool = False, required:bool = False, display_icon:str = None):
+        super().__init__(name, default, required, display_icon if display_icon else "fa fa-check")
+
+class ComboBoxProperty(Property):
+    def __init__(self, name:str, items:List[Any], default:str = None, required:bool = False, display_icon:str = None):
+        if not isinstance(items, list):
+            raise TypeError("items of ComboBoxProperty must be type: 'list'")
+        
+        if default is None:
+            if len(items) > 0:
+                default = items[0]
+            else:
+                default = ""
+        
+        super().__init__(name, default, required, display_icon if display_icon else "fa fa-filter")
+        self.items = items
+
+# class FileProperty(Property):
+#     def __init__(self, name:str, default:str = None, required:bool = False, display_icon:str = None):
+#         super().__init__(name, default if default else "", required, display_icon if display_icon else "fa fa-file-text-o")
+
+# class TableProperty(Property):
+#     def __init__(self, name:str, default:pd.DataFrame = None, required:bool = False, display_icon:str = None):
+#         super().__init__(name, default if default else pd.DataFrame(), required, display_icon if display_icon else "fa fa-table")
