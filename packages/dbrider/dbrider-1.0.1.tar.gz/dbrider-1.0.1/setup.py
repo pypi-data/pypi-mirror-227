d3d0bb5f4133d74bf057c1a53d6eaa38fb6c70d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-28 14:20:10 +0000

    Add package dbrider/1.0.1/dbrider-1.0.1.tar.gz

---
+++ packages/dbrider/dbrider-1.0.1.tar.gz/dbrider-1.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+setup(
+    name='dbrider',
+    packages=find_packages(include=['src/dbrider']),
+    version='1.0.1',
+    description='Database Rider for Python',
+    author='Nishant Shah',
+    license='Apache License 2.0',
+    install_requires=[
+        'PyYAML==6.0',
+        'peewee==3.14.10'
+    ],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest==7.1.1', 'pytest-mock==3.7.0', 'pytest-cov==3.0.0'],
+    test_suite='tests'
+)
