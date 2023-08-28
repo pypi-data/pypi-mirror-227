6dc95250b167a8e2b93e2cd4cf788b9cd9fb3038
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-28 04:13:05 +0000

    Add package GEDSpy/1.9.2/GEDSpy-1.9.2.tar.gz

---
+++ packages/GEDSpy/GEDSpy-1.9.2.tar.gz/GEDSpy-1.9.2/GEDSpy/__init__.py
@@ -0,0 +1,29 @@
+print()
+print('Welcome in GEDSpy v.1.9.2 library')
+print('')
+print('Loading required packages...')
+
+import zipfile
+import os
+import urllib.request 
+
+
+_libd = os.getcwd()
+_libd = os.path.join(_libd, '..')
+
+
+
+if 'data' not in os.listdir(_libd):
+    print('The first run of the GEDSpy library requires additional requirements to be installed, so it may take some time...')
+    urllib.request.urlretrieve('https://github.com/jkubis96/GEDSpy/raw/v.2.0.0/data.zip', _libd + '/data.zip')
+    os.makedirs(_libd + '/data', exist_ok=True)
+    with zipfile.ZipFile(_libd + '/data.zip', 'r') as zipf:
+        zipf.extractall(_libd + '/data'),
+    os.makedirs(_libd + '/data/tmp', exist_ok=True)
+    os.remove(_libd + '/data.zip')
+   
+
+
+print('GEDSpy is ready to use')
+
+
