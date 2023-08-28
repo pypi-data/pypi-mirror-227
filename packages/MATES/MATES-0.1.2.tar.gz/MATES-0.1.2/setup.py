7276866d866079f68223dc714a6e341f6c4fa002
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-28 04:13:04 +0000

    Add package MATES/0.1.2/MATES-0.1.2.tar.gz

---
+++ packages/MATES/MATES-0.1.2.tar.gz/MATES-0.1.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+setup(
+    name="MATES",
+    version="0.1.2",
+    packages=find_packages(),
+    install_requires=["matplotlib==3.7.2",
+        "numpy==1.25.2",
+        "pandas==2.0.0",
+        "pybedtools==0.9.1",
+        "pysam==0.21.0",
+        "scipy==1.11.2",
+        "torch==2.0.1",
+        "tqdm==4.66.1"],
+)
