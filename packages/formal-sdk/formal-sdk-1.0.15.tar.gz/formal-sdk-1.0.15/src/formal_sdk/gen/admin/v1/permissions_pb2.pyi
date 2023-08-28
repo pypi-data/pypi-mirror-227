4a0a49a700050872f681ed1a8b4f1b1eab3369df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-28 03:12:20 +0000

    Add package formal-sdk/1.0.15/formal-sdk-1.0.15.tar.gz

---
+++ packages/formal-sdk/formal-sdk-1.0.15.tar.gz/formal-sdk-1.0.15/src/formal_sdk/gen/admin/v1/permissions_pb2.pyi
@@ -0,0 +1,23 @@
+from google.protobuf.internal import containers as _containers
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from typing import ClassVar as _ClassVar, Mapping as _Mapping, Optional as _Optional
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class GetPermissionsRequest(_message.Message):
+    __slots__ = []
+    def __init__(self) -> None: ...
+
+class GetPermissionsResponse(_message.Message):
+    __slots__ = ["permissions"]
+    class PermissionsEntry(_message.Message):
+        __slots__ = ["key", "value"]
+        KEY_FIELD_NUMBER: _ClassVar[int]
+        VALUE_FIELD_NUMBER: _ClassVar[int]
+        key: str
+        value: bool
+        def __init__(self, key: _Optional[str] = ..., value: bool = ...) -> None: ...
+    PERMISSIONS_FIELD_NUMBER: _ClassVar[int]
+    permissions: _containers.ScalarMap[str, bool]
+    def __init__(self, permissions: _Optional[_Mapping[str, bool]] = ...) -> None: ...
