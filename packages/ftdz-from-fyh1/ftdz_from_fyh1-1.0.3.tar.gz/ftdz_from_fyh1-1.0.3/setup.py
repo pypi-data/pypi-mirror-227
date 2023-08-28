f6886f1dec3c4084c369736f92c130cbf594d883
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-28 14:20:11 +0000

    Add package ftdz-from-fyh1/1.0.3/ftdz_from_fyh1-1.0.3.tar.gz

---
+++ packages/ftdz-from-fyh1/ftdz_from_fyh1-1.0.3.tar.gz/ftdz_from_fyh1-1.0.3/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+setuptools.setup(
+    name='ftdz_from_fyh1',
+    version='1.0.3',
+    author='bob',
+    author_email='56141537@qq.com',
+    description='XESAPI',
+    long_description_content_type="""text/markdown""",
+    url='https://www.github.com/',
+    packages=['ftdz_from_fyh1'],
+    install_requires=["requests"],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ]
+)
