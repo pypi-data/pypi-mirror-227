b7fabd8d4aad9761cb7e961e5eff3edbad0cbc71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-28 04:11:59 +0000

    Add package pyscope/0.1.1/pyscope-0.1.1.tar.gz

---
+++ packages/pyscope/pyscope-0.1.1.tar.gz/pyscope-0.1.1/pyscope/observatory/ascom_filter_wheel.py
@@ -0,0 +1,32 @@
+import logging
+
+from .ascom_driver import ASCOMDriver
+from .filter_wheel import FilterWheel
+
+logger = logging.getLogger(__name__)
+
+
+class ASCOMFilterWheel(FilterWheel, ASCOMDriver):
+    def Choose(self, FilterWheelID):
+        logger.debug(f"ASCOMFilterWheel.Choose({FilterWheelID}) called")
+        self._com_object.Choose(FilterWheelID)
+
+    @property
+    def FocusOffsets(self):
+        logger.debug(f"ASCOMFilterWheel.FocusOffsets property called")
+        return self._com_object.FocusOffsets
+
+    @property
+    def Names(self):
+        logger.debug(f"ASCOMFilterWheel.Names property called")
+        return self._com_object.Names
+
+    @property
+    def Position(self):
+        logger.debug(f"ASCOMFilterWheel.Position property called")
+        return self._com_object.Position
+
+    @Position.setter
+    def Position(self, value):
+        logger.debug(f"ASCOMFilterWheel.Position property set to {value}")
+        self._com_object.Position = value
