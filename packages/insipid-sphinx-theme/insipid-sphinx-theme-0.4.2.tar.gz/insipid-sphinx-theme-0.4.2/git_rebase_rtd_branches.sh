21e6fba46038b780355ebfc4baf4739a7bcdffc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-28 03:10:28 +0000

    Add package insipid-sphinx-theme/0.4.2/insipid-sphinx-theme-0.4.2.tar.gz

---
+++ packages/insipid-sphinx-theme/insipid-sphinx-theme-0.4.2.tar.gz/insipid-sphinx-theme-0.4.2/git_rebase_rtd_branches.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -e
+
+fmt='
+git checkout %(refname)
+git rebase master
+git push origin HEAD:%(refname:lstrip=3) --force
+'
+
+eval "$(git for-each-ref --shell --format="$fmt" \
+    refs/remotes/origin/breadcrumbs \
+    refs/remotes/origin/sphinx3 \
+    refs/remotes/origin/sphinx4 \
+    refs/remotes/origin/sphinx5 \
+    refs/remotes/origin/readthedocs-sphinx-search \
+    refs/remotes/origin/rightsidebar \
+    )"
+
+git checkout master
