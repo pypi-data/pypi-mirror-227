27cd6eea28dcf70747066f23094666809ef1bdb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-28 04:12:05 +0000

    Add package fastapi-sqlalchemy-toolkit/0.1/fastapi_sqlalchemy_toolkit-0.1.tar.gz

---
+++ packages/fastapi-sqlalchemy-toolkit/fastapi_sqlalchemy_toolkit-0.1.tar.gz/fastapi_sqlalchemy_toolkit-0.1/examples/app/schemas.py
@@ -0,0 +1,29 @@
+from uuid import UUID
+
+from pydantic import BaseModel
+
+
+class ChildBaseSchema(BaseModel):
+    title: str
+    slug: str
+
+
+class CreateUpdateChildSchema(ChildBaseSchema):
+    parent_id: UUID
+
+
+class ParentBaseSchema(BaseModel):
+    title: str
+    slug: str
+
+
+class RetrieveChildSchema(ChildBaseSchema):
+    parent: ParentBaseSchema
+
+
+class RetrieveParentSchema(ParentBaseSchema):
+    children: list[ChildBaseSchema] | None
+
+
+class HTTPErrorSchema(BaseModel):
+    detail: str
