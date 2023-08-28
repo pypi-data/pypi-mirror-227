0fcf3fe11f8308340dcfa41e9c0b09b3d3b13753
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-28 04:11:55 +0000

    Add package signalslite/0.1a4/signalslite-0.1a4.tar.gz

---
+++ packages/signalslite/signalslite-0.1a4.tar.gz/signalslite-0.1a4/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+print(setuptools.find_packages())
+
+setuptools.setup(
+    name="signalslite",
+    version="0.1-alpha.4",
+    author="Suraj Parmar",
+    author_email="parmarsuraj99@gmail.com",
+    description="A small package for Numerai Signals locally",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/parmarsuraj99/signalslite",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+)
