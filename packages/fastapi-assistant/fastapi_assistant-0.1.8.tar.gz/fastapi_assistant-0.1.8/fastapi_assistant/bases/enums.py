54b85db395b532177bb7ae0ad88fa1e1fc761810
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-28 04:11:40 +0000

    Add package fastapi-assistant/0.1.8/fastapi_assistant-0.1.8.tar.gz

---
+++ packages/fastapi-assistant/fastapi_assistant-0.1.8.tar.gz/fastapi_assistant-0.1.8/fastapi_assistant/bases/enums.py
@@ -0,0 +1,25 @@
+from enum import Enum
+from typing import Optional, Any
+
+
+class BasicEnum(Enum):
+    def __new__(cls, key, desc):
+        obj = object.__new__(cls)
+        obj.key = obj._value_ = key
+        obj.desc = desc
+        return obj
+
+    @classmethod
+    def get_enum(cls, value) -> Optional[Any]:
+        try:
+            return cls(value)
+        except ValueError:
+            return None
+
+    @classmethod
+    def choices(cls):
+        return [(item.key, item.desc) for item in cls]
+
+    @classmethod
+    def items(cls) -> list:
+        return [item.key for item in cls]
