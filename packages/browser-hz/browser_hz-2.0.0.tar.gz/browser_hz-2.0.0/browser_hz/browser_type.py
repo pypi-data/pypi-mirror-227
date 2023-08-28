c6386cf4c11d8d61999f9bf4e3f139397f4980f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-28 03:10:28 +0000

    Add package browser-hz/2.0.0/browser_hz-2.0.0.tar.gz

---
+++ packages/browser-hz/browser_hz-2.0.0.tar.gz/browser_hz-2.0.0/browser_hz/browser_type.py
@@ -0,0 +1,13 @@
+from enum import Enum, auto
+
+
+class BrowserType(Enum):
+    CHROME = auto()
+
+    @classmethod
+    def get_browser(cls, name: str) -> 'BrowserType':
+        try:
+            return cls[name.upper()]
+        except KeyError:
+            raise KeyError(f"[{name.upper()}] browser isn't supported "
+                           f"- choose one of {list(map(lambda l: str(l).split('.')[1], cls))}")
