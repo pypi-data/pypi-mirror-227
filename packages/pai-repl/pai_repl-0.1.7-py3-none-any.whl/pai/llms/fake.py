3c39ca44596c7b37a1112e542f9a29cd72ecc436
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-28 14:20:09 +0000

    Add package pai-repl/0.1.7/pai_repl-0.1.7-py3-none-any.whl

---
+++ packages/pai-repl/pai_repl-0.1.7-py3-none-any.whl/pai/llms/fake.py
@@ -0,0 +1,34 @@
+from typing import Any
+from pai.history import HistoryNode
+from pai.llms.llm_protocol import (
+    LLM,
+    LLMError,
+    LLMResponse,
+    LLMResponseCode,
+    LLMResponseMessage,
+)
+
+
+class FakeLLM(LLM):
+    def prompt(self, history: list[HistoryNode], prompt: str) -> Any:
+        return prompt
+
+    def description(self) -> str:
+        return "FakeLLM"
+
+    def call(self, history: list[HistoryNode], prompt: str) -> LLMResponse:
+        if prompt == "indent":
+            code = "for i in range(2):\n    for j in range(2):\n        print(i, j)\n\n    for k in range(2):\n        print(i, k)\n"
+            return LLMResponseCode(
+                prompt=prompt,
+                code=code,
+                message=None,
+                raw=None,
+            )
+
+        return LLMResponseCode(
+            prompt=prompt,
+            code="import os\nos.listdir()",
+            message="This code will list the files",
+            raw=None,
+        )
