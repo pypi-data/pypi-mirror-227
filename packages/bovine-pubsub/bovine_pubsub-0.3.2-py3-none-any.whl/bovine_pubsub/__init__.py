43e104a6b42f803992f59444441b3bb1d4250da3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-28 03:12:03 +0000

    Add package bovine-pubsub/0.3.2/bovine_pubsub-0.3.2-py3-none-any.whl

---
+++ packages/bovine-pubsub/bovine_pubsub-0.3.2-py3-none-any.whl/bovine_pubsub/__init__.py
@@ -0,0 +1,32 @@
+import os
+from quart_redis import RedisHandler
+
+from .amqp import AmqpBovinePubSub
+from .redis import RedisBovinePubSub
+from .queue import QueueBovinePubSub
+
+
+def BovinePubSub(app):
+    redis_url = os.environ.get("BOVINE_REDIS")
+    amqp_url = os.environ.get("BOVINE_AMQP")
+
+    if amqp_url:
+        app.config["AMQP_URI"] = amqp_url
+
+        @app.before_serving
+        async def configure_bovine_pub_sub_amqp():
+            app.config["bovine_pub_sub"] = AmqpBovinePubSub(amqp_url)
+
+    elif redis_url:
+        app.config["REDIS_URI"] = redis_url
+        RedisHandler(app)
+
+        @app.before_serving
+        async def configure_bovine_pub_sub_redis():
+            app.config["bovine_pub_sub"] = RedisBovinePubSub()
+
+    else:
+
+        @app.before_serving
+        async def configure_bovine_pub_sub_queues():
+            app.config["bovine_pub_sub"] = QueueBovinePubSub()
