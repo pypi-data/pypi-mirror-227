fc3f8d4b5c98b99840143fab62686f198d361fec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-28 03:11:51 +0000

    Add package AIJobs-Batch/1.0.0a1/AIJobs_Batch-1.0.0a1-py3.9-py3-any.whl

---
+++ packages/AIJobs-Batch/AIJobs_Batch-1.0.0a1-py3.9-py3-any.whl/aijobs/data/headers.py
@@ -0,0 +1,7 @@
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36",
+    "Accept-Encoding": "gzip, deflate, br, utf-8",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+    "Connection": "keep-alive",
+    "Accept-Language": "en-US,en;q=0.9,lt;q=0.8,et;q=0.7,de;q=0.6",
+}
