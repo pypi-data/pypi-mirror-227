1a044d521b10ec3867c7839fffb022c12f99423f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-28 03:10:57 +0000

    Add package your-physics-library/0.1.0/your-physics-library-0.1.0.tar.gz

---
+++ packages/your-physics-library/your-physics-library-0.1.0.tar.gz/your-physics-library-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="your-physics-library",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[],  # List any dependencies here
+    author="Kautilya Katariya",
+    author_email="katariya.i@gmail.com",
+    description="A Python physics library for 2D and 3D simulations",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
