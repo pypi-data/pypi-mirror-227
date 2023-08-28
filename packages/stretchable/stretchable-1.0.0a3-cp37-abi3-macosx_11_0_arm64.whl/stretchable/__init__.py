3b7d938f5b46f83e477f00a01fee859fc68e3979
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-28 03:10:56 +0000

    Add package stretchable/1.0.0a3/stretchable-1.0.0a3-cp37-abi3-macosx_11_0_arm64.whl

---
+++ packages/stretchable/stretchable-1.0.0a3-cp37-abi3-macosx_11_0_arm64.whl/stretchable/__init__.py
@@ -0,0 +1,9 @@
+# stretchable is inspired by/based on stretched (archived) by nmichlo:
+# https://github.com/nmichlo/stretched
+
+# Stretch is an implementation of CSS Flexbox written in Rust:
+# https://github.com/vislyhq/stretch
+
+# import bindings etc.
+from .node import Box, Layout, Node, reset
+from .style import Rect, Size, Style
