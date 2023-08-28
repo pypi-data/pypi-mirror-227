333a2bdfd08c0928d5a2b604dfd32a39cbd2d5bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-28 14:19:52 +0000

    Add package packaging-extrapolation/0.0.2/packaging_extrapolation-0.0.2-py3-none-any.whl

---
+++ packages/packaging-extrapolation/packaging_extrapolation-0.0.2-py3-none-any.whl/examples/CalEnergy.py
@@ -0,0 +1,6 @@
+from method.Extrapolation import *
+from util import UtilTools
+import pandas as pd
+
+if __name__ == '__main__':
+    data = pd.read_csv(r'E:\my_work\基组外推\jupyter_table\data_all.xlsx')
