16a6e77c6feab569bcdb4d8cc77e2df101424c12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-28 04:13:07 +0000

    Add package brutelib/0.1.8/brutelib-0.1.8.tar.gz

---
+++ packages/brutelib/brutelib-0.1.8.tar.gz/brutelib-0.1.8/brutelib/__init__.py
@@ -0,0 +1,35 @@
+import hashlib
+import subprocess
+def md5Hex(args):
+    arg = hashlib.md5(args.encode('utf-8')).hexdigest()
+    print(arg)
+
+def md5(args):
+    arg = hashlib.md5(args.encode('utf-8')).digest()
+    print(arg)
+
+def sha256(args):
+    sha = hashlib.sha256(args.encode('utf-8')).digest()
+    print(sha)
+    
+def sha256Hex(args):
+    sha = hashlib.sha256(args.encode('utf-8')).hexdigest()
+    print(sha)
+    
+def hexList(args):
+    sha = hashlib.sha256(args.encode('utf-8')).hexdigest()
+    print(sha)
+    arg = hashlib.md5(args.encode('utf-8')).hexdigest
+    print(arg)
+
+def crack(wordlist):
+    charlist = [wordlist]
+    abv = 0
+    for i in charlist():
+        while(password != wordlist):
+            guess = ""
+            guess = "#".join(guess)
+            print(guess)
+            print("Cracking Password...")
+            found = abv + 1
+    print(f"Passwords found: {found}, {password}")
