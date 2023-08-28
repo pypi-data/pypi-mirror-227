b8aebe71f889b29c4c94cc399458d5af6862bb68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-28 03:10:22 +0000

    Add package url-local/0.0.11/url_local-0.0.11-py3-none-any.whl

---
+++ packages/url-local/url_local-0.0.11-py3-none-any.whl/src/entity_name_enum.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+class EntityName(Enum):
+    MARKETPLACE_GOODS = "marketplace-goods"
+    EVENT = "event"
+    GENDER_DETECTION = "gender-detection"
+    LOGGER = "logger"
+    GROUP = "group"
+    USER_REGISTRATION = "user-registration"
+    AUTH_LOGIN = "auth"
+    TIMELINE = "timeline"
+    RUN = "run"
