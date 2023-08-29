9a96797354e5dd1da968b0a9b114cd4f6cb5330e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-29 15:10:42 +0000

    Add package gpuv/0.0.6/gpuv-0.0.6.tar.gz

---
+++ packages/gpuv/gpuv-0.0.6.tar.gz/gpuv-0.0.6/gpuv/cli.py
@@ -0,0 +1,15 @@
+import typer
+from gpuv.server import app as flask_app
+
+app = typer.Typer()
+
+@app.command()
+def start():
+    print("Starting server...")
+    flask_app.run(port=5000)
+
+def cli():
+    app()
+
+if __name__ == "__main__":
+    cli()
