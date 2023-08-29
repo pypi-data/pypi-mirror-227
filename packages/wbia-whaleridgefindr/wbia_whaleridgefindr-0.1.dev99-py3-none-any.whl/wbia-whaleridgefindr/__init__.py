45b1f104e1c703ed64fc56872ec527fbf7676571
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-29 03:12:43 +0000

    Add package wbia-whaleridgefindr/0.1.dev99/wbia_whaleridgefindr-0.1.dev99-py3-none-any.whl

---
+++ packages/wbia-whaleridgefindr/wbia_whaleridgefindr-0.1.dev99-py3-none-any.whl/wbia-whaleridgefindr/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+try:
+    from wbia-whaleridgefindr._version import __version__
+except ImportError:
+    __version__ = '0.1.1'
+
+from wbia-whaleridgefindr import _plugin  # NOQA
