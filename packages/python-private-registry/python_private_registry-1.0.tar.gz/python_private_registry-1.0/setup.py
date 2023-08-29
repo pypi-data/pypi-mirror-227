c3cd1e09f3b86ea6f3d57b1b724cf841d1b2abea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-29 16:13:03 +0000

    Add package python-private-registry/1.0/python_private_registry-1.0.tar.gz

---
+++ packages/python-private-registry/python_private_registry-1.0.tar.gz/python_private_registry-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+        name='python_private_registry',
+        version='1.0',
+        description='Python Distribution Utilities',
+        author='hyoungwook.kim',
+        author_email='hyoungwook.kim@konantech.com',
+)
