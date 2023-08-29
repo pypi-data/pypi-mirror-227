f9d2698ab0c92c661e4c4918e2d8ad9596e486af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-29 04:13:35 +0000

    Add package openva-pipeline/0.96/openva_pipeline-0.96.tar.gz

---
+++ packages/openva-pipeline/openva_pipeline-0.96.tar.gz/openva_pipeline-0.96/tests/context.py
@@ -0,0 +1,12 @@
+import os
+import sys
+import context
+sys.path.insert(0,
+                os.path.abspath(
+                    os.path.join(
+                        os.path.dirname(__file__),
+                        '../'
+                    )
+                )
+)
+import openva_pipeline
