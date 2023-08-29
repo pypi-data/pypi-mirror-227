1162800384de931a221bb77ce0934374ff3c3b70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-29 04:14:05 +0000

    Add package rrlpy/0.1.0/rrlpy-0.1.0.tar.gz

---
+++ packages/rrlpy/rrlpy-0.1.0.tar.gz/rrlpy-0.1.0/tests/test_rrlpy.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+"""Tests for `rrlpy` package."""
+
+import subprocess
+
+
+def test_cli():
+    """Sample pytest test function with the pytest fixture as an argument."""
+    subprocess.check_call("rrlpy")
