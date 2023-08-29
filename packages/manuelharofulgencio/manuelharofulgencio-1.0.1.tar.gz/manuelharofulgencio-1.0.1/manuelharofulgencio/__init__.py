61592bceb6954fffef15d28f1e3cd42c9ea86ea6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-29 14:09:37 +0000

    Add package manuelharofulgencio/1.0.1/manuelharofulgencio-1.0.1.tar.gz

---
+++ packages/manuelharofulgencio/manuelharofulgencio-1.0.1.tar.gz/manuelharofulgencio-1.0.1/manuelharofulgencio/__init__.py
@@ -0,0 +1,1 @@
+from .opengate_client import *
---
+++ packages/manuelharofulgencio/manuelharofulgencio-1.0.1.tar.gz/manuelharofulgencio-1.0.1/setup.py
@@ -0,0 +1,29 @@
+import pathlib
+from setuptools import find_packages, setup
+
+HERE = pathlib.Path(__file__).parent
+
+VERSION = '1.0.1'
+PACKAGE_NAME = 'manuelharofulgencio'
+AUTHOR = 'manuel'
+AUTHOR_EMAIL = 'manuel.haro@amplia.es'
+
+LICENSE = 'Apache License 2.0'
+DESCRIPTION = 'description'
+
+INSTALL_REQUIRES = [
+    'pandas',
+    'requests'
+]
+
+setup(
+    name=PACKAGE_NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    install_requires=INSTALL_REQUIRES,
+    license=LICENSE,
+    packages=find_packages(),
+    include_package_data=True
+)
