c3461db6086f427f5e2d0cc958289f8399d91c6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-29 05:08:37 +0000

    Add package PennyLane/0.32.0/PennyLane-0.32.0-py3-none-any.whl

---
+++ packages/PennyLane/PennyLane-0.32.0-py3-none-any.whl/pennylane/transforms/broadcast_expand.py
@@ -0,0 +1,141 @@
+# Copyright 2018-2021 Xanadu Quantum Technologies Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""This module contains the tape expansion function for expanding a
+broadcasted tape into multiple tapes."""
+import pennylane as qml
+from .batch_transform import batch_transform
+
+
+def _split_operations(ops, num_tapes):
+    """
+    Given a list of operators, return a list containing lists
+    of new operators with length num_tapes, with the parameters split.
+    """
+    # for some reason pylint thinks "qml.ops" is a set
+    # pylint: disable=no-member
+    new_ops = [[] for _ in range(num_tapes)]
+    for op in ops:
+        # determine if any parameters of the operator are batched
+        if op.batch_size:
+            for b in range(num_tapes):
+                new_params = tuple(
+                    p if qml.math.ndim(p) == op.ndim_params[j] else p[b]
+                    for j, p in enumerate(op.data)
+                )
+                new_op = qml.ops.functions.bind_new_parameters(op, new_params)
+                new_ops[b].append(new_op)
+        else:
+            # no batching in the operator; don't copy
+            for b in range(num_tapes):
+                new_ops[b].append(op)
+
+    return new_ops
+
+
+@batch_transform
+def broadcast_expand(tape):
+    r"""Expand a broadcasted tape into multiple tapes
+    and a function that stacks and squeezes the results.
+
+    .. warning::
+
+        Currently, not all templates have been updated to support broadcasting.
+
+    Args:
+        tape (.QuantumTape): Broadcasted tape to be expanded
+
+    Returns:
+        function or tuple[list[.QuantumTape], function]:
+
+        - If the input is a QNode, an object resembling the (broadcasted) input QNode
+          that computes the QNode output serially with multiple circuit evaluations and
+          stacks (and squeezes) the results into one batch of results.
+
+        - If the input is a tape, a tuple containing a list of generated tapes, together with
+          a post-processing function. The number of tapes matches the broadcasting dimension
+          of the input tape, and the results from the evaluated tapes are stacked and squeezed
+          together in the post-processing function.
+
+    This expansion function is used internally whenever a device does not
+    support broadcasting.
+
+    **Example**
+
+    We may use ``broadcast_expand`` on a ``QNode`` to separate it
+    into multiple calculations. For this we will provide ``qml.RX`` with
+    the ``ndim_params`` attribute that allows the operation to detect
+    broadcasting, and set up a simple ``QNode`` with a single operation and
+    returned expectation value:
+
+    >>> qml.RX.ndim_params = (0,)
+    >>> dev = qml.device("default.qubit", wires=1)
+    >>> @qml.qnode(dev)
+    >>> def circuit(x):
+    ...     qml.RX(x, wires=0)
+    ...     return qml.expval(qml.PauliZ(0))
+
+    We can then call ``broadcast_expand`` on the QNode and store the
+    expanded ``QNode``:
+
+    >>> expanded_circuit = qml.transforms.broadcast_expand(circuit)
+
+    Let's use the expanded QNode and draw it for broadcasted parameters
+    with broadcasting axis of length ``3`` passed to ``qml.RX``:
+
+    >>> x = pnp.array([0.2, 0.6, 1.0], requires_grad=True)
+    >>> print(qml.draw(expanded_circuit)(x))
+    0: ──RX(0.20)─┤  <Z>
+    0: ──RX(0.60)─┤  <Z>
+    0: ──RX(1.00)─┤  <Z>
+
+    Executing the expanded ``QNode`` results in three values, corresponding
+    to the three parameters in the broadcasted input ``x``:
+
+    >>> expanded_circuit(x)
+    tensor([0.98006658, 0.82533561, 0.54030231], requires_grad=True)
+
+    We also can call the transform manually on a tape:
+
+    >>> ops = [qml.RX(pnp.array([0.2, 0.6, 1.0], requires_grad=True), wires=0)]
+    >>> measurements = [qml.expval(qml.PauliZ(0))]
+    >>> tape = qml.tape.QuantumTape(ops, measurements)
+    >>> tapes, fn = qml.transforms.broadcast_expand(tape)
+    >>> tapes
+    [<QuantumTape: wires=[0], params=1>, <QuantumTape: wires=[0], params=1>, <QuantumTape: wires=[0], params=1>]
+    >>> fn(qml.execute(tapes, qml.device("default.qubit", wires=1), None))
+    array([0.98006658, 0.82533561, 0.54030231])
+    """
+    # pylint: disable=protected-access
+    num_tapes = tape.batch_size
+    if num_tapes is None:
+        raise ValueError("The provided tape is not broadcasted.")
+
+    new_ops = _split_operations(tape.operations, num_tapes)
+
+    output_tapes = []
+    for ops in new_ops:
+        new_tape = qml.tape.QuantumScript(ops, tape.measurements, shots=tape.shots)
+        new_tape.trainable_params = tape.trainable_params
+        output_tapes.append(new_tape)
+
+    def processing_fn(results: qml.typing.ResultBatch) -> qml.typing.Result:
+        if len(tape.measurements) > 1 and qml.active_return():
+            processed_results = [
+                qml.math.squeeze(qml.math.stack([results[b][i] for b in range(tape.batch_size)]))
+                for i in range(len(tape.measurements))
+            ]
+            return tuple(processed_results)
+        return qml.math.squeeze(qml.math.stack(results))
+
+    return output_tapes, processing_fn
---
+++ packages/PennyLane/PennyLane-0.32.0-py3-none-any.whl/pennylane/transforms/defer_measurements.py
@@ -0,0 +1,180 @@
+# Copyright 2018-2022 Xanadu Quantum Technologies Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Code for the tape transform implementing the deferred measurement principle."""
+import pennylane as qml
+from pennylane.measurements import MidMeasureMP
+from pennylane.ops.op_math import ctrl
+from pennylane.queuing import apply
+from pennylane.tape import QuantumTape
+from pennylane.transforms import qfunc_transform
+from pennylane.wires import Wires
+
+# pylint: disable=too-many-branches
+
+
+@qfunc_transform
+def defer_measurements(tape: QuantumTape):
+    """Quantum function transform that substitutes operations conditioned on
+    measurement outcomes to controlled operations.
+
+    This transform uses the `deferred measurement principle
+    <https://en.wikipedia.org/wiki/Deferred_Measurement_Principle>`_ and
+    applies to qubit-based quantum functions.
+
+    Support for mid-circuit measurements is device-dependent. If a device
+    doesn't support mid-circuit measurements natively, then the QNode will
+    apply this transform.
+
+    .. note::
+
+        The transform uses the :func:`~.ctrl` transform to implement operations
+        controlled on mid-circuit measurement outcomes. The set of operations
+        that can be controlled as such depends on the set of operations
+        supported by the chosen device.
+
+    .. note::
+
+        Devices that inherit from :class:`~pennylane.QubitDevice` **must** be initialized
+        with an additional wire for each mid-circuit measurement after which the measured
+        wire is reused or reset for ``defer_measurements`` to transform the quantum tape
+        correctly. Hence, devices and quantum tapes must also be initialized without custom
+        wire labels for correct behaviour.
+
+    .. note::
+
+        This transform does not change the list of terminal measurements returned by
+        the quantum function.
+
+    .. note::
+
+        When applying the transform on a quantum function that returns
+        :func:`~pennylane.state` as the terminal measurement or contains the
+        :class:`~.Snapshot` instruction, state information corresponding to
+        simulating the transformed circuit will be obtained. No
+        post-measurement states are considered.
+
+    Args:
+        tape (.QuantumTape): a quantum tape
+
+    **Example**
+
+    Suppose we have a quantum function with mid-circuit measurements and
+    conditional operations:
+
+    .. code-block:: python3
+
+        def qfunc(par):
+            qml.RY(0.123, wires=0)
+            qml.Hadamard(wires=1)
+            m_0 = qml.measure(1)
+            qml.cond(m_0, qml.RY)(par, wires=0)
+            return qml.expval(qml.PauliZ(0))
+
+    The ``defer_measurements`` transform allows executing such quantum
+    functions without having to perform mid-circuit measurements:
+
+    >>> dev = qml.device('default.qubit', wires=2)
+    >>> transformed_qfunc = qml.defer_measurements(qfunc)
+    >>> qnode = qml.QNode(transformed_qfunc, dev)
+    >>> par = np.array(np.pi/2, requires_grad=True)
+    >>> qnode(par)
+    tensor(0.43487747, requires_grad=True)
+
+    We can also differentiate parameters passed to conditional operations:
+
+    >>> qml.grad(qnode)(par)
+    tensor(-0.49622252, requires_grad=True)
+
+    Reusing and reseting measured wires will work as expected with the
+    ``defer_measurements`` transform:
+
+    .. code-block:: python3
+
+        dev = qml.device("default.qubit", wires=3)
+
+        @qml.qnode(dev)
+        def func(x, y):
+            qml.RY(x, wires=0)
+            qml.CNOT(wires=[0, 1])
+            m_0 = qml.measure(1, reset=True)
+
+            qml.cond(m_0, qml.RY)(y, wires=0)
+            qml.RX(np.pi/4, wires=1)
+            return qml.probs(wires=[0, 1])
+
+    Executing this QNode:
+
+    >>> pars = np.array([0.643, 0.246], requires_grad=True)
+    >>> func(*pars)
+    tensor([0.76960924, 0.13204407, 0.08394415, 0.01440254], requires_grad=True)
+    """
+
+    cv_types = (qml.operation.CVOperation, qml.operation.CVObservable)
+    ops_cv = any(isinstance(op, cv_types) for op in tape.operations)
+    obs_cv = any(isinstance(getattr(op, "obs", None), cv_types) for op in tape.measurements)
+    if ops_cv or obs_cv:
+        raise ValueError("Continuous variable operations and observables are not supported.")
+
+    # Find wires that are reused after measurement
+    measured_wires = set()
+    reused_measurement_wires = set()
+
+    for op in tape.operations:
+        if isinstance(op, MidMeasureMP):
+            if op.wires[0] in measured_wires or op.reset is True:
+                reused_measurement_wires.add(op.wires[0])
+            measured_wires.add(op.wires[0])
+
+        else:
+            reused_measurement_wires = reused_measurement_wires.union(
+                measured_wires.intersection(op.wires.toset())
+            )
+
+    # Apply controlled operations to store measurement outcomes and replace
+    # classically controlled operations
+    control_wires = {}
+    cur_wire = max(tape.wires) + 1 if reused_measurement_wires else None
+
+    for op in tape:
+        if isinstance(op, MidMeasureMP):
+            # Only store measurement outcome in new wire if wire gets reused
+            if op.wires[0] in reused_measurement_wires:
+                control_wires[op.id] = cur_wire
+
+                qml.CNOT([op.wires[0], cur_wire])
+                if op.reset:
+                    qml.CNOT([cur_wire, op.wires[0]])
+
+                cur_wire += 1
+            else:
+                control_wires[op.id] = op.wires[0]
+
+        elif op.__class__.__name__ == "Conditional":
+            _add_control_gate(op, control_wires)
+        else:
+            apply(op)
+
+    return tape._qfunc_output  # pylint: disable=protected-access
+
+
+def _add_control_gate(op, control_wires):
+    """Helper function to add control gates"""
+    control = [control_wires[m.id] for m in op.meas_val.measurements]
+    for branch, value in op.meas_val._items():  # pylint: disable=protected-access
+        if value:
+            ctrl(
+                lambda: apply(op.then_op),  # pylint: disable=cell-var-from-loop
+                control=Wires(control),
+                control_values=branch,
+            )()
