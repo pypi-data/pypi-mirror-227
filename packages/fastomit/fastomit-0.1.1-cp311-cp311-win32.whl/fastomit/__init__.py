02bc2fc0480ab81719348fbd8678fd9b7bae22c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-29 15:10:56 +0000

    Add package fastomit/0.1.1/fastomit-0.1.1-cp311-cp311-win32.whl

---
+++ packages/fastomit/fastomit-0.1.1-cp311-cp311-win32.whl/fastomit/__init__.py
@@ -0,0 +1,14 @@
+# SPDX-FileCopyrightText: 2023-present joshua-auchincloss <joshua.auchincloss@proton.me>
+#
+# SPDX-License-Identifier: MIT
+from fastomit.omit import (
+    NoTrustOmitter,
+    Omitter,
+    always_omit,
+    always_trust,
+    globally_hidden,
+    globally_trusted,
+    hide,
+    reset_omissions,
+    reset_trusted,
+)
