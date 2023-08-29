fa3516fc0882933537a3e71799a287c84f1ee8b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-29 03:12:31 +0000

    Add package fluss/0.1.60/fluss-0.1.60.tar.gz

---
+++ packages/fluss/fluss-0.1.60.tar.gz/fluss-0.1.60/fluss/__init__.py
@@ -0,0 +1,4 @@
+from .fluss import Fluss
+from .structures import structure_reg
+
+__all__ = ["Fluss", "structure_reg"]
---
+++ packages/fluss/fluss-0.1.60.tar.gz/fluss-0.1.60/fluss/fluss.py
@@ -0,0 +1,12 @@
+
+from koil.composition import Composition
+from pydantic import Field
+
+from fluss.rath import FlussRath
+
+
+class Fluss(Composition):
+    rath: FlussRath = Field(default_factory=FlussRath)
+
+    def _repr_html_inline_(self):
+        return f'''<p>Fluss </p>'''
