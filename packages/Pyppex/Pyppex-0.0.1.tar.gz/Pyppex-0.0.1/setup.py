d8e1f0bbc45295b94c86f1a1316761f92b5fd2be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-29 04:13:02 +0000

    Add package Pyppex/0.0.1/Pyppex-0.0.1.tar.gz

---
+++ packages/Pyppex/Pyppex-0.0.1.tar.gz/Pyppex-0.0.1/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name='Pyppex',
+    version='0.0.1',
+    description='All sorts of utilities designed to ease developers productivity.',
+    author='n0t10n',
+    author_email='0ts.notion@gmail.com',
+    maintainer='Marcos',
+    maintainer_email='0ts.notion@gmail.com',
+    packages=['pyppex']
+)
