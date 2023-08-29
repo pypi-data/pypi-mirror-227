d6b97b16719e867997ba14f7665ca4539a677620
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-29 16:14:21 +0000

    Add package url-local/0.0.14/url_local-0.0.14-py3-none-any.whl

---
+++ packages/url-local/url_local-0.0.14-py3-none-any.whl/src/action_name_enum.py
@@ -0,0 +1,20 @@
+from enum import Enum
+
+
+# TODO: We should order them by alphabet of the Entity
+
+class ActionName(Enum):
+    ADD_LOG = "add"
+    ANALYZE_FACIAL_IMAGE = "analyzeFacialImage"
+    CREATE_USER = "createUser"
+    EVENT = "event"
+    GENDER_DETECTION = "process"    
+    GET_ALL_GROUPS = "getAllGroups"
+    GET_GROUP_BY_ID = "getGroupById"
+    GET_GROUP_BY_NAME = "getGroupByName"
+    GRAPHQL = "graphql"
+    LOGIN = "login"
+    TIMELINE = "timeline"
+    UPDATE_USER = "updateUser"
+    VALIDATE_JWT = "validateJwt"
+    
