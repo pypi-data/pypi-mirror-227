839899910905527e439899c10f67c458541e4e2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-29 03:15:07 +0000

    Add package dotnetpymodule33/0.2/dotnetpymodule33-0.2.tar.gz

---
+++ packages/dotnetpymodule33/dotnetpymodule33-0.2.tar.gz/dotnetpymodule33-0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dotnetpymodule33',
+    version='0.2',
+    packages=find_packages(),
+    install_requires=[],
+    author='David',
+    author_email='ddickson@gleason.com',
+    description='A simple Python package',
+)
