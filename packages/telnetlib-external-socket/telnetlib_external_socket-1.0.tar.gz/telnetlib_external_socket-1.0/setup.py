dfceb36864c6843e3a4867ed5686f293458bea18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-29 04:12:59 +0000

    Add package telnetlib-external-socket/1.0/telnetlib_external_socket-1.0.tar.gz

---
+++ packages/telnetlib-external-socket/telnetlib_external_socket-1.0.tar.gz/telnetlib_external_socket-1.0/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+
+name = 'telnetlib_external_socket',
+version = '1.0',
+py_modules = ['telnetlib_external_socket'],
+author = 'myxx921',
+author_email = 'myxx921@gmail.com',
+url = 'https://github.com/myxx921/telnetlib_external_socket',
+description = 'added external socket to standart telnetlib',
+)
