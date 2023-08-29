ff433c55c96e91d267dfcd35b16391ca23711e64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-29 03:15:13 +0000

    Add package textbase-framework/0.1.3/textbase_framework-0.1.3-py3-none-any.whl

---
+++ packages/textbase-framework/textbase_framework-0.1.3-py3-none-any.whl/textbase_framework/message.py
@@ -0,0 +1,11 @@
+# textbase/message.py
+from pydantic import BaseModel
+from typing import List
+
+class Content(BaseModel):
+    data_type: str
+    value: str
+
+class Message(BaseModel):
+    role: str  # "user" or "assistant"
+    content: List[Content]
