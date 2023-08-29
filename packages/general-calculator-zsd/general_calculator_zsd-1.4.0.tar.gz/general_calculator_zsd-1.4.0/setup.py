235dd730713a48c51e2f7034a6d78f1bdf2069bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-29 03:13:32 +0000

    Add package general-calculator-zsd/1.4.0/general_calculator_zsd-1.4.0.tar.gz

---
+++ packages/general-calculator-zsd/general_calculator_zsd-1.4.0.tar.gz/general_calculator_zsd-1.4.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='general_calculator_zsd',
+    version='1.4.0',
+    author='colin zhang',
+    author_email='zsd0830@163.com',
+    description='通用脚本及数据导入脚本，不定期按需更新。1.2.2-加入抖音数据导入；加入实时监测的情感识别。1.4.0-加入实时数据导入',
+    long_description_content_type="""text/markdown""",
+    url='',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ]
+)
