2f8d401f7d98f48cc5fd7414dc372463bd81dee3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-29 16:14:15 +0000

    Add package tybase/0.4.9/tybase-0.4.9.tar.gz

---
+++ packages/tybase/tybase-0.4.9.tar.gz/tybase-0.4.9/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tybase',
+    version='0.4.9',
+    include_package_data=True,
+    description='微调了图片保存的方法,改成了返回图片的本地地址和图片完整的urls的功能',
+    long_description=open('README.md', 'r', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',  # 版本描述
+    author='Tuya',
+    author_email='353335447@qq.com',
+    url='https://github.com/yourusername/your_package',
+    packages=find_packages(),
+    install_requires=[
+        'setuptools',
+        "opencv-python",
+        "redis",
+        "celery",
+        "loguru",
+        "openpyxl",
+        'requests',
+        "python-dotenv",
+        "retrying",
+        "pymysql",
+        "mysql-connector-python",
+        "sqlalchemy",
+        "pandas",
+        "langchain",
+        "openai",
+        'python-dotenv',
+        "oss2"
+        ""
+
+        # List your package dependencies here
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.10',
+    ],
+)
