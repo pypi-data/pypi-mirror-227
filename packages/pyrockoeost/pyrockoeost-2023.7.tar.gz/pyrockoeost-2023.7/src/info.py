55a66347424a631a255f93c544fa547f41d51833
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-29 04:12:41 +0000

    Add package pyrockoeost/2023.7/pyrockoeost-2023.7.tar.gz

---
+++ packages/pyrockoeost/pyrockoeost-2023.7.tar.gz/pyrockoeost-2023.7/src/info.py
@@ -0,0 +1,7 @@
+# This module is automatically created from setup.py
+git_sha1 = None
+local_modifications = None
+version = '2023.006'
+long_version = 'pyrockoeost-2023.006-2023-08-28_15:12:17'  # noqa
+installed_date = '2023-08-28_15:12:17'
+src_path = '/home/user/Music'
