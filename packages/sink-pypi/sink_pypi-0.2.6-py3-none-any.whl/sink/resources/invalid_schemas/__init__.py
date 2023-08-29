4dba68e64fe854fbfb9b90f19a0d43215c8e5181
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-29 04:14:05 +0000

    Add package sink-pypi/0.2.6/sink_pypi-0.2.6-py3-none-any.whl

---
+++ packages/sink-pypi/sink_pypi-0.2.6-py3-none-any.whl/sink/resources/invalid_schemas/__init__.py
@@ -0,0 +1,6 @@
+# File generated from our OpenAPI spec by Stainless.
+
+from .arrays import Arrays, AsyncArrays
+from .invalid_schemas import InvalidSchemas, AsyncInvalidSchemas
+
+__all__ = ["Arrays", "AsyncArrays", "InvalidSchemas", "AsyncInvalidSchemas"]
