ffe147ddac2e3e732882d6ccb4e80ce9915e0153
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-29 04:13:56 +0000

    Add package scpup/0.5.0/scpup-0.5.0-py3-none-any.whl

---
+++ packages/scpup/scpup-0.5.0-py3-none-any.whl/scpup/utils/loader.py
@@ -0,0 +1,24 @@
+import os
+import pygame
+
+__all__ = [
+  "load_image",
+  "load_sound"
+]
+
+BASE_PATH = os.path.join(os.getcwd(), "assets")
+
+def load_image(*paths: str, alpha: bool = True, **rectargs) -> tuple[pygame.Surface, pygame.Rect]:
+  path = os.path.join(BASE_PATH, *paths)
+  image = pygame.image.load(path)
+  if alpha:
+    image = image.convert_alpha()
+  else:
+    image = image.convert()
+  rect = image.get_rect(**rectargs)
+  return image, rect
+
+def load_sound(*paths: str):
+  path = os.path.join(BASE_PATH, *paths)
+  sound = pygame.mixer.Sound(path)
+  return sound
