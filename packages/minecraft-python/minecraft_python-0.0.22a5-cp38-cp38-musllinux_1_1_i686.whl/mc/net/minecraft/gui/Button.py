d182206cc999f2b95e8c4a735fec6e602a52e674
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-29 14:10:55 +0000

    Add package minecraft-python/0.0.22a5/minecraft_python-0.0.22a5-cp38-cp38-musllinux_1_1_i686.whl

---
+++ packages/minecraft-python/minecraft_python-0.0.22a5-cp38-cp38-musllinux_1_1_i686.whl/mc/net/minecraft/gui/Button.py
@@ -0,0 +1,11 @@
+class Button:
+
+    def __init__(self, id_, x, y, w, h, msg):
+        self.id = id_
+        self.x = x
+        self.y = y
+        self.w = 200
+        self.h = 20
+        self.msg = msg
+        self.enabled = True
+        self.visible = True
