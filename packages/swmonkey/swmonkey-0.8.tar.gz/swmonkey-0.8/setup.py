29ec70be8beb0f0b80c57904f6f4d00a769673e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-29 03:12:58 +0000

    Add package swmonkey/0.8/swmonkey-0.8.tar.gz

---
+++ packages/swmonkey/swmonkey-0.8.tar.gz/swmonkey-0.8/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='swmonkey',
+    version='0.8',
+    packages=['swmonkey'],
+    author='Li Saifei',
+    author_email='waltermitty121906@gmail.com',
+    description='A tool for monkey test on Linux GUI',
+    entry_points={
+        'console_scripts': [
+            'swmonkey = swmonkey.main:swmonkey'
+        ]
+    },
+    install_requires=[
+        'pyautogui',
+        'psutil',
+    ]
+)
