ae39a1bc3992ac78c90b66e3ad898189a5e2d8a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-29 04:12:53 +0000

    Add package kuzu/0.0.8/kuzu-0.0.8-cp38-cp38-win_amd64.whl

---
+++ packages/kuzu/kuzu-0.0.8-cp38-cp38-win_amd64.whl/kuzu/types.py
@@ -0,0 +1,28 @@
+from enum import Enum
+
+
+class Type(Enum):
+    """The type of a value in the database."""
+    ANY = "ANY"
+    NODE = "NODE"
+    REL = "REL"
+    RECURSIVE_REL = "RECURSIVE_REL"
+    SERIAL = "SERIAL"
+    BOOL = "BOOL"
+    INT64 = "INT64"
+    INT32 = "INT32"
+    INT16 = "INT16"
+    DOUBLE = "DOUBLE"
+    FLOAT = "FLOAT"
+    DATE = "DATE"
+    TIMESTAMP = "TIMESTAMP"
+    INTERVAL = "INTERVAL"
+    FIXED_LIST = "FIXED_LIST"
+    INTERNAL_ID = "INTERNAL_ID"
+    ARROW_COLUMN = "ARROW_COLUMN"
+    STRING = "STRING"
+    BLOB = "BLOB"
+    VAR_LIST = "VAR_LIST"
+    STRUCT = "STRUCT"
+    MAP = "MAP"
+    UNION = "UNION"
