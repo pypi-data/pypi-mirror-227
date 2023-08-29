07050e5e316c14fdbd228a55a666a6403bde876d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-29 04:14:10 +0000

    Add package Medl/0.1.5/medl-0.1.5-py3-none-any.whl

---
+++ packages/Medl/medl-0.1.5-py3-none-any.whl/medl/common/classes/init/ffmpeg_initializer.py
@@ -0,0 +1,16 @@
+from typing import Callable
+import static_ffmpeg
+
+from medl.common.interfaces import BaseInitializer
+
+__all__ = ["FfmpegInitializer"]
+
+
+class FfmpegInitializer(BaseInitializer):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def _handle(self, request: None, next: Callable[[None], None]) -> None:
+        result = static_ffmpeg.add_paths()
+
+        return next(request)
