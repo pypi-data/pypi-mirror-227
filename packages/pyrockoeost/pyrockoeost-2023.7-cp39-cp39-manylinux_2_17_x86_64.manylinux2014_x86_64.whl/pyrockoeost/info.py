cbff9277bff1e4f7d12f228ccbf66faba6b5bd2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-29 04:12:41 +0000

    Add package pyrockoeost/2023.7/pyrockoeost-2023.7-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/pyrockoeost/pyrockoeost-2023.7-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/pyrockoeost/info.py
@@ -0,0 +1,7 @@
+# This module is automatically created from setup.py
+git_sha1 = None
+local_modifications = None
+version = '2023.007'
+long_version = 'pyrockoeost-2023.007-2023-08-28_15:51:49'  # noqa
+installed_date = '2023-08-28_15:51:49'
+src_path = '/tmp/build-via-sdist-y2l3p0rv/pyrockoeost-2023.7'
