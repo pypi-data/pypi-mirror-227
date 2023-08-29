1cebaef6c4d17dec40d64d63c0ad6e94e83254e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-29 03:16:26 +0000

    Add package fastapi-middleware-logger/0.0.4/fastapi-middleware-logger-0.0.4.tar.gz

---
+++ packages/fastapi-middleware-logger/fastapi-middleware-logger-0.0.4.tar.gz/fastapi-middleware-logger-0.0.4/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+with open("requirements.txt", "r", encoding="utf-8") as file:
+    requirements = file.read().split("\n")
+
+with open("README.md", "r", encoding="utf-8") as file:
+    long_description = file.read()
+
+setup(
+    name="fastapi-middleware-logger",
+    version="0.0.4",
+    author="Paul Déchorgnat",
+    author_email="paul.dechorgnat@gmail.com",
+    url="https://github.com/pauldechorgnat/fastapi-custom-logger",
+    description="Simple library to customize logger for FastAPI",
+    license="MIT",
+    include_package_data=True,
+    long_description=long_description,
+    data_files=[("config", ["requirements.txt"])],
+    long_description_content_type="text/markdown",
+    install_requires=requirements,
+    classifiers=[
+        "Framework :: FastAPI",
+    ],
+)
