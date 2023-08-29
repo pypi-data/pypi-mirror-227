ce130c17c24b7a85fd59b078e894858629067b43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-29 16:12:35 +0000

    Add package ifgen/1.1.0/ifgen-1.1.0-py3-none-any.whl

---
+++ packages/ifgen/ifgen-1.1.0-py3-none-any.whl/ifgen/__init__.py
@@ -0,0 +1,13 @@
+# =====================================
+# generator=datazen
+# version=3.1.3
+# hash=98bf88c49dfee15d176169be4c894053
+# =====================================
+
+"""
+Useful defaults and other package metadata.
+"""
+
+DESCRIPTION = "An interface generator for distributed computing."
+PKG_NAME = "ifgen"
+VERSION = "1.1.0"
