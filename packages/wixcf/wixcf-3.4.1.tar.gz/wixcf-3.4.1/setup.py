812b2d8005e08203be6586b4b61698c5f09f58a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-29 05:08:47 +0000

    Add package wixcf/3.4.1/wixcf-3.4.1.tar.gz

---
+++ packages/wixcf/wixcf-3.4.1.tar.gz/wixcf-3.4.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+setup(
+    name="wixcf",
+    version="3.4.1",
+    author="Aras Tokdemir",
+    author_email="aras.tokdemir@outlook.com",
+    description="Wix Package",
+    packages=["Wix"],
+    install_requires=[
+        "wikipedia",
+        "numpy",
+        "pandas",
+        "cryptocompare",
+        "keras",
+        "tensorflow",
+        "scikit-learn",
+        "faker",
+        "matplotlib",
+        "keras",
+        "requests",
+        "beautifulsoup4",
+    ],
+    entry_points={
+        "console_scripts": [
+            "wix = Wix.main:main"
+        ]
+    },
+)
+
+#❯ python setup.py sdist bdist_wheel
+#❯ twine upload dist/wixcf-3.3.4.tar.gz
