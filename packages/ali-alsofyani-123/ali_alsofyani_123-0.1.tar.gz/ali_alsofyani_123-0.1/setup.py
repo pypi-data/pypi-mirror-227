9d45eb73c9380b244169ab8393d46bd672ba93c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-29 14:09:31 +0000

    Add package ali-alsofyani-123/0.1/ali_alsofyani_123-0.1.tar.gz

---
+++ packages/ali-alsofyani-123/ali_alsofyani_123-0.1.tar.gz/ali_alsofyani_123-0.1/setup.py
@@ -0,0 +1,8 @@
+import setuptools
+setuptools.setup(
+    name= 'ali_alsofyani_123',
+    version='0.1',
+    author= 'ali-sofyani',
+    description= 'simple',
+    packages=setuptools.find_packages(),  
+)
