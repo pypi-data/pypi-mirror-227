61983c9fed5ee0ddf55396d007861a39187f4b98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-29 15:10:55 +0000

    Add package ahuiwechat/1.0.3/ahuiwechat-1.0.3-py3-none-any.whl

---
+++ packages/ahuiwechat/ahuiwechat-1.0.3-py3-none-any.whl/ahuiwechat/__init__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# 从库中导入模块：
+# try:
+#     from ahuiwechat import pywechat
+# except Exception as e:
+#     print(e)
+
+
+# # 从库中的模块中导入函数：
+# try:
+#     from pywechat.pywechat import (
+#         wechat_markdown,
+#         wechat_markdowns,
+#         wechat_text,
+#         wechat_at,
+#         wechat_image,
+#         wechat_file,
+#         wechat_files)
+# except Exception as e:
+#     print(e)
+
+
+blog = ''
+tip = f'函数应用详细教程见作者(阿辉)博客：\n{blog}'
+print(tip)
