2f139bf0ff7a9920da92668b61da17605dd2651d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-29 15:11:08 +0000

    Add package MeUtils/2023.8.29.11.18.13/MeUtils-2023.8.29.11.18.13-py3-none-any.whl

---
+++ packages/MeUtils/MeUtils-2023.8.29.11.18.13-py3-none-any.whl/meutils/clis/notice.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : AI.  @by PyCharm
+# @File         : notice
+# @Time         : 2023/8/29 10:20
+# @Author       : betterme
+# @WeChat       : meutils
+# @Software     : PyCharm
+# @Description  : 
+
+
+from meutils.pipe import *
+from meutils.notice import feishu as _feishu
+from meutils.notice.wecom import Wecom
+
+cli = typer.Typer(name="MeUtils CLI")
+
+
+@cli.command()
+def email(title, text='', hook_url=None):
+    """sh管道传参 echo args | xargs -I {} mecli notice {}"""
+    Wecom(hook_url).send_markdown(title, text)
+    return 'ok'
+
+
+@cli.command()
+def wechat(path, type='file', hook_url=None):
+    """mecli notice file_path"""
+    Wecom(hook_url).send_file(path, type)
+    return 'ok'
+
+
+@cli.command()
+def feishu(
+        title='',
+        content='',
+        url: Optional[str] = None
+):
+    """
+    echo args | xargs -I {} notice {}
+
+    python notice.py feishu --title t --content c
+    """
+    _feishu.send_message(title=title, content=content, url=url)
+
+
+@cli.command()
+def feishu4openai(trace: Optional[bool] = False, openai_api_base='https://api.openai-proxy.com/v1'):
+    def fn():
+        from langchain.chat_models import ChatOpenAI
+        llm = ChatOpenAI(openai_api_base=openai_api_base)
+        return llm.predict('1+1')
+
+    _feishu.catch(fn, task_name='CheckOpenaiKey', trace=trace)()
+
+
+if __name__ == '__main__':
+    cli()
