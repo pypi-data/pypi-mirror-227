61349dd7f4d3652c4a658c47ccd7a0c4773d62c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-29 04:14:06 +0000

    Add package dicergirl/3.2.0/dicergirl-3.2.0.tar.gz

---
+++ packages/dicergirl/dicergirl-3.2.0.tar.gz/dicergirl-3.2.0/dicergirl/coc/__init__.py
@@ -0,0 +1,9 @@
+from .investigator import Investigator
+from .nbhandlers import commands
+
+__type__ = "plugin"
+__charactor__ = Investigator
+__name__ = "coc"
+__cname__ = "调查员"
+__nbhandler__ = nbhandlers
+__nbcommands__ = commands
---
+++ packages/dicergirl/dicergirl-3.2.0.tar.gz/dicergirl-3.2.0/dicergirl/dnd/__init__.py
@@ -0,0 +1,9 @@
+from .adventurer import Adventurer
+from .nbhandlers import commands
+
+__type__ = "plugin"
+__charactor__ = Adventurer
+__name__ = "dnd"
+__cname__ = "冒险者"
+__nbhandler__ = nbhandlers
+__nbcommands__ = commands
