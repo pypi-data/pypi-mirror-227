3642eb805bfb94eb67c8ea64dd755ed6d42ff01f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-29 04:13:27 +0000

    Add package darkhan/0.1/darkhan-0.1.tar.gz

---
+++ packages/darkhan/darkhan-0.1.tar.gz/darkhan-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='darkhan',
+      version='0.1',
+      description='desc',
+      packages=['darkhan'],
+      author_email='erdauletov.1512@gmail.com',
+      zip_safe=False)
