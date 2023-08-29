db89d9e77883afc817175fecbbc101f7fc01d04b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-29 15:10:39 +0000

    Add package composipy/1.1.1/composipy-1.1.1-py3-none-any.whl

---
+++ packages/composipy/composipy-1.1.1-py3-none-any.whl/composipy/optimize/__init__.py
@@ -0,0 +1,5 @@
+# TODO: remove duplicated function between the modules
+
+from ._maximize_buckling import maximize_buckling_load
+from ._minimize_panel_weight import minimize_panel_weight
+from ._calculate_lp_proportions import calc_lp_proportions
