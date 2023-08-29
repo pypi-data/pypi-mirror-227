8366596211c21ebf7c034d3295c03aa3cd38eac2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-29 04:13:40 +0000

    Add package spgci/0.0.28/spgci-0.0.28-py3-none-any.whl

---
+++ packages/spgci/spgci-0.0.28-py3-none-any.whl/spgci/__init__.py
@@ -0,0 +1,48 @@
+"""
+S&P Global Commodity Insights API\n
+More details available here: https://developer.platts.com
+
+Getting Started
+---------------
+>>> import spgci as ci
+>>> ci.set_credentials(``username``, ``password``, ``appkey``)
+>>> ci.MarketData().get_assessments_by_symbol(symbol="PCAAS00")
+\n
+"""
+
+from .market_data import MarketData
+from .forward_curves import ForwardCurves
+from .ewindow_md import EWindowMarketData
+from .wos import WorldOilSupply
+from .wrd import WorldRefineryData
+from .oil_demand import GlobalOilDemand
+from .api_client import get_token
+from .epf import EnergyPriceForecast
+from .insights import Insights
+from .na_gas import NANaturalGasAnalytics
+from .giem import GlobalIntegratedEnergyModel
+from .arbflow import Arbflow
+
+from .config import username, password, set_credentials, appkey, version
+
+__version__ = version
+
+
+__all__ = [
+    "MarketData",
+    "username",
+    "password",
+    "appkey",
+    "set_credentials",
+    "ForwardCurves",
+    "EWindowMarketData",
+    "WorldOilSupply",
+    "WorldRefineryData",
+    "get_token",
+    "EnergyPriceForecast",
+    "Insights",
+    "GlobalOilDemand",
+    "NANaturalGasAnalytics",
+    "Arbflow",
+    "GlobalIntegratedEnergyModel"
+]
