085f75b5a64f9ff93b97de4283333c0e6a59901b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-29 03:12:31 +0000

    Add package fluss/0.1.61/fluss-0.1.61-py3-none-any.whl

---
+++ packages/fluss/fluss-0.1.61-py3-none-any.whl/fluss/funcs.py
@@ -0,0 +1,36 @@
+from fluss.rath import FlussRath, current_fluss_rath
+
+
+def execute(operation, variables, rath: FlussRath = None):
+    rath = rath or current_fluss_rath.get()
+    return operation(
+        **rath.query(
+            operation.Meta.document,
+            operation.Arguments(**variables).dict(by_alias=True),
+        ).data
+    )
+
+
+async def aexecute(operation, variables, rath: FlussRath = None):
+    rath = rath or current_fluss_rath.get()
+    x = await rath.aquery(
+        operation.Meta.document, operation.Arguments(**variables).dict(by_alias=True)
+    )
+    return operation(**x.data)
+
+
+def subscribe(operation, variables, rath: FlussRath = None):
+    rath = rath or current_fluss_rath.get()
+
+    for ev in rath.subscribe(
+        operation.Meta.document, operation.Arguments(**variables).dict(by_alias=True)
+    ):
+        yield operation(**ev.data)
+
+
+async def asubscribe(operation, variables, rath: FlussRath = None):
+    rath = rath or current_fluss_rath.get()
+    async for event in rath.asubscribe(
+        operation.Meta.document, operation.Arguments(**variables).dict(by_alias=True)
+    ):
+        yield operation(**event.data)
