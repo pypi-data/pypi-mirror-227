7d8ed1bb40cd110cab99ef8ee0df307e052c8d4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-29 14:09:05 +0000

    Add package aioinject/0.13.0/aioinject-0.13.0.tar.gz

---
+++ packages/aioinject/aioinject-0.13.0.tar.gz/aioinject-0.13.0/aioinject/__init__.py
@@ -0,0 +1,20 @@
+from aioinject.containers import Container
+from aioinject.context import InjectionContext, SyncInjectionContext
+from aioinject.decorators import inject
+from aioinject.markers import Inject
+from aioinject.providers import Callable, Object, Provider, Singleton
+
+
+__all__ = [
+    "Container",
+    "InjectionContext",
+    "SyncInjectionContext",
+    "inject",
+    "Inject",
+    "Callable",
+    "Provider",
+    "Singleton",
+    "Object",
+]
+
+__version__ = "0.13.0"
