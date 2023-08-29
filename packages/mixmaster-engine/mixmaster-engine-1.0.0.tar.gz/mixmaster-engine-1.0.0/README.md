f485fd8132bfba861e4272dc024aa94cc3e2508b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-29 05:08:41 +0000

    Add package mixmaster-engine/1.0.0/mixmaster-engine-1.0.0.tar.gz

---
+++ packages/mixmaster-engine/mixmaster-engine-1.0.0.tar.gz/mixmaster-engine-1.0.0/README.md
@@ -0,0 +1,6 @@
+MixMaster Engine
+
+Esta biblioteca tem como objetivo facilitar e gerencia do banco de dados do mixmaster (MMORPG)
+
+Instalar
+pip3 install mixmaster-engine
---
+++ packages/mixmaster-engine/mixmaster-engine-1.0.0.tar.gz/mixmaster-engine-1.0.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+with open("README.md", "r") as arq:
+    readme = arq.read()
+
+setup(name='mixmaster-engine',
+      version='1.0.0',
+      license='MIT License',
+      author='Gabriel Fernandes',
+      long_description=readme,
+      long_description_content_type="text/markdown",
+      author_email='g.silvafdc@gmail.com',
+      keywords='mixmaster',
+      description=u'Engine para banco de dados do MixMaster',
+      python_requires='>=3.9',
+      packages=[
+          'MixMasterEngine',
+          'MixMasterEngine/app',
+          'MixMasterEngine/app/gamedata',
+          'MixMasterEngine/app/LogDB',
+          'MixMasterEngine/app/Member',
+          'MixMasterEngine/app/Profile',
+          'MixMasterEngine/app/S_Data',
+          'MixMasterEngine/app/Web_Account'],
+      install_requires=['termcolor', 'SQLAlchemy', 'dynaconf', 'DateTime'],)
