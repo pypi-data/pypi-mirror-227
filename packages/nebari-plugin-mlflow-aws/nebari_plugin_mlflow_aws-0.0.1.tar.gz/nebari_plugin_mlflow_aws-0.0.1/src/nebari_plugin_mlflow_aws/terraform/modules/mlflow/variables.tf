7432e3ee16dddd27657c26fca5a5fd47491f6d2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-29 15:10:19 +0000

    Add package nebari-plugin-mlflow-aws/0.0.1/nebari_plugin_mlflow_aws-0.0.1.tar.gz

---
+++ packages/nebari-plugin-mlflow-aws/nebari_plugin_mlflow_aws-0.0.1.tar.gz/nebari_plugin_mlflow_aws-0.0.1/src/nebari_plugin_mlflow_aws/terraform/modules/mlflow/variables.tf
@@ -0,0 +1,32 @@
+variable "create_namespace" {
+  type = bool
+}
+
+variable "ingress_host" {
+  description = "DNS name for Traefik host"
+  type        = string
+}
+
+variable "s3_bucket_name" {
+  description = "Name of S3 bucket for MLFlow artifacts"
+  type        = string
+}
+
+variable "keycloak_config" {
+  description = "Keycloak configuration settings"
+  type        = map(string)
+}
+
+variable "namespace" {
+  type = string
+}
+
+variable "mlflow_sa_name" {
+  description = "Name of K8S service account for MLflow workloads"
+  type        = string
+}
+
+variable "mlflow_sa_iam_role_arn" {
+  description = "ARN of IAM role for Mlflow SA to assume"
+  type        = string
+}
