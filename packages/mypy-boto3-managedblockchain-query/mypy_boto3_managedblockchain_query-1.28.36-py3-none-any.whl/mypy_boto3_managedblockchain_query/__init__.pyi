ccdb0652dff227de73d347b008a116a609cbac65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-29 14:08:41 +0000

    Add package mypy-boto3-managedblockchain-query/1.28.36/mypy_boto3_managedblockchain_query-1.28.36-py3-none-any.whl

---
+++ packages/mypy-boto3-managedblockchain-query/mypy_boto3_managedblockchain_query-1.28.36-py3-none-any.whl/mypy_boto3_managedblockchain_query/__init__.pyi
@@ -0,0 +1,39 @@
+"""
+Main interface for managedblockchain-query service.
+
+Usage::
+
+    ```python
+    from boto3.session import Session
+    from mypy_boto3_managedblockchain_query import (
+        Client,
+        ListTokenBalancesPaginator,
+        ListTransactionEventsPaginator,
+        ListTransactionsPaginator,
+        ManagedBlockchainQueryClient,
+    )
+
+    session = Session()
+    client: ManagedBlockchainQueryClient = session.client("managedblockchain-query")
+
+    list_token_balances_paginator: ListTokenBalancesPaginator = client.get_paginator("list_token_balances")
+    list_transaction_events_paginator: ListTransactionEventsPaginator = client.get_paginator("list_transaction_events")
+    list_transactions_paginator: ListTransactionsPaginator = client.get_paginator("list_transactions")
+    ```
+"""
+from .client import ManagedBlockchainQueryClient
+from .paginator import (
+    ListTokenBalancesPaginator,
+    ListTransactionEventsPaginator,
+    ListTransactionsPaginator,
+)
+
+Client = ManagedBlockchainQueryClient
+
+__all__ = (
+    "Client",
+    "ListTokenBalancesPaginator",
+    "ListTransactionEventsPaginator",
+    "ListTransactionsPaginator",
+    "ManagedBlockchainQueryClient",
+)
