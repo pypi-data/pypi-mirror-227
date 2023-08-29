193a414ae10e60b58561a7f209dc68d60e7183aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-29 14:08:37 +0000

    Add package mypy-boto3-controltower/1.28.36/mypy-boto3-controltower-1.28.36.tar.gz

---
+++ packages/mypy-boto3-controltower/mypy-boto3-controltower-1.28.36.tar.gz/mypy-boto3-controltower-1.28.36/mypy_boto3_controltower/type_defs.pyi
@@ -0,0 +1,154 @@
+"""
+Type annotations for controltower service type definitions.
+
+[Open documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_controltower/type_defs/)
+
+Usage::
+
+    ```python
+    from mypy_boto3_controltower.type_defs import ControlOperationTypeDef
+
+    data: ControlOperationTypeDef = ...
+    ```
+"""
+import sys
+from datetime import datetime
+from typing import Dict, List
+
+from .literals import ControlOperationStatusType, ControlOperationTypeType
+
+if sys.version_info >= (3, 12):
+    from typing import NotRequired
+else:
+    from typing_extensions import NotRequired
+if sys.version_info >= (3, 12):
+    from typing import TypedDict
+else:
+    from typing_extensions import TypedDict
+
+__all__ = (
+    "ControlOperationTypeDef",
+    "DisableControlInputRequestTypeDef",
+    "ResponseMetadataTypeDef",
+    "EnableControlInputRequestTypeDef",
+    "EnabledControlSummaryTypeDef",
+    "GetControlOperationInputRequestTypeDef",
+    "PaginatorConfigTypeDef",
+    "ListEnabledControlsInputRequestTypeDef",
+    "DisableControlOutputTypeDef",
+    "EnableControlOutputTypeDef",
+    "GetControlOperationOutputTypeDef",
+    "ListEnabledControlsOutputTypeDef",
+    "ListEnabledControlsInputListEnabledControlsPaginateTypeDef",
+)
+
+ControlOperationTypeDef = TypedDict(
+    "ControlOperationTypeDef",
+    {
+        "endTime": NotRequired[datetime],
+        "operationType": NotRequired[ControlOperationTypeType],
+        "startTime": NotRequired[datetime],
+        "status": NotRequired[ControlOperationStatusType],
+        "statusMessage": NotRequired[str],
+    },
+)
+
+DisableControlInputRequestTypeDef = TypedDict(
+    "DisableControlInputRequestTypeDef",
+    {
+        "controlIdentifier": str,
+        "targetIdentifier": str,
+    },
+)
+
+ResponseMetadataTypeDef = TypedDict(
+    "ResponseMetadataTypeDef",
+    {
+        "RequestId": str,
+        "HostId": str,
+        "HTTPStatusCode": int,
+        "HTTPHeaders": Dict[str, str],
+        "RetryAttempts": int,
+    },
+)
+
+EnableControlInputRequestTypeDef = TypedDict(
+    "EnableControlInputRequestTypeDef",
+    {
+        "controlIdentifier": str,
+        "targetIdentifier": str,
+    },
+)
+
+EnabledControlSummaryTypeDef = TypedDict(
+    "EnabledControlSummaryTypeDef",
+    {
+        "controlIdentifier": NotRequired[str],
+    },
+)
+
+GetControlOperationInputRequestTypeDef = TypedDict(
+    "GetControlOperationInputRequestTypeDef",
+    {
+        "operationIdentifier": str,
+    },
+)
+
+PaginatorConfigTypeDef = TypedDict(
+    "PaginatorConfigTypeDef",
+    {
+        "MaxItems": NotRequired[int],
+        "PageSize": NotRequired[int],
+        "StartingToken": NotRequired[str],
+    },
+)
+
+ListEnabledControlsInputRequestTypeDef = TypedDict(
+    "ListEnabledControlsInputRequestTypeDef",
+    {
+        "targetIdentifier": str,
+        "maxResults": NotRequired[int],
+        "nextToken": NotRequired[str],
+    },
+)
+
+DisableControlOutputTypeDef = TypedDict(
+    "DisableControlOutputTypeDef",
+    {
+        "operationIdentifier": str,
+        "ResponseMetadata": ResponseMetadataTypeDef,
+    },
+)
+
+EnableControlOutputTypeDef = TypedDict(
+    "EnableControlOutputTypeDef",
+    {
+        "operationIdentifier": str,
+        "ResponseMetadata": ResponseMetadataTypeDef,
+    },
+)
+
+GetControlOperationOutputTypeDef = TypedDict(
+    "GetControlOperationOutputTypeDef",
+    {
+        "controlOperation": ControlOperationTypeDef,
+        "ResponseMetadata": ResponseMetadataTypeDef,
+    },
+)
+
+ListEnabledControlsOutputTypeDef = TypedDict(
+    "ListEnabledControlsOutputTypeDef",
+    {
+        "enabledControls": List[EnabledControlSummaryTypeDef],
+        "nextToken": str,
+        "ResponseMetadata": ResponseMetadataTypeDef,
+    },
+)
+
+ListEnabledControlsInputListEnabledControlsPaginateTypeDef = TypedDict(
+    "ListEnabledControlsInputListEnabledControlsPaginateTypeDef",
+    {
+        "targetIdentifier": str,
+        "PaginationConfig": NotRequired[PaginatorConfigTypeDef],
+    },
+)
