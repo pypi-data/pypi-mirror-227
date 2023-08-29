b5120a14b788510ec4ac8a3ddd19862a2220d9b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-29 03:13:38 +0000

    Add package demo-robot/1.0.6/demo_robot-1.0.6.tar.gz

---
+++ packages/demo-robot/demo_robot-1.0.6.tar.gz/demo_robot-1.0.6/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='demo_robot',  # 需要打包的名字,即本模块要发布的名字
+    version='1.0.6',  # 版本
+    description='init',
+    author='jax',
+    author_email='ming.li@fftai.com',
+    license='MIT'
+)
