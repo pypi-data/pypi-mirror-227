cc4e358e27410268686804e20edbf438d8a15303
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-29 04:13:47 +0000

    Add package venn-abers/0.1/venn-abers-0.1.tar.gz

---
+++ packages/venn-abers/venn-abers-0.1.tar.gz/venn-abers-0.1/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+setup(
+  name='venn-abers',
+  packages=['src/'],
+  version='0.1',
+  license='MIT',
+  description='Venn-ABERS calibration package',
+  author='Ivan Petej',
+  author_email='ivan.petej@gmail.com',
+  url='https://github.com/ip200/venn-abers',
+  download_url='https://github.com/ip200/venn-abers/archive/refs/tags/v_02.tar.gz',
+  keywords=['Probabilistic classification', 'calibration'],
+  install_requires=[
+          'numpy',
+          'scikit-learn',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    "License :: OSI Approved :: MIT License",
+    'Programming Language :: Python :: 3',
+  ],
+)
