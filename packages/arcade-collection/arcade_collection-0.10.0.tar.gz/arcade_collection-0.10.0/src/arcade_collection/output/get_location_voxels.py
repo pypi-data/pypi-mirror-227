b7816620baf0d9324913a5b1ab6a9af5e8d68544
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-29 05:08:47 +0000

    Add package arcade-collection/0.10.0/arcade_collection-0.10.0.tar.gz

---
+++ packages/arcade-collection/arcade_collection-0.10.0.tar.gz/arcade_collection-0.10.0/src/arcade_collection/output/get_location_voxels.py
@@ -0,0 +1,11 @@
+from typing import Optional
+
+
+def get_location_voxels(location: dict, region: Optional[str] = None) -> list[tuple[int, int, int]]:
+    voxels = [
+        voxel
+        for loc in location["location"]
+        for voxel in loc["voxels"]
+        if not region or loc["region"] == region
+    ]
+    return voxels
