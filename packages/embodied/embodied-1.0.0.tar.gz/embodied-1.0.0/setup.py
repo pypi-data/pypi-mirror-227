4798e605f8b490ffb633332838899989854cbf19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-29 15:10:18 +0000

    Add package embodied/1.0.0/embodied-1.0.0.tar.gz

---
+++ packages/embodied/embodied-1.0.0.tar.gz/embodied-1.0.0/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+import pathlib
+
+requirements = pathlib.Path('embodied/requirements.txt')
+requirements = requirements.read_text().split('\n')
+requirements = [x for x in requirements if x.strip()]
+
+
+setuptools.setup(
+    name='embodied',
+    version='1.0.0',
+    author='Danijar Hafner',
+    author_email='mail@danijar.com',
+    description='Fast reinforcement learning research',
+    url='http://github.com/danijar/embodied',
+    long_description=pathlib.Path('README.md').read_text(),
+    long_description_content_type='text/markdown',
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=requirements,
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    ],
+)
