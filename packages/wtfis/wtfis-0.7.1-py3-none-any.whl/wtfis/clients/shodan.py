833c26554b3ed3cec797ca5851c754aef6b0c709
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-29 16:14:08 +0000

    Add package wtfis/0.7.1/wtfis-0.7.1-py3-none-any.whl

---
+++ packages/wtfis/wtfis-0.7.1-py3-none-any.whl/wtfis/clients/shodan.py
@@ -0,0 +1,48 @@
+from shodan import Shodan
+from shodan.exception import APIError
+from typing import Optional
+
+from wtfis.models.shodan import ShodanIp, ShodanIpMap
+from wtfis.models.virustotal import Resolutions
+
+
+class ShodanClient:
+    """
+    Shodan client
+    """
+    def __init__(self, api_key: str) -> None:
+        self.s = Shodan(api_key)
+
+    @property
+    def name(self) -> str:
+        return "Shodan"
+
+    def get_ip(self, ip: str) -> Optional[ShodanIp]:
+        try:
+            return ShodanIp.model_validate(self.s.host(ip, minify=False))
+        except APIError as e:
+            if str(e) == "Invalid API key":
+                raise APIError("Invalid Shodan API key")
+            else:
+                raise
+
+    def bulk_get_ip(
+        self,
+        resolutions: Resolutions,
+        max_ips_to_enrich: int
+    ) -> ShodanIpMap:
+        shodan_map = {}
+        for idx, ip in enumerate(resolutions.data):
+            if idx == max_ips_to_enrich:
+                break
+            ip_data = self.get_ip(ip.attributes.ip_address)
+            if ip_data:
+                shodan_map[ip_data.ip_str] = ip_data
+        return ShodanIpMap.model_validate(shodan_map)
+
+    def single_get_ip(self, ip: str) -> ShodanIpMap:
+        shodan_map = {}
+        ip_data = self.get_ip(ip)
+        if ip_data:
+            shodan_map[ip] = ip_data
+        return ShodanIpMap.model_validate(shodan_map)
---
+++ packages/wtfis/wtfis-0.7.1-py3-none-any.whl/wtfis/handlers/base.py
@@ -0,0 +1,94 @@
+import abc
+
+from pydantic import ValidationError
+from requests.exceptions import (
+    ConnectionError,
+    HTTPError,
+    JSONDecodeError,
+    Timeout,
+)
+from rich.console import Console
+from rich.progress import Progress
+from shodan.exception import APIError
+from typing import Callable, List, Optional, Union
+
+from wtfis.clients.greynoise import GreynoiseClient
+from wtfis.clients.ip2whois import Ip2WhoisClient
+from wtfis.clients.ipwhois import IpWhoisClient
+from wtfis.clients.passivetotal import PTClient
+from wtfis.clients.shodan import ShodanClient
+from wtfis.clients.virustotal import VTClient
+from wtfis.models.common import WhoisBase
+from wtfis.models.greynoise import GreynoiseIpMap
+from wtfis.models.ipwhois import IpWhoisMap
+from wtfis.models.shodan import ShodanIpMap
+from wtfis.models.virustotal import Domain, IpAddress
+from wtfis.ui.theme import Theme
+from wtfis.utils import error_and_exit, refang
+
+
+def common_exception_handler(func: Callable) -> Callable:
+    """ Decorator for handling common fetch errors """
+    def inner(*args, **kwargs) -> None:
+        progress: Progress = args[0].progress  # args[0] is the method's self input
+        try:
+            func(*args, **kwargs)
+        except (APIError, ConnectionError, HTTPError, JSONDecodeError, Timeout) as e:
+            progress.stop()
+            error_and_exit(f"Error fetching data: {e}")
+        except ValidationError as e:
+            progress.stop()
+            error_and_exit(f"Data model validation error: {e}")
+    return inner
+
+
+class BaseHandler(abc.ABC):
+    def __init__(
+        self,
+        entity: str,
+        console: Console,
+        progress: Progress,
+        vt_client: VTClient,
+        ip_enricher_client: Union[IpWhoisClient, ShodanClient],
+        whois_client: Union[Ip2WhoisClient, PTClient, VTClient],
+        greynoise_client: Optional[GreynoiseClient],
+    ):
+        # Process-specific
+        self.entity = refang(entity)
+        self.console = console
+        self.progress = progress
+
+        # Clients
+        self._vt = vt_client
+        self._enricher = ip_enricher_client
+        self._whois = whois_client
+        self._greynoise = greynoise_client
+
+        # Dataset containers
+        self.vt_info:   Union[Domain, IpAddress]
+        self.ip_enrich: Union[IpWhoisMap, ShodanIpMap] = IpWhoisMap.empty()
+        self.whois:     WhoisBase
+        self.greynoise: GreynoiseIpMap = GreynoiseIpMap.empty()
+
+        # Warning messages container
+        self.warnings: List[str] = []
+
+    @abc.abstractmethod
+    def fetch_data(self) -> None:
+        """ Main method that controls what get fetched """
+        return NotImplemented  # type: ignore  # pragma: no coverage
+
+    @common_exception_handler
+    def _fetch_whois(self) -> None:
+        # Let continue if rate limited
+        try:
+            self.whois = self._whois.get_whois(self.entity)
+        except HTTPError as e:
+            if e.response.status_code == 429:
+                self.warnings.append(f"Could not fetch Whois: {e}")
+            else:
+                raise
+
+    def print_warnings(self):
+        for message in self.warnings:
+            self.console.print(f"WARN: {message}", style=Theme().warn)
