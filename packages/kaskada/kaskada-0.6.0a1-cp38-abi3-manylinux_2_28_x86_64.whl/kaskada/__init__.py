a5f583cfef3a03be6e50446d6e40811a95dcee4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-29 14:08:11 +0000

    Add package kaskada/0.6.0a1/kaskada-0.6.0a1-cp38-abi3-manylinux_2_28_x86_64.whl

---
+++ packages/kaskada/kaskada-0.6.0a1-cp38-abi3-manylinux_2_28_x86_64.whl/kaskada/__init__.py
@@ -0,0 +1,27 @@
+"""Kaskada query builder and local execution engine."""
+from __future__ import annotations
+
+from . import plot
+from . import sources
+from . import windows
+from ._execution import ExecutionOptions
+from ._result import Result
+from ._session import init_session
+from ._timestream import Literal
+from ._timestream import Timestream
+from ._timestream import record
+from .udf import udf
+
+
+__all__ = [
+    "ExecutionOptions",
+    "init_session",
+    "Literal",
+    "plot",
+    "record",
+    "Result",
+    "sources",
+    "Timestream",
+    "udf",
+    "windows",
+]
