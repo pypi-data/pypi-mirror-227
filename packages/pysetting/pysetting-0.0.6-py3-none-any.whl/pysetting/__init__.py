c246268c439c25131b09a886822e4cbc29a33478
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-29 03:15:33 +0000

    Add package pysetting/0.0.6/pysetting-0.0.6-py3-none-any.whl

---
+++ packages/pysetting/pysetting-0.0.6-py3-none-any.whl/pysetting/__init__.py
@@ -0,0 +1,2 @@
+# IMPORTS
+from .pysetting import * # since this package (running) contains a single module (pysetting.py), flatten the module so that users can access items within the module pysetting.py by using just the package name (pysetting.<entity>) instead of having to add the module name too (pysetting.pysetting.<entity>)
