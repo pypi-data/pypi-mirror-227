d459e33c378ac77c50174039f17e30bbf79c9b52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-29 04:14:10 +0000

    Add package mep-tools/1.0.11/mep_tools-1.0.11-py3-none-any.whl

---
+++ packages/mep-tools/mep_tools-1.0.11-py3-none-any.whl/mep_tools/connector_pb2.pyi
@@ -0,0 +1,18 @@
+from google.protobuf import any_pb2 as _any_pb2
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from typing import ClassVar as _ClassVar, Optional as _Optional
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class DispatcherRequest(_message.Message):
+    __slots__ = ["payload"]
+    PAYLOAD_FIELD_NUMBER: _ClassVar[int]
+    payload: str
+    def __init__(self, payload: _Optional[str] = ...) -> None: ...
+
+class DispatcherResponse(_message.Message):
+    __slots__ = ["payload"]
+    PAYLOAD_FIELD_NUMBER: _ClassVar[int]
+    payload: str
+    def __init__(self, payload: _Optional[str] = ...) -> None: ...
