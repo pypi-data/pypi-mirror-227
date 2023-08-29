03b95ebd05537ce23b8b82625287f3e5eb59ee1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-29 03:16:09 +0000

    Add package textuals-custom-components/0.0.2/textuals_custom_components-0.0.2.tar.gz

---
+++ packages/textuals-custom-components/textuals_custom_components-0.0.2.tar.gz/textuals_custom_components-0.0.2/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="textuals_custom_components",
+    version="0.0.1",
+    author="T0misLaV",
+    description="Extra cool features for textuals",
+    packages=["textuals_custom_components"]
+)
