08ffac2665080258afb6cfd15985f6951d679ec1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-30 04:15:56 +0000

    Add package utils-c/0.0.1/utils_c-0.0.1-py3-none-any.whl

---
+++ packages/utils-c/utils_c-0.0.1-py3-none-any.whl/LatestValidProxies/__init__.py
@@ -0,0 +1,10 @@
+__all__ = [
+    'log',
+    'Proxy',
+    'settings',
+    'Spider',
+    'Spiders',
+    'SpiderConfiger'
+]
+
+from . import log, Proxy, settings, Spider, Spiders, SpiderConfiger
