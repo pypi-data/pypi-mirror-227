480614ce0a88a34ae9ba0352347c91b23845e336
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-30 04:16:24 +0000

    Add package pythonic_archive_kit/1.0.5/pythonic_archive_kit-1.0.5-py2.py3-none-any.whl

---
+++ packages/pythonic_archive_kit/pythonic_archive_kit-1.0.5-py2.py3-none-any.whl/pythonic_archive_kit/__init__.py
@@ -0,0 +1,18 @@
+"""The Pythonic Archive Kit"""
+__version__ = "1.0.5"
+
+from . import mini_pak
+
+try:
+    from . import pak
+except ImportError:
+    pak = mini_pak
+    print(
+        "WARNING: cryptography is not installed. PAK files will not be encrypted.", 
+        "To install cryptography, run `pip install cryptography`", 
+        sep="\n"
+        )
+    
+open = pak.open_pak
+load = pak.load_pak
+save = pak.save_pak
