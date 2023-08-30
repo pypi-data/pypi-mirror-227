55a49840a4613ab667dfc589a1201f2651b4e34c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-30 04:14:37 +0000

    Add package nvtabular/23.8.0/nvtabular-23.8.0-cp310-cp310-manylinux_2_17_i686.manylinux2014_i686.whl

---
+++ packages/nvtabular/nvtabular-23.8.0-cp310-cp310-manylinux_2_17_i686.manylinux2014_i686.whl/nvtabular/ops/__init__.py
@@ -0,0 +1,54 @@
+#
+# Copyright (c) 2021, NVIDIA CORPORATION.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+from merlin.dag import ColumnSelector
+
+# alias submodules here to avoid breaking everything with moving to submodules
+# flake8: noqa
+from nvtabular.ops.add_metadata import (
+    AddMetadata,
+    AddProperties,
+    AddTags,
+    TagAsItemFeatures,
+    TagAsItemID,
+    TagAsUserFeatures,
+    TagAsUserID,
+)
+from nvtabular.ops.bucketize import Bucketize
+from nvtabular.ops.categorify import Categorify, get_embedding_sizes
+from nvtabular.ops.clip import Clip
+from nvtabular.ops.column_similarity import ColumnSimilarity
+from nvtabular.ops.data_stats import DataStats
+from nvtabular.ops.difference_lag import DifferenceLag
+from nvtabular.ops.drop_low_cardinality import DropLowCardinality
+from nvtabular.ops.dropna import Dropna
+from nvtabular.ops.fill import FillMedian, FillMissing
+from nvtabular.ops.filter import Filter
+from nvtabular.ops.groupby import Groupby
+from nvtabular.ops.hash_bucket import HashBucket
+from nvtabular.ops.hashed_cross import HashedCross
+from nvtabular.ops.join_external import JoinExternal
+from nvtabular.ops.join_groupby import JoinGroupby
+from nvtabular.ops.lambdaop import LambdaOp
+from nvtabular.ops.list_slice import ListSlice
+from nvtabular.ops.logop import LogOp
+from nvtabular.ops.normalize import Normalize, NormalizeMinMax
+from nvtabular.ops.operator import Operator
+from nvtabular.ops.reduce_dtype_size import ReduceDtypeSize
+from nvtabular.ops.rename import Rename
+from nvtabular.ops.stat_operator import StatOperator
+from nvtabular.ops.target_encoding import TargetEncoding
+from nvtabular.ops.value_counts import ValueCount
