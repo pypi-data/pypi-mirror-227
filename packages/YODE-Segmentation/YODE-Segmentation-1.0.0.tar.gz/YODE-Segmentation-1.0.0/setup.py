c2d79ee18fe7db61cda8f506bc0e83feb62bef96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-30 03:11:11 +0000

    Add package YODE-Segmentation/1.0.0/YODE-Segmentation-1.0.0.tar.gz

---
+++ packages/YODE-Segmentation/YODE-Segmentation-1.0.0.tar.gz/YODE-Segmentation-1.0.0/setup.py
@@ -0,0 +1,46 @@
+import codecs
+import os
+from setuptools import setup, find_packages
+
+# these things are needed for the README.md show on pypi (if you dont need delete it)
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+# you need to change all these
+VERSION = '1.0.0'
+DESCRIPTION = '简单描述'
+
+setup(
+    name="YODE-Segmentation",
+    version=VERSION,
+    author="作者",
+    author_email="zconechorm@163.com",
+    description=DESCRIPTION,
+    # 长描述内容的类型设置为markdown
+    long_description_content_type="text/plain",
+    # 长描述设置为README.md的内容
+    long_description=long_description,
+    # 使用find_packages()自动发现项目中的所有包
+    packages=find_packages(),
+    # 许可协议
+    license='MIT',
+    # 要安装的依赖包
+    install_requires=[
+        "Faker>=18.7.0",
+        "ddddocr>=1.4.7",
+        "requests>=2.30.0",
+        "loguru>=0.7.0",
+        "lxml>=4.9.2",
+    ],
+    # keywords=['python', 'menu', 'dumb_menu','windows','mac','linux'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
