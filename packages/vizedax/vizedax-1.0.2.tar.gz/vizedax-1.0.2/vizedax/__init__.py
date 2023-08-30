0046c3a43c928b0eaf98c8502095b084248c6ed1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-30 05:11:04 +0000

    Add package vizedax/1.0.2/vizedax-1.0.2.tar.gz

---
+++ packages/vizedax/vizedax-1.0.2.tar.gz/vizedax-1.0.2/vizedax/__init__.py
@@ -0,0 +1,12 @@
+# Import functions from the vizeda module
+from .vizedax import aboutit, cleanit, graphit
+
+# Optional: Specify which symbols should be imported with 'from package import *'
+__all__ = ['aboutit', 'cleanit', 'graphit']
+
+# Optional: Define package-level constants or variables
+__version__ = '1.0.0'
+
+# Optional: Add initialization code if needed
+print("Vizedax package is being imported.")
+
