eeb18e66d682232325405604abe4df32c5918efd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-30 05:09:55 +0000

    Add package mattress/0.1.1/mattress-0.1.1-cp310-cp310-macosx_10_9_x86_64.whl

---
+++ packages/mattress/mattress-0.1.1-cp310-cp310-macosx_10_9_x86_64.whl/mattress/extern/tatami_hdf5/include/tatami_hdf5/tatami_hdf5.hpp
@@ -0,0 +1,20 @@
+#ifndef TATAMI_HDF5_HPP
+#define TATAMI_HDF5_HPP
+
+/**
+ * @file tatami_hdf5.hpp
+ * @brief Create matrix representations for HDF5-backed data.
+ */
+
+/**
+ * @namespace tatami_hdf5
+ * @brief Representations for matrix data in HDF5 files.
+ */
+namespace tatami_hdf5 {}
+
+#include "Hdf5CompressedSparseMatrix.hpp"
+#include "Hdf5DenseMatrix.hpp"
+#include "load_hdf5_matrix.hpp"
+#include "write_sparse_matrix_to_hdf5.hpp"
+
+#endif
