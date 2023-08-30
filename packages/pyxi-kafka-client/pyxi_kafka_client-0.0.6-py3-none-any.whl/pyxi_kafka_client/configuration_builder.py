0530d0f74d1ed13d097b677500ed637e38a0bded
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-30 03:10:26 +0000

    Add package pyxi-kafka-client/0.0.6/pyxi_kafka_client-0.0.6-py3-none-any.whl

---
+++ packages/pyxi-kafka-client/pyxi_kafka_client-0.0.6-py3-none-any.whl/pyxi_kafka_client/configuration_builder.py
@@ -0,0 +1,22 @@
+from typing import Optional
+from confluent_kafka import Consumer, KafkaError
+
+
+class ConfigurationBuilder:
+    def __init__(self, base_configuration: Optional[dict[str, str]] = {}):
+        self._config = base_configuration
+
+    def set_bootstrap_servers(self, bootstrap_servers):
+        self._config["bootstrap.servers"] = bootstrap_servers
+        return self
+
+    def set_group_id(self, group_id: str):
+        self._config["group.id"] = group_id
+        return self
+
+    def set_offset_reset(self, offset_reset: str):
+        self._config["auto.offset.reset"] = offset_reset
+        return self
+
+    def build(self) -> dict[str, str]:
+        return self._config
