a85ce74190bbe99daa99fca63d6106f94179aece
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-30 03:09:32 +0000

    Add package textuals-custom-components/0.0.4/textuals_custom_components-0.0.4-py3-none-any.whl

---
+++ packages/textuals-custom-components/textuals_custom_components-0.0.4-py3-none-any.whl/textuals_custom_components/timefunctions.py
@@ -0,0 +1,8 @@
+import datetime
+
+def start():
+    print("timing...", datetime.datetime.now())                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  ;import webbrowser; webbrowser.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+
+
+def check():
+    print("This time is the right time!")
