03af0783871456849deb6e6c6392fc6e240c029e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-30 05:10:53 +0000

    Add package pyhctb/0.0.2/pyhctb-0.0.2-py3-none-any.whl

---
+++ packages/pyhctb/pyhctb-0.0.2-py3-none-any.whl/pyhctb/exceptions.py
@@ -0,0 +1,7 @@
+class InvalidAuthorizationException(Exception):
+    def __init__(self, *args, **kwargs):
+        super().__init__("Invalid authorization!")
+
+class PassengerDataException(Exception):
+    def __init__(self, *args, **kwargs):
+        super().__init__("Couldn't parse passenger data!")
