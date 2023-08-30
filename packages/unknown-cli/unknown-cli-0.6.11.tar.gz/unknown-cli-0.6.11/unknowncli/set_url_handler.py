d3424f9c9c5b4b50f4185fc5a4967a0dbd9a2b9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-30 04:14:25 +0000

    Add package unknown-cli/0.6.11/unknown-cli-0.6.11.tar.gz

---
+++ packages/unknown-cli/unknown-cli-0.6.11.tar.gz/unknown-cli-0.6.11/unknowncli/set_url_handler.py
@@ -0,0 +1,18 @@
+import pyuac, winreg
+
+handler_name = "sn2"
+key = f"{handler_name}\shell\open\command"
+
+@pyuac.main_requires_admin
+def main():
+    try:
+        h = winreg.CreateKey(winreg.HKEY_CLASSES_ROOT, handler_name)
+        winreg.SetValueEx(h, "URL Protocol", 0, winreg.REG_SZ, "")
+        h = winreg.CreateKey(winreg.HKEY_CLASSES_ROOT, key)
+        cmd = "\"C:\\Python310\\Scripts\\unk.exe\" unreal asset \"%1\""
+        winreg.SetValueEx(h, "", 0, winreg.REG_SZ, cmd)
+    except PermissionError as e:
+        print(f"Unable to set url handler for sn2://. Please open up your shell as Administrator (through shift-right click menu) and run this command again ({str(e)})")
+
+if __name__ == "__main__":
+    main()
