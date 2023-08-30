e2a301255d338300cb588cf2350458c6c54a68a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-30 05:10:30 +0000

    Add package pyappi/1.1.7/pyappi-1.1.7-py3-none-any.whl

---
+++ packages/pyappi/pyappi-1.1.7-py3-none-any.whl/pyappi/service/service.py
@@ -0,0 +1,46 @@
+from pyappi.client.client_handle import get_connection_details
+from pyappi.util.login import encode_session
+from pyappi.client.session import get_session
+from pyappi.client.config import set_config
+import threading
+import time
+
+
+class RawService:
+    def __init__(self, key, handler, name="default", interval=3, config=None, session={}):
+        self.key = key
+        self.name = name
+        self.handler = handler
+        self.interval = interval
+
+        self.running = True
+        self.thread = threading.Thread(target=self.run)
+        self.thread.start()
+
+        if config:
+            set_config(config)
+
+        self.session = get_session() if not len(session) else encode_session(session)
+
+    def stop(self):
+        self.running = False
+        self.thread.join()
+
+    def run(self):
+        client, config = get_connection_details()
+
+        res = client.get(f'{config["protocol"]}{config["host"]}/service/cursor?{self.session}').json()
+        
+        tsx = res["cursor"]
+
+        while self.running:
+            time.sleep(self.interval)
+
+            res = client.get(f'{config["protocol"]}{config["host"]}/service/read_events/{tsx}?{self.session}').json()
+
+            if res["events"]:
+                self.handler(res)
+
+    
+
+    
