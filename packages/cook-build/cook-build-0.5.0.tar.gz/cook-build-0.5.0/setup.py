38080707c60f43e90619a0f4da8fd8d3c2121ae0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-30 05:10:55 +0000

    Add package cook-build/0.5.0/cook-build-0.5.0.tar.gz

---
+++ packages/cook-build/cook-build-0.5.0.tar.gz/cook-build-0.5.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+
+with open("README.rst") as fp:
+    long_description = fp.read()
+long_description = long_description \
+    .replace(":func:", ":code:") \
+    .replace(".. doctest::", ".. code-block::\n") \
+    .replace(".. shtest::", ".. code-block::\n") \
+    .replace(":class:", ":code:") \
+    .replace(".. toctree::", "..") \
+    .replace(".. sh::", "..")
+
+
+setup(
+    name="cook-build",
+    version="0.5.0",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    python_requires=">=3.8",
+    install_requires=[
+        "colorama",
+        "networkx",
+    ],
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "cook = cook.__main__:__main__",
+        ],
+    },
+)
