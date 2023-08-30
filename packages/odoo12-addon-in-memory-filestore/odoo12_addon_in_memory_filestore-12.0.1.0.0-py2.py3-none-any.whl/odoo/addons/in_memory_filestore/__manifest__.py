1d17bf79a1a4cf3b82232172f7f81224ed5e9a8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-30 03:09:41 +0000

    Add package odoo12-addon-in-memory-filestore/12.0.1.0.0/odoo12_addon_in_memory_filestore-12.0.1.0.0-py2.py3-none-any.whl

---
+++ packages/odoo12-addon-in-memory-filestore/odoo12_addon_in_memory_filestore-12.0.1.0.0-py2.py3-none-any.whl/odoo/addons/in_memory_filestore/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "in_memory_filestore",
+
+    'summary': """
+        Change the filestore to in memory. Only for testing (CI) purposes.""",
+
+    'description': """
+        This module changes the filestore to in memory. The goal is to use it
+        to save time in the CI process. It is not intended to be used in
+        production.
+    """,
+
+    'author': "Coopdevs",
+    'website': "http://www.coopdevs.org",
+    'category': 'Technical',
+    'version': '12.0.1.0.0',
+
+
+    'depends': ['base'],
+
+    'data': [
+        'data/ir_config_parameter.xml',
+    ],
+}
