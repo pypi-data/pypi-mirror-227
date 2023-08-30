4e7919f9c82d7202e9626b73c1c592cc770381c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-30 05:09:54 +0000

    Add package snp-transformer/0.0.4/snp-transformer-0.0.4.tar.gz

---
+++ packages/snp-transformer/snp-transformer-0.0.4.tar.gz/snp-transformer-0.0.4/tests/conftest.py
@@ -0,0 +1,48 @@
+from pathlib import Path
+
+import pandas as pd
+import pytest
+from snp_transformer import Individual, IndividualsDataset
+from snp_transformer.loaders import load_details, load_fam, load_sparse
+
+
+@pytest.fixture()
+def individuals(test_data_folder: Path) -> list[Individual]:
+    ind_dataset = IndividualsDataset(test_data_folder / "data")
+
+    return [ind_dataset[i] for i in range(len(ind_dataset))]
+
+
+@pytest.fixture()
+def test_data_folder() -> Path:
+    return Path("tests/data/")
+
+
+@pytest.fixture()
+def fam_path(test_data_folder: Path) -> Path:
+    return test_data_folder / "data.fam"
+
+
+@pytest.fixture()
+def sparse_path(test_data_folder: Path) -> Path:
+    return test_data_folder / "data.sparse"
+
+
+@pytest.fixture()
+def details_path(test_data_folder: Path) -> Path:
+    return test_data_folder / "data.details"
+
+
+@pytest.fixture()
+def details(details_path: Path) -> pd.DataFrame:
+    return load_details(details_path)
+
+
+@pytest.fixture()
+def fam(fam_path: Path) -> pd.DataFrame:
+    return load_fam(fam_path)
+
+
+@pytest.fixture()
+def sparse(sparse_path: Path) -> pd.DataFrame:
+    return load_sparse(sparse_path)
