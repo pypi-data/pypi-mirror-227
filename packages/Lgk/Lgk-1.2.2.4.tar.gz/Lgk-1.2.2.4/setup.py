5cc1e1774c51a9f16ac0ca2a090c05ece1fbb665
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-30 03:09:34 +0000

    Add package Lgk/1.2.2.4/Lgk-1.2.2.4.tar.gz

---
+++ packages/Lgk/Lgk-1.2.2.4.tar.gz/Lgk-1.2.2.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Lgk',
+    version='1.2.2.004',
+    packages=['Lgk'],
+    description='Description: just write Lgk.Help.Help()',
+    author='BT Ansixs',
+    author_email='noobra24l8w@gmail.com',
+    license='LICENSE',
+)
