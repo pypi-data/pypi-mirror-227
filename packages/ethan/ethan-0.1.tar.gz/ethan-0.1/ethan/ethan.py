16a0c146b3ce3da9770647b1ca7b7f983fd06418
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-30 05:09:40 +0000

    Add package ethan/0.1/ethan-0.1.tar.gz

---
+++ packages/ethan/ethan-0.1.tar.gz/ethan-0.1/ethan/ethan.py
@@ -0,0 +1,3 @@
+from cfonts import render
+def logo(name):
+    return render(name,colors=['red','yellow'],align='center')
---
+++ packages/ethan/ethan-0.1.tar.gz/ethan-0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+setup(
+    name= 'ethan',
+    version='0.1',
+    author= 'Ethan',
+    description='Ethan Programming',
+    packages= ['ethan'],
+    author_email='ethan@gmail.com',
+    zip_safe=False,
+    classifiers=[
+    "Programming Language :: Python :: 3",
+    "Operating System :: OS Independent",
+    "License :: OSI Approved :: MIT License"
+    ]
+)
