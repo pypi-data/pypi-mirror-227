49abc076c644ab803f1aebe34270b8f568ec097b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-30 05:10:40 +0000

    Add package spiff-element-units/0.3.1/spiff_element_units-0.3.1-cp39-abi3-win_amd64.whl

---
+++ packages/spiff-element-units/spiff_element_units-0.3.1-cp39-abi3-win_amd64.whl/spiff_element_units/__init__.pyi
@@ -0,0 +1,20 @@
+def cache_element_units_for_workflow(
+        cache_dir: str,
+        cache_key: str,
+        workflow_specs_json: str) -> None:
+    """
+    Creates and caches elements units for the given workflow described by
+    workflow_specs_json and associates them with cache_key for later 
+    retrieval.
+    """
+
+def workflow_from_cached_element_unit(
+        cache_dir: str,
+        cache_key: str,
+        process_id: str,
+        element_id: str) -> str:
+    """
+    Returns a workflow described in specs json format that is capable of 
+    executing an element unit that contains element_id within process_id 
+    and is associated with cache_key.
+    """
