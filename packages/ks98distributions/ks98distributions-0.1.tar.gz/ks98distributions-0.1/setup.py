878b3f09d35d200d4eb9decad029e8061bf72923
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-30 03:10:24 +0000

    Add package ks98distributions/0.1/ks98distributions-0.1.tar.gz

---
+++ packages/ks98distributions/ks98distributions-0.1.tar.gz/ks98distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='ks98distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['ks98distributions'],
+      zip_safe=False)
