59b571ac3683b90e62c93412333460d7e4eca580
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-30 04:13:51 +0000

    Add package django-admin-search-filter/0.1.0/django_admin_search_filter-0.1.0.tar.gz

---
+++ packages/django-admin-search-filter/django_admin_search_filter-0.1.0.tar.gz/django_admin_search_filter-0.1.0/README.rst
@@ -0,0 +1,35 @@
+django_admin_search_filter
+==========================
+
+.. image:: https://img.shields.io/pypi/v/django_admin_search_filter.svg
+    :target: https://pypi.python.org/pypi/django_admin_search_filter
+    :alt: Latest PyPI version
+
+.. image:: https://travis-ci.org/kragniz/cookiecutter-pypackage-minimal.png
+   :target: https://travis-ci.org/kragniz/cookiecutter-pypackage-minimal
+   :alt: Latest Travis CI build status
+
+Allows to create admin list filter as a search box for free-input
+
+Idea is taken from this Medium article: https://hakibenita.medium.com/how-to-add-a-text-filter-to-django-admin-5d1db93772d8
+
+
+Usage
+-----
+
+Installation
+------------
+
+Requirements
+^^^^^^^^^^^^
+
+Compatibility
+-------------
+
+Licence
+-------
+
+Authors
+-------
+
+`django_admin_search_filter` was written by `Mikhail Koipish <mkoypish@gmail.com>`_.
---
+++ packages/django-admin-search-filter/django_admin_search_filter-0.1.0.tar.gz/django_admin_search_filter-0.1.0/django_admin_search_filter/__init__.py
@@ -0,0 +1,75 @@
+"""django_admin_search_filter - Allows to create admin list filter as a search box for free-input"""
+
+__version__ = '0.1.0'
+__author__ = 'Mikhail Koipish <mkoypish@gmail.com>'
+__all__ = []
+
+
+from django.contrib import admin
+from django.db.models import Q
+
+
+class InputFilter(admin.SimpleListFilter):
+    template = 'input_admin_filter.html'
+
+    def lookups(self, request, model_admin):
+        # Dummy, required to show the filter.
+        return [
+            ('x','x')
+        ]
+    def choices(self, changelist):
+        # Grab only the "all" option.
+        all_choice = next(super().choices(changelist))
+        all_choice['query_parts'] = (
+            (k, v)
+            for k, v in changelist.get_filters_params().items()
+            if k != self.parameter_name
+        )
+        yield all_choice
+
+import typing
+def get_icontains_input_filter(title_, attrs: typing.Union[list, tuple, str]):
+
+
+    if isinstance(attrs, str):
+        attrs = (attrs, )
+
+    class FilterCls(InputFilter):
+
+        parameter_name = '-'.join(attrs) + '-icontains'
+        title = title_
+
+        def queryset(self, request, queryset):
+
+            if self.value():
+                query = Q()
+                for a in attrs:
+                    query |= Q(**{
+                        '%s__icontains' % a: self.value()
+                    })
+
+                return queryset.filter(query)
+            return queryset
+
+    return FilterCls
+
+
+def get_exact_equals_input_filter(title_, attrs: typing.Union[list, tuple, str]):
+    if isinstance(attrs, str):
+        attrs = (attrs, )
+
+    class FilterCls(InputFilter):
+        parameter_name = '-'.join(attrs) + '-equals'
+        title = title_
+
+        def queryset(self, request, queryset):
+            if self.value():
+                query = Q()
+                for a in attrs:
+                    query |= Q(**{
+                        '%s__iexact' % a: self.value()
+                    })
+                return queryset.filter(query)
+            return queryset
+
+    return FilterCls
