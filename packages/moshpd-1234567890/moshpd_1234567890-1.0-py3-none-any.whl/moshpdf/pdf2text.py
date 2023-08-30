2a3e84e5d8ce13169c234a174826f980d2f69137
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-30 05:10:55 +0000

    Add package moshpd-1234567890/1.0/moshpd_1234567890-1.0-py3-none-any.whl

---
+++ packages/moshpd-1234567890/moshpd_1234567890-1.0-py3-none-any.whl/moshpdf/pdf2text.py
@@ -0,0 +1,17 @@
+"""This module contains functions to convert a pdf to text."""
+
+class Converter:
+     """A simple converter for converter PDFs to text."""
+     
+     def convert(self, path):
+        """
+        Convert the given pdf to text
+
+        Parameter:
+        path (str): the path to a pdf file.
+
+        Return:
+        str: The content of the pdf file as text.
+        """
+        print("pdf2text")
+
