aeff23512b8286c6d2baa3d183de074e66e47cee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-30 03:09:51 +0000

    Add package redpic/0.7.49/redpic-0.7.49-py3-none-any.whl

---
+++ packages/redpic/redpic-0.7.49-py3-none-any.whl/redpic/core/config.py
@@ -0,0 +1,17 @@
+from logging import config as logging_config
+
+from redpic.core.logger import LOGGING
+
+logging_config.dictConfig(LOGGING)
+
+PROJECT_NAME = "redpic"
+
+PROJECT_VERSION = "0.7.49"
+
+PROJECT_AUTHOR = "Vyacheslav Fedorov"
+
+PROJECT_AUTHOR_EMAIL = "slava@fuodorov.ru"
+
+PROJECT_DOC = "Relativistic Difference Scheme Particle-in-Cell code (REDPIC)"
+
+PROJECT_URL = "https://github.com/fuodorov/redpic"
