20e67ebdb12963eb127bdb352c2c0aa6fbd2b891
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-30 04:12:58 +0000

    Add package factorial-calculator/0.1/factorial_calculator-0.1.tar.gz

---
+++ packages/factorial-calculator/factorial_calculator-0.1.tar.gz/factorial_calculator-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='factorial_calculator',
+    version='0.1',
+    description='A simple factorial calculator package',
+    author='Your Name',
+    author_email='your.email@example.com',
+    url='https://github.com/your_username/factorial_calculator',
+    packages=find_packages(),
+)
