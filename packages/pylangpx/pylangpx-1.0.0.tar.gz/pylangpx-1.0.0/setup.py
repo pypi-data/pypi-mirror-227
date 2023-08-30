57e4449a3753a60da988f245393d8334e8cb6fad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-30 14:10:03 +0000

    Add package pylangpx/1.0.0/pylangpx-1.0.0.tar.gz

---
+++ packages/pylangpx/pylangpx-1.0.0.tar.gz/pylangpx-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+       name='pylangpx',
+       version='1.0.0',
+       author='LibraryMHD',
+       author_email='atgmail@gmail.com',
+       description='For ALL Language (--)',
+       py_modules=['mylibrary'],
+   )
