b9ec2486465cb10ed085a67d9e97dfcbcf302348
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-30 04:13:57 +0000

    Add package amplpy/0.12.0/amplpy-0.12.0-cp36-cp36m-win_amd64.whl

---
+++ packages/amplpy/amplpy-0.12.0-cp36-cp36m-win_amd64.whl/amplpy/vendor/ampltools/modules/__init__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+__version__ = "0.5.6"
+
+from .amplpypi import (
+    path,
+    find,
+    run_command as run,
+    activate_license as activate,
+    install_modules as install,
+    uninstall_modules as uninstall,
+    load_modules as load,
+    unload_modules as unload,
+    preload_modules as preload,
+    installed_modules as installed,
+    available_modules as available,
+    generate_requirements as requirements,
+)
+
+from .commands import _main
