258784e7e2ddcfdbbe4bb9ebba87a45725602ce9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-30 04:12:27 +0000

    Add package sphinx-cache/0.0.1/sphinx_cache-0.0.1.tar.gz

---
+++ packages/sphinx-cache/sphinx_cache-0.0.1.tar.gz/sphinx_cache-0.0.1/sphinx_cache/utils.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+from typing import Optional
+
+
+def check_env_pickle_exist(doctree_dir: Path) -> Optional[bool]:
+    """
+    Function to check if the `environment.pickle` file is in the `.doctrees` directory
+
+    :param doctree_dir:  path to the `.doctrees` directory
+    :return: True or False
+    """
+    if doctree_dir.exists():
+        return doctree_dir.joinpath("environment.pickle").exists()
+    return None
