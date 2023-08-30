ed723161007ceb35fcee607be8b510e02d4b9b3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-30 04:14:39 +0000

    Add package dataset-tools/0.0.16/dataset-tools-0.0.16.tar.gz

---
+++ packages/dataset-tools/dataset-tools-0.0.16.tar.gz/dataset-tools-0.0.16/dataset_tools/templates/cv_tasks.py
@@ -0,0 +1,16 @@
+class CVTask:
+    class SemanticSegmentation:
+        def __new__(cls):
+            return "semantic segmentation"
+
+    class InstanceSegmentation:
+        def __new__(cls):
+            return "instance segmentation"
+
+    class ObjectDetection:
+        def __new__(cls):
+            return "object detection"
+
+    class Classification:
+        def __new__(cls):
+            return "classification"
