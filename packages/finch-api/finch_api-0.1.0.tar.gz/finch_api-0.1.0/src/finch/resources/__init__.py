a838d9c3a977c3bc38e6832cd2f74004ec3e13d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-30 05:10:41 +0000

    Add package finch-api/0.1.0/finch_api-0.1.0.tar.gz

---
+++ packages/finch-api/finch_api-0.1.0.tar.gz/finch_api-0.1.0/src/finch/resources/__init__.py
@@ -0,0 +1,8 @@
+# File generated from our OpenAPI spec by Stainless.
+
+from .ats import ATS, AsyncATS
+from .hris import HRIS, AsyncHRIS
+from .account import Account, AsyncAccount
+from .providers import Providers, AsyncProviders
+
+__all__ = ["HRIS", "AsyncHRIS", "ATS", "AsyncATS", "Providers", "AsyncProviders", "Account", "AsyncAccount"]
